5a8dd8583c2381bf501436a270e55949198063ec
Ben <benlawson@google.com>
2023-04-27 16:39:08 +0000

    [bt][host] Migrate LowEnergyScanner to pw_async
    Bug: 100594
    Test: fx test //src/connectivity/bluetooth/core/bt-host
    Change-Id: Ia8e5ac44244830ae9e563975b230037c8ce23052

--- src/connectivity/bluetooth/core/bt-host/gap/low_energy_discovery_manager.cc
+++ src/connectivity/bluetooth/core/bt-host/gap/low_energy_discovery_manager.cc
@@ -461,7 +461,7 @@ void LowEnergyDiscoveryManager::StartScan(bool active) {
       .filter_duplicates = true,
       .filter_policy = pw::bluetooth::emboss::LEScanFilterPolicy::BASIC_UNFILTERED,
       .period = scan_period_,
-      .scan_response_timeout = kLEScanResponseTimeout,
+      .scan_response_timeout = kPwLEScanResponseTimeout,
   };
 
   // See Vol 3, Part C, 9.3.11 "Connection Establishment Timing Parameters".
