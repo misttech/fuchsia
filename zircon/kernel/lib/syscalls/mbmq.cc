b0bd50420ee28a40971b7a4d22f1571a38457471
Mark Seaborn <mseaborn@google.com>
2023-07-28 15:50:01 -0700

    Add zx_mbo_create() as a stub syscall

---
+++ zircon/kernel/lib/syscalls/mbmq.cc
@@ -0,0 +1,11 @@
+// Copyright 2023 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <inttypes.h>
+#include <lib/syscalls/forward.h>
+#include <zircon/errors.h>
+
+zx_status_t sys_mbo_create(uint32_t options, user_out_handle* out0) { return ZX_OK; }
