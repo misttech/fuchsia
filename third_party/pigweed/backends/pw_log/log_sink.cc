367dfdc7f2326d55deae043406a144210a8d59d0
Chris Suter <csuter@google.com>
2025-08-06 09:35:45 -0700

    [pigweed] Add check for severity for the syslog backend
    Bug: 377963307
    Change-Id: Id1adc732ec3e9f5765fa91249404ce9c33056947
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1339264
    Fuchsia-Auto-Submit: Chris Suter <csuter@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>

--- third_party/pigweed/backends/pw_log/log_sink.cc
+++ third_party/pigweed/backends/pw_log/log_sink.cc
@@ -13,20 +13,22 @@
 
 namespace {
 
-FuchsiaLogSeverity FuchsiaLogSeverityFromFidl(fuchsia_diagnostics::Severity severity) {
+FuchsiaLogSeverity FuchsiaLogSeverityFromFidl(fuchsia_diagnostics_types::Severity severity) {
   switch (severity) {
-    case fuchsia_diagnostics::Severity::kTrace:
+    case fuchsia_diagnostics_types::Severity::kTrace:
       return FUCHSIA_LOG_TRACE;
-    case fuchsia_diagnostics::Severity::kDebug:
+    case fuchsia_diagnostics_types::Severity::kDebug:
       return FUCHSIA_LOG_DEBUG;
-    case fuchsia_diagnostics::Severity::kInfo:
+    case fuchsia_diagnostics_types::Severity::kInfo:
       return FUCHSIA_LOG_INFO;
-    case fuchsia_diagnostics::Severity::kWarn:
+    case fuchsia_diagnostics_types::Severity::kWarn:
       return FUCHSIA_LOG_WARNING;
-    case fuchsia_diagnostics::Severity::kError:
+    case fuchsia_diagnostics_types::Severity::kError:
       return FUCHSIA_LOG_ERROR;
-    case fuchsia_diagnostics::Severity::kFatal:
+    case fuchsia_diagnostics_types::Severity::kFatal:
       return FUCHSIA_LOG_FATAL;
+    default:
+      return FUCHSIA_LOG_ERROR;
   }
 }
 
@@ -71,7 +73,7 @@ class LogState {
     WaitForInterestChanged();
   }
 
-  void HandleInterest(fuchsia_diagnostics::wire::Interest& interest) {
+  void HandleInterest(fuchsia_diagnostics_types::wire::Interest& interest) {
     if (!interest.has_min_severity()) {
       severity_ = FUCHSIA_LOG_INFO;
     } else {
