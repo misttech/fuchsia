2836fd59328d86c10ad7e1fe608404369146bd64
Aidan Wolter <awolter@google.com>
2025-08-12 19:28:08 -0700

    [ffx][product-bundle] Makes help text clearer
    1. Gives examples of how to use the command
    2. Mentions that the user can use 'latest' as the version
    3. Uses real version numbers in the example
    
    Bug: 438193331, 438193415, 438193986
    Change-Id: I21ec95b975df25788763739696992e7bfa31eacb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1344125
    Commit-Queue: Aidan Wolter <awolter@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>

--- src/developer/ffx/plugins/product/create/src/args.rs
+++ src/developer/ffx/plugins/product/create/src/args.rs
@@ -8,36 +8,63 @@ use argh::{ArgsInfo, FromArgs};
 use camino::Utf8PathBuf;
 use ffx_core::ffx_command;
 
-/**
-Construct a product bundle using a platform, product config, and board config.
-The inputs can be local paths or CIPD references.
-
-Artifact formats:
-  path/to/artifacts: Use local artifact with specified path
-  cipd://path/to/artifact@1.2.3.4: Use artifact from CIPD with specified url
-
-Additional formats for the platform:
-  <omitted>: Use default local platform
-  1.2.3.4: Use platform from CIPD with specified version
-*/
+/// Construct a product bundle using a platform, product config, and board
+/// config.
 #[ffx_command()]
 #[derive(ArgsInfo, FromArgs, Debug, PartialEq)]
-#[argh(subcommand, name = "create")]
+#[argh(
+    subcommand,
+    name = "create",
+    note = "\
+    PLATFORM_ARTIFACT formats:
+  <omitted>: in a fuchsia checkout, use locally built platform
+
+  latest: use latest prebuilt platform from CIPD
+
+  29.20250812.4.1: version of a prebuilt platform in
+    https://chrome-infra-packages.appspot.com/p/fuchsia/assembly/platform/
+
+  path/to/platform: local path to a platform
+    ",
+    note = "\
+    ARTIFACT formats:
+  <name>: in a fuchsia checkout, use locally built artifact by its name
+
+  cipd://<cipd-package>@<cipd-tag>: cipd url for a prebuilt artifact.
+    `latest` can be used as <cipd-tag> to fetch the newest artifact.
+
+  path/to/artifact: a path to a locally built artifact
+    ",
+    example = "\
+    // Create a minimal.arm64 product bundle in a local fuchsia checkout.
+ffx product-bundle create --product-config minimal --board-config arm64
+
+// This is shorthand for the above command.
+ffx product-bundle create minimal.arm64
+    ",
+    example = "\
+    // Create a minimal.arm64 product bundle using prebuilts in CIPD.
+ffx product-bundle create
+    --platform 29.20250812.5.1
+    --product-config cipd://fuchsia/assembly/products/minimal@version:29.20250812.5.1
+    --board-config cipd://fuchsia/assembly/boards/arm64@latest
+    "
+)]
 pub struct CreateCommand {
     /// product_config.board_config combination to build when inside a fuchsia
     /// checkout.
     #[argh(positional)]
     pub product_config_board_config_combo: Option<String>,
 
-    /// the platform artifacts to use.
+    /// the platform artifacts to use. See PLATFORM_ARTIFACT below.
     #[argh(option)]
     pub platform: Option<String>,
 
-    /// the product config to use.
+    /// the product config to use. See ARTIFACT below.
     #[argh(option)]
     pub product_config: Option<String>,
 
-    /// the board config to use.
+    /// the board config to use. See ARTIFACT below.
     #[argh(option)]
     pub board_config: Option<String>,
 
