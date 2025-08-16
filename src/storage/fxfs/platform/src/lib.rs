54770d0a23256de54b830f8da2a3c63deb4526f2
Gulfem Savrun Yeniceri <gulfem@google.com>
2025-08-15 17:16:16 -0700

    [fxfs] Bump recursion limit for toolchain roll
    Bug: 439053417
    Change-Id: Id155fb0a9ab96e5313a515a50a4758d84d299aec
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1347164
    Fuchsia-Auto-Submit: Gulfem Savrun Yeniceri <gulfem@google.com>
    Commit-Queue: Gulfem Savrun Yeniceri <gulfem@google.com>
    Reviewed-by: James Sullivan <jfsulliv@google.com>

--- src/storage/fxfs/platform/src/lib.rs
+++ src/storage/fxfs/platform/src/lib.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(https://fxbug.dev/439053417): Investigate why the recursion limit was bumped to unblock the toolchain.
+#![recursion_limit = "256"]
+
 #[cfg(target_os = "fuchsia")]
 pub mod fuchsia;
 
