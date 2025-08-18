7fc133b1a4c6c6c34ecedf1922049a9162e92ad1
Aidan Wolter <awolter@google.com>
2025-08-18 12:29:51 -0700

    [fx] Use bazel gn args prefix/full instead of target
    The _target gn argument is not added to args.json, therefore it was
    always returning "", therefore causing build directories such as
    minimal.x64 to report as a multi-product build.
    
    prefix/full arguments are always supplied in the product/board,
    therefore they will always get added to args.json.
    
    Change-Id: Ie391366d3ef08c32286922c4995fb4f6ff1b3719
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1348184
    Fuchsia-Auto-Submit: Aidan Wolter <awolter@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Reviewed-by: Wez <wez@google.com>
    Commit-Queue: Aidan Wolter <awolter@google.com>

--- tools/devshell/lib/vars.sh
+++ tools/devshell/lib/vars.sh
@@ -235,17 +235,19 @@ function fx-fail-if-main-pb-is-not-set {
   mapfile -t values < <(fx-command-run jq -r \
     '.main_pb_label // "",
      .use_bazel_images_only // "",
-     .bazel_product_bundle_target // ""' \
+     .bazel_product_bundle_full // "",
+     .bazel_product_bundle_prefix // ""' \
     "${FUCHSIA_BUILD_DIR}/args.json")
 
   # Pull the args out of the array and into their own variables.
-  local main_pb_label use_bazel_images_only bazel_product_bundle_target
+  local main_pb_label use_bazel_images_only bazel_product_bundle_full bazel_product_bundle_prefix
   main_pb_label="${values[0]}"
   use_bazel_images_only="${values[1]}"
-  bazel_product_bundle_target="${values[2]}"
+  bazel_product_bundle_full="${values[2]}"
+  bazel_product_bundle_prefix="${values[3]}"
 
   # Fail if this is a multi-product build and main_pb_label is not set.
-  if [[ "${use_bazel_images_only}" == "true" && -z "${bazel_product_bundle_target}" && -z "${main_pb_label}" ]]; then
+  if [[ "${use_bazel_images_only}" == "true" && -z "${bazel_product_bundle_full}" && -z "${bazel_product_bundle_prefix}" && -z "${main_pb_label}" ]]; then
     fx-error "The 'main_pb_label' GN argument is not set. Please set it with: fx set-main-pb <name>"
     exit 1
   fi
