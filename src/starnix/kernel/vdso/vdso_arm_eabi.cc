d4e066b7cbe7e86ee0e6a7224547ce105159f2e5
Benjamin Lerman <qsr@google.com>
2025-08-27 13:19:28 -0700

    [starnix] Optimize 64-bit division
    This patch introduces fast paths for 64-bit division for specific
    divisors. The following divisors are now handled with magic number
    multiplication which is significantly faster than the previously used
    bit-shifting algorithm.
    
    - 1
    - 1,000,000,000
    - 19,200,000
    
    Change-Id: I3cd87b0d6ea956419d274586c9bb3e4c01e8b1f0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1342587
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Benjamin Lerman <qsr@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>

--- src/starnix/kernel/vdso/vdso_arm_eabi.cc
+++ src/starnix/kernel/vdso/vdso_arm_eabi.cc
@@ -4,10 +4,44 @@
 
 #include <stdint.h>
 
+#include "third_party/github.com/ridiculousfish/libdivide/include/libdivide.h"
+
+// Divides a 64-bit number `n` by a constant `d`, and returns the quotient
+// and remainder.
+//
+// This function uses libdivide to ensure an efficient operation.
+template <uint64_t d>
+static inline uint64_t div_rem(uint64_t n, uint64_t* rem) {
+  constexpr libdivide::branchfree_divider<uint64_t> divider(d);
+  uint64_t q = n / divider;
+  *rem = n - q * d;
+  return q;
+}
+
 static __attribute__((used)) uint64_t udiv64(uint64_t dividend, uint64_t divisor,
                                              uint64_t* remainder) asm("udiv64");
 
+// Specific constant to improve the division
+// Used for the conversion between nanoseconds and seconds.
+constexpr uint64_t k1e9 = 1'000'000'000;
+// Used to convert cpu ticks to nanoseconds.
+constexpr uint64_t k_19200000 = 19'200'000;
+
+// Performs 64-bit integer division.
+//
+// This function is a standard division algorithm that is used when a magic
+// multiplier is not available for the given divisor.
 static uint64_t udiv64(uint64_t dividend, uint64_t divisor, uint64_t* remainder) {
+  switch (divisor) {
+    case 1:
+      *remainder = 0;
+      return dividend;
+    case k1e9:
+      return div_rem<k1e9>(dividend, remainder);
+    case k_19200000:
+      return div_rem<k_19200000>(dividend, remainder);
+  }
+
   uint64_t quotient = 0;
   uint32_t count = 1;
 
@@ -52,14 +86,14 @@ static uint64_t udiv64(uint64_t dividend, uint64_t divisor, uint64_t* remainder)
 }
 
 extern "C" __attribute__((naked)) void __aeabi_uldivmod() {
-  asm("push { r11, lr}");
-  asm("sub sp, sp, #16");
-  asm("add r12, sp, #8");
-  asm("str r12, [sp]");
-  asm("bl udiv64");
-  asm("ldr r2, [sp, #8]");
-  asm("ldr r3, [sp, #12]");
-  asm("add sp, sp, #16");
-  asm("pop {r11, lr}");
-  asm("bx lr");
+  asm("push {r11, lr}");
+  asm("sub  sp, sp, #16");
+  asm("add  r12, sp, #8");
+  asm("str  r12, [sp]");
+  asm("bl   udiv64");
+  asm("ldr  r2, [sp, #8]");
+  asm("ldr  r3, [sp, #12]");
+  asm("add  sp, sp, #16");
+  asm("pop  {r11, lr}");
+  asm("bx   lr");
 }
---
+++ third_party/github.com/ridiculousfish/libdivide/include/libdivide.h
@@ -0,0 +1,14 @@
+// Copyright 2025 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_GITHUB_COM_RIDICULOUSFISH_LIBDIVIDE_INCLUDE_LIBDIVIDE_H_
+#define THIRD_PARTY_GITHUB_COM_RIDICULOUSFISH_LIBDIVIDE_INCLUDE_LIBDIVIDE_H_
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wimplicit-int-conversion-on-negation"
+#pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+#include "third_party/github.com/ridiculousfish/libdivide/src/libdivide.h"
+#pragma clang diagnostic pop
+
+#endif  // THIRD_PARTY_GITHUB_COM_RIDICULOUSFISH_LIBDIVIDE_INCLUDE_LIBDIVIDE_H_
