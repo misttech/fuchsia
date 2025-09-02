a249df8ce0e86c0741e208e731f747b64b1781e5
Chris Suter <csuter@google.com>
2025-09-01 21:04:09 -0700

    [syslog] Make a copy of FakeLogSink
    I am planning to change FakeLogSink to work with IOBuffers, but we this
    legacy code needs a version that works with sockets, so make a copy
    here.
    
    Bug: 377963307
    Change-Id: I60077857b1ef602f49c810f6dc05e57750253b10
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1356529
    Fuchsia-Auto-Submit: Chris Suter <csuter@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Chris Suter <csuter@google.com>

--- zircon/system/ulib/syslog/syslog_socket_tests.cc
+++ zircon/system/ulib/syslog/syslog_socket_tests.cc
@@ -4,6 +4,8 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <fidl/fuchsia.logger/cpp/fidl.h>
+#include <lib/async-loop/cpp/loop.h>
 #include <lib/syslog/internal/global.h>
 #include <lib/syslog/internal/logger.h>
 #include <lib/syslog/internal/wire_format.h>
@@ -16,9 +18,11 @@
 #include <fbl/string.h>
 #include <fbl/string_printf.h>
 #include <fbl/unique_fd.h>
+#include <sdk/lib/diagnostics/reader/cpp/logs.h>
+#include <sdk/lib/syslog/cpp/log_level.h>
 #include <zxtest/zxtest.h>
 
-#include "src/lib/diagnostics/fake-log-sink/cpp/fake_log_sink.h"
+#include "src/lib/diagnostics/log/message/rust/cpp-log-decoder/log_decoder.h"
 #include "zircon/system/ulib/syslog/helpers.h"
 
 namespace {
@@ -26,12 +30,106 @@ namespace {
 const char* kFileName = syslog::internal::StripPath(__FILE__);
 const char* kFilePath = syslog::internal::StripDots(__FILE__);
 
-zx::result<fuchsia_logging::FakeLogSink> init_helper(std::span<const char*> tags = {},
-                                                     fx_log_severity_t severity = FX_LOG_INFO) {
+class FakeLogSink {
+ public:
+  explicit FakeLogSink(fidl::ServerEnd<fuchsia_logger::LogSink> server_end)
+      : loop_(std::make_unique<async::Loop>(&kAsyncLoopConfigNeverAttachToThread)),
+        impl_(std::make_shared<Impl>()) {
+    loop_->StartThread();
+
+    fidl::BindServer(loop_->dispatcher(), std::move(server_end), impl_);
+  }
+
+  FakeLogSink(FakeLogSink&&) = default;
+  FakeLogSink& operator=(FakeLogSink&&) = default;
+
+  std::optional<diagnostics::reader::LogsData> ReadLogsData() {
+    auto record = impl_->ReadRecord();
+    if (record.empty()) {
+      return {};
+    }
+    auto raw_message = fuchsia_decode_log_message_to_json(record.data(), record.size());
+    rapidjson::Document document;
+    document.Parse(raw_message);
+    fuchsia_free_decoded_log_message(raw_message);
+    rapidjson::Document log;
+    log.CopyFrom(document.GetArray()[0], log.GetAllocator());
+    return diagnostics::reader::LogsData(std::move(log));
+  }
+
+  // Returns true if a record is available for reading.
+  bool WaitForRecord(zx::time deadline) const { return impl_->WaitForRecord(deadline); }
+
+ private:
+  class Impl : public fidl::Server<fuchsia_logger::LogSink> {
+   public:
+    Impl() = default;
+
+    bool WaitForRecord(zx::time deadline) const {
+      std::unique_lock lock(mutex_);
+      return WaitForRecord(lock, deadline) != 0;
+    }
+
+    std::vector<uint8_t> ReadRecord() {
+      std::unique_lock lock(mutex_);
+      size_t amount = WaitForRecord(lock, zx::time::infinite());
+      std::vector<uint8_t> buffer(amount);
+      size_t actual;
+      if (socket_.read(0, buffer.data(), buffer.size(), &actual) != ZX_OK) {
+        return {};
+      };
+      buffer.resize(actual);
+      return buffer;
+    }
+
+   private:
+    size_t WaitForRecord(std::unique_lock<std::mutex>& lock, zx::time deadline) const {
+      condition_.wait(lock, [this] { return socket_.is_valid(); });
+      zx_info_socket_t info;
+      for (;;) {
+        if (socket_.get_info(ZX_INFO_SOCKET, &info, sizeof(info), nullptr, nullptr) != ZX_OK) {
+          return 0;
+        }
+        if (info.rx_buf_available > 0) {
+          return info.rx_buf_available;
+        }
+        if (socket_.wait_one(ZX_SOCKET_READABLE, deadline, nullptr) != ZX_OK) {
+          return 0;
+        }
+      }
+    }
+
+    void ConnectStructured(ConnectStructuredRequest& request,
+                           ConnectStructuredCompleter::Sync& completer) override {
+      std::unique_lock lock(mutex_);
+      socket_ = std::move(request.socket());
+      condition_.notify_all();
+    }
+
+    void WaitForInterestChange(WaitForInterestChangeCompleter::Sync& completer) override {
+      ZX_PANIC("Unexpected call to WaitForInterestChange");
+    }
+
+    void handle_unknown_method(fidl::UnknownMethodMetadata<fuchsia_logger::LogSink> metadata,
+                               fidl::UnknownMethodCompleter::Sync& completer) override {
+      ZX_PANIC("Unexpected call to handle_unknown_method");
+    }
+
+    mutable std::mutex mutex_;
+    mutable std::condition_variable condition_;
+    zx::socket socket_;
+  };
+
+  std::unique_ptr<async::Loop> loop_;
+  std::shared_ptr<Impl> impl_;
+};
+
+zx::result<FakeLogSink> init_helper(std::span<const char*> tags = {},
+                                    fx_log_severity_t severity = FX_LOG_INFO) {
   if (auto endpoints = fidl::CreateEndpoints<fuchsia_logger::LogSink>(); endpoints.is_error()) {
     return endpoints.take_error();
   } else {
-    fuchsia_logging::FakeLogSink sink(severity, std::move(endpoints->server));
+    FakeLogSink sink(std::move(endpoints->server));
     fx_logger_config_t config = {
         .min_severity = severity,
         .log_sink_channel = endpoints->client.TakeChannel().release(),
@@ -55,8 +153,8 @@ bool ends_with(const char* str, const fbl::String& suffix) {
   return strcmp(str, suffix.c_str()) == 0;
 }
 
-void output_compare_helper(fuchsia_logging::FakeLogSink& sink, fx_log_severity_t severity,
-                           const char* msg, int line, std::span<const char*> tags = {}) {
+void output_compare_helper(FakeLogSink& sink, fx_log_severity_t severity, const char* msg, int line,
+                           std::span<const char*> tags = {}) {
   auto message = sink.ReadLogsData();
   ASSERT_TRUE(message);
   ASSERT_EQ(message->metadata().tags.size(), tags.size());
