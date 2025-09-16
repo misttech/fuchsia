ef15732daccd958363752c19523f0a94cdc93675
Filip Filmar <fmil@google.com>
2025-09-16 15:14:37 -0700

    [starnix][timer] Implement UtcWaiter for the interval timer
    The UtcWaiter can pause correctly on the UTC timeline.
    
    Bug: 422533641
    Change-Id: I2711a9b3e571a738527a5216909ba789e6b4457f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1365892
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>
    Commit-Queue: Filip Filmar <fmil@google.com>
    Fuchsia-Auto-Submit: Filip Filmar <fmil@google.com>

--- src/starnix/kernel/task/interval_timer.rs
+++ src/starnix/kernel/task/interval_timer.rs
@@ -2,15 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use crate::power::OnWakeOps;
 use crate::signals::{SignalDetail, SignalEvent, SignalEventNotify, SignalInfo, send_signal};
 use crate::task::{
     CurrentTask, GenericDuration, HrTimer, HrTimerHandle, Kernel, TargetTime, ThreadGroup,
     Timeline, TimerId, TimerWakeup,
 };
-use crate::time::utc::estimate_boot_deadline_from_utc;
+use crate::time::utc::{estimate_boot_deadline_from_utc, utc_now};
 use crate::vfs::timer::TimerOps;
 use assert_matches::assert_matches;
+use fuchsia_runtime::UtcInstant;
+use futures::channel::mpsc;
+use futures::{FutureExt, StreamExt, select};
+use starnix_logging::log_debug;
 use std::ops::DerefMut;
+use std::pin::pin;
 
 use futures::stream::AbortHandle;
 use starnix_logging::{log_error, log_trace, log_warn, track_stub};
@@ -53,6 +59,116 @@ pub struct IntervalTimer {
     state: Mutex<IntervalTimerMutableState>,
 }
 pub type IntervalTimerHandle = Arc<IntervalTimer>;
+
+/// Emulates waiting on the UTC timeline for the interval timer.
+///
+/// Combines two functionalities offered by the Fuchsia runtime's timer
+/// and the wake alarm:
+/// * Fuchsia's runtime timer can wake process after a wait,
+/// * Wake alarm can wake the system after a period expires.
+///
+/// The UtcWaiter combines the two to ensure that once [UtcWaiter.wait()]
+/// returns, the correct amount of UTC (wall clock) time has expired.
+#[derive(Debug)]
+struct UtcWaiter {
+    // Used in `on_wake` below.
+    send: mpsc::UnboundedSender<()>,
+    // Call to obtain the current UtcInstant. Injected in tests.
+    utc_now_fn: fn() -> UtcInstant,
+}
+
+impl OnWakeOps for UtcWaiter {
+    // This fn is called when a wake alarm expires. [UtcWaiter] must be
+    // submitted to HrTimerManager for that to happen.
+    fn on_wake(&self, _: &CurrentTask, _: &zx::Handle) {
+        self.on_wake_internal()
+    }
+}
+
+impl UtcWaiter {
+    /// Creates a new UtcWaiter.
+    ///
+    /// Await on `UtcWaiter::wait` to pass the time.
+    ///
+    /// # Returns
+    /// A pair of:
+    /// * `Self`: the waiter itself. Call `UtcWaiter::wait` on it.
+    /// * `UnboundedSender<_>`: a channel used for async notification from the
+    /// wake alarm subsystem.  Feed it into `UtcWaiter::wait`.
+    pub fn new() -> (Self, mpsc::UnboundedReceiver<()>) {
+        Self::new_internal(utc_now)
+    }
+
+    fn on_wake_internal(&self) {
+        self.send
+            .unbounded_send(())
+            .inspect_err(|err| log_error!("UtcWaiter::on_wake: {err:?}"))
+            .unwrap_or(());
+    }
+
+    fn new_internal(clock_fn: fn() -> UtcInstant) -> (Self, mpsc::UnboundedReceiver<()>) {
+        let (send, recv) = mpsc::unbounded();
+        // Returning recv instead of adding to Self avoids the need to take a Mutex.
+        (Self { send, utc_now_fn: clock_fn }, recv)
+    }
+
+    /// Awaits until `deadline` expires. Get `utc_signal` from a call to `UtcWaiter::new()`.
+    pub async fn wait(&self, deadline: UtcInstant, mut utc_signal: mpsc::UnboundedReceiver<()>) {
+        loop {
+            let mut utc_wait_fut = utc_signal.next().fuse();
+            let (deadline_boot, _) = estimate_boot_deadline_from_utc(deadline);
+            let mut boot_wait_fut = pin!(fuchsia_async::Timer::new(deadline_boot));
+            log_debug!(
+                "UtcWaiter::wait: waiting for: deadline_utc={:?}, deadline_boot={:?}",
+                deadline,
+                deadline_boot
+            );
+
+            // The UTC deadline can move. Initially, the UTC and the boot deadline
+            // are in sync. But if after starting the wait the UTC timeline changes,
+            // the actual UTC deadline may move to be sooner or later than the boot
+            // deadline, meaning that we can wake either earlier or later than
+            // the user requested.
+            //
+            // If we woke up correctly, we return. If we woke up too early, we
+            // recompute and continue waiting.
+            select! {
+                // While nominally the waits on the boot and UTC timelines will trigger at about
+                // the same wall clock instant absent timeline changes and wakes, the boot timer
+                // has a much finer resolution than the UTC timer. For example, some devices
+                // support at most 1 second resolution for UTC wakes, vs effectively millisecond
+                // resolution on boot timers or even finer.
+                //
+                // So if we have an opportunity to be more accurate by waking on boot timer, we
+                // should probably take it.
+                _ = boot_wait_fut => {
+                    log_debug!("UtcWaiter::wait: woken by boot deadline.");
+                },
+                // The UTC timer has an additional property that it is able to wake the device
+                // from sleep. As a tradeoff, it usually has orders of magnitude coarser resolution
+                // than the boot timer. So, we also wait on UTC to get the wake functionality.
+                _ = utc_wait_fut => {
+                    log_debug!("UtcWaiter::wait: woken by UTC deadline.");
+                },
+            }
+            let utc_now = (self.utc_now_fn)();
+            if deadline <= utc_now {
+                log_debug!(
+                    "UtcWaiter::wait: UTC deadline reached: now={:?}, deadline={:?}",
+                    utc_now,
+                    deadline
+                );
+                break;
+            } else {
+                log_debug!(
+                    "UtcWaiter::wait: UTC deadline NOT reached: now={:?}, deadline={:?}",
+                    utc_now,
+                    deadline
+                );
+            }
+        }
+    }
+}
 
 #[derive(Debug)]
 struct IntervalTimerMutableState {
@@ -138,13 +254,17 @@ impl IntervalTimer {
                         .delta(&target_time)
                         .expect("timer timeline and target time are comparable");
                 }
+                let (utc_waiter, utc_signal) = UtcWaiter::new();
+                let utc_waiter = Arc::new(utc_waiter);
                 if let Some(hr_timer) = &self.hr_timer {
                     assert_matches!(
                         target_time,
                         TargetTime::BootInstant(_) | TargetTime::RealTime(_),
                         "monotonic times can't be alarm deadlines",
                     );
-                    if let Err(e) = hr_timer.start(system_task, None, target_time) {
+                    let weak_utc_waiter = Arc::downgrade(&utc_waiter);
+                    if let Err(e) = hr_timer.start(system_task, Some(weak_utc_waiter), target_time)
+                    {
                         log_error!("Failed to start the HrTimer to trigger wakeup: {e}");
                     }
                 }
@@ -152,10 +272,7 @@ impl IntervalTimer {
                 match target_time {
                     TargetTime::Monotonic(t) => fuchsia_async::Timer::new(t).await,
                     TargetTime::BootInstant(t) => fuchsia_async::Timer::new(t).await,
-                    TargetTime::RealTime(t) => {
-                        let (deadline_boot, _) = estimate_boot_deadline_from_utc(t);
-                        fuchsia_async::Timer::new(deadline_boot).await
-                    }
+                    TargetTime::RealTime(t) => utc_waiter.wait(t, utc_signal).await,
                 }
             };
             if !self.state.lock().armed {
@@ -342,3 +459,102 @@ impl PartialEq for IntervalTimer {
     }
 }
 impl Eq for IntervalTimer {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::time::utc::UtcClockOverrideGuard;
+    use assert_matches::assert_matches;
+    use std::task::Poll;
+    use zx::HandleBased;
+    use {fuchsia_async as fasync, fuchsia_runtime as fxr};
+
+    struct TestContext {
+        _initial_time_mono: zx::MonotonicInstant,
+        initial_time_utc: UtcInstant,
+        _utc_clock: fxr::UtcClock,
+        _guard: UtcClockOverrideGuard,
+    }
+
+    impl TestContext {
+        async fn new() -> Self {
+            // Make them the same initially.
+            let _initial_time_mono = zx::MonotonicInstant::from_nanos(1000);
+            let initial_time_utc = UtcInstant::from_nanos(_initial_time_mono.into_nanos());
+            fasync::TestExecutor::advance_to(_initial_time_mono.into()).await;
+
+            // Create and start the UTC clock.
+            let utc_clock =
+                fxr::UtcClock::create(zx::ClockOpts::empty(), Some(initial_time_utc)).unwrap();
+            let utc_clock_clone = utc_clock.duplicate_handle(zx::Rights::SAME_RIGHTS).unwrap();
+            let initial_time_boot = zx::BootInstant::from_nanos(_initial_time_mono.into_nanos());
+            utc_clock
+                .update(
+                    fxr::UtcClockUpdate::builder()
+                        .absolute_value(initial_time_boot, initial_time_utc)
+                        .build(),
+                )
+                .unwrap();
+
+            // Inject the clock into Starnix infra.
+            let _guard = UtcClockOverrideGuard::new(utc_clock_clone);
+
+            Self { _initial_time_mono, initial_time_utc, _utc_clock: utc_clock, _guard }
+        }
+    }
+
+    // If the UTC signal is received and the wait expired, we are done.
+    #[fuchsia::test(allow_stalls = false)]
+    async fn test_utc_waiter_on_utc_expired() {
+        let _context = TestContext::new().await;
+
+        let (waiter, utc_signal) = UtcWaiter::new();
+        // Expired deadline, and notification.
+        waiter.on_wake_internal();
+        let deadline_utc = _context.initial_time_utc - fxr::UtcDuration::from_nanos(10);
+        let wait_fut = pin!(waiter.wait(deadline_utc, utc_signal));
+        assert_matches!(
+            fasync::TestExecutor::poll_until_stalled(wait_fut).await,
+            Poll::Ready(_),
+            "UTC deadline should have expired"
+        );
+    }
+
+    // If the UTC signal is received, but the deadline is not reached, we
+    // must still pend.
+    #[fuchsia::test(allow_stalls = false)]
+    async fn test_utc_waiter_on_utc_still_pending() {
+        let context = TestContext::new().await;
+
+        let (waiter, utc_signal) =
+            UtcWaiter::new_internal(|| -> fxr::UtcInstant { fxr::UtcInstant::from_nanos(2000) });
+        // Notified, but not expired yet.
+        waiter.on_wake_internal();
+        let deadline_utc = context.initial_time_utc + fxr::UtcDuration::INFINITE;
+
+        let wait_fut = pin!(waiter.wait(deadline_utc, utc_signal));
+        assert_matches!(
+            fasync::TestExecutor::poll_until_stalled(wait_fut).await,
+            Poll::Pending,
+            "UTC deadline should not have expired"
+        );
+    }
+
+    // If we are woken by the timer, and UTC deadline has passed, we are done.
+    #[fuchsia::test(allow_stalls = false)]
+    async fn test_utc_waiter_on_boot_expires() {
+        let context = TestContext::new().await;
+
+        let (waiter, utc_signal) =
+            UtcWaiter::new_internal(|| -> fxr::UtcInstant { fxr::UtcInstant::from_nanos(5000) });
+        let deadline_utc = context.initial_time_utc + fxr::UtcDuration::from_nanos(4000);
+        let wait_fut = pin!(waiter.wait(deadline_utc, utc_signal));
+
+        fasync::TestExecutor::advance_to(zx::MonotonicInstant::from_nanos(10000).into()).await;
+        assert_matches!(
+            fasync::TestExecutor::poll_until_stalled(wait_fut).await,
+            Poll::Ready(_),
+            "UTC deadline should have expired, and we got notified via the timer wait"
+        );
+    }
+}
