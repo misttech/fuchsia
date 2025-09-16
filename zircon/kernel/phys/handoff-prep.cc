bdb4300e8cec892225a56c98af634caac73f4258
Roland McGrath <mcgrathr@google.com>
2025-09-15 18:36:00 -0700

    [kernel][phys] Move HandoffPrep::SetVersionString into its own file
    The handoff-prep.cc file is a reasonable size as it is, but more
    changes will add more code.  Move the SetVersionString method out
    in preparation for changes to that code.
    
    Change-Id: Ic86eab21ec47fcce66dd9f79dfd6b0497142897f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1367616
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Joshua Seaton <joshuaseaton@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/phys/handoff-prep.cc
+++ zircon/kernel/phys/handoff-prep.cc
@@ -331,35 +331,6 @@ void HandoffPrep::UsePackageFiles(KernelStorage::Bootfs kernel_package) {
   ZX_ASSERT_MSG(!handoff_->version_string.empty(), "no version.txt file in kernel package");
 }
 
-void HandoffPrep::SetVersionString(ktl::string_view version) {
-  constexpr ktl::string_view kSpace = " \t\r\n";
-  size_t skip = version.find_first_not_of(kSpace);
-  size_t trim = version.find_last_not_of(kSpace);
-  if (skip == ktl::string_view::npos || trim == ktl::string_view::npos) {
-    ZX_PANIC("version.txt of %zu chars empty after trimming whitespace", version.size());
-  }
-  trim = version.size() - (trim + 1);
-  version.remove_prefix(skip);
-  version.remove_suffix(trim);
-
-  fbl::AllocChecker ac;
-  ktl::string_view installed = New(handoff_->version_string, ac, version);
-  if (!ac.check()) {
-    ZX_PANIC("cannot allocate %zu chars of handoff space for version string", version.size());
-  }
-  ZX_ASSERT(installed == version);
-  if (gBootOptions->phys_verbose) {
-    if (skip + trim == 0) {
-      printf("%s: zx_system_get_version_string (%zu chars): %.*s\n", ProgramName(), version.size(),
-             static_cast<int>(version.size()), version.data());
-    } else {
-      printf("%s: zx_system_get_version_string (%zu chars trimmed from %zu): %.*s\n", ProgramName(),
-             version.size(), version.size() + skip + trim, static_cast<int>(version.size()),
-             version.data());
-    }
-  }
-}
-
 PhysElfImage HandoffPrep::MakePhysElfImage(KernelStorage::Bootfs::iterator file,
                                            ktl::string_view name) {
   ElfImage elf;
