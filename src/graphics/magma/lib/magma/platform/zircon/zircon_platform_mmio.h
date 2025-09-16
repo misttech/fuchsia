d9405e87b3bf33a8996ee01cf49aa1fa83e79769
Christopher Anderson <cja@google.com>
2025-09-15 18:40:55 -0700

    [magma] Add missing header guard
    Change-Id: Ic68ce675c651eaa5b11f0176844c6465d97bb316
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1368855
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>
    Commit-Queue: Christopher Anderson <cja@google.com>
    Reviewed-by: Matt Sandy <msandy@google.com>

--- src/graphics/magma/lib/magma/platform/zircon/zircon_platform_mmio.h
+++ src/graphics/magma/lib/magma/platform/zircon/zircon_platform_mmio.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_MMIO_H_
+#define SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_MMIO_H_
+
 #include <lib/magma/platform/platform_mmio.h>
 #include <lib/magma/util/dlog.h>
 #include <lib/magma/util/short_macros.h>
@@ -24,3 +27,5 @@ class ZirconPlatformMmio : public PlatformMmio {
 };
 
 }  // namespace magma
+
+#endif  // SRC_GRAPHICS_MAGMA_LIB_MAGMA_PLATFORM_ZIRCON_ZIRCON_PLATFORM_MMIO_H_
