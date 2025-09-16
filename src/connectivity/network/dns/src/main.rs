a4132d8dff21d961481ba4348ca308092fb864d3
Zeling Feng <zeling@google.com>
2025-09-16 12:46:41 -0700

    [dns] Make it clear that sorting address can only fail on FIDL errors
    Test: No behavior change
    Change-Id: I6a6a69645bf36e7a15c0a2a1296beac48a5bca7a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1370037
    Fuchsia-Auto-Submit: Zeling Feng <zeling@google.com>
    Reviewed-by: Bruno Dal Bo <brunodalbo@google.com>
    Commit-Queue: Zeling Feng <zeling@google.com>

--- src/connectivity/network/dns/src/main.rs
+++ src/connectivity/network/dns/src/main.rs
@@ -549,10 +549,7 @@ async fn sort_preferred_addresses(
             // reallocate.
             .drain(..)
             .map(|addr| async move {
-                let source_addr = match routes.resolve(&addr).await.map_err(|e: fidl::Error| {
-                    warn!("fuchsia.net.routes/State.resolve FIDL error {:?}", e);
-                    fname::LookupError::InternalError
-                })? {
+                let source_addr = match routes.resolve(&addr).await? {
                     Ok(fnet_routes::Resolved::Direct(fnet_routes::Destination {
                         source_address,
                         ..
@@ -575,7 +572,12 @@ async fn sort_preferred_addresses(
                 Ok((addr, DasCmpInfo::from_addrs(&addr, source_addr.as_ref())))
             }),
     )
-    .await?;
+    .await
+    .map_err(|e: fidl::Error| {
+        warn!("fuchsia.net.routes/State.resolve FIDL error {:?}", e);
+        fname::LookupError::InternalError
+    })?;
+
     let () = addrs_info.sort_by(|(_laddr, left), (_raddr, right)| left.cmp(right));
     // Reinsert the addresses in order from addr_info.
     let () = addrs.extend(addrs_info.into_iter().map(|(addr, _)| addr));
