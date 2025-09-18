5b898f64e77dd387f63c4170d9ad1909aa067f6b
Sam Hansen <hansens@google.com>
2025-09-18 09:58:30 -0700

    [clock][vim3] Use canned fdf_fake::FakePDev instead of bespoke fake
    Change-Id: I6e219679cca2fb10942de2cafb13b4fc96166180
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1371181
    Commit-Queue: Sam Hansen <hansens@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/clock/drivers/vim3-clk/test.cc
+++ src/devices/clock/drivers/vim3-clk/test.cc
@@ -3,14 +3,14 @@
 // found in the LICENSE file.
 
 #include <fidl/fuchsia.hardware.platform.device/cpp/fidl.h>
-#include <fidl/fuchsia.hardware.platform.device/cpp/wire_test_base.h>
 #include <lib/async_patterns/testing/cpp/dispatcher_bound.h>
+#include <lib/driver/fake-mmio-reg/cpp/fake-mmio-reg.h>
+#include <lib/driver/fake-platform-device/cpp/fake-pdev.h>
 #include <lib/driver/testing/cpp/driver_test.h>
 #include <lib/mmio/mmio-buffer.h>
 #include <lib/zx/vmo.h>
 
 #include <cstdint>
-#include <memory>
 
 #include <gtest/gtest.h>
 #include <soc/aml-a311d/a311d-hw.h>
@@ -22,135 +22,89 @@
 
 namespace vim3_clock {
 
-class FakePDev : public fidl::testing::WireTestBase<fuchsia_hardware_platform_device::Device> {
+namespace fclockimpl = fuchsia_hardware_clockimpl;
+namespace fpdev = fuchsia_hardware_platform_device;
+
+class FakeMmio {
  public:
-  FakePDev()
-      : hui_mmio_(fdf_testing::CreateMmioBuffer(A311D_HIU_LENGTH, ZX_CACHE_POLICY_UNCACHED_DEVICE)),
-        dos_mmio_(
-            fdf_testing::CreateMmioBuffer(A311D_DOS_LENGTH, ZX_CACHE_POLICY_UNCACHED_DEVICE)) {
-    hui_mmio_.Write32(HHI_PLL_LOCK, HHI_GP0_PLL_CNTL0);
+  FakeMmio(size_t count) : count_{count}, regs_{sizeof(uint32_t), count} {
+    for (size_t reg = 0; reg < count_; reg++) {
+      regs_[reg * sizeof(uint32_t)].SetReadCallback(
+          [reg, this]() { return values_.find(reg) == values_.end() ? 0 : values_.at(reg); });
+
+      regs_[reg * sizeof(uint32_t)].SetWriteCallback(
+          [reg, this](uint64_t value) { values_[reg] = value; });
+    }
   }
 
-  fuchsia_hardware_platform_device::Service::InstanceHandler GetInstanceHandler(
-      async_dispatcher_t* dispatcher) {
-    return fuchsia_hardware_platform_device::Service::InstanceHandler({
-        .device = binding_group_.CreateHandler(this, dispatcher, fidl::kIgnoreBindingClosure),
-    });
-  }
-
-  void ClearHiu() { ClearMmioBuffer(hui_mmio_); }
-  void ClearDos() { ClearMmioBuffer(dos_mmio_); }
-
-  bool IsHiuDirty() const { return IsMmioBufferDirty(hui_mmio_); }
-  bool IsDosDirty() const { return IsMmioBufferDirty(dos_mmio_); }
-
-  void NotImplemented_(const std::string& name, ::fidl::CompleterBase& completer) override {}
-
-  void GetMmioById(GetMmioByIdRequestView request, GetMmioByIdCompleter::Sync& completer) override {
-    zx::vmo vmo;
-    zx_status_t st;
-    size_t sz;
-    switch (request->index) {
-      case kHiuMmioIndex:
-        st = hui_mmio_.get_vmo()->duplicate(ZX_RIGHT_SAME_RIGHTS, &vmo);
-        sz = A311D_HIU_LENGTH;
-        break;
-      case kDosMmioIndex:
-        st = dos_mmio_.get_vmo()->duplicate(ZX_RIGHT_SAME_RIGHTS, &vmo);
-        sz = A311D_DOS_LENGTH;
-        break;
-      default:
-        completer.ReplyError(ZX_ERR_INVALID_ARGS);
-        return;
-    }
-
-    if (st != ZX_OK) {
-      completer.ReplyError(st);
-      return;
-    }
-
-    fidl::Arena arena;
-    auto result = fuchsia_hardware_platform_device::wire::Mmio::Builder(arena)
-                      .size(sz)
-                      .offset(0)
-                      .vmo(std::move(vmo))
-                      .Build();
-
-    completer.ReplySuccess(std::move(result));
-  }
-
-  void GetMetadata(GetMetadataRequestView request, GetMetadataCompleter::Sync& completer) override {
-    auto metadata_id = request->id.get();
-
-#if FUCHSIA_API_LEVEL_AT_LEAST(HEAD)
-    if (metadata_id == fuchsia_hardware_clockimpl::ClockIdsMetadata::kSerializableName) {
-      fuchsia_hardware_clockimpl::ClockIdsMetadata metadata{{.clock_nodes{}}};
-      fit::result encoded_metadata = fidl::Persist(metadata);
-      if (encoded_metadata.is_error()) {
-        completer.ReplyError(encoded_metadata.error_value().status());
-        return;
-      }
-      completer.ReplySuccess(fidl::VectorView<uint8_t>::FromExternal(encoded_metadata.value()));
-      return;
-    }
-#endif
-
-    if (metadata_id == fuchsia_hardware_clockimpl::InitMetadata::kSerializableName) {
-      fuchsia_hardware_clockimpl::InitMetadata metadata{{.steps{}}};
-      fit::result encoded_metadata = fidl::Persist(metadata);
-      if (encoded_metadata.is_error()) {
-        completer.ReplyError(encoded_metadata.error_value().status());
-        return;
-      }
-      completer.ReplySuccess(fidl::VectorView<uint8_t>::FromExternal(encoded_metadata.value()));
-      return;
-    }
-
-    completer.ReplyError(ZX_ERR_NOT_FOUND);
-  }
+  fdf::MmioBuffer mmio() { return regs_.GetMmioBuffer(); }
+  std::map<size_t, uint64_t>& values() { return values_; }
+  const std::map<size_t, uint64_t>& values() const { return values_; }
 
  private:
-  static void ClearMmioBuffer(fdf::MmioBuffer& buffer) {
-    const size_t sz = buffer.get_size();
-    auto empty = std::make_unique<uint8_t[]>(sz);
-    memset(empty.get(), 0, sz);
-    buffer.WriteBuffer(0, empty.get(), sz);
+  size_t count_;  // of registers.
+  fake_mmio::FakeMmioRegRegion regs_;
+  std::map<size_t, uint64_t> values_;
+};
+
+class TestEnvironment : public fdf_testing::Environment {
+ public:
+  TestEnvironment() {
+    auto config = fdf_fake::FakePDev::Config{};
+    config.mmios[kHiuMmioIndex] = hiu_regs_.mmio();
+    config.mmios[kDosMmioIndex] = dos_regs_.mmio();
+    config.use_fake_bti = true;
+    config.use_fake_irq = true;
+
+    pdev_.SetConfig(std::move(config));
+
+    hiu_regs_.mmio().Write32(HHI_PLL_LOCK, HHI_GP0_PLL_CNTL0);
   }
 
-  static bool IsMmioBufferDirty(const fdf::MmioBuffer& buffer) {
-    const size_t sz = buffer.get_size();
-    auto contents = std::make_unique<uint8_t[]>(sz);
-    buffer.ReadBuffer(0, contents.get(), sz);
+  zx::result<> Serve(fdf::OutgoingDirectory& incoming) override {
+    async_dispatcher_t* dispatcher = fdf::Dispatcher::GetCurrent()->async_dispatcher();
 
-    for (size_t i = 0; i < sz; i++) {
-      if (contents.get()[i] != 0x0) {
+    EXPECT_TRUE(incoming.AddService<fpdev::Service>(pdev_.GetInstanceHandler(dispatcher)).is_ok());
+
+#if FUCHSIA_API_LEVEL_AT_LEAST(HEAD)
+    fclockimpl::ClockIdsMetadata metadata{{.clock_nodes {} }};
+    EXPECT_EQ(ZX_OK, pdev_.AddFidlMetadata(fclockimpl::ClockIdsMetadata::kSerializableName,
+                                           std::move(metadata)));
+#else
+    fclockimpl::InitMetadata metadata{{.steps{}}};
+    EXPECT_EQ(ZX_OK, incoming.AddFidlMetadata(
+        fclockimpl::InitMetadata::kSerializableName, std::move(metadata));
+#endif
+
+    return zx::ok();
+  }
+
+  void ClearHiu() { ClearMmioBuffer(hiu_regs_); }
+  void ClearDos() { ClearMmioBuffer(dos_regs_); }
+  bool IsHiuDirty() { return IsMmioBufferDirty(hiu_regs_); }
+  bool IsDosDirty() { return IsMmioBufferDirty(dos_regs_); }
+
+ private:
+  static void ClearMmioBuffer(FakeMmio& mmio) { mmio.values().clear(); }
+
+  static bool IsMmioBufferDirty(const FakeMmio& mmio) {
+    if (mmio.values().empty()) {
+      return false;
+    }
+
+    for (const auto& [k, v] : mmio.values()) {
+      if (v != 0) {
         return true;
       }
     }
     return false;
   }
 
-  fidl::ServerBindingGroup<fuchsia_hardware_platform_device::Device> binding_group_;
+  static constexpr size_t kRegSize = sizeof(uint32_t);
 
-  fdf::MmioBuffer hui_mmio_;
-  fdf::MmioBuffer dos_mmio_;
-};
-
-class TestEnvironment : public fdf_testing::Environment {
- public:
-  zx::result<> Serve(fdf::OutgoingDirectory& to_driver_vfs) override {
-    async_dispatcher_t* dispatcher = fdf::Dispatcher::GetCurrent()->async_dispatcher();
-    auto result = to_driver_vfs.AddService<fuchsia_hardware_platform_device::Service>(
-        pdev_.GetInstanceHandler(dispatcher));
-    EXPECT_EQ(ZX_OK, result.status_value());
-
-    return zx::ok();
-  }
-
-  FakePDev& PDev() { return pdev_; }
-
- private:
-  FakePDev pdev_;
+  fdf_fake::FakePDev pdev_;
+  FakeMmio hiu_regs_{A311D_HIU_LENGTH / kRegSize};
+  FakeMmio dos_regs_{A311D_DOS_LENGTH / kRegSize};
 };
 
 class FixtureConfig final {
@@ -278,16 +232,16 @@ TEST_F(DriverTest, ClkTestHiuRegRegion) {
   fdf::Arena arena('TEST');
 
   driver_test().RunInEnvironmentTypeContext([](TestEnvironment& env) {
-    env.PDev().ClearDos();
-    env.PDev().ClearHiu();
+    env.ClearDos();
+    env.ClearHiu();
   });
 
   auto enable_result = client_.buffer(arena)->Enable(g12b_clk::G12B_CLK_AUDIO);
   ASSERT_TRUE(enable_result.ok());
 
   driver_test().RunInEnvironmentTypeContext([](TestEnvironment& env) {
-    ASSERT_TRUE(env.PDev().IsHiuDirty());
-    ASSERT_FALSE(env.PDev().IsDosDirty());
+    ASSERT_TRUE(env.IsHiuDirty());
+    ASSERT_FALSE(env.IsDosDirty());
   });
 }
 
@@ -296,16 +250,16 @@ TEST_F(DriverTest, ClkTestDosRegRegion) {
   fdf::Arena arena('TEST');
 
   driver_test().RunInEnvironmentTypeContext([](TestEnvironment& env) {
-    env.PDev().ClearDos();
-    env.PDev().ClearHiu();
+    env.ClearDos();
+    env.ClearHiu();
   });
 
   auto enable_result = client_.buffer(arena)->Enable(g12b_clk::G12B_CLK_DOS_GCLK_VDEC);
   ASSERT_TRUE(enable_result.ok());
 
   driver_test().RunInEnvironmentTypeContext([](TestEnvironment& env) {
-    ASSERT_FALSE(env.PDev().IsHiuDirty());
-    ASSERT_TRUE(env.PDev().IsDosDirty());
+    ASSERT_FALSE(env.IsHiuDirty());
+    ASSERT_TRUE(env.IsDosDirty());
   });
 }
 
