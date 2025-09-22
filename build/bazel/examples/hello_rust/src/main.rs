e9f2dcbd953c3928548d81a551a7da5ad3f6da9c
Jay Zhuang <jayzhuang@google.com>
2025-09-22 15:37:52 -0700

    [bazel][rust] Use third-party deps in target example
    Added anyhow and assert_matches as dependencies to make sure some
    third-party Rust dependencies are exercised in target builds.
    
    Tested with:
    
    > fx set fuchsia.x64 --with //build/bazel/examples
    > fx bazel build --platforms=//build/bazel/platforms:fuchsia_x64 //build/bazel/examples/hello_rust:pkg //build/bazel/examples/hello_rust:test_pkg
    > fx set fuchsia.arm64 --with //build/bazel/examples
    > fx bazel build --platforms=//build/bazel/platforms:fuchsia_arm64 //build/bazel/examples/hello_rust:pkg //build/bazel/examples/hello_rust:test_pkg
    
    Change-Id: I0399714bfd1acc3077d689405c8097a1ce15d26d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1156873
    Reviewed-by: David Turner <digit@google.com>
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>

--- build/bazel/examples/hello_rust/src/main.rs
+++ build/bazel/examples/hello_rust/src/main.rs
@@ -2,20 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-pub fn say_hello() -> &'static str {
-    "Hello, Rust!"
+use anyhow::Result;
+
+pub fn say_hello() -> Result<&'static str> {
+    Ok("Hello, Rust!")
 }
 
 fn main() {
-    println!("{}", say_hello())
+    println!("{}", say_hello().unwrap())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use assert_matches::assert_matches;
 
     #[test]
     fn test_hello() {
-        assert_eq!("Hello, Rust!", say_hello());
+        assert_matches!(say_hello(), Ok("Hello, Rust!"));
     }
 }
