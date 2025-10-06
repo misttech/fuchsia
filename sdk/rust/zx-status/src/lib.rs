388562da0684cf1ecb99d2545f77d40b3718d387
Filip Filmar <fmil@google.com>
2025-10-06 12:27:01 -0700

    [sdk][doc] Convert docs into doc attributes
    This should make the commends appear in rustdocs.
    
    Change-Id: I6a8120aad1f7e48880449f23437dede7a8a8c45f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1382703
    Commit-Queue: Filip Filmar <fmil@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>

--- sdk/rust/zx-status/src/lib.rs
+++ sdk/rust/zx-status/src/lib.rs
@@ -26,7 +26,6 @@ macro_rules! assoc_values {
             fn assoc_const_name(&self) -> Option<&'static str> {
                 match self.0 {
                     $(
-                        $(#[$attr])*
                         $value => Some(stringify!($name)),
                     )*
                     _ => None,
@@ -79,172 +78,172 @@ pub fn ok(raw: sys::zx_status_t) -> Result<(), Status> {
 
 // LINT.IfChange(zx_status_t)
 assoc_values!(Status, [
-    // Indicates an operation was successful.
+    #[doc = "Indicates an operation was successful."]
     OK                     = sys::ZX_OK;
-    // The system encountered an otherwise unspecified error while performing the
-    // operation.
+    #[doc = "The system encountered an otherwise unspecified error while performing the"]
+    #[doc = "operation."]
     INTERNAL               = sys::ZX_ERR_INTERNAL;
-    // The operation is not implemented, supported, or enabled.
+    #[doc = "The operation is not implemented, supported, or enabled."]
     NOT_SUPPORTED          = sys::ZX_ERR_NOT_SUPPORTED;
-    // The system was not able to allocate some resource needed for the operation.
+    #[doc = "The system was not able to allocate some resource needed for the operation."]
     NO_RESOURCES           = sys::ZX_ERR_NO_RESOURCES;
-    // The system was not able to allocate memory needed for the operation.
+    #[doc = "The system was not able to allocate memory needed for the operation."]
     NO_MEMORY              = sys::ZX_ERR_NO_MEMORY;
-    // The system call was interrupted, but should be retried. This should not be
-    // seen outside of the VDSO.
+    #[doc = "The system call was interrupted, but should be retried. This should not be"]
+    #[doc = "seen outside of the VDSO."]
     INTERRUPTED_RETRY      = sys::ZX_ERR_INTERRUPTED_RETRY;
-    // An argument is invalid. For example, a null pointer when a null pointer is
-    // not permitted.
+    #[doc = "An argument is invalid. For example, a null pointer when a null pointer is"]
+    #[doc = "not permitted."]
     INVALID_ARGS           = sys::ZX_ERR_INVALID_ARGS;
-    // A specified handle value does not refer to a handle.
+    #[doc = "A specified handle value does not refer to a handle."]
     BAD_HANDLE             = sys::ZX_ERR_BAD_HANDLE;
-    // The subject of the operation is the wrong type to perform the operation.
-    //
-    // For example: Attempting a message_read on a thread handle.
+    #[doc = "The subject of the operation is the wrong type to perform the operation."]
+    #[doc = ""]
+    #[doc = "For example: Attempting a message_read on a thread handle."]
     WRONG_TYPE             = sys::ZX_ERR_WRONG_TYPE;
-    // The specified syscall number is invalid.
+    #[doc = "The specified syscall number is invalid."]
     BAD_SYSCALL            = sys::ZX_ERR_BAD_SYSCALL;
-    // An argument is outside the valid range for this operation.
+    #[doc = "An argument is outside the valid range for this operation."]
     OUT_OF_RANGE           = sys::ZX_ERR_OUT_OF_RANGE;
-    // The caller-provided buffer is too small for this operation.
+    #[doc = "The caller-provided buffer is too small for this operation."]
     BUFFER_TOO_SMALL       = sys::ZX_ERR_BUFFER_TOO_SMALL;
-    // The operation failed because the current state of the object does not allow
-    // it, or a precondition of the operation is not satisfied.
+    #[doc = "The operation failed because the current state of the object does not allow"]
+    #[doc = "it, or a precondition of the operation is not satisfied."]
     BAD_STATE              = sys::ZX_ERR_BAD_STATE;
-    // The time limit for the operation elapsed before the operation completed.
+    #[doc = "The time limit for the operation elapsed before the operation completed."]
     TIMED_OUT              = sys::ZX_ERR_TIMED_OUT;
-    // The operation cannot be performed currently but potentially could succeed if
-    // the caller waits for a prerequisite to be satisfied, like waiting for a
-    // handle to be readable or writable.
-    //
-    // Example: Attempting to read from a channel that has no messages waiting but
-    // has an open remote will return `ZX_ERR_SHOULD_WAIT`. In contrast, attempting
-    // to read from a channel that has no messages waiting and has a closed remote
-    // end will return `ZX_ERR_PEER_CLOSED`.
+    #[doc = "The operation cannot be performed currently but potentially could succeed if"]
+    #[doc = "the caller waits for a prerequisite to be satisfied, like waiting for a"]
+    #[doc = "handle to be readable or writable."]
+    #[doc = ""]
+    #[doc = "Example: Attempting to read from a channel that has no messages waiting but"]
+    #[doc = "has an open remote will return `ZX_ERR_SHOULD_WAIT`. In contrast, attempting"]
+    #[doc = "to read from a channel that has no messages waiting and has a closed remote"]
+    #[doc = "end will return `ZX_ERR_PEER_CLOSED`."]
     SHOULD_WAIT            = sys::ZX_ERR_SHOULD_WAIT;
-    // The in-progress operation, for example, a wait, has been canceled.
+    #[doc = "The in-progress operation, for example, a wait, has been canceled."]
     CANCELED               = sys::ZX_ERR_CANCELED;
-    // The operation failed because the remote end of the subject of the operation
-    // was closed.
+    #[doc = "The operation failed because the remote end of the subject of the operation"]
+    #[doc = "was closed."]
     PEER_CLOSED            = sys::ZX_ERR_PEER_CLOSED;
-    // The requested entity is not found.
+    #[doc = "The requested entity is not found."]
     NOT_FOUND              = sys::ZX_ERR_NOT_FOUND;
-    // An object with the specified identifier already exists.
-    //
-    // Example: Attempting to create a file when a file already exists with that
-    // name.
+    #[doc = "An object with the specified identifier already exists."]
+    #[doc = ""]
+    #[doc = "Example: Attempting to create a file when a file already exists with that"]
+    #[doc = "name."]
     ALREADY_EXISTS         = sys::ZX_ERR_ALREADY_EXISTS;
-    // The operation failed because the named entity is already owned or controlled
-    // by another entity. The operation could succeed later if the current owner
-    // releases the entity.
+    #[doc = "The operation failed because the named entity is already owned or controlled"]
+    #[doc = "by another entity. The operation could succeed later if the current owner"]
+    #[doc = "releases the entity."]
     ALREADY_BOUND          = sys::ZX_ERR_ALREADY_BOUND;
-    // The subject of the operation is currently unable to perform the operation.
-    //
-    // This is used when there's no direct way for the caller to observe when the
-    // subject will be able to perform the operation and should thus retry.
+    #[doc = "The subject of the operation is currently unable to perform the operation."]
+    #[doc = ""]
+    #[doc = "This is used when there's no direct way for the caller to observe when the"]
+    #[doc = "subject will be able to perform the operation and should thus retry."]
     UNAVAILABLE            = sys::ZX_ERR_UNAVAILABLE;
-    // The caller did not have permission to perform the specified operation.
+    #[doc = "The caller did not have permission to perform the specified operation."]
     ACCESS_DENIED          = sys::ZX_ERR_ACCESS_DENIED;
-    // Otherwise-unspecified error occurred during I/O.
+    #[doc = "Otherwise-unspecified error occurred during I/O."]
     IO                     = sys::ZX_ERR_IO;
-    // The entity the I/O operation is being performed on rejected the operation.
-    //
-    // Example: an I2C device NAK'ing a transaction or a disk controller rejecting
-    // an invalid command, or a stalled USB endpoint.
+    #[doc = "The entity the I/O operation is being performed on rejected the operation."]
+    #[doc = ""]
+    #[doc = "Example: an I2C device NAK'ing a transaction or a disk controller rejecting"]
+    #[doc = "an invalid command, or a stalled USB endpoint."]
     IO_REFUSED             = sys::ZX_ERR_IO_REFUSED;
-    // The data in the operation failed an integrity check and is possibly
-    // corrupted.
-    //
-    // Example: CRC or Parity error.
+    #[doc = "The data in the operation failed an integrity check and is possibly"]
+    #[doc = "corrupted."]
+    #[doc = ""]
+    #[doc = "Example: CRC or Parity error."]
     IO_DATA_INTEGRITY      = sys::ZX_ERR_IO_DATA_INTEGRITY;
-    // The data in the operation is currently unavailable and may be permanently
-    // lost.
-    //
-    // Example: A disk block is irrecoverably damaged.
+    #[doc = "The data in the operation is currently unavailable and may be permanently"]
+    #[doc = "lost."]
+    #[doc = ""]
+    #[doc = "Example: A disk block is irrecoverably damaged."]
     IO_DATA_LOSS           = sys::ZX_ERR_IO_DATA_LOSS;
-    // The device is no longer available (has been unplugged from the system,
-    // powered down, or the driver has been unloaded).
+    #[doc = "The device is no longer available (has been unplugged from the system,"]
+    #[doc = "powered down, or the driver has been unloaded)."]
     IO_NOT_PRESENT         = sys::ZX_ERR_IO_NOT_PRESENT;
-    // More data was received from the device than expected.
-    //
-    // Example: a USB "babble" error due to a device sending more data than the
-    // host queued to receive.
+    #[doc = "More data was received from the device than expected."]
+    #[doc = ""]
+    #[doc = "Example: a USB \"babble\" error due to a device sending more data than the"]
+    #[doc = "host queued to receive."]
     IO_OVERRUN             = sys::ZX_ERR_IO_OVERRUN;
-    // An operation did not complete within the required timeframe.
-    //
-    // Example: A USB isochronous transfer that failed to complete due to an
-    // overrun or underrun.
+    #[doc = "An operation did not complete within the required timeframe."]
+    #[doc = ""]
+    #[doc = "Example: A USB isochronous transfer that failed to complete due to an"]
+    #[doc = "overrun or underrun."]
     IO_MISSED_DEADLINE     = sys::ZX_ERR_IO_MISSED_DEADLINE;
-    // The data in the operation is invalid parameter or is out of range.
-    //
-    // Example: A USB transfer that failed to complete with TRB Error
+    #[doc = "The data in the operation is invalid parameter or is out of range."]
+    #[doc = ""]
+    #[doc = "Example: A USB transfer that failed to complete with TRB Error"]
     IO_INVALID             = sys::ZX_ERR_IO_INVALID;
-    // Path name is too long.
+    #[doc = "Path name is too long."]
     BAD_PATH               = sys::ZX_ERR_BAD_PATH;
-    // The object is not a directory or does not support directory operations.
-    //
-    // Example: Attempted to open a file as a directory or attempted to do
-    // directory operations on a file.
+    #[doc = "The object is not a directory or does not support directory operations."]
+    #[doc = ""]
+    #[doc = "Example: Attempted to open a file as a directory or attempted to do"]
+    #[doc = "directory operations on a file."]
     NOT_DIR                = sys::ZX_ERR_NOT_DIR;
-    // Object is not a regular file.
+    #[doc = "Object is not a regular file."]
     NOT_FILE               = sys::ZX_ERR_NOT_FILE;
-    // This operation would cause a file to exceed a filesystem-specific size
-    // limit.
+    #[doc = "This operation would cause a file to exceed a filesystem-specific size"]
+    #[doc = "limit."]
     FILE_BIG               = sys::ZX_ERR_FILE_BIG;
-    // The filesystem or device space is exhausted.
+    #[doc = "The filesystem or device space is exhausted."]
     NO_SPACE               = sys::ZX_ERR_NO_SPACE;
-    // The directory is not empty for an operation that requires it to be empty.
-    //
-    // For example, non-recursively deleting a directory with files still in it.
+    #[doc = "The directory is not empty for an operation that requires it to be empty."]
+    #[doc = ""]
+    #[doc = "For example, non-recursively deleting a directory with files still in it."]
     NOT_EMPTY              = sys::ZX_ERR_NOT_EMPTY;
-    // An indicate to not call again.
-    //
-    // The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
-    // not errors and will never be returned by a system call or public API. They
-    // allow callbacks to request their caller perform some other operation.
-    //
-    // For example, a callback might be called on every event until it returns
-    // something other than `ZX_OK`. This status allows differentiation between
-    // "stop due to an error" and "stop because work is done."
+    #[doc = "An indicate to not call again."]
+    #[doc = ""]
+    #[doc = "The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are"]
+    #[doc = "not errors and will never be returned by a system call or public API. They"]
+    #[doc = "allow callbacks to request their caller perform some other operation."]
+    #[doc = ""]
+    #[doc = "For example, a callback might be called on every event until it returns"]
+    #[doc = "something other than `ZX_OK`. This status allows differentiation between"]
+    #[doc = "\"stop due to an error\" and \"stop because work is done.\""]
     STOP                   = sys::ZX_ERR_STOP;
-    // Advance to the next item.
-    //
-    // The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
-    // not errors and will never be returned by a system call or public API. They
-    // allow callbacks to request their caller perform some other operation.
-    //
-    // For example, a callback could use this value to indicate it did not consume
-    // an item passed to it, but by choice, not due to an error condition.
+    #[doc = "Advance to the next item."]
+    #[doc = ""]
+    #[doc = "The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are"]
+    #[doc = "not errors and will never be returned by a system call or public API. They"]
+    #[doc = "allow callbacks to request their caller perform some other operation."]
+    #[doc = ""]
+    #[doc = "For example, a callback could use this value to indicate it did not consume"]
+    #[doc = "an item passed to it, but by choice, not due to an error condition."]
     NEXT                   = sys::ZX_ERR_NEXT;
-    // Ownership of the item has moved to an asynchronous worker.
-    //
-    // The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are
-    // not errors and will never be returned by a system call or public API. They
-    // allow callbacks to request their caller perform some other operation.
-    //
-    // Unlike `ZX_ERR_STOP`, which implies that iteration on an object
-    // should stop, and `ZX_ERR_NEXT`, which implies that iteration
-    // should continue to the next item, `ZX_ERR_ASYNC` implies
-    // that an asynchronous worker is responsible for continuing iteration.
-    //
-    // For example, a callback will be called on every event, but one event needs
-    // to handle some work asynchronously before it can continue. `ZX_ERR_ASYNC`
-    // implies the worker is responsible for resuming iteration once its work has
-    // completed.
+    #[doc = "Ownership of the item has moved to an asynchronous worker."]
+    #[doc = ""]
+    #[doc = "The flow control values `ZX_ERR_STOP`, `ZX_ERR_NEXT`, and `ZX_ERR_ASYNC` are"]
+    #[doc = "not errors and will never be returned by a system call or public API. They"]
+    #[doc = "allow callbacks to request their caller perform some other operation."]
+    #[doc = ""]
+    #[doc = "Unlike `ZX_ERR_STOP`, which implies that iteration on an object"]
+    #[doc = "should stop, and `ZX_ERR_NEXT`, which implies that iteration"]
+    #[doc = "should continue to the next item, `ZX_ERR_ASYNC` implies"]
+    #[doc = "that an asynchronous worker is responsible for continuing iteration."]
+    #[doc = ""]
+    #[doc = "For example, a callback will be called on every event, but one event needs"]
+    #[doc = "to handle some work asynchronously before it can continue. `ZX_ERR_ASYNC`"]
+    #[doc = "implies the worker is responsible for resuming iteration once its work has"]
+    #[doc = "completed."]
     ASYNC                  = sys::ZX_ERR_ASYNC;
-    // The specified protocol is not supported.
+    #[doc = "The specified protocol is not supported."]
     PROTOCOL_NOT_SUPPORTED = sys::ZX_ERR_PROTOCOL_NOT_SUPPORTED;
-    // The host is unreachable.
+    #[doc = "The host is unreachable."]
     ADDRESS_UNREACHABLE    = sys::ZX_ERR_ADDRESS_UNREACHABLE;
-    // Address is being used by someone else.
+    #[doc = "Address is being used by someone else."]
     ADDRESS_IN_USE         = sys::ZX_ERR_ADDRESS_IN_USE;
-    // The socket is not connected.
+    #[doc = "The socket is not connected."]
     NOT_CONNECTED          = sys::ZX_ERR_NOT_CONNECTED;
-    // The remote peer rejected the connection.
+    #[doc = "The remote peer rejected the connection."]
     CONNECTION_REFUSED     = sys::ZX_ERR_CONNECTION_REFUSED;
-    // The connection was reset.
+    #[doc = "The connection was reset."]
     CONNECTION_RESET       = sys::ZX_ERR_CONNECTION_RESET;
-    // The connection was aborted.
+    #[doc = "The connection was aborted."]
     CONNECTION_ABORTED     = sys::ZX_ERR_CONNECTION_ABORTED;
 ]);
 // LINT.ThenChange(//zircon/vdso/errors.fidl)
