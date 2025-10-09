a325797524ee280e9563395c0a1da9c89d302d30
Onath Dillinger <claridge@google.com>
2025-10-09 10:37:52 -0700

    [time][power] Use SuspendBlocker in timekeeper.
    This is part of the removal of opportunistic dependencies from Power
    Framework.
    
    Bug: 450382381
    Change-Id: I6c0cc692e148f8661209aa44d28e4b7d096bf21a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1387103
    Reviewed-by: Filip Filmar <fmil@google.com>
    Commit-Queue: Onath Dillinger <claridge@google.com>

--- src/sys/time/timekeeper/src/power_topology_integration.rs
+++ src/sys/time/timekeeper/src/power_topology_integration.rs
@@ -2,20 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{anyhow, Context, Error, Result};
-use fidl::endpoints::{create_endpoints, ProtocolMarker};
+use anyhow::{Context, Error, Result, anyhow};
+use fidl::endpoints::{ProtocolMarker, create_endpoints};
 use fuchsia_component::client;
 use futures::channel::mpsc;
-use futures::{future, Future, SinkExt, StreamExt, TryStreamExt};
+use futures::{Future, SinkExt, StreamExt, TryStreamExt, future};
 use log::{debug, error};
-use {fidl_fuchsia_power_broker as fpb, fidl_fuchsia_power_system as fps, fuchsia_async as fasync};
+use {fidl_fuchsia_power_system as fps, fuchsia_async as fasync};
 
-const ELEMENT_NAME: &str = "timekeeper-pe";
+const SUSPEND_BLOCKER_NAME: &str = "timekeeper";
 
 const POWER_ON: u8 = 0xff;
 const POWER_OFF: u8 = 0x00;
 
-const REQUIRED_LEVEL: u8 = fps::ExecutionStateLevel::Suspending.into_primitive();
+enum SuspendBlockerResponder {
+    BeforeSuspend(fps::SuspendBlockerBeforeSuspendResponder),
+    AfterResume(fps::SuspendBlockerAfterResumeResponder),
+}
 
 pub async fn manage(activity_signal: mpsc::Sender<super::Command>) -> Result<fasync::Task<()>> {
     let governor_proxy = client::connect_to_protocol::<fps::ActivityGovernorMarker>()
@@ -23,82 +26,56 @@ pub async fn manage(activity_signal: mpsc::Sender<super::Command>) -> Result<fas
             format!("while connecting to: {:?}", fps::ActivityGovernorMarker::DEBUG_NAME)
         })?;
 
-    let topology_proxy = client::connect_to_protocol::<fpb::TopologyMarker>()
-        .with_context(|| format!("while connecting to: {:?}", fpb::TopologyMarker::DEBUG_NAME))?;
-
-    manage_internal(governor_proxy, topology_proxy, activity_signal, management_loop).await
+    manage_internal(governor_proxy, activity_signal, management_loop).await
 }
 
 async fn manage_internal<F, G>(
     governor_proxy: fps::ActivityGovernorProxy,
-    topology_proxy: fpb::TopologyProxy,
     mut activity: mpsc::Sender<super::Command>,
     // Injected in tests.
     loop_fn: F,
 ) -> Result<fasync::Task<()>>
 where
     G: Future<Output = fasync::Task<()>>,
-    F: Fn(fpb::ElementRunnerRequestStream) -> G,
+    F: Fn(fps::SuspendBlockerRequestStream) -> G,
 {
-    let power_elements = governor_proxy
-        .get_power_elements()
-        .await
-        .context("in a call to ActivityGovernor/GetPowerElements")?;
-
     let _ignore = activity.send(super::Command::PowerManagement).await;
-    if let Some(execution_state) = power_elements.execution_state {
-        if let Some(token) = execution_state.opportunistic_dependency_token {
-            let deps = vec![fpb::LevelDependency {
-                dependency_type: fpb::DependencyType::Opportunistic,
-                dependent_level: POWER_ON,
-                requires_token: token,
-                requires_level_by_preference: vec![REQUIRED_LEVEL],
-            }];
 
-            let (element_runner_client, element_runner_server) =
-                create_endpoints::<fpb::ElementRunnerMarker>();
-            let result = topology_proxy
-                .add_element(fpb::ElementSchema {
-                    element_name: Some(ELEMENT_NAME.into()),
-                    initial_current_level: Some(POWER_ON),
-                    // The level ordering is important, and must be increasing.
-                    valid_levels: Some(vec![POWER_OFF, POWER_ON]),
-                    dependencies: Some(deps),
-                    element_runner: Some(element_runner_client),
-                    ..Default::default()
-                })
-                .await
-                .context("while calling fuchsia.power.broker.Topology/AddElement")?;
-            let element_runner = element_runner_server.into_stream();
-            match result {
-                Ok(_) => return Ok(loop_fn(element_runner).await),
-                Err(e) => return Err(anyhow!("error while adding element: {:?}", e)),
-            }
-        }
-    } else {
-        debug!(
-            "no execution state power token found, power management integration is shutting down."
-        );
+    let (suspend_blocker_client, suspend_blocker_server) =
+        create_endpoints::<fps::SuspendBlockerMarker>();
+    let result = governor_proxy
+        .register_suspend_blocker(fps::ActivityGovernorRegisterSuspendBlockerRequest {
+            suspend_blocker: Some(suspend_blocker_client),
+            name: Some(SUSPEND_BLOCKER_NAME.into()),
+            ..Default::default()
+        })
+        .await
+        .context("while calling fuchsia.power.system.ActivityGovernor/RegisterSuspendBlocker")?;
+    let suspend_blocker = suspend_blocker_server.into_stream();
+    match result {
+        Ok(_) => Ok(loop_fn(suspend_blocker).await),
+        Err(e) => Err(anyhow!("error while registering suspend blocker: {:?}", e)),
     }
-    Ok(fasync::Task::local(async {}))
 }
 
-// Loop around and react to level control messages. Use separate tasks to ensure
+// Loop around and react to SuspendBlocker messages. Use separate tasks to ensure
 // we can insert a power transition process in between.
 //
 // Returns the task spawned for transition control.
-async fn management_loop(mut element_runner: fpb::ElementRunnerRequestStream) -> fasync::Task<()> {
+async fn management_loop(
+    mut suspend_blocker_stream: fps::SuspendBlockerRequestStream,
+) -> fasync::Task<()> {
     // The Sender is used to ensure that rcv_task does not send before send_task
     // is done.
     let (mut send, mut rcv) = mpsc::channel::<(u8, mpsc::Sender<()>)>(1);
 
     let rcv_task = fasync::Task::local(async move {
         loop {
-            let Ok(Some(request)) = element_runner.try_next().await else {
-                error!("error while waiting for element runner request, bailing");
+            let Ok(Some(request)) = suspend_blocker_stream.try_next().await else {
+                error!("error while waiting for suspend blocker request, bailing");
                 break;
             };
-            let result = handle_element_runner_set_level(request).await;
+            let result = handle_suspend_blocker_request(request).await;
             match result {
                 Ok((level, responder)) => {
                     let (s, mut r) = mpsc::channel::<()>(1);
@@ -109,8 +86,16 @@ async fn management_loop(mut element_runner: fpb::ElementRunnerRequestStream) ->
                     }
                     // Wait until rcv_task propagates the new required level.
                     r.next().await.unwrap();
-                    // Respond to SetLevel, indicating the level transition is complete.
-                    responder.send().expect("set_level resp failed");
+                    // Respond to the SuspendBlockerRequest, indicating the level transition is
+                    // complete.
+                    match responder {
+                        SuspendBlockerResponder::BeforeSuspend(responder) => {
+                            responder.send().expect("BeforeSuspend resp failed");
+                        }
+                        SuspendBlockerResponder::AfterResume(responder) => {
+                            responder.send().expect("AfterResume resp failed");
+                        }
+                    }
                 }
                 Err(e) => {
                     error!("error while watching level, bailing: {:?}", e);
@@ -143,15 +128,18 @@ async fn management_loop(mut element_runner: fpb::ElementRunnerRequestStream) ->
     })
 }
 
-async fn handle_element_runner_set_level(
-    request: fpb::ElementRunnerRequest,
-) -> Result<(u8, fpb::ElementRunnerSetLevelResponder), Error> {
+async fn handle_suspend_blocker_request(
+    request: fps::SuspendBlockerRequest,
+) -> Result<(u8, SuspendBlockerResponder), Error> {
     match request {
-        fpb::ElementRunnerRequest::SetLevel { level, responder } => {
-            return Ok((level, responder));
+        fps::SuspendBlockerRequest::BeforeSuspend { responder } => {
+            return Ok((POWER_OFF, SuspendBlockerResponder::BeforeSuspend(responder)));
         }
-        fidl_fuchsia_power_broker::ElementRunnerRequest::_UnknownMethod { .. } => {
-            return Err(Error::msg("ElementRunnerRequest::_UnknownMethod received"));
+        fps::SuspendBlockerRequest::AfterResume { responder } => {
+            return Ok((POWER_ON, SuspendBlockerResponder::AfterResume(responder)));
+        }
+        fps::SuspendBlockerRequest::_UnknownMethod { .. } => {
+            return Err(Error::msg("SuspendBlockerRequest::_UnknownMethod received"));
         }
     }
 }
@@ -165,25 +153,25 @@ mod tests {
 
     #[fuchsia::test]
     async fn propagate_level() {
-        let (element_runner_client, element_runner_server) =
-            create_endpoints::<fpb::ElementRunnerMarker>();
-        let element_runner = element_runner_server.into_stream();
-        let element_runner_proxy = element_runner_client.into_proxy();
+        let (suspend_blocker_client, suspend_blocker_server) =
+            create_endpoints::<fps::SuspendBlockerMarker>();
+        let suspend_blocker = suspend_blocker_server.into_stream();
+        let suspend_blocker_proxy = suspend_blocker_client.into_proxy();
 
         // Management loop is also asynchronous.
         fasync::Task::local(async move {
-            management_loop(element_runner).await.await;
+            management_loop(suspend_blocker).await.await;
         })
         .detach();
 
-        assert!(element_runner_proxy.set_level(POWER_ON).await.is_ok());
+        assert!(suspend_blocker_proxy.after_resume().await.is_ok());
 
-        assert!(element_runner_proxy.set_level(POWER_OFF).await.is_ok());
+        assert!(suspend_blocker_proxy.before_suspend().await.is_ok());
 
-        assert!(element_runner_proxy.set_level(POWER_ON).await.is_ok());
+        assert!(suspend_blocker_proxy.after_resume().await.is_ok());
     }
 
-    async fn empty_loop(_: fpb::ElementRunnerRequestStream) -> fasync::Task<()> {
+    async fn empty_loop(_: fps::SuspendBlockerRequestStream) -> fasync::Task<()> {
         fasync::Task::local(async move {})
     }
 
@@ -191,24 +179,18 @@ mod tests {
     async fn test_manage_internal() {
         let (g_proxy, mut g_stream) =
             endpoints::create_proxy_and_stream::<fps::ActivityGovernorMarker>();
-        let (t_proxy, mut _t_stream) = endpoints::create_proxy_and_stream::<fpb::TopologyMarker>();
         let (_activity_s, mut activity_r) = mpsc::channel::<Command>(1);
 
         // Run the server side activity governor.
         fasync::Task::local(async move {
             while let Some(request) = g_stream.next().await {
                 match request {
-                    Ok(fps::ActivityGovernorRequest::GetPowerElements { responder }) => {
-                        let event = zx::Event::create();
-                        responder
-                            .send(fps::PowerElements {
-                                execution_state: Some(fps::ExecutionState {
-                                    opportunistic_dependency_token: Some(event),
-                                    ..Default::default()
-                                }),
-                                ..Default::default()
-                            })
-                            .expect("never fails");
+                    Ok(fps::ActivityGovernorRequest::RegisterSuspendBlocker {
+                        payload: _,
+                        responder,
+                    }) => {
+                        let (_unused_server_token, client_token) = fps::LeaseToken::create();
+                        responder.send(Ok(client_token)).expect("never fails");
                     }
                     Ok(_) | Err(_) => unimplemented!(),
                 }
@@ -217,21 +199,8 @@ mod tests {
         })
         .detach();
 
-        // Run the server side topology proxy
         fasync::Task::local(async move {
-            while let Some(request) = _t_stream.next().await {
-                match request {
-                    Ok(fpb::TopologyRequest::AddElement { payload: _, responder }) => {
-                        responder.send(Ok(())).expect("never fails");
-                    }
-                    Ok(_) | Err(_) => unimplemented!(),
-                }
-            }
-        })
-        .detach();
-
-        fasync::Task::local(async move {
-            manage_internal(g_proxy, t_proxy, _activity_s, empty_loop).await.unwrap().await;
+            manage_internal(g_proxy, _activity_s, empty_loop).await.unwrap().await;
         })
         .detach();
 
