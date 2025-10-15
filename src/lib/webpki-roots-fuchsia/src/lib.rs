b6f4394bd090c4c71802ae20fa965b338f7fe0a5
Thomas Zander <thomasalpinus@google.com>
2025-10-15 08:01:14 -0700

    [webpki] Migrate from lazy_static to LazyLock
    This migrates //src/lib/webpki-roots-fuchsia away from lazy_static.
    
    Change-Id: I7efea705db720ce7dfb3e7e89982be42b67a102d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1393515
    Reviewed-by: David Koloski <dkoloski@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>
    Fuchsia-Auto-Submit: Thomas Zander <thomasalpinus@google.com>
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>

--- src/lib/webpki-roots-fuchsia/src/lib.rs
+++ src/lib/webpki-roots-fuchsia/src/lib.rs
@@ -2,59 +2,59 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[macro_use]
-extern crate lazy_static;
-
-use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
 use base64::engine::Engine as _;
+use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
+use std::sync::LazyLock;
 
 static CERT_PATH: &'static str = "/config/ssl/cert.pem";
 
-lazy_static! {
-    // To meet the required lifetime constraints we need to chain together
-    // a series of statics.
-    static ref RAW_DATA: String = {
-        // I could have an environment variable override this, but i'd want it keyed
-        // on being a dev build so you couldn't harm trust in prod
-        std::fs::read_to_string(CERT_PATH)
-            .map_err(|e| log::error!("unable to find root certificate store: {}, {:?}", CERT_PATH, e))
-            .unwrap()
-    };
-    static ref CERT_DERS: Vec<Vec<u8>> = {
-        let lines: Vec<&str> = RAW_DATA
-            .split('\n')
-            .filter(|l| !l.is_empty() && !l.starts_with(':') && !l.starts_with('#'))
-            .collect();
-        let mut cert_ders = vec![];
-        let mut i = 0;
-        while i < lines.len() {
-            let start = i;
-            if lines[i] != "-----BEGIN CERTIFICATE-----" {
-                panic!("Missing certificate prefix");
-            }
-            while i < lines.len() && lines[i] != "-----END CERTIFICATE-----" {
-                i += 1;
-            }
-            if i == lines.len() {
-                panic!("Missing certificate suffix");
-            }
-            let end = i;
-            i += 1;
-            let cert_base64 = &lines[start + 1..end].join("");
-
-            let cert_bytes = BASE64_STANDARD.decode(cert_base64.as_bytes())
-                .expect("Invalid base64 encoding in root store");
-            cert_ders.push(cert_bytes);
+// To meet the required lifetime constraints we need to chain together
+// a series of statics.
+static RAW_DATA: LazyLock<String> = LazyLock::new(|| {
+    // I could have an environment variable override this, but i'd want it keyed
+    // on being a dev build so you couldn't harm trust in prod
+    std::fs::read_to_string(CERT_PATH)
+        .map_err(|e| log::error!("unable to find root certificate store: {}, {:?}", CERT_PATH, e))
+        .unwrap()
+});
+static CERT_DERS: LazyLock<Vec<Vec<u8>>> = LazyLock::new(|| {
+    let lines: Vec<&str> = RAW_DATA
+        .split('\n')
+        .filter(|l| !l.is_empty() && !l.starts_with(':') && !l.starts_with('#'))
+        .collect();
+    let mut cert_ders = vec![];
+    let mut i = 0;
+    while i < lines.len() {
+        let start = i;
+        if lines[i] != "-----BEGIN CERTIFICATE-----" {
+            panic!("Missing certificate prefix");
         }
-        cert_ders
-    };
-    pub static ref TLS_SERVER_ROOTS: Vec<webpki::TrustAnchor<'static>> = {
-        CERT_DERS.iter().map(|cert_bytes| {
+        while i < lines.len() && lines[i] != "-----END CERTIFICATE-----" {
+            i += 1;
+        }
+        if i == lines.len() {
+            panic!("Missing certificate suffix");
+        }
+        let end = i;
+        i += 1;
+        let cert_base64 = &lines[start + 1..end].join("");
+
+        let cert_bytes = BASE64_STANDARD
+            .decode(cert_base64.as_bytes())
+            .expect("Invalid base64 encoding in root store");
+        cert_ders.push(cert_bytes);
+    }
+    cert_ders
+});
+pub static TLS_SERVER_ROOTS: LazyLock<Vec<webpki::TrustAnchor<'static>>> = LazyLock::new(|| {
+    CERT_DERS
+        .iter()
+        .map(|cert_bytes| {
             webpki::TrustAnchor::try_from_cert_der(cert_bytes)
                 .expect("Parsing root certificate failed")
-        }).collect()
-    };
-}
+        })
+        .collect()
+});
 
 #[cfg(test)]
 mod test {
