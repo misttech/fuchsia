33950d2cb0e373af4bbc9af5e55326e18b9df14f
Ian McKellar <ianloic@google.com>
2025-10-16 17:12:52 -0700

    [gidl][rust-next] More tests pass
    Change-Id: I0275e5c32d0efb341204f6e5c726cb4d61b8beab
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1392763
    Commit-Queue: Ian McKellar <ianloic@google.com>
    Reviewed-by: David Koloski <dkoloski@google.com>

--- tools/fidl/gidl/backend/rust_next/conformance.go
+++ tools/fidl/gidl/backend/rust_next/conformance.go
@@ -256,7 +256,7 @@ var decodeErrorPatternMap = map[ir.ErrorCode]string{
 	ir.InvalidInlineBitInEnvelope:         `"TODO: InvalidInlineBitInEnvelope"`,
 	ir.InvalidInlineMarkerInEnvelope:      `DecodeError::InvalidEnvelopeFlags(_)`,
 	ir.InvalidNumBytesInEnvelope:          "DecodeError::InvalidEnvelopeSize(_)",
-	ir.InvalidNumHandlesInEnvelope:        "DecodeError::InvalidEnvelopeSize(_)",
+	ir.InvalidNumHandlesInEnvelope:        "DecodeError::IncorrectNumberOfHandlesConsumed{ .. }",
 	ir.InvalidPaddingByte:                 `"TODO: InvalidPaddingByte"`,
 	ir.InvalidPresenceIndicator:           "DecodeError::InvalidPointerPresence(_)",
 	ir.MissingRequiredHandleRights:        `"TODO: MissingRequiredHandleRights"`,
