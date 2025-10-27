5bf3179b3dbcbc863a4c193366dc0ff28ac56605
Jeff Martin <martinjeffrey@google.com>
2025-10-27 08:38:50 -0700

    [network-conformance] Audit ipv6_autoconfig
    Explain the Netstack3 failures in the ipv6_autoconfig suite.
    
    Fixed: 454298676
    Bug: 454651266
    Change-Id: I35931c465ba460b8e63da6d54d0a684fce14f939
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1404513
    Fuchsia-Auto-Submit: Jeff Martin <martinjeffrey@google.com>
    Reviewed-by: Peter DeLong <pdelong@google.com>
    Commit-Queue: Jeff Martin <martinjeffrey@google.com>

--- src/connectivity/network/testing/conformance/expectation/ipv6_autoconfig.go
+++ src/connectivity/network/testing/conformance/expectation/ipv6_autoconfig.go
@@ -47,26 +47,37 @@ var ipv6AutoconfigExpectations map[AnvlCaseNumber]outcome.Outcome = map[AnvlCase
 }
 
 var ipv6AutoconfigExpectationsNS3 map[AnvlCaseNumber]outcome.Outcome = map[AnvlCaseNumber]outcome.Outcome{
-	{1, 1}:  Pass,
-	{1, 2}:  Pass,
-	{1, 3}:  Pass,
-	{1, 4}:  Pass,
-	{1, 5}:  Pass,
-	{2, 1}:  Pass,
-	{2, 2}:  Fail,
-	{2, 3}:  Pass,
-	{3, 1}:  Pass,
-	{3, 2}:  AnvlSkip, // TODO(https://fxbug.dev/416093959): Need router version of this suite.
-	{3, 3}:  Pass,
-	{3, 4}:  Pass,
-	{3, 5}:  Pass,
-	{4, 1}:  Pass,
-	{4, 2}:  Pass,
-	{5, 1}:  Pass,
-	{5, 2}:  Pass,
-	{5, 3}:  Pass,
-	{5, 4}:  Pass,
-	{6, 1}:  Pass,
+	{1, 1}: Pass,
+	{1, 2}: Pass,
+	{1, 3}: Pass,
+	{1, 4}: Pass,
+	{1, 5}: Pass,
+	{2, 1}: Pass,
+	// This test fails because ANVL observes an unexpected Neighbor Solicitation
+	// from the netstack. The unexpected solicitation is for the interface's
+	// link-local address, which is undergoing DAD. This solicitation is in-line
+	// with expected behavior, and is infeasible to suppress. See
+	// https://fxbug.dev/454298676#comment2.
+	{2, 2}: Fail,
+	{2, 3}: Pass,
+	{3, 1}: Pass,
+	{3, 2}: AnvlSkip, // TODO(https://fxbug.dev/416093959): Need router version of this suite.
+	{3, 3}: Pass,
+	{3, 4}: Pass,
+	{3, 5}: Pass,
+	{4, 1}: Pass,
+	{4, 2}: Pass,
+	{5, 1}: Pass,
+	{5, 2}: Pass,
+	{5, 3}: Pass,
+	{5, 4}: Pass,
+	{6, 1}: Pass,
+	// TODO(https://fxbug.dev/454651266): Delay the start of DAD. This test
+	// generates a conflicting Neighbor Solicitations while the DUT is
+	// initializing DAD for a tentative address. It expects the DUT to recognize
+	// the conflict before sending the first Neighbor Solicitation, and abandon
+	// the address. This currently fails, because the DUT sends its Neighbor
+	// Solicitation immediately rather than after a randomized delay.
 	{6, 2}:  Fail,
 	{6, 3}:  Pass,
 	{7, 1}:  Pass,
