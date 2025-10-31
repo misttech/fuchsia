1decc2ab213c055fcbe2badc455a0edc0e75b63f
Zeling Feng <zeling@google.com>
2025-10-31 13:46:08 -0700

    Revert "[reachability] Improve default route detection"
    This reverts commit 3bc15dd0ca9f993988d6499c69bba4ff0dfc4748.
    
    Reason for revert: The `has_default_route` semantics has changed in https://fxrev.dev/1399053. We no longer need to go through all routes
    
    Bug: 445454306
    
    Original change's description:
    > [reachability] Improve default route detection
    >
    > The default route telemetry reporting was relying on the
    > has_default_v{4,6}_route fields from the interfaces watcher. These
    > fields are only set to true if the default route for the device is in
    > the main table. It is possible for an interface to have a default route
    > active in a separate table, so we should check through the routes to
    > determine whether there are any v4/v6 default routes present.
    >
    > Fixed: 440616646
    > Change-Id: I019b1e5ae5f0c0155b92890d73cd72ca972d65fc
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1361557
    > Reviewed-by: Zoey Bush <zmbush@google.com>
    > Commit-Queue: Antonio Linhart <antoniolinhart@google.com>
    
    Change-Id: I5869bf36898885003ddabf17fdf95c1f727d5cd0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1409467
    Commit-Queue: Zeling Feng <zeling@google.com>
    Reviewed-by: Antonio Linhart <antoniolinhart@google.com>

--- src/connectivity/policy/reachability/core/src/lib.rs
+++ src/connectivity/policy/reachability/core/src/lib.rs
@@ -1146,8 +1146,8 @@ impl<Time: TimeProvider> NetworkChecker for Monitor<Time> {
                     port_class,
                     online,
                     ref addresses,
-                    has_default_ipv4_route: _,
-                    has_default_ipv6_route: _,
+                    has_default_ipv4_route,
+                    has_default_ipv6_route,
                 },
             routes,
             neighbors,
@@ -1161,20 +1161,6 @@ impl<Time: TimeProvider> NetworkChecker for Monitor<Time> {
         // short period of time, for example changing between online and offline multiple times
         // over the span of a few seconds. It is safe that this happens, as the system is
         // eventually consistent.
-        let device_routes: Vec<_> = routes.device_routes(id).collect();
-        let relevant_routes: Vec<_> = device_routes
-            .iter()
-            .filter(|Route { destination, outbound_interface: _, next_hop: _ }| {
-                *destination == UNSPECIFIED_V4 || *destination == UNSPECIFIED_V6
-            })
-            .collect();
-        // Default routes for IPv4 and IPv6 must be calculated independently from the
-        // `InterfaceView` provided properties due to those fields only being set if the
-        // default route is present in the main route table.
-        let has_default_ipv4_route =
-            relevant_routes.iter().any(|Route { destination, .. }| *destination == UNSPECIFIED_V4);
-        let has_default_ipv6_route =
-            relevant_routes.iter().any(|Route { destination, .. }| *destination == UNSPECIFIED_V6);
         let telemetry_context = TelemetryContext::new(
             port_class,
             &addresses,
@@ -1216,6 +1202,8 @@ impl<Time: TimeProvider> NetworkChecker for Monitor<Time> {
 
         // TODO(https://fxbug.dev/42154208) Check if packet count has increased, and if so upgrade
         // the state to LinkLayerUp.
+        let device_routes: Vec<_> = routes.device_routes(id).collect();
+
         let neighbor_scan_health = scan_neighbor_health(neighbors, &device_routes);
 
         let has_route = IpVersions {
@@ -1246,9 +1234,12 @@ impl<Time: TimeProvider> NetworkChecker for Monitor<Time> {
             ctx.discovered_state.ipv6.set_link_state(LinkState::Local);
         }
 
-        let gateway_ping_addrs = relevant_routes
+        let gateway_ping_addrs = device_routes
             .iter()
-            .filter_map(move |Route { destination: _, outbound_interface, next_hop }| {
+            .filter_map(move |Route { destination, outbound_interface, next_hop }| {
+                if *destination != UNSPECIFIED_V4 && *destination != UNSPECIFIED_V6 {
+                    return None;
+                }
                 next_hop.and_then(|next_hop| {
                     let fnet_ext::IpAddress(next_hop) = next_hop.into();
                     match next_hop.into() {
