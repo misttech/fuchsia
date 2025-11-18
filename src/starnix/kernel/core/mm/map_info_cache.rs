f08b981a5d8af8fff077bb5488fe12ac66f4c2c3
Adam Perry <adamperry@google.com>
2025-11-18 07:26:00 -0800

    [starnix] Don't track inspect in MapInfoCache.
    This instrumentation is a nice-to-have and probably causes
    deadlocks. We can follow up with something better later.
    
    Bug: 461523287
    Change-Id: I5d7e5791df3757355ee72d2384870fbb1499b913
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1425975
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Reviewed-by: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Adam Perry <adamperry@google.com>

--- src/starnix/kernel/core/mm/map_info_cache.rs
+++ src/starnix/kernel/core/mm/map_info_cache.rs
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 use crate::task::CurrentTask;
-use fuchsia_inspect::{HistogramProperty, UintLinearHistogramProperty};
 use memory_pinning::ShadowProcess;
 use page_buf::PageBuf;
 use starnix_sync::Mutex;
@@ -19,9 +18,6 @@ use std::sync::Arc;
 /// in practice files like /proc/pid/status are not regularly accessed concurrently.
 pub struct MapInfoCache {
     buf: Mutex<PageBuf<zx::MapInfo>>,
-
-    _node: fuchsia_inspect::Node,
-    spilled_allocation_sizes: UintLinearHistogramProperty,
 }
 
 const ZIRCON_NAME: zx::Name = zx::Name::new_lossy("starnix_zx_map_info_cache");
@@ -41,32 +37,16 @@ impl MapInfoCache {
 
             let num_cache_elements = kernel.features.cached_zx_map_info_bytes as usize
                 / std::mem::size_of::<zx::MapInfo>();
-            Self::new(&pinned_shadow_process.0, num_cache_elements, &kernel.inspect_node)
+            Self::new(&pinned_shadow_process.0, num_cache_elements)
         })
     }
 
-    fn new(
-        shadow_process: &ShadowProcess,
-        num_cache_elements: usize,
-        parent: &fuchsia_inspect::Node,
-    ) -> Result<Self, Errno> {
+    fn new(shadow_process: &ShadowProcess, num_cache_elements: usize) -> Result<Self, Errno> {
         let buf = PageBuf::new_with_extra_vmar(num_cache_elements, shadow_process.vmar())
             .map_err(|e| from_status_like_fdio!(e))?;
         buf.set_name(&ZIRCON_NAME);
 
-        let _node = parent.create_child("map_info_cache");
-        let len_bytes = buf.len_bytes() as u64;
-        _node.record_uint("cache_size_bytes", len_bytes);
-        let spilled_allocation_sizes = _node.create_uint_linear_histogram(
-            "spilled_allocation_sizes_bytes",
-            fuchsia_inspect::LinearHistogramParams {
-                floor: len_bytes,
-                step_size: 100 * 1024, // 100 KiB
-                buckets: 8,            // 800 KiB
-            },
-        );
-
-        Ok(Self { buf: Mutex::new(buf), _node, spilled_allocation_sizes })
+        Ok(Self { buf: Mutex::new(buf) })
     }
 
     pub fn with_map_infos<R>(
@@ -76,16 +56,17 @@ impl MapInfoCache {
     ) -> R {
         let mut buf = self.buf.lock();
         match vmar.maps(buf.as_mut()) {
-            Ok((maps, _, avail)) if maps.len() == avail => op(Ok(maps)),
-            Ok((_, _, avail)) => {
-                // The buffer was not large enough, fall back to a heap allocation.
-                let spilled_bytes = avail * std::mem::size_of::<zx::MapInfo>();
-                self.spilled_allocation_sizes.insert(spilled_bytes as u64);
-                match vmar.maps_vec() {
-                    Ok(maps) => op(Ok(&maps)),
-                    Err(e) => op(Err(e)),
-                }
-            }
+            Ok((maps, _, avail)) if maps.len() == avail => return op(Ok(maps)),
+            Err(e) => return op(Err(e)),
+
+            // The call succeeded but the buffer wasn't big enough, fall back to a heap allocation.
+            Ok(_) => (),
+        }
+        // No need to hold this lock while we're using the heap instead.
+        drop(buf);
+
+        match vmar.maps_vec() {
+            Ok(maps) => op(Ok(&maps)),
             Err(e) => op(Err(e)),
         }
     }
@@ -94,13 +75,11 @@ impl MapInfoCache {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use diagnostics_assertions::{AnyProperty, assert_data_tree};
 
     #[fuchsia::test]
     async fn basic_test() {
-        let inspector = fuchsia_inspect::Inspector::default();
         let shadow_process = ShadowProcess::new(zx::Name::new_lossy("testing123")).unwrap();
-        let cache = MapInfoCache::new(&shadow_process, 100, inspector.root()).unwrap();
+        let cache = MapInfoCache::new(&shadow_process, 100).unwrap();
         let maps_base_addr = cache.with_map_infos(&shadow_process.vmar(), |maps| {
             let maps = maps.unwrap();
             assert_ne!(maps, &[]);
@@ -108,19 +87,12 @@ mod tests {
         });
         let cache_base_addr = cache.buf.lock().as_mut().as_ptr() as usize;
         assert_eq!(maps_base_addr, cache_base_addr, "map infos must have been read into cache");
-        assert_data_tree!(inspector, root: {
-            map_info_cache: {
-                cache_size_bytes: 16384u64,
-                spilled_allocation_sizes_bytes: AnyProperty,
-            }
-        });
     }
 
     #[fuchsia::test]
     async fn fall_back_to_heap_over_limit() {
-        let inspector = fuchsia_inspect::Inspector::default();
         let shadow_process = ShadowProcess::new(zx::Name::new_lossy("testing123")).unwrap();
-        let cache = MapInfoCache::new(&shadow_process, 1, inspector.root()).unwrap();
+        let cache = MapInfoCache::new(&shadow_process, 1).unwrap();
 
         // Ensure that the test process' root VMAR has more mappings than can fit in the single page
         // of the cache's buffer.
