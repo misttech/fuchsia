37fa8b07abb3db1a03ab090c5639aeceb6ffa5cc
John Wittrock <wittrock@google.com>
2025-11-19 09:28:30 -0800

    [examples][fidl] Port canvas client_requested_draw to rust_next
    This CL ports the `client_requested_draw` example to use the new
    `rust_next` FIDL bindings.
    
    Test: fx test examples_fidl_canvas_clientrequesteddraw_rust_next_tests
    Fixed: b/460459750
    Change-Id: If4fbd5be08778609cfbbe5af011e8bd7c8625b3b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1422554
    Reviewed-by: David Koloski <dkoloski@google.com>
    Commit-Queue: John Wittrock <wittrock@google.com>

--- examples/fidl/new/canvas/client_requested_draw/rust_next/client/src/main.rs
+++ examples/fidl/new/canvas/client_requested_draw/rust_next/client/src/main.rs
@@ -4,10 +4,39 @@
 
 use anyhow::{Context as _, Error, format_err};
 use config::Config;
-use fidl_examples_canvas_clientrequesteddraw::{InstanceEvent, InstanceMarker, Point};
-use fuchsia_component::client::connect_to_protocol;
-use futures::TryStreamExt;
-use std::{thread, time};
+use fidl_next_examples_canvas_clientrequesteddraw::instance::OnDrawn;
+use fidl_next_examples_canvas_clientrequesteddraw::{Instance, InstanceClientHandler, Point};
+use fuchsia_component::client::fidl_next::connect_to_protocol;
+use futures::channel::mpsc::UnboundedSender;
+use futures::stream::StreamExt;
+
+#[derive(Debug)]
+enum InstanceEvent {
+    OnDrawn { top_left: Point, bottom_right: Point },
+    Unknown { ordinal: u64 },
+}
+
+struct CanvasClient {
+    sender: UnboundedSender<InstanceEvent>,
+}
+
+impl InstanceClientHandler for CanvasClient {
+    async fn on_drawn(&mut self, event: fidl_next::Request<OnDrawn>) {
+        let payload = event.payload();
+        let event = InstanceEvent::OnDrawn {
+            top_left: payload.top_left,
+            bottom_right: payload.bottom_right,
+        };
+        let _ = self.sender.unbounded_send(event);
+    }
+
+    async fn on_unknown_interaction(&mut self, ordinal: u64) {
+        self.sender
+            .unbounded_send(InstanceEvent::Unknown { ordinal })
+            .context("Error sending unknown event to channel")
+            .unwrap();
+    }
+}
 
 #[fuchsia::main]
 async fn main() -> Result<(), Error> {
@@ -16,35 +45,30 @@ async fn main() -> Result<(), Error> {
     // Load the structured config values passed to this component at startup.
     let config = Config::take_from_startup_handle();
 
-    // Use the Component Framework runtime to connect to the newly spun up server component. We wrap
-    // our retained client end in a proxy object that lets us asynchronously send Instance requests
-    // across the channel.
-    let instance = connect_to_protocol::<InstanceMarker>()?;
+    // Create an MPSC channel to receive events from the InstanceClientHandler.
+    let (sender, mut receiver) = futures::channel::mpsc::unbounded::<InstanceEvent>();
+    let client_impl = CanvasClient { sender };
+
+    // Use the Component Framework runtime to connect to the newly spun up server component.
+    let instance = connect_to_protocol::<Instance>()?
+        .spawn_handler(client_impl);
     println!("Outgoing connection enabled");
 
-    // [START diff_1]
     let mut batched_lines = Vec::<[Point; 2]>::new();
     for action in config.script.into_iter() {
         // If the next action in the script is to "PUSH", send a batch of lines to the server.
         if action == "PUSH" {
-            instance.add_lines(&batched_lines).context("Could not send lines")?;
+            instance.add_lines(&batched_lines).await.context("Could not send lines")?;
             println!("AddLines request sent");
             batched_lines.clear();
             continue;
         }
-        // [END diff_1]
 
         // If the next action in the script is to "WAIT", block until an OnDrawn event is received
         // from the server.
         if action == "WAIT" {
-            let mut event_stream = instance.take_event_stream();
             loop {
-                match event_stream
-                    .try_next()
-                    .await
-                    .context("Error getting event response from proxy")?
-                    .ok_or_else(|| format_err!("Proxy sent no events"))?
-                {
+                match receiver.next().await.context("Proxy sent no events")? {
                     InstanceEvent::OnDrawn { top_left, bottom_right } => {
                         println!(
                             "OnDrawn event received: top_left: {:?}, bottom_right: {:?}",
@@ -52,20 +76,18 @@ async fn main() -> Result<(), Error> {
                         );
                         break;
                     }
-                    InstanceEvent::_UnknownEvent { ordinal, .. } => {
+                    InstanceEvent::Unknown { ordinal } => {
                         println!("Received an unknown event with ordinal {ordinal}");
                     }
                 }
             }
 
-            // [START diff_2]
             // Now, inform the server that we are ready to receive more updates whenever they are
             // ready for us.
             println!("Ready request sent");
             instance.ready().await.context("Could not send ready call")?;
             println!("Ready success");
             continue;
-            // [END diff_2]
         }
 
         // Add a line to the next batch. Parse the string input, making two points out of it.
@@ -80,7 +102,6 @@ async fn main() -> Result<(), Error> {
             })
             .collect::<Vec<Point>>();
 
-        // [START diff_3]
         // Assemble a line from the two points.
         let from = points.pop().ok_or_else(|| format_err!("line requires 2 points, but has 0"))?;
         let to = points.pop().ok_or_else(|| format_err!("line requires 2 points, but has 1"))?;
@@ -89,11 +110,7 @@ async fn main() -> Result<(), Error> {
         // Batch a line for drawing to the canvas using the two points provided.
         println!("AddLines batching line: {:?}", &mut line);
         batched_lines.push(line);
-        // [END diff_3]
     }
 
-    // TODO(https://fxbug.dev/42156498): We need to sleep here to make sure all logs get drained. Once the
-    // referenced bug has been resolved, we can remove the sleep.
-    thread::sleep(time::Duration::from_secs(2));
     Ok(())
 }
---
+++ examples/fidl/new/canvas/client_requested_draw/rust_next/server/src/main.rs
@@ -0,0 +1,150 @@
+// Copyright 2025 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::Error;
+use fidl_next::{Request, Responder, Server};
+use fidl_next_examples_canvas_clientrequesteddraw::{
+    BoundingBox, Instance, InstanceReadyResponse, InstanceServerHandler, Point, instance,
+};
+use fuchsia_async::{MonotonicInstant, Scope, Timer};
+use fuchsia_component::server::ServiceFs;
+use futures::StreamExt;
+use std::sync::{Arc, Mutex};
+use zx;
+
+// A struct that stores the two things we care about for this example: the bounding box the lines
+// that have been added thus far, and bit to track whether or not there have been changes since the
+// last `OnDrawn` event.
+#[derive(Debug)]
+struct CanvasState {
+    // Tracks whether there has been a change since the last send, to prevent redundant updates.
+    changed: bool,
+    // Tracks whether or not the client has declared itself ready to receive more updated.
+    ready: bool,
+    bounding_box: BoundingBox,
+}
+
+struct CanvasServer {
+    state: Arc<Mutex<CanvasState>>,
+    // We don't strictly need sender here if we don't send events from the handler,
+    // but it's good practice or might be needed for some patterns.
+    // In this example, events are sent from a separate task.
+}
+
+impl InstanceServerHandler for CanvasServer {
+    async fn add_lines(&mut self, request: Request<instance::AddLines>) {
+        let lines = &request.payload().lines;
+        println!("AddLines request received");
+
+        let mut state = self.state.lock().unwrap();
+
+        // Update the bounding box to account for the new lines we've just "added" to the canvas.
+        let bounds = &mut state.bounding_box;
+        for line in lines {
+            println!("AddLines printing line: {:?}", line);
+            for point in line {
+                if point.x < bounds.top_left.x {
+                    bounds.top_left.x = point.x;
+                }
+                if point.y > bounds.top_left.y {
+                    bounds.top_left.y = point.y;
+                }
+                if point.x > bounds.bottom_right.x {
+                    bounds.bottom_right.x = point.x;
+                }
+                if point.y < bounds.bottom_right.y {
+                    bounds.bottom_right.y = point.y;
+                }
+            }
+        }
+
+        // Mark the state as "dirty", so that an update is sent back to the client on the next tick.
+        state.changed = true;
+    }
+
+    async fn ready(&mut self, responder: Responder<instance::Ready>) {
+        println!("Ready request received");
+        // The client must only call `Ready() -> ();` after receiving an `-> OnDrawn();`
+        // event; if two "consecutive" `Ready() -> ();` calls are received, this
+        // interaction has entered an invalid state, and should be aborted immediately.
+        {
+            let mut state = self.state.lock().unwrap();
+            if state.ready {
+                // Invalid back-to-back Ready requests.
+                println!("Invalid back-to-back `Ready` requests received");
+                return;
+            }
+
+            state.ready = true;
+        }
+        responder.respond(fidl_next::Flexible::Ok(InstanceReadyResponse {})).await.unwrap();
+    }
+}
+
+/// A separate watcher task periodically "draws" the canvas, and notifies the client of the new
+/// state.
+async fn run_updater(state: Arc<Mutex<CanvasState>>, sender: Server<Instance>) {
+    loop {
+        // Our server sends one update per second, but only if the client has declared that it
+        // is ready to receive one.
+        Timer::new(MonotonicInstant::after(zx::Duration::from_seconds(1))).await;
+
+        let bounds = {
+            let mut state = state.lock().unwrap();
+            if !state.changed || !state.ready {
+                continue;
+            }
+
+            // Reset the change and ready trackers.
+            state.ready = false;
+            state.changed = false;
+            state.bounding_box.clone()
+        };
+
+        // Send the event.
+        // on_drawn(top_left, bottom_right)
+        if sender.on_drawn(&bounds.top_left, &bounds.bottom_right).await.is_err() {
+            break;
+        }
+
+        println!(
+            "OnDrawn event sent: top_left: {:?}, bottom_right: {:?}",
+            bounds.top_left, bounds.bottom_right
+        );
+    }
+}
+
+#[fuchsia::main]
+async fn main() -> Result<(), Error> {
+    println!("Started");
+
+    // Add a discoverable instance of our `Instance` protocol - this will allow the client to see
+    // the server and connect to it.
+    let scope = Arc::new(Scope::new());
+    let mut fs = ServiceFs::new_local();
+    fs.dir("svc").add_fidl_next_protocol::<Instance, _>(move |sender| {
+        let state = Arc::new(Mutex::new(CanvasState {
+            changed: true,
+            ready: true,
+            bounding_box: BoundingBox {
+                top_left: Point { x: 0, y: 0 },
+                bottom_right: Point { x: 0, y: 0 },
+            },
+        }));
+
+        let server = CanvasServer { state: state.clone() };
+
+        // Spawn a task to run the updater.
+        scope.spawn(run_updater(state, sender));
+
+        server
+    });
+    fs.take_and_serve_directory_handle()?;
+    println!("Listening for incoming connections");
+
+    // Run the service fs.
+    fs.collect::<()>().await;
+
+    Ok(())
+}
