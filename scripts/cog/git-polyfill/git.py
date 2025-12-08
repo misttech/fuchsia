210680cdf957352ecfec57f0a29e9e1209ad53bd
Chase Latta <chaselatta@google.com>
2025-12-08 09:03:38 -0800

    [cog] Implement git ls-files polyfill
    The git-polyfill script is updated to include a polyfill for the "git ls-files" command. The current implementation just forwards the invocation to the real-git for now.
    
    Change-Id: Ief8adc16f53fae06c73affa1a1c17df1c1d7498d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1442662
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Darren Chan <chandarren@google.com>
    Commit-Queue: Chase Latta <chaselatta@google.com>

--- scripts/cog/git-polyfill/git.py
+++ scripts/cog/git-polyfill/git.py
@@ -74,6 +74,39 @@ def get_relative_git_dir(
     return path
 
 
+def namespace_to_args(
+    namespace: argparse.Namespace, ignored_flags: List[str] = []
+) -> List[str]:
+    """Converts an argparse.Namespace object back into a list of command-line arguments."""
+    args: List[str] = []
+    for key, value in vars(namespace).items():
+        if value is None:
+            continue
+
+        flag = ""
+        if len(key) == 1:
+            # Assume single character keys are for short options (e.g. -C)
+            flag = f"-{key}"
+        else:
+            flag = f"--{key.replace('_', '-')}"
+
+        if flag in ignored_flags:
+            continue
+
+        if isinstance(value, bool):
+            if value:
+                args.append(flag)
+        elif isinstance(value, list):
+            for item in value:
+                args.append(flag)
+                args.append(str(item))
+        else:
+            args.append(flag)
+            args.append(str(value))
+
+    return args
+
+
 class GitSubCommand(abc.ABC):
     """Abstract base class for git subcommands."""
 
@@ -189,6 +222,114 @@ class StatusCommand(GitSubCommand):
         print(args)
         print("not implemented yet")
         return 0
+
+
+@register_command("ls-files")
+class LsFilesCommand(GitSubCommand):
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "-z",
+            action="store_true",
+            help="\\0 line termination on output and do not quote filenames.",
+        )
+        parser.add_argument(
+            "-c",
+            "--cached",
+            action="store_true",
+            help="Show all files cached in Git’s index, i.e. all tracked files.",
+        )
+        parser.add_argument(
+            "-d",
+            "--deleted",
+            action="store_true",
+            help="Show files with an unstaged deletion.",
+        )
+        parser.add_argument(
+            "-o",
+            "--others",
+            action="store_true",
+            help="Show other (i.e. untracked) files in the output.",
+        )
+        parser.add_argument(
+            "-i",
+            "--ignored",
+            action="store_true",
+            help="Show only ignored files in the output.",
+        )
+        parser.add_argument(
+            "-s",
+            "--stage",
+            action="store_true",
+            help="Show staged contents' mode bits, object name and stage number in the output.",
+        )
+        parser.add_argument(
+            "-u",
+            "--unmerged",
+            action="store_true",
+            help="Show information about unmerged files in the output.",
+        )
+        parser.add_argument(
+            "-m",
+            "--modified",
+            action="store_true",
+            help="Show files with an unstaged modification.",
+        )
+        parser.add_argument(
+            "-x",
+            "--exclude",
+            type=str,
+            metavar="<pattern>",
+            nargs="*",
+            help="Skip untracked files matching pattern.",
+        )
+        parser.add_argument(
+            "--format",
+            type=str,
+            metavar="<format>",
+            help="A string that interpolates %(fieldname) from the result being shown.",
+        )
+        parser.add_argument("file", nargs="*", help="Files to show.")
+
+    def execute(
+        self, top_level_args: argparse.Namespace, args: argparse.Namespace
+    ) -> int:
+        try:
+            cmd = [
+                top_level_args.real_git,
+            ]
+            cmd.extend(
+                namespace_to_args(
+                    top_level_args,
+                    ignored_flags=["--real-git", "--invoker-cwd"],
+                )
+            )
+            cmd.append("ls-files")
+
+            # The namespace_to_args function does not know how to handle positional
+            # arguments, it will convert all positional arguments to flags and so we need to
+            # handle it here.
+            cmd.extend(namespace_to_args(args, ignored_flags=["--file"]))
+
+            if args.file:
+                cmd.extend(args.file)
+
+            result = subprocess.run(
+                cmd,
+                capture_output=True,
+                text=True,
+                cwd=top_level_args.invoker_cwd,
+            )
+
+            if result.returncode != 0:
+                print(result.stderr, file=sys.stderr)
+                return result.returncode
+
+            end = "\0" if args.z else "\n"
+            print(result.stdout, end=end)
+            return 0
+        except Exception as e:
+            print(f"fatal: {e}", file=sys.stderr)
+            return 10
 
 
 def _find_command_name_and_position(
