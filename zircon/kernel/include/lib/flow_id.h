2004056d49aa5fce12fa3f09de8dc7ab0ab8ba79
Corey Tabaka <eieio@google.com>
2025-12-09 08:57:16 -0800

    [kernel][lib] Globally unique flow id generator
    Add a simple utility to generate globally unique 64bit flow ids. These
    ids are generated in the upper half of the 64bit id space to avoid
    collisions with zero-based userspace flow id generators.
    
    Change-Id: Iac0954a16e1055db7bcb11c53bf391411199e477
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1445428
    Fuchsia-Auto-Submit: Corey Tabaka <eieio@google.com>
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Corey Tabaka <eieio@google.com>

---
+++ zircon/kernel/include/lib/flow_id.h
@@ -0,0 +1,30 @@
+// Copyright 2025 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_INCLUDE_LIB_FLOW_ID_H_
+#define ZIRCON_KERNEL_INCLUDE_LIB_FLOW_ID_H_
+
+#include <stdint.h>
+
+#include <ktl/atomic.h>
+
+// Generates globally unique 64bit flow ids for tracing.
+class FlowId {
+ public:
+  // Allocates and returns a flow id.
+  static uint64_t Generate() {
+    return flow_id_generator_.fetch_add(1ULL, ktl::memory_order_relaxed);
+  }
+
+ private:
+  // Decrease the likelihood of collisions with zero-based userspace flow id
+  // generators by starting in the second half of the flow id space.
+  static constexpr uint64_t kFirstKernelFlowId{uint64_t{1} << 63};
+
+  inline static ktl::atomic<uint64_t> flow_id_generator_{kFirstKernelFlowId};
+};
+
+#endif  // ZIRCON_KERNEL_INCLUDE_LIB_FLOW_ID_H_
