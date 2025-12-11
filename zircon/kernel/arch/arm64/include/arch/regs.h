e68921bc548e149443912b9dbda0095a4a1675f1
John Grossman <johngro@google.com>
2025-12-10 18:55:41 -0800

    [arm64][iframe] Fix some assumptions about iframe layout.
    Fix some assumptions about iframe layout which were made when
    launching a user-mode thread.
    
    Specifically, the layout (currently) looks like this:
    ```
    r[30]
    lr
    usp
    elr
    spsr
    ```
    
    The initial register array covers [r0, r29] and is immediately
    followed by LR, which is technically the same as r30.  This said, the
    structure layout _explicitly_ does not make `lr` part of the `r`
    array.
    
    Prior to this change, `arm64_uspace_entry` was assuming that `r30`
    always followed `r29`.  It was treating the `r` array as if it was
    declared as `uint64_t r[31]`, instead of how it was actually declared
    (`uint64_t r[30]`).  This happens on the following line of the
    implementation:
    
    ```
    ldp x29, x30, [x0, ARM64_IFRAME_OFFSET_R + (29 * 8)]
    ```
    
    If changes are made to the structure layout (to place the lr member
    somewhere else, for example) this code will fail.
    
    With all of that said, there is a LOT of low level code which makes
    the assumption that certain registers are "paired" (eg; one directly
    follows another in the structure layout).  This is so that
    save/restore code can use the ARM load/store pair instructions to save
    cycles, instead of loading registers one at a time.
    
    To keep things clear moving forward, this CL does two things.
    
    1) It changes `arm64_uspace_entry` to not read "past the end" of the
       C-style `r` array.  `r[30]` has 30 elements, not 31.  Don't assume
       it is either safe or correct to read past this.
    2) It adds static asserts near the declaration of `iframe_t` to
       formalize the load-bearing "pairing" assumptions made by asm code.
       `r[30]` has an even number of elements, so it is implicit that
       `r[1]` always follows `r[0]`.  The new asserts also assert that
       `usp` always follows `lr`, and `spsr` always follows `elr`.
    
    Change-Id: I2b8609b417338578cadbac281f0a8f2f5378db0b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1446950
    Fuchsia-Auto-Submit: John Grossman <johngro@google.com>
    Commit-Queue: John Grossman <johngro@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>

--- zircon/kernel/arch/arm64/include/arch/regs.h
+++ zircon/kernel/arch/arm64/include/arch/regs.h
@@ -30,11 +30,24 @@ struct iframe_t {
 static_assert(sizeof(iframe_t) % 16u == 0u);
 static_assert(sizeof(iframe_t) == ARM64_IFRAME_SIZE);
 
+// Registers in the iframe are logically laid out in specific pairs, and *must*
+// either remain this way, or a lot of lower level ASM code will need to be
+// updated.  The low level ASM code uses load/store pair instructions when
+// restoring/saving register state from/to the iframe. The first 30 registers
+// (r0-29) are implicitly paired together by nature of the C-array.  After that,
+// the following layout rules need to be enforced:
+//
+// 1) [r[0], r[29]] are all paired.  IOW, r[N+1] must always follow r[N] for all
+//    even N in the range [0, 2, 4, ... 28].
+// 2) ELR/SPSR are a pair; SPSR must always immediately follow ELR.
+// 3) LR/USP are a pair; USP must always immediately follow LR.
 static_assert(__offsetof(iframe_t, r[0]) == ARM64_IFRAME_OFFSET_R);
 static_assert(__offsetof(iframe_t, lr) == ARM64_IFRAME_OFFSET_LR);
 static_assert(__offsetof(iframe_t, usp) == ARM64_IFRAME_OFFSET_USP);
+static_assert(__offsetof(iframe_t, lr) + sizeof(uint64_t) == __offsetof(iframe_t, usp));
 static_assert(__offsetof(iframe_t, elr) == ARM64_IFRAME_OFFSET_ELR);
 static_assert(__offsetof(iframe_t, spsr) == ARM64_IFRAME_OFFSET_SPSR);
+static_assert(__offsetof(iframe_t, elr) + sizeof(uint64_t) == __offsetof(iframe_t, spsr));
 
 // Registers saved on entering the kernel via syscall.
 using syscall_regs_t = iframe_t;
