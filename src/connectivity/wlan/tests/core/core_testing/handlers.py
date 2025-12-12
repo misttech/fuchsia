899508d92ecf8b464163ed94ed59cb31d15a7734
Charles Celerier <chcl@google.com>
2025-12-11 17:22:51 -0800

    [wlan] Add a test for the presence of a phy through core
    The test added in this change checks for the presence of a phy with
    fuchsia.wlan.device.service/DeviceMonitor.
    
    Change-Id: I50d111a4a5c3c45adda934765512301fb4b367b1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1441531
    Commit-Queue: Charles Celerier <chcl@google.com>
    Reviewed-by: Dylan Swiggett <swiggett@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/connectivity/wlan/tests/core/core_testing/handlers.py
+++ src/connectivity/wlan/tests/core/core_testing/handlers.py
@@ -14,8 +14,78 @@ import asyncio
 from dataclasses import dataclass
 from typing import Any
 
+import fidl_fuchsia_wlan_device_service as fidl_device_service
 import fidl_fuchsia_wlan_sme as fidl_sme
 from fuchsia_controller_py import Channel
+
+
+@dataclass
+class DeviceWatcherContext:
+    txn_queue: asyncio.Queue[
+        fidl_device_service.DeviceWatcherOnPhyAddedRequest
+        | fidl_device_service.DeviceWatcherOnPhyRemovedRequest
+        | fidl_device_service.DeviceWatcherOnIfaceAddedRequest
+        | fidl_device_service.DeviceWatcherOnIfaceRemovedRequest
+    ]
+
+
+class DeviceWatcherEventHandler(fidl_device_service.DeviceWatcherEventHandler):
+    def __init__(
+        self,
+        client: fidl_device_service.DeviceWatcherClient,
+        verbose: bool = True,
+    ) -> None:
+        self.client = client
+        self.verbose = verbose
+        self.txn_queue: asyncio.Queue[
+            fidl_device_service.DeviceWatcherOnPhyAddedRequest
+            | fidl_device_service.DeviceWatcherOnPhyRemovedRequest
+            | fidl_device_service.DeviceWatcherOnIfaceAddedRequest
+            | fidl_device_service.DeviceWatcherOnIfaceRemovedRequest
+        ] = asyncio.Queue()
+
+    def on_phy_added(
+        self,
+        request: fidl_device_service.DeviceWatcherOnPhyAddedRequest,
+    ) -> None:
+        if self.verbose:
+            logger.info("phy added: %s", request)
+        self.txn_queue.put_nowait(request)
+
+    def on_phy_removed(
+        self,
+        request: fidl_device_service.DeviceWatcherOnPhyRemovedRequest,
+    ) -> None:
+        if self.verbose:
+            logger.info("phy removed: %s", request)
+        self.txn_queue.put_nowait(request)
+
+    def on_iface_added(
+        self,
+        request: fidl_device_service.DeviceWatcherOnIfaceAddedRequest,
+    ) -> None:
+        if self.verbose:
+            logger.info("iface added: %s", request)
+        self.txn_queue.put_nowait(request)
+
+    def on_iface_removed(
+        self,
+        request: fidl_device_service.DeviceWatcherOnIfaceRemovedRequest,
+    ) -> None:
+        if self.verbose:
+            logger.info("iface removed: %s", request)
+        self.txn_queue.put_nowait(request)
+
+    async def __aenter__(self) -> DeviceWatcherContext:
+        super().__init__(client=self.client)
+        self.server_task = asyncio.create_task(self.serve())
+        return DeviceWatcherContext(
+            txn_queue=self.txn_queue,
+        )
+
+    async def __aexit__(self, *args: Any, **kwargs: Any) -> None:
+        if self.server_task:
+            self.server_task.cancel()
 
 
 @dataclass
