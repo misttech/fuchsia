bed3d091d1bea9658a5ef38b85e275107e063db9
John Grossman <johngro@google.com>
2025-12-15 10:12:35 -0800

    [arm64][exceptions] Include ELR in EL1 backtraces
    When we take a synchronous exception from EL1 -> EL1, synthesize a
    fake frame from the current frame pointer (at the time the exception
    was taken) and the current ELR.  This way, if the exception is fatal,
    we will automatically include the ELR in the backtrace generated and
    sent to the serial port and crashlog.  This can then be automatically
    symbolized, and help to save a bit of time for folks attempting to
    triage kernel crashes.
    
    This is accomplished by doing two things.
    
    First, we modify the layout of our iframe structure.  This is the
    structure that we store our registers in when we take an exception.
    The layout is arbitrary, and we are free to change it as we see fit.
    Prior to this, the  layout looked something like this:
    
    ```
    [x0 - x29]
    x30/LR
    USP
    ELR
    SPSR
    ```
    
    This CL swaps the position of the LR/USP and ELR/SPSR pairs, giving
    us:
    
    ```
    [x0 - x29]
    ELR
    SPSR
    x30/LR
    USP
    ```
    
    So, we have re-arranged the structure so that where we used to have
    `x29/x30` next to each other, we now have `x29/ELR`.  We've also done
    it in such a way that we have kept all of the pairs of registers in
    the structure together.
    
    When registers are saved and restored during exceptions, they are
    stored and loaded using the ARM load/store-pair instructions.  By
    keeping pairs together and only changing their offsets in the
    structure, we don't have to change _any_ of the existing structure of
    the code which does the saving and loading.
    
    And, now that we have `x29` next to `ELR` in the iframe layout, we can
    pull one more trick.
    
    When we fault, `x29` holds the value of the frame pointer at the time
    of the exception, while ELR holds the address of where the synchronous
    exception happened.  This is basically identical to the definition of
    a frame according to the ARM ABI.  Now, just before jumping into the
    C/C++ level of exception handing, if we load `x29` with a value
    pointing to `x29` in our iframe, we have make it look like there is a
    new "exception handler frame" on the stack.  After this, when we
    render a backtrace (to dump to the serial port, or to stuff into a
    crashlog) it will automatically include the instruction where the
    fault actually happened as a level of the stack, allowing it to be
    automatically symbolized by `ffx debug symbolize`.
    
    While the change in structure layout affects all EL1 exception
    handlers, the loading of `x29` happens only in synchronous EL1 to EL1
    exceptions, which really should only happen when attempting to access
    user-memory and hitting a serviceable page fault, or when taking a
    fatal exception.
    
    Please note that most of this information (esp the value of the ELR)
    is already in these crash-dumps.  Unfortunately, the tools which
    symbolise the backtraces don't know that they need to look for the
    ELR, nor do they have unambiguous knowledge of where the ELR belongs
    in the backtrace, although adding a bit more information is probably
    all it would take to nail it down precisely.  Reworking the code which
    produces the backtraces as well as the code which interprets them can
    achieve similar results, however it is significantly more work than
    this small tweak.
    
    So, add this tweak for now.  Someday in the future when we have
    consolidated our backtrace handling, we can come back here and remove
    this approach and save an instruction in the process.
    
    Change-Id: I86d1fa412839500d90369212566ed548fb8977c3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1441275
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: John Grossman <johngro@google.com>

--- zircon/kernel/arch/arm64/include/arch/regs.h
+++ zircon/kernel/arch/arm64/include/arch/regs.h
@@ -7,10 +7,10 @@
 #define ZIRCON_KERNEL_ARCH_ARM64_INCLUDE_ARCH_REGS_H_
 
 #define ARM64_IFRAME_OFFSET_R (0 * 8)
-#define ARM64_IFRAME_OFFSET_LR (30 * 8)
-#define ARM64_IFRAME_OFFSET_USP (31 * 8)
-#define ARM64_IFRAME_OFFSET_ELR (32 * 8)
-#define ARM64_IFRAME_OFFSET_SPSR (33 * 8)
+#define ARM64_IFRAME_OFFSET_ELR (30 * 8)
+#define ARM64_IFRAME_OFFSET_SPSR (31 * 8)
+#define ARM64_IFRAME_OFFSET_LR (32 * 8)
+#define ARM64_IFRAME_OFFSET_USP (33 * 8)
 #define ARM64_IFRAME_SIZE ((30 + 4) * 8)
 
 #ifndef __ASSEMBLER__
@@ -19,35 +19,74 @@
 #include <zircon/compiler.h>
 
 // Registers saved on entering the kernel via architectural exception.
+// See the notes below for more details.
+//
 struct iframe_t {
   uint64_t r[30];  // x0-x29
-  uint64_t lr;     // x30 (arm64 lr)
-  uint64_t usp;    // either SP_EL0 if from lower EL otherwise the SP before the iframe was pushed
+  // **NOTE:** It is important that ELR immediately follows r[29] in memory.  See below.
   uint64_t elr;    // ELR_EL1
   uint64_t spsr;   // SPSR_EL1
+  uint64_t lr;     // x30 (arm64 lr)
+  uint64_t usp;    // either SP_EL0 if from lower EL otherwise the SP before the iframe was pushed
 };
 
 static_assert(sizeof(iframe_t) % 16u == 0u);
 static_assert(sizeof(iframe_t) == ARM64_IFRAME_SIZE);
 
-// Registers in the iframe are logically laid out in specific pairs, and *must*
-// either remain this way, or a lot of lower level ASM code will need to be
-// updated.  The low level ASM code uses load/store pair instructions when
-// restoring/saving register state from/to the iframe. The first 30 registers
-// (r0-29) are implicitly paired together by nature of the C-array.  After that,
-// the following layout rules need to be enforced:
+// The first 30 entries here are a direct mapping to r0-r29.  Typically, they
+// would be followed by r30 (the link register). That said, the layout of the
+// structure is technically arbitrary.  We just need to save all of the
+// registers so we can properly restore them later on.
+//
+// In this case, we have re-arranged them just slightly so we can implement a
+// small trick in our synchronous EL1 -> EL1 exception handlers.  Instead of
+// placing the LR where r30 would normally be, we place the ELR there instead,
+// followed by the SPSR, and then the saved value of r30 (the LR).
+static_assert(__offsetof(iframe_t, r) == ARM64_IFRAME_OFFSET_R);
+static_assert(__offsetof(iframe_t, r[29]) + sizeof(uint64_t) == __offsetof(iframe_t, elr));
+
+// During an exception handler, after saving our registers, but before
+// transferring control to the top level C exception handler, we can point r29
+// (the frame pointer) at the FP/ELR (eg, r[29]/ELR) pair in our iframe.  Then
+// we proceed as normal.
+//
+// If the exception turns out to be fatal, and we attempt to produce a backtrace
+// during our panic handlers, it will look (to the backtrace code) like we have
+// and extra frame on the stack, only the frame is embedded in our iframe
+// structure instead of being a typical frame.  The FP in this frame points to
+// the last valid frame before the exception happened.  IOW - the frame which
+// points to the function which called the function which took the exception.
+// By following this with the ELR, the backtrace code will see the address at
+// which the exception was taken as the place that the virtual "exception
+// handler frame" was called from.
+//
+// The cost of this (in addition to the structure re-ordering) is just a single
+// instruction which loads x29 with the value of the stack pointer plus a
+// constant offset.
+//
+// During unwind, the proper value of x29 will be reloaded using the r[29]
+// member of the iframe, automatically destroying our "virtual frame" in the
+// process.
+static_assert(__offsetof(iframe_t, elr) == ARM64_IFRAME_OFFSET_ELR);
+static_assert(__offsetof(iframe_t, spsr) == ARM64_IFRAME_OFFSET_SPSR);
+static_assert(__offsetof(iframe_t, elr) + sizeof(uint64_t) == __offsetof(iframe_t, spsr));
+
+// Additionally, the registers in the iframe are logically laid out in specific
+// pairs, and *must* either remain this way, or a lot of lower level ASM code
+// will need to be updated.  The low level ASM code uses load/store pair
+// instructions when restoring/saving register state from/to the iframe. The
+// first 30 registers (r0-29) are implicitly paired together by nature of the
+// C-array.  After that, the following layout rules need to be enforced:
 //
 // 1) [r[0], r[29]] are all paired.  IOW, r[N+1] must always follow r[N] for all
 //    even N in the range [0, 2, 4, ... 28].
 // 2) ELR/SPSR are a pair; SPSR must always immediately follow ELR.
 // 3) LR/USP are a pair; USP must always immediately follow LR.
-static_assert(__offsetof(iframe_t, r[0]) == ARM64_IFRAME_OFFSET_R);
+// 4) ELR must always follow r[29].  This makes the "exception frame" trick
+//    work.  See above.
 static_assert(__offsetof(iframe_t, lr) == ARM64_IFRAME_OFFSET_LR);
 static_assert(__offsetof(iframe_t, usp) == ARM64_IFRAME_OFFSET_USP);
 static_assert(__offsetof(iframe_t, lr) + sizeof(uint64_t) == __offsetof(iframe_t, usp));
-static_assert(__offsetof(iframe_t, elr) == ARM64_IFRAME_OFFSET_ELR);
-static_assert(__offsetof(iframe_t, spsr) == ARM64_IFRAME_OFFSET_SPSR);
-static_assert(__offsetof(iframe_t, elr) + sizeof(uint64_t) == __offsetof(iframe_t, spsr));
 
 // Registers saved on entering the kernel via syscall.
 using syscall_regs_t = iframe_t;
