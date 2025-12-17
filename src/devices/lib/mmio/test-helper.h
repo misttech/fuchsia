525bc84ff63e9180ac90dfee65624f7876737771
Christopher Anderson <cja@google.com>
2025-12-17 13:32:22 -0800

    [lib][mmio] Delete //src/devices/lib/mmio
    All clients have been updated to use //sdk/lib/driver/mmio/cpp
    
    Bug: 441711303
    Change-Id: I2409b2f115f46a5b80800bfc66e10726729622f2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1384664
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Christopher Anderson <cja@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>

--- src/devices/lib/mmio/test-helper.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-#ifndef SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
-#define SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
-
-#include <lib/driver/mmio/testing/cpp/test-helper.h>
-
-#endif  // SRC_DEVICES_LIB_MMIO_TEST_HELPER_H_
