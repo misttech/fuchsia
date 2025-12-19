bc8908aed17d38368e3825d87bc9c5043d060bf7
Joshua Seaton <joshuaseaton@google.com>
2025-12-18 16:27:35 -0800

    [core-tests] More gracefully handle absent midr.txt
    Currently we only emit midr.txt for arm64 when
    ZX_DEBUG_ASSERT_IMPLEMENTED in the kernel is nonzero. However, we are
    trying to run core tests in a configuration where it is zero
    (*-no_kernel_debug) and are failing to skip the
    ProcessTest.GetInfoRuntime* tests in that context since there is no
    midr.txt to make the "is Cavium?" check.
    
    In this change we update the "is Cavium?" logic to allow for
    "I don't know" (i.e., when midr.txt is absent on arm64), and then update
    the skipping logic to explicitly log that this was the case and that it
    will be pessimistically assuming that we are on a Cavium. This ensures
    that we don't unintentionally run these tests on a Cavium when
    ZX_DEBUG_ASSERT_IMPLEMENTED is zero. Not ideal, but we should still have
    sufficient test coverage for these cases on other arm64 hardware for a
    variety of configurations.
    
    Fixed: 467750183
    Change-Id: I501e596f9f94fc75db660667826f935d5a15836d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1451512
    Reviewed-by: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Joshua Seaton <joshuaseaton@google.com>
    Fuchsia-Auto-Submit: Joshua Seaton <joshuaseaton@google.com>

--- zircon/system/utest/core/process/process.cc
+++ zircon/system/utest/core/process/process.cc
@@ -51,24 +51,54 @@ constexpr std::string_view kCaviumMidr = "Cavium CN99XX";
 constexpr std::string_view kCaviumMidr{};
 #endif
 
-std::string ReadMidrTxt() {
+// Returns std::nullopt if no midr.txt was present (as distinguished from the
+// empty string, which indicates that it was present and empty).
+std::optional<std::string> ReadMidrTxt() {
   std::string midr;
   zx::unowned_vmo midr_vmo = standalone::GetVmo("midr.txt");
-  if (*midr_vmo) {
-    uint64_t size = 0;
-    EXPECT_OK(midr_vmo->get_prop_content_size(&size));
-    midr.resize(size);
-    EXPECT_OK(midr_vmo->read(midr.data(), 0, midr.size()));
-    EXPECT_STRNE(midr, "");
+  if (!*midr_vmo) {
+    return std::nullopt;
   }
+
+  uint64_t size = 0;
+  EXPECT_OK(midr_vmo->get_prop_content_size(&size));
+  midr.resize(size);
+  EXPECT_OK(midr_vmo->read(midr.data(), 0, midr.size()));
+  EXPECT_STRNE(midr, "");
+  printf("INFO: Contents of midr.txt: \"%s\"\n", midr.c_str());
   return midr;
 }
 
-bool CheckIsCavium() { return !kCaviumMidr.empty() && ReadMidrTxt().starts_with(kCaviumMidr); }
+// Returns std::nullopt if we were unable to determine for certain whether we
+// are on a Cavium (i.e., if no midr.txt was present on arm64).
+[[maybe_unused]] std::optional<bool> CheckIsCavium() {
+  if (kCaviumMidr.empty()) {
+    return false;
+  }
 
-bool SkipBug363254896() {
-  static bool is_cavium = CheckIsCavium();
-  return is_cavium;
+  // No midr.txt, so cannot determine Cavium-ness at this stage.
+  std::optional<std::string> midr = ReadMidrTxt();
+  if (!midr) {
+    return std::nullopt;
+  }
+  return midr->starts_with(kCaviumMidr);
+}
+
+// Returns std::nullopt if skipping should not occur, or else will return the
+// message that should be passed to ZXTEST_SKIP().
+std::optional<std::string_view> SkipBug363254896() {
+#ifdef __aarch64__
+  static std::optional<bool> is_cavium = CheckIsCavium();
+
+  if (!is_cavium.has_value()) {
+    return "WARNING: No midr.txt was present, so assuming the worst (i.e., that "
+           "we're on a Cavium) and skipping per https://fxbug.dev/363254896\n";
+  }
+  if (*is_cavium) {
+    return "We are on a Cavium, so skipping per https://fxbug.dev/363254896\n";
+  }
+#endif
+  return std::nullopt;
 }
 
 TEST(ProcessTest, LongNameSucceeds) {
@@ -729,8 +759,8 @@ TEST(ProcessTest, SuspendWithDyingThread) {
 
 template <typename InfoT>
 static void TestProcessGetInfoRuntime(const uint32_t topic) {
-  if (SkipBug363254896()) {
-    ZXTEST_SKIP("https://fxbug.dev/363254896 skipped on buggy hardware");
+  if (std::optional<std::string_view> skip = SkipBug363254896(); skip) {
+    ZXTEST_SKIP(*skip);
   }
 
   TestProcess test_process;
