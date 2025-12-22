c63548eb89a5104f521823c4258e65970a1b923e
Charles Celerier <chcl@google.com>
2025-12-22 12:36:43 -0800

    [wlan] Create end-to-end tests for creating client and AP interfaces with core
    The added tests create client and AP ifaces with null and particular MAC
    addresses.
    
    This change deletes the now redundant WlanInterfaceTest from antlion.
    
    Change-Id: Iadc1905a25ed6f40f6ecfeafd7ce621b5a5cfa17
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1454309
    Reviewed-by: Kiet Tran <kiettran@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

---
+++ src/connectivity/wlan/tests/core/CreateApIfaceWithParticularMacTest.py
@@ -0,0 +1,65 @@
+# Copyright 2025 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Test that the device has exactly one phy.
+
+This test could assert at least one phy; however, it's likely an error today
+if there is more than one phy listed.
+"""
+import logging
+
+from honeydew.affordances.connectivity.wlan.utils.types import MacAddress
+
+logger = logging.getLogger(__name__)
+
+import random
+
+import fidl_fuchsia_wlan_common as fw_common
+from core_testing import base_test
+from fuchsia_controller_py.wrappers import asyncmethod
+from mobly import asserts, test_runner
+
+
+class CreateApIfaceWithParticularMacTest(base_test.CoreBaseTestClass):
+    @asyncmethod
+    async def test_create_ap_iface_with_particular_mac(self) -> None:
+        random_sta_addr = None
+        while True:
+            random_six_bytes = [random.randint(0, 255) for _ in range(6)]
+            if random_six_bytes != [0, 0, 0, 0, 0, 0]:
+                random_sta_addr = MacAddress.from_bytes(bytes(random_six_bytes))
+                break
+
+        logger.info(f"Creating AP iface with MAC {random_sta_addr}")
+        # TODO(https://fxbug.dev/470568403): This fails on some devices.
+        create_iface_response = (
+            await self.test_kit.device_monitor.create_iface(
+                phy_id=self.test_kit.phy_id,
+                role=fw_common.WlanMacRole.AP,
+                sta_address=random_sta_addr.bytes(),
+            )
+        ).unwrap()
+        assert (
+            create_iface_response.iface_id is not None
+        ), "DeviceMonitor.CreateIface() response is missing a iface_id"
+        iface_id = create_iface_response.iface_id
+
+        query_iface_response = (
+            (await self.test_kit.device_monitor.query_iface(iface_id=iface_id))
+            .unwrap()
+            .resp
+        )
+        asserts.assert_equal(iface_id, query_iface_response.id_)
+        asserts.assert_equal(self.test_kit.phy_id, query_iface_response.phy_id)
+        asserts.assert_equal(
+            fw_common.WlanMacRole.AP, query_iface_response.role
+        )
+        asserts.assert_equal(
+            random_sta_addr,
+            MacAddress.from_bytes(bytes(query_iface_response.sta_addr)),
+        )
+
+
+if __name__ == "__main__":
+    test_runner.main()
---
+++ src/connectivity/wlan/tests/core/CreateClientIfaceWithNullMacTest.py
@@ -0,0 +1,53 @@
+# Copyright 2025 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Test that the device has exactly one phy.
+
+This test could assert at least one phy; however, it's likely an error today
+if there is more than one phy listed.
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+import fidl_fuchsia_wlan_common as fw_common
+from core_testing import base_test
+from fuchsia_controller_py.wrappers import asyncmethod
+from mobly import asserts, test_runner
+
+
+class CreateClientIfaceWithNullMacTest(base_test.CoreBaseTestClass):
+    @asyncmethod
+    async def test_create_client_iface_with_null_mac(self) -> None:
+        create_iface_response = (
+            await self.test_kit.device_monitor.create_iface(
+                phy_id=self.test_kit.phy_id,
+                role=fw_common.WlanMacRole.CLIENT,
+                sta_address=[0, 0, 0, 0, 0, 0],
+            )
+        ).unwrap()
+        assert (
+            create_iface_response.iface_id is not None
+        ), "DeviceMonitor.CreateIface() response is missing a iface_id"
+        iface_id = create_iface_response.iface_id
+
+        query_iface_response = (
+            (await self.test_kit.device_monitor.query_iface(iface_id=iface_id))
+            .unwrap()
+            .resp
+        )
+
+        asserts.assert_equal(iface_id, query_iface_response.id_)
+        asserts.assert_equal(self.test_kit.phy_id, query_iface_response.phy_id)
+        asserts.assert_equal(
+            fw_common.WlanMacRole.CLIENT, query_iface_response.role
+        )
+        asserts.assert_not_equal(
+            [0, 0, 0, 0, 0, 0], query_iface_response.sta_addr
+        )
+
+
+if __name__ == "__main__":
+    test_runner.main()
