misttech/fuchsia

commit 185311099de611011eeebe586afe1b8ef1715ac6

[zircon] Simplify logic for moving and duping handles for transfer
This centralizes the logic for moving and duplicating handles for the
purpose of transferring them to a channel. The source handle is now
looked up and removed (if applicable) from the handle table first, then
the operation is attempted.

This also updates the types to more consistently reflect the state of
handles during these operations. HandleOwner is used to represent
ownership and references are used instead of pointers after null pointer
checks.

Change-Id: Iaa5d9070959eb82b0f44aa146e070d3bcbbeaf76
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1448048
Commit-Queue: James Robinson <jamesr@google.com>
Reviewed-by: Nick Maniscalco <maniscalco@google.com>

zircon/kernel/object/user_handles.cc
@@ -11,26 +11,69 @@
 #include <ktl/enforce.h>
 
 namespace {
+
 // Basic checks for a |handle| to be able to be sent via |channel|.
-static zx_status_t handle_checks_locked(const Handle& handle, const Dispatcher* channel,
-                                        zx_handle_op_t operation, zx_rights_t desired_rights,
-                                        zx_obj_type_t type) {
+static zx_status_t common_handle_checks_locked(const Handle& handle, const Dispatcher* channel,
+                                               zx_rights_t desired_rights, zx_obj_type_t type) {
   if (!handle.HasRights(ZX_RIGHT_TRANSFER))
     return ZX_ERR_ACCESS_DENIED;
   if (handle.dispatcher().get() == channel)
     return ZX_ERR_NOT_SUPPORTED;
   if (type != ZX_OBJ_TYPE_NONE && handle.dispatcher()->get_type() != type)
     return ZX_ERR_WRONG_TYPE;
-  if (operation != ZX_HANDLE_OP_MOVE && operation != ZX_HANDLE_OP_DUPLICATE)
-    return ZX_ERR_INVALID_ARGS;
   if (desired_rights != ZX_RIGHT_SAME_RIGHTS) {
     if ((handle.rights() & desired_rights) != desired_rights) {
       return ZX_ERR_INVALID_ARGS;
     }
   }
-  if ((operation == ZX_HANDLE_OP_DUPLICATE) && !handle.HasRights(ZX_RIGHT_DUPLICATE))
-    return ZX_ERR_ACCESS_DENIED;
   return ZX_OK;
+}
+
+zx::result<HandleOwner> move_handle_for_transfer(HandleOwner handle, const Dispatcher* channel,
+                                                 zx_handle_t handle_val, zx_obj_type_t type,
+                                                 zx_rights_t desired_rights) {
+  zx_status_t check_status = common_handle_checks_locked(*handle, channel, desired_rights, type);
+  if (check_status != ZX_OK) {
+    return zx::error(check_status);
+  }
+
+  // If the caller has requested a different set of rights, we have to mint a new handle for them.
+  if (desired_rights != ZX_RIGHT_SAME_RIGHTS && desired_rights != handle->rights()) {
+    // common_handle_checks_locked verifies that the desired rights are a subset of the handle's
+    // current rights.
+    auto descoped_rights_handle = Handle::Dup(*handle, desired_rights);
+    if (!descoped_rights_handle) {
+      return zx::error(ZX_ERR_NO_MEMORY);
+    }
+    handle = ktl::move(descoped_rights_handle);
+  }
+
+  return zx::ok(ktl::move(handle));
+}
+
+zx::result<HandleOwner> duplicate_handle_for_transfer(const Handle& source,
+                                                      const Dispatcher* channel,
+                                                      zx_handle_t handle_val, zx_obj_type_t type,
+                                                      zx_rights_t desired_rights) {
+  zx_status_t check_status = common_handle_checks_locked(source, channel, desired_rights, type);
+  if (check_status != ZX_OK) {
+    return zx::error(check_status);
+  }
+
+  if (!source.HasRights(ZX_RIGHT_DUPLICATE))
+    return zx::error(ZX_ERR_ACCESS_DENIED);
+
+  // common_handle_checks_locked verifies that the desired rights are a subset of the handle's
+  // current rights.
+  const auto dest_rights =
+      (desired_rights == ZX_RIGHT_SAME_RIGHTS) ? source.rights() : desired_rights;
+
+  auto duped_handle = Handle::Dup(source, dest_rights);
+  if (!duped_handle) {
+    return zx::error(ZX_ERR_NO_MEMORY);
+  }
+
+  return zx::ok(ktl::move(duped_handle));
 }
 
 }  // namespace
@@ -63,84 +106,61 @@ zx_status_t get_user_handles_to_consume(user_inout_ptr<zx_handle_disposition_t> 
 }
 
 // This overload is used by zx_channel_write.
-zx::result<Handle*> get_handle_for_message_locked(ProcessDispatcher* process,
-                                                  const Dispatcher* channel,
-                                                  const zx_handle_t* handle_val) {
-  Handle* source = process->handle_table().GetHandleLocked(*process, *handle_val);
-
+zx::result<HandleOwner> get_handle_for_message_locked(ProcessDispatcher* process,
+                                                      const Dispatcher* channel,
+                                                      zx_handle_t handle_val) {
+  HandleOwner source = process->handle_table().RemoveHandleLocked(*process, handle_val);
   if (!source) {
     return zx::error(ZX_ERR_BAD_HANDLE);
   }
-
-  // At this point we're committed to removing the handle from our handle table
-  // even if the operation cannot be completed.
-  auto handle = process->handle_table().RemoveHandleLocked(source);
-
-  auto status = handle_checks_locked(*handle, channel, ZX_HANDLE_OP_MOVE, ZX_RIGHT_SAME_RIGHTS,
-                                     ZX_OBJ_TYPE_NONE);
-  if (status != ZX_OK)
-    return zx::error(status);
-
-  return zx::ok(handle.release());
+  return move_handle_for_transfer(ktl::move(source), channel, handle_val, ZX_OBJ_TYPE_NONE,
+                                  ZX_RIGHT_SAME_RIGHTS);
 }
 
 // This overload is used by zx_channel_write_etc.
-zx::result<Handle*> get_handle_for_message_locked(ProcessDispatcher* process,
-                                                  const Dispatcher* channel,
-                                                  zx_handle_disposition_t* handle_disposition) {
-  const zx_handle_op_t operation = handle_disposition->operation;
-  const zx_rights_t desired_rights = handle_disposition->rights;
-  const zx_obj_type_t type = handle_disposition->type;
+zx::result<HandleOwner> get_handle_for_message_locked(ProcessDispatcher* process,
+                                                      const Dispatcher* channel,
+                                                      zx_handle_disposition_t& handle_disposition) {
+  const zx_handle_op_t operation = handle_disposition.operation;
+  const zx_rights_t desired_rights = handle_disposition.rights;
+  const zx_obj_type_t type = handle_disposition.type;
+  const zx_handle_t handle_val = handle_disposition.handle;
 
-  Handle* source = process->handle_table().GetHandleLocked(*process, handle_disposition->handle);
-
+  auto source = process->handle_table().GetHandleLocked(*process, handle_val);
   if (!source) {
-    handle_disposition->result = ZX_ERR_BAD_HANDLE;
     return zx::error(ZX_ERR_BAD_HANDLE);
   }
 
-  auto status = handle_checks_locked(*source, channel, operation, desired_rights, type);
+  zx::result<HandleOwner> operation_result;
 
-  if (status != ZX_OK) {
-    handle_disposition->result = status;
-    if (operation != ZX_HANDLE_OP_DUPLICATE) {
-      // The documentation for zx_channel_write_etc says this about the operation performed on
-      // handles:
-      /// The operation applied to *handle* is one of:
-      ///
-      /// *   `ZX_HANDLE_OP_MOVE` This is equivalent to first issuing [`zx_handle_replace()`] then
-      ///      [`zx_channel_write()`]. The source handle is always closed.
-      ///
-      /// *   `ZX_HANDLE_OP_DUPLICATE` This is equivalent to first issuing [`zx_handle_duplicate()`]
-      ///     then [`zx_channel_write()`]. The source handle always remains open and accessible to
-      ///     the caller.
-      // So if we were asked to perform an operation other than ZX_HANDLE_OP_DUPLICATE and we failed
-      // a handle check, we still need to close the source handle.
-      process->handle_table().RemoveHandleLocked(source);
+  // The documentation for zx_channel_write_etc says this about the operation performed on
+  // handles:
+  /// The operation applied to *handle* is one of:
+  ///
+  /// *   `ZX_HANDLE_OP_MOVE` This is equivalent to first issuing [`zx_handle_replace()`] then
+  ///      [`zx_channel_write()`]. The source handle is always closed.
+  ///
+  /// *   `ZX_HANDLE_OP_DUPLICATE` This is equivalent to first issuing [`zx_handle_duplicate()`]
+  ///     then [`zx_channel_write()`]. The source handle always remains open and accessible to
+  ///     the caller.
+  // So when duplicating a handle, we leave the source handle in the handle table. For all other
+  // operations (including invalid operations) we immediately remove the source handle from the
+  // handle table and then attempt to move it.
+
+  if (operation == ZX_HANDLE_OP_DUPLICATE) {
+    operation_result =
+        duplicate_handle_for_transfer(*source, channel, handle_val, type, desired_rights);
+  } else {
+    HandleOwner source_owner = process->handle_table().RemoveHandleLocked(source);
+    if (operation == ZX_HANDLE_OP_MOVE) {
+      operation_result = move_handle_for_transfer(ktl::move(source_owner), channel, handle_val,
+                                                  type, desired_rights);
+    } else {
+      operation_result = zx::error(ZX_ERR_INVALID_ARGS);
     }
-    return zx::error(status);
   }
-  // This if() block is purely an optimization and can be removed without
-  // the rest of the function having to change.
-  if ((operation == ZX_HANDLE_OP_MOVE) && (desired_rights == ZX_RIGHT_SAME_RIGHTS)) {
-    return zx::ok(process->handle_table().RemoveHandleLocked(source).release());
+  if (operation_result.is_error()) {
+    handle_disposition.result = operation_result.error_value();
   }
-  // For the non-optimized case, we always need to create a new handle because
-  // the rights are a const member of Handle.
-  const auto dest_rights =
-      (desired_rights == ZX_RIGHT_SAME_RIGHTS) ? source->rights() : desired_rights;
-
-  auto raw_handle = Handle::Dup(source, dest_rights).release();
-  if (!raw_handle) {
-    // It's possible for the dup operation to fail if we run out of handles exactly
-    // at this point.
-    return zx::error(ZX_ERR_NO_MEMORY);
-  }
-
-  // Use !ZX_HANDLE_OP_DUPLICATE so that we handle the case where operation
-  // is an invalid value.
-  if (operation != ZX_HANDLE_OP_DUPLICATE) {
-    process->handle_table().RemoveHandleLocked(source);
-  }
-  return zx::ok(raw_handle);
+  return operation_result;
 }

zircon/kernel/lib/syscalls/channel.cc
@@ -326,13 +326,13 @@ static __WARN_UNUSED_RESULT zx_status_t msg_put_handles(ProcessDispatcher* up, M
     Guard<BrwLockPi, BrwLockPi::Writer> guard{up->handle_table().get_lock()};
 
     for (size_t ix = 0; ix != num_handles; ++ix) {
-      zx::result<Handle*> inner_status = get_handle_for_message_locked(up, channel, &handles[ix]);
-      if (!inner_status.is_ok() && (status == ZX_OK)) {
+      zx::result handle = get_handle_for_message_locked(up, channel, handles[ix]);
+      if (!handle.is_ok() && (status == ZX_OK)) {
         // Latch the first error encountered. It will be what the function returns.
-        status = inner_status.error_value();
+        status = handle.error_value();
       }
 
-      msg->mutable_handles()[ix] = inner_status.is_ok() ? inner_status.value() : nullptr;
+      msg->mutable_handles()[ix] = handle.is_ok() ? (*handle).release() : nullptr;
     }
   }
 

zircon/kernel/lib/syscalls/handle_ops.cc
@@ -64,7 +64,7 @@ static zx_status_t handle_dup_replace(bool is_replace, zx_handle_t handle_value,
     return ZX_ERR_INVALID_ARGS;
   }
 
-  HandleOwner handle = Handle::Dup(source, rights);
+  HandleOwner handle = Handle::Dup(*source, rights);
   if (!handle) {
     return ZX_ERR_NO_MEMORY;
   }

zircon/kernel/object/handle.cc
@@ -151,9 +151,9 @@ Handle::Handle(fbl::RefPtr<Dispatcher> dispatcher, zx_rights_t rights, uint32_t 
       rights_(rights),
       base_value_(base_value) {}
 
-HandleOwner Handle::Dup(Handle* source, zx_rights_t rights) {
+HandleOwner Handle::Dup(const Handle& source, zx_rights_t rights) {
   uint32_t base_value;
-  void* addr = gHandleTableArena.Alloc(source->dispatcher(), "duplicate", &base_value);
+  void* addr = gHandleTableArena.Alloc(source.dispatcher(), "duplicate", &base_value);
   if (unlikely(!addr))
     return nullptr;
   kcounter_add(handle_count_duped, 1);
@@ -162,12 +162,12 @@ HandleOwner Handle::Dup(Handle* source, zx_rights_t rights) {
 }
 
 // Called only by Dup.
-Handle::Handle(Handle* rhs, zx_rights_t rights, uint32_t base_value)
-    // Although this handle is intended to become owned by rhs->handle_table_id_ at the point of
+Handle::Handle(const Handle& rhs, zx_rights_t rights, uint32_t base_value)
+    // Although this handle is intended to become owned by rhs.handle_table_id_ at the point of
     // creation it is stack owned and may be destroyed without actually being assigned to the
     // handle table. If this happens the assert in TearDown would get triggered.
     : handle_table_id_(ZX_KOID_INVALID),
-      dispatcher_(rhs->dispatcher_),
+      dispatcher_(rhs.dispatcher_),
       rights_(rights),
       base_value_(base_value) {}
 

zircon/kernel/object/include/object/executor.h
@@ -34,7 +34,7 @@ class Executor {
 
   const fbl::RefPtr<JobDispatcher>& GetRootJobDispatcher() { return root_job_; }
 
-  Handle* GetRootJobHandle() { return root_job_handle_.get(); }
+  const Handle& GetRootJobHandle() { return *root_job_handle_; }
 
   fbl::RefPtr<EventDispatcher> GetMemPressureEvent(uint32_t kind) {
     return memory_watchdog_.GetMemPressureEvent(kind);

zircon/kernel/object/include/object/handle.h
@@ -86,7 +86,7 @@ class Handle final {
   // Handle should never be created by anything other than Make or Dup.
   static HandleOwner Make(fbl::RefPtr<Dispatcher> dispatcher, zx_rights_t rights);
   static HandleOwner Make(KernelHandle<Dispatcher> kernel_handle, zx_rights_t rights);
-  static HandleOwner Dup(Handle* source, zx_rights_t rights);
+  static HandleOwner Dup(const Handle& source, zx_rights_t rights);
 
   // Use a manually declared linked list node state instead of inheriting so that the early
   // memory of the class can be used by the members we want to preserve, and our NodeState can
@@ -102,7 +102,7 @@ class Handle final {
   // Called only by Make.
   Handle(fbl::RefPtr<Dispatcher> dispatcher, zx_rights_t rights, uint32_t base_value);
   // Called only by Dup.
-  Handle(Handle* rhs, zx_rights_t rights, uint32_t base_value);
+  Handle(const Handle& rhs, zx_rights_t rights, uint32_t base_value);
 
   // Handle should never be destroyed by anything other than HandleTableArena::Delete.
   ~Handle() = default;

zircon/kernel/object/include/object/job_dispatcher.h
@@ -304,7 +304,7 @@ class JobDispatcher final
 
 // Returns the job that is the ancestor of all other tasks.
 const fbl::RefPtr<JobDispatcher>& GetRootJobDispatcher();
-Handle* GetRootJobHandle();
+const Handle& GetRootJobHandle();
 
 // Start the RootJobObserver. Must be called after the root job has at
 // least one child process or child job.

zircon/kernel/object/include/object/user_handles.h
@@ -47,16 +47,16 @@ zx_status_t RemoveUserHandles(T user_handles, size_t num_handles, ProcessDispatc
   return status;
 }
 
-// Returns a |raw_handle| that should be sent over |channel|. In case
+// Returns a handle that should be sent over |channel|. In case
 // of error, the return value should be reflected back to the user.
-zx::result<Handle*> get_handle_for_message_locked(ProcessDispatcher* process,
-                                                  const Dispatcher* channel,
-                                                  const zx_handle_t* handle_val)
+zx::result<HandleOwner> get_handle_for_message_locked(ProcessDispatcher* process,
+                                                      const Dispatcher* channel,
+                                                      zx_handle_t handle_val)
     TA_REQ(process->handle_table().get_lock());
 
-zx::result<Handle*> get_handle_for_message_locked(ProcessDispatcher* process,
-                                                  const Dispatcher* channel,
-                                                  zx_handle_disposition_t* handle_disposition)
+zx::result<HandleOwner> get_handle_for_message_locked(ProcessDispatcher* process,
+                                                      const Dispatcher* channel,
+                                                      zx_handle_disposition_t& handle_disposition)
     TA_REQ(process->handle_table().get_lock());
 
 #endif  // ZIRCON_KERNEL_OBJECT_INCLUDE_OBJECT_USER_HANDLES_H_

commit d639aaae79a0e990f0ed32ed9500e280d9199dcd

[zircon] Set result in handle disposition for zx_channel_write_etc
..in the case where one of the source handles cannot be found. The
disposition's result field for the corresponding entry should be set to
ZX_ERR_BAD_HANDLE in addition to the overall system call return status.

This bug was introduced by commit
185311099de611011eeebe586afe1b8ef1715ac6 and the regression was caught
by an internal test case. The relevant portion of the test has been
added to the core test suite.

Fixed: 473830545
Change-Id: I9a69e9c9bda54171f0c7d76d5c664c7d1d1ea47a
Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1468272
Reviewed-by: Nick Maniscalco <maniscalco@google.com>
Fuchsia-Auto-Submit: James Robinson <jamesr@google.com>
Commit-Queue: James Robinson <jamesr@google.com>

zircon/kernel/object/user_handles.cc
@@ -126,39 +126,39 @@ zx::result<HandleOwner> get_handle_for_message_locked(ProcessDispatcher* process
   const zx_obj_type_t type = handle_disposition.type;
   const zx_handle_t handle_val = handle_disposition.handle;
 
-  auto source = process->handle_table().GetHandleLocked(*process, handle_val);
-  if (!source) {
-    return zx::error(ZX_ERR_BAD_HANDLE);
-  }
+  zx::result<HandleOwner> operation_result =
+      [&]() TA_REQ(process->handle_table().get_lock()) -> zx::result<HandleOwner> {
+    auto source = process->handle_table().GetHandleLocked(*process, handle_val);
+    if (!source) {
+      return zx::error(ZX_ERR_BAD_HANDLE);
+    }
 
-  zx::result<HandleOwner> operation_result;
+    // The documentation for zx_channel_write_etc says this about the operation performed on
+    // handles:
+    /// The operation applied to *handle* is one of:
+    ///
+    /// *   `ZX_HANDLE_OP_MOVE` This is equivalent to first issuing [`zx_handle_replace()`] then
+    ///      [`zx_channel_write()`]. The source handle is always closed.
+    ///
+    /// *   `ZX_HANDLE_OP_DUPLICATE` This is equivalent to first issuing [`zx_handle_duplicate()`]
+    ///     then [`zx_channel_write()`]. The source handle always remains open and accessible to
+    ///     the caller.
+    // So when duplicating a handle, we leave the source handle in the handle table. For all other
+    // operations (including invalid operations) we immediately remove the source handle from the
+    // handle table and then attempt to move it.
 
-  // The documentation for zx_channel_write_etc says this about the operation performed on
-  // handles:
-  /// The operation applied to *handle* is one of:
-  ///
-  /// *   `ZX_HANDLE_OP_MOVE` This is equivalent to first issuing [`zx_handle_replace()`] then
-  ///      [`zx_channel_write()`]. The source handle is always closed.
-  ///
-  /// *   `ZX_HANDLE_OP_DUPLICATE` This is equivalent to first issuing [`zx_handle_duplicate()`]
-  ///     then [`zx_channel_write()`]. The source handle always remains open and accessible to
-  ///     the caller.
-  // So when duplicating a handle, we leave the source handle in the handle table. For all other
-  // operations (including invalid operations) we immediately remove the source handle from the
-  // handle table and then attempt to move it.
+    if (operation == ZX_HANDLE_OP_DUPLICATE) {
+      return duplicate_handle_for_transfer(*source, channel, handle_val, type, desired_rights);
+    }
 
-  if (operation == ZX_HANDLE_OP_DUPLICATE) {
-    operation_result =
-        duplicate_handle_for_transfer(*source, channel, handle_val, type, desired_rights);
-  } else {
     HandleOwner source_owner = process->handle_table().RemoveHandleLocked(source);
     if (operation == ZX_HANDLE_OP_MOVE) {
-      operation_result = move_handle_for_transfer(ktl::move(source_owner), channel, handle_val,
-                                                  type, desired_rights);
-    } else {
-      operation_result = zx::error(ZX_ERR_INVALID_ARGS);
+      return move_handle_for_transfer(ktl::move(source_owner), channel, handle_val, type,
+                                      desired_rights);
     }
-  }
+    return zx::error(ZX_ERR_INVALID_ARGS);
+  }();
+
   if (operation_result.is_error()) {
     handle_disposition.result = operation_result.error_value();
   }

zircon/system/utest/core/channel-write-etc/write_etc.cc
@@ -110,6 +110,23 @@ TEST(ChannelWriteEtcTest, MoveHandlesWithRightsCheckFailure) {
   EXPECT_EQ(ZX_OK, send_handle_list[1].result);
 }
 
+TEST(ChannelWriteEtcTest, InvalidHandleResult) {
+  zx::channel channel_local, channel_remote;
+
+  ASSERT_OK(zx::channel::create(0, &channel_local, &channel_remote));
+
+  constexpr zx_handle_t kInvalidHandleValue = 0b100;
+
+  zx_handle_disposition_t send_handle_list[] = {
+      {ZX_HANDLE_OP_MOVE, kInvalidHandleValue, ZX_OBJ_TYPE_CHANNEL, ZX_RIGHT_SAME_RIGHTS, ZX_OK}};
+
+  EXPECT_EQ(ZX_ERR_BAD_HANDLE,
+            channel_local.write_etc(0, &kChannelData, sizeof(kChannelData), send_handle_list,
+                                    std::size(send_handle_list)));
+
+  EXPECT_EQ(ZX_ERR_BAD_HANDLE, send_handle_list[0].result);
+}
+
 TEST(ChannelWriteEtcTest, ImproperlyInitalizedResultsArgReportedBackAsOriginallyInitalized) {
   zx::channel channel_local, channel_remote;
   zx::event event;