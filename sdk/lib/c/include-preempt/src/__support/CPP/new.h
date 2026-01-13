e3f151802668e43e1d53246a39ffcb5be71079d4
Roland McGrath <mcgrathr@google.com>
2026-01-13 10:40:51 -0800

    [libc] Roll llvm-project/libc 2894029a3..00d9bdefa
    Manual roll with updates for new.h / alloc-checker.h split.
    
    //third_party/llvm-libc/src https://llvm.googlesource.com/llvm-project/libc/+log/2894029a3..00d9bdefa
    00d9bdefa [libc] add missing header to alloc-checker (#173400)
    ee094f27e [libc] Split out src/__support/alloc-checker.h (#173104)
    051d65ea9 [libc][math] Add LIBC_CONF_MATH_USE_SYSTEM_FENV / LIBC_MATH_USE_SYSTEM_FENV (#172902)
    fec853677 [libc][docs] Update website to reflect new strategy (#168637)
    50d29f24b [libc][math] Refactor expm1f implementation to header-only in src/__support/math folder. (#162131)
    25deb47e2 [libc] Add `IN6_IS_ADDR_V4COMPAT` (#172646)
    dc504ccfe [libc] Add `IN6_IS_ADDR_V4MAPPED` (#172645)
    0669dd08a [libc] Add `IN6_IS_ADDR_MC*` (#172643)
    77f4c5bd1 [libc] Add `IN6_IS_ADDR_MULTICAST` (#172498)
    6ba388c46 [libc] Add `IN6_IS_ADDR_LOOPBACK` (#172312)
    1a7f0b9d9 [libc] Add `IN6_IS_ADDR_UNSPECIFIED` (#172311)
    346455156 [libc] Cast the error value to int (#172724)
    c8a44f870 [libc][fenv] Fix performance regression for x86 mxcsr utilities. (#172717)
    262bddf6d [libc] Support opaque FILE* on baremetal (#168931)
    e4a8e6d1b libc: Prefix Rounding Control Error enum to avoid conflict in windows build (#172562)
    4277b1600 [libc][CPP] make the string trap on OOM (#172260)
    1a9f6da54 [libc][fuzzing] Improve printf long double fuzzing (#172113)
    1064e9224 [libc] Add Darwin mutex support via os_sync primitives (#167722)
    34b48e989 [libc] Fix ifdef in compiler.h. (#172324)
    e5640ca00 Revert "[APFloat] Add exp function for APFloat::IEEESsingle using expf implementation from LLVM libc. (#143959)" (#172325)
    febf11cfd [APFloat] Add exp function for APFloat::IEEESsingle using expf implementation from LLVM libc. (#143959)
    68b66a78f [libc] Add `IN6_IS_ADDR_{LINK, SITE}LOCAL` (#168207)
    4b5034a5d [libc] Properly fix printf long double subnormals (#172103)
    8c5cb7b04 [libc][wctype][codegen] Add generation script for conversion data (#170868)
    7ff7d61cc [libc] Refactor static polymorphism in WriteBuffer (NFC). (#169089)
    bdd2786e3 [libc] Add support for getpagesize. (#171713)
    62f6653ca [libc][darwin] avoid importing macros conflicting with function names in overlay mode (#171691)
    9655006b2 [libc][docs] Add a sunset policy to porting docs (#168936)
    60f86bbe8 [libc] move abs_timesout and monotonicity out of linux dir (#167719)
    ff817675d Include inline_strlen.h on aarch64 only if the target has vector instrucions (#170892)
    806bae78c [libc][fenv] Refactor x86 fenv implementations to make it work for various fenv_t. (#165015)
    551b523bb [libc] Remove extra commas in linux/config.json. (#170801)
    b1ebd7ee4 Add commas for riscv and arm configs. (#170776)
    fe8e3feb2 Reland Refactor WIDE_READ to allow finer control over high-performance function selection (#165613) (#170738)
    1546c0b8d Revert "Refactor WIDE_READ to allow finer control over high-performance function selection" (#170717)
    8d496343a Refactor WIDE_READ to allow finer control over high-performance function selection (#165613)
    
    Cq-Include-Trybots: luci.turquoise.global.try:run-postsubmit-tryjobs
    Change-Id: I5454208cc5372addebc5b9a06a05ba0272f25888
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1473620
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Paul Kirth <paulkirth@google.com>

--- sdk/lib/c/include-preempt/src/__support/CPP/new.h
+++ sdk/lib/c/include-preempt/src/__support/CPP/new.h
@@ -9,60 +9,13 @@
 // that header uses <tuple>, it will also reach <new>.  But the llvm-libc
 // "src/__support/CPP/new.h" conflicts with that.  Fortunately, as with tuple.h
 // it's all just meant to be a namespace-renamed polyfill for what libc++
-// already provides, _mostly_.  However, new.h also provides AllocChecker new!
+// already provides.
 
 #include <stdlib.h>
 
-#include <new>
+#include <new>  // IWYU pragma: export
 
 #include "src/__support/common.h"
 #include "src/__support/macros/config.h"
-
-namespace LIBC_NAMESPACE_DECL {
-
-// TODO(mcgrathr): This matches llvm-libc's AllocChecker, which is not as
-// useful as fbl::AllocChecker and should be cleaned up.  But that's orthogonal
-// to why it's here.  It shouldn't be here because "src/__support/CPP/new.h"
-// should be a pure (namespace-renamed) polyfill for <new> and the nonstandard
-// AllocChecker APIs should be in a different header.  Until that is done
-// upstream, it's just reimplemented here.
-class AllocChecker {
- public:
-  explicit operator bool() const { return success_; }
-
-  static void* alloc(size_t s, AllocChecker& ac) { return ac.Checked(::malloc(s)); }
-
-  static void* aligned_alloc(size_t s, std::align_val_t align, AllocChecker& ac) {
-    return ac.Checked(::aligned_alloc(static_cast<size_t>(align), s));
-  }
-
- private:
-  void* Checked(void* ptr) {
-    success_ = ptr != nullptr;
-    return ptr;
-  }
-
-  bool success_ = false;
-};
-
-}  // namespace LIBC_NAMESPACE_DECL
-
-inline void* operator new(size_t size, LIBC_NAMESPACE::AllocChecker& ac) noexcept {
-  return LIBC_NAMESPACE::AllocChecker::alloc(size, ac);
-}
-
-inline void* operator new(size_t size, std::align_val_t align,
-                          LIBC_NAMESPACE::AllocChecker& ac) noexcept {
-  return LIBC_NAMESPACE::AllocChecker::aligned_alloc(size, align, ac);
-}
-
-inline void* operator new[](size_t size, LIBC_NAMESPACE::AllocChecker& ac) noexcept {
-  return LIBC_NAMESPACE::AllocChecker::alloc(size, ac);
-}
-
-inline void* operator new[](size_t size, std::align_val_t align,
-                            LIBC_NAMESPACE::AllocChecker& ac) noexcept {
-  return LIBC_NAMESPACE::AllocChecker::aligned_alloc(size, align, ac);
-}
 
 #endif  // PREEMPT_SRC___SUPPORT_CPP_NEW_H_
