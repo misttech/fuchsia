1a2fecf04e1f65bfaff1ab171d80f3ea5b566286
Adrian Danis <adanis@google.com>
2026-01-21 14:32:58 -0800

    [kernel] Define a NullLockPolicy
    The DefaultLockPolicy is not suitable for the kernel, as it does not
    define an appropriate ValidationGuard, so this change defines an
    appropriate policy that uses the kernels LockValidationGuard.
    
    Bug: 477283657
    Change-Id: I14dfecff40a585d7cdbf3b1ccd38c8aa01d939d3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1480616
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Adrian Danis <adanis@google.com>

--- zircon/kernel/include/kernel/null_lock.h
+++ zircon/kernel/include/kernel/null_lock.h
@@ -7,12 +7,39 @@
 #ifndef ZIRCON_KERNEL_INCLUDE_KERNEL_NULL_LOCK_H_
 #define ZIRCON_KERNEL_INCLUDE_KERNEL_NULL_LOCK_H_
 
+#include <lib/zircon-internal/thread_annotations.h>
+
 #include <fbl/null_lock.h>
+#include <kernel/lock_validation_guard.h>
 #include <kernel/lockdep.h>
 
 using NullLock = fbl::NullLock;
 
+struct NullLockPolicy {
+  struct State {};
+
+  // Protects the thread local lock list and validation.
+  using ValidationGuard = LockValidationGuard;
+
+  template <typename LockType>
+  static void PreValidate(LockType*, State*) {}
+
+  template <typename LockType>
+  static bool Acquire(LockType* lock, State* state) TA_ACQ(lock) {
+    lock->Acquire();
+    return true;
+  }
+
+  template <typename LockType>
+  static void Release(LockType* lock, State* state) TA_REL(lock) {
+    lock->Release();
+  }
+
+  template <typename LockType>
+  static void AssertHeld(const LockType& lock) TA_ASSERT(lock) {}
+};
+
 LOCK_DEP_TRAITS(NullLock, lockdep::LockFlagsNone);
-LOCK_DEP_POLICY(NullLock, lockdep::DefaultLockPolicy);
+LOCK_DEP_POLICY(NullLock, NullLockPolicy);
 
 #endif  // ZIRCON_KERNEL_INCLUDE_KERNEL_NULL_LOCK_H_
