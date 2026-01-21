657403f1121a2e175a76be826c7cb0382785c3ab
Nick Eaton <nickeaton@google.com>
2026-01-21 09:54:10 -0800

    [haptics] Add ability to play composite vibrations
    Update the fuchsia.hardware.haptics.Device FIDL protocol to support
    playing composite waveforms.
    
    Bug: 457385782
    Change-Id: I78080a0abdf25f957025b40d64e69d7f4bd5f6b5
    Cq-Include-Trybots: luci.turquoise.global.try:bringup.sorrel-debug,minimal.sorrel-debug
    Depends-on: turquoise-internal:I6163675d84dc9d8ec1954f5f306113fc3d822580
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1409855
    Dependencies-Satisfied: CL Deps Checker <cl-deps-checker@fuchsia-internal-service-accts.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Nick Eaton <nickeaton@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Commit-Queue: Nick Eaton <nickeaton@google.com>
    API-Review: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/haptics/testing/fake_haptics_service/src/main.rs
+++ src/devices/haptics/testing/fake_haptics_service/src/main.rs
@@ -5,15 +5,22 @@
 //! The fake haptics service's goal is to provide a component that offers a fake
 //! fuchsia.hardware.haptics.Service` FIDL service instance.
 
+use std::time::Duration;
+
 use anyhow::Context;
 use fidl_fuchsia_hardware_haptics::{
-    DeviceRequest, DeviceRequestStream, Effect, ServiceRequest, SupportedEffect,
+    CompositeEffectWaveform, DeviceRequest, DeviceRequestStream, Effect, ServiceRequest,
+    SupportedCompositeEffectWaveform, SupportedEffect,
 };
+use fuchsia_async::Timer;
 use fuchsia_component::server::ServiceFs;
 use futures::{StreamExt, TryFutureExt, TryStreamExt};
 use log::error;
 
 async fn handle_device_requests(mut requests: DeviceRequestStream) -> anyhow::Result<()> {
+    // Value must be greater than 0 in order to pass tests.
+    const COMPOSITE_EFFECT_DURATION: Duration = Duration::from_millis(1);
+
     while let Some(request) = requests.try_next().await.context("Failed to get request")? {
         match request {
             DeviceRequest::StartVibration { responder } => {
@@ -23,6 +30,24 @@ async fn handle_device_requests(mut requests: DeviceRequestStream) -> anyhow::Re
                 responder.send(Ok(())).context("Failed to send response")?;
             }
             DeviceRequest::PlayEffect { effect: _, strength: _, responder } => {
+                responder.send(Ok(())).context("Failed to send response")?;
+            }
+            DeviceRequest::PlayCompositeWaveform { composite_waveform, responder } => {
+                let duration = composite_waveform
+                    .iter()
+                    .map(|efffect| {
+                        let delay = Duration::from_nanos(
+                            efffect.delay.try_into().context("Failed to convert to u64")?,
+                        );
+                        let duration = match efffect.waveform {
+                            CompositeEffectWaveform::Noop => Duration::default(),
+                            _ => COMPOSITE_EFFECT_DURATION,
+                        };
+                        Ok(delay + duration)
+                    })
+                    .sum::<anyhow::Result<Duration>>()
+                    .context("Failed to get composite waveform duration")?;
+                Timer::new(duration).await;
                 responder.send(Ok(())).context("Failed to send response")?;
             }
             DeviceRequest::StopVibration { responder } => {
@@ -70,11 +95,70 @@ async fn handle_device_requests(mut requests: DeviceRequestStream) -> anyhow::Re
                     SupportedEffect { effect: Effect::TextureTick, duration: 0 },
                 ];
 
+                let composite_effect_duration_ns: i64 = COMPOSITE_EFFECT_DURATION
+                    .as_nanos()
+                    .try_into()
+                    .context("Failed to conver to i64")?;
+
+                // The specified items are required in order to pass tests.
+                let supported_composite_effect_waveforms: [SupportedCompositeEffectWaveform; 9] = [
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::Noop,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::Click,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::Thud,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::Spin,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::QuickRise,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::SlowRise,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::QuickFall,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::LightTick,
+                        duration: composite_effect_duration_ns,
+                    },
+                    SupportedCompositeEffectWaveform {
+                        waveform: CompositeEffectWaveform::LowTick,
+                        duration: composite_effect_duration_ns,
+                    },
+                ];
+
+                // Value must be greater than 100ms in order to execute all related tests.
+                const MAX_COMPOSITE_EFFECT_DELAY: Duration = Duration::from_millis(100);
+
+                // Value must be greater than 0 in order to pass tests.
+                // Value must be greater than 5 in order to execute all related tests.
+                const MAX_COMPOSITE_EFFECT_COUNT: u64 = 5;
+
+                let max_composite_effect_delay = MAX_COMPOSITE_EFFECT_DELAY
+                    .as_nanos()
+                    .try_into()
+                    .context("Failed to conver to i64")?;
                 responder
                     .send(Ok((
                         FUNDAMENTAL_RESONANT_FREQUENCY_HZ,
                         QUALITY_FACTOR,
                         &SUPPORTED_EFFECTS,
+                        &supported_composite_effect_waveforms,
+                        MAX_COMPOSITE_EFFECT_COUNT,
+                        max_composite_effect_delay,
                     )))
                     .context("Failed to send response")?;
             }
