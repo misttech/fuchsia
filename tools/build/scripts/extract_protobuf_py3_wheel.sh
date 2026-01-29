bbface404f21e3bbc24164fab78c6988a5209585
David 'Digit' Turner <digit@google.com>
2026-01-29 11:19:48 -0800

    [protobuf-py3] Add CIPD package + extraction hook
    Make the protobuf-py3 Python module usable for pre-build
    Python scripts. While the wheel file is available as a CIPD
    package, it needs to be unpacked, which is achieved here with
    a new Jiri hook
    
    - prebuilts: Add <package> definition for the wheel package.
    - platform: Add a <hook> element to run the extraction script.
    - jiri.lock: Related updates, after running update-lockfiles.sh
    - tools/build/scripts/extract_protobuf_py3_wheel.sh: The
      script invoked by the hook to perform the extraction.
    
    - Remove the asserts/readmes/.../README.fuchsia file, the script
      will write a README.fuchsia file that contains the correct
      version and Fuchsia-specific update instructions.
    
    - Add a licensing exception in _config.json for protobuf-py3-wheel
    
    Bug: 399960746
    Change-Id: I9fbabf20c4f30904e26d6c737fbf990cd0df4a49
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1487619
    Reviewed-by: Adam Barth <abarth@google.com>
    Reviewed-by: David Fang <fangism@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>
    Commit-Queue: David Turner <digit@google.com>

---
+++ tools/build/scripts/extract_protobuf_py3_wheel.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# A Jiri hook used to extract the protobuf-py3 wheel file to
+# a hard-coded destination directory to make it directly usable
+# before the build.
+set -e
+
+# Where to find the source wheel file. Exact name is versioned and unknown here.
+SOURCE_SUBDIR="prebuilt/third_party/protobuf-py3-wheel"
+
+# Where to extract the wheel file.
+DEST_SUBDIR="prebuilt/third_party/protobuf-py3"
+
+function die {
+  echo >&2 "ERROR: $*"
+  exit 1
+}
+
+# Find FUCHSIA_DIR if not defined in the environment.
+if [[ -z "${FUCHSIA_DIR}" ]]; then
+  # Assume this script is under //tools/build/scripts/
+  _SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+  FUCHSIA_DIR=$(cd "${_SCRIPT_DIR}/../../.." && pwd)
+fi
+[[ -f "${FUCHSIA_DIR}/.jiri_manifest" ]] ||
+  die "Invalid FUCHSIA_DIR directory: ${FUCHSIA_DIR}"
+
+# The source and destination directories are hard-coded here.
+SOURCE_DIR="$FUCHSIA_DIR/$SOURCE_SUBDIR"
+echo "Using source directory: $SOURCE_DIR"
+
+# Locate the wheel file from the source directory.
+SRC_FILE=$(ls "$SOURCE_DIR"/*.whl 2>/dev/null)
+[[ -n "$SRC_FILE" ]] || die "No .whl found in source directory: ${SOURCE_DIR}"
+
+echo "Found wheel file: $SRC_FILE"
+
+# Extract version number from the wheel file name, as this
+# will be required for the README.fuchsia file.
+SRC_FILENAME=$(basename "${SRC_FILE}")
+SRC_VERSION=$(echo "${SRC_FILENAME}" | sed -E -e 's|protobuf-([0-9.]+)-py3-.*|\1|')
+[[ -n "${SRC_VERSION}" ]] || die "Could not find version in filename: ${SRC_FILENAME}"
+echo "Found version: [$SRC_VERSION]"
+
+# A file used to verify that the extracted content is correct.
+CHECK_FILE=google/protobuf/descriptor_pb2.py
+
+DEST_DIR="${FUCHSIA_DIR}/${DEST_SUBDIR}"
+echo "Using destination directory: ${DEST_DIR}"
+mkdir -p "${DEST_DIR}"
+rm -rf "${DEST_DIR:?}"/*
+
+echo "Extracting archive"
+unzip -o -q "${SRC_FILE}" -d "${DEST_DIR}" || die "Could not unzip ${SRC_FILE}"
+[[ -f "${DEST_DIR}/${CHECK_FILE}" ]] ||
+    die "Missing file from extracted archive: ${DEST_DIR}/${CHECK_FILE}"
+
+echo "Writing README.fuchsia file"
+cat > "${DEST_DIR}/README.fuchsia" <<EOF
+Name: protobuf-py3
+
+URL: https://chrome-infra-packages.appspot.com/p/infra/python/wheels/protobuf-py3
+
+License File: protobuf-${SRC_VERSION}.dist-info/LICENSE
+ -> License File Format: Single License
+ -> License Classifications: BSD-3
+
+Description:
+These are protoc-generated source files produced by following
+these instructions: https://github.com/protocolbuffers/protobuf/tree/main/python#building-packages-from-this-repo
+
+As a special case, these files are extracted from the wheel file coming
+from the following package: infra/python/wheel/protobuf-py3.
+
+The infra code for building this package is in:
+https://chromium.googlesource.com/infra/infra/+/main/infra/tools/dockerbuild/
+https://chromium.googlesource.com/infra/infra/+/main/infra/tools/dockerbuild/wheels.py#2528
+
+Builders are https://ci.chromium.org/ui/p/infra-internal/g/wheel_builders/builders
+
+The extraction script is in \$FUCHSIA_DIR/tools/build/scripts, and invoked
+as a Jiri hook.
+
+To update to a new version:
+
+1) Modify the "version" attribute of the "<package>" element called
+   "protobuf-py3" in \$FUCHSIA_DIR/manifest/prebuilts
+
+2) Run: \$FUCHSIA_DIR/manifests/update-lockfiles.sh
+   This verifies that the version exists and updates critical Jiri files.
+
+3) Run: jiri update -local-manifest-project=fuchsia
+   This installs the new wheel file to \$FUCHSIA_DIR/${SOURCE_SUBDIR}
+   then runs a hook to update \$FUCHSIA_DIR/${DEST_SUBDIR}
+
+4) Verify that everything works
+
+5) Update new CL to Gerrit with your modifications.
+EOF
+
+echo "Done"
+exit 0
