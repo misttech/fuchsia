7247352f02cf19a3f4234eac79ac6836f7d2e7f0
Roland McGrath <mcgrathr@google.com>
2026-01-29 18:01:33 -0800

    [libc] Remove zxr_thread_join
    Fold this into its only caller.
    
    Cq-Include-Trybots: luci.turquoise.global.try:run-postsubmit-tryjobs
    Change-Id: Iaec76504c592e1f78449b782875389b295d443fe
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1452630
    Reviewed-by: Paul Kirth <paulkirth@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>

--- sdk/lib/c/threads/thread-join.cc
+++ sdk/lib/c/threads/thread-join.cc
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/zircon-internal/unique-backtrace.h>
 #include <lib/zx/result.h>
+#include <zircon/syscalls.h>
 
 #include <tuple>
 
@@ -12,11 +14,57 @@
 
 namespace LIBC_NAMESPACE_DECL {
 
+using ThreadState = zxr_thread_t::State;
+
 zx::result<intptr_t> ThreadJoin(Thread& thread) {
-  // This does the synchronization with the thread exit.
-  zx_status_t status = zxr_thread_join(&thread.zxr_thread);
-  if (status != ZX_OK) {
-    return zx::error{status};
+  auto wait = [&thread](ThreadState old_state) {
+    do {
+      switch (_zx_futex_wait(thread.zxr_thread.StateFutex(), static_cast<int>(old_state),
+                             ZX_HANDLE_INVALID, ZX_TIME_INFINITE)) {
+        case ZX_ERR_BAD_STATE:  // Never blocked because it had changed.
+        case ZX_OK:             // Woke up because it might have changed.
+          old_state = thread.zxr_thread.state.load(std::memory_order_acquire);
+          break;
+        default:
+          CRASH_WITH_UNIQUE_BACKTRACE();
+      }
+
+      // Wait until we reach the DONE state, even if we observe the
+      // intermediate EXITING state.
+    } while (old_state == ThreadState::JOINED || old_state == ThreadState::EXITING);
+
+    if (old_state != ThreadState::DONE)
+      CRASH_WITH_UNIQUE_BACKTRACE();
+  };
+
+  // Try to claim the join slot on this thread.
+  if (auto old_state = thread.zxr_thread.JoinOrDetachState(ThreadState::JOINED); !old_state) {
+    wait(ThreadState::JOINED);
+  } else {
+    switch (*old_state) {
+      case ThreadState::JOINED:
+      case ThreadState::DETACHED:
+        return zx::error{ZX_ERR_INVALID_ARGS};
+
+      case ThreadState::EXITING:
+        // Since it is undefined to call ThreadJoin on a thread that has
+        // already been detached or joined, we assume the state prior to
+        // EXITING was JOINABLE, and act as if we had successfully transitioned
+        // to JOINED.
+        wait(ThreadState::EXITING);
+        [[fallthrough]];
+
+      case ThreadState::DONE:
+        break;
+
+      default:
+        CRASH_WITH_UNIQUE_BACKTRACE();
+    }
+  }
+
+  // Take the handle and synchronize with readers.  Then close the handle.
+  if (zx::thread handle = thread.zxr_thread.TakeHandle(ThreadState::DONE); !handle) {
+    CRASH_WITH_UNIQUE_BACKTRACE();
   }
 
   // Now the Thread object can be removed from the list of all threads.
