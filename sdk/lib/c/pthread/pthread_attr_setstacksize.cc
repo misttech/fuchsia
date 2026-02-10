19aeee6cff75acbbc1a2bee6350a9c44d993fbed
Roland McGrath <mcgrathr@google.com>
2026-02-10 15:46:49 -0800

    [libc] Add argument checking back to pthread_attr_setstacksize
    The old code's check for an invalid size was lost in the
    refactoring.  Bring it back.
    
    Change-Id: Ia575685d8afc29ad728775dc2bfe0fefb9147f40
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1502156
    Reviewed-by: Paul Kirth <paulkirth@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- sdk/lib/c/pthread/pthread_attr_setstacksize.cc
+++ sdk/lib/c/pthread/pthread_attr_setstacksize.cc
@@ -4,6 +4,8 @@
 
 #include "src/pthread/pthread_attr_setstacksize.h"
 
+#include <cstdint>
+
 #include "attr.h"
 #include "src/__support/common.h"
 
@@ -11,6 +13,9 @@ namespace LIBC_NAMESPACE_DECL {
 
 LLVM_LIBC_FUNCTION(int, pthread_attr_setstacksize,
                    (pthread_attr_t *__restrict attr, size_t stacksize)) {
+  if (stacksize < PTHREAD_STACK_MIN ||               // Detect too-small size
+      stacksize - PTHREAD_STACK_MIN > SIZE_MAX / 4)  // ... or overflow risk.
+    return EINVAL;
   attr->_a_stacksize = stacksize;
   return 0;
 }
