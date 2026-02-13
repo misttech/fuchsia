e57133895a83ecd22f2f8f11bb5d7fcef076b3b6
Roland McGrath <mcgrathr@google.com>
2026-02-13 01:37:13 -0800

    [libc] Move pthread_getattr_np into C++
    Change-Id: Idba9259a351936fa74b6682234e8c2bd5d7b1fb4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1489137
    Reviewed-by: Paul Kirth <paulkirth@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Leonard Chan <leonardchan@google.com>

---
+++ sdk/lib/c/pthread/pthread_getattr_np.cc
@@ -0,0 +1,22 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/pthread/pthread_getattr_np.h"
+
+#include "src/__support/common.h"
+#include "threads_impl.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+LLVM_LIBC_FUNCTION(int, pthread_getattr_np, (pthread_t th, pthread_attr_t* attr)) {
+  __pthread* t = reinterpret_cast<__pthread*>(th);
+  *attr = {
+      ._a_stacksize = t->safe_stack.iov_len,
+      ._a_stackaddr = t->safe_stack.iov_base,
+      ._a_detach = zxr_thread_detached(&t->zxr_thread),
+  };
+  return 0;
+}
+
+}  // namespace LIBC_NAMESPACE_DECL
