f9e35b7f625d5e536279fd48510c75adf7d5a257
Thomas Zander <thomasalpinus@google.com>
2026-02-18 10:48:05 -0800

    [rust-3p] Update omaha-client and mock-omaha-server to 0.3.8
    Fixes: 485192957
    Change-Id: Ic1ef2d9c2f66591b83de4e84254d6ebe7c62c821
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1508736
    Fuchsia-Auto-Submit: Thomas Zander <thomasalpinus@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>
    Reviewed-by: Sen Jiang <senj@google.com>
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>

--- third_party/rust_crates/vendor/typed-builder-0.21.2/run-retrospective-crate-version-tagging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(
-    retrospective-crate-version-tagging detect \
-        --crate-name typed-builder \
-        --changelog-path CHANGELOG.md \
-        --tag-prefix v \
-) | retrospective-crate-version-tagging create-releases
