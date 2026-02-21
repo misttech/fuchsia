a6094da93a616c346e4b42e48211173db31e0fb4
Jerry Belton <jcecil@google.com>
2026-02-20 17:06:47 -0800

    [bisect] Add a bit more context to the command-line arguments.
    Make the "name" argument a required positional parameter instead of an
    optional one.
    
    Reorganize the args by importance, so the "--help" output is easier to
    grok.
    
    Change-Id: Id8138bec8b617d1e0d5bb8b28bc701cde24c7002
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1512107
    Commit-Queue: Jerry Belton <jcecil@google.com>
    Reviewed-by: Jiaming Li <lijiaming@google.com>

--- src/developer/ffx/plugins/product_bundle/bisect/src/args.rs
+++ src/developer/ffx/plugins/product_bundle/bisect/src/args.rs
@@ -29,40 +29,46 @@ impl FromArgValue for Strategy {
     }
 }
 
-/// Arguments to run bisection.
+/// Generate a list of released versions between [--from-success] and [--to-failure] for every
+/// artifact contained within the given product bundle (e.g. platform, product, board).
+///
+/// Assemble intermediate product bundles using combinations of the artifacts, and facilitate
+/// running tests on those PBs to quickly identify which artifact is the source of a bug or
+/// behavior change.
 #[ffx_command()]
 #[derive(ArgsInfo, FromArgs, Debug, PartialEq, Clone)]
 #[argh(subcommand, name = "bisect")]
 pub struct BisectCommand {
-    /// product bundle name to bisect.
-    #[argh(option)]
+    /// name of the product bundle being bisected.
+    #[argh(positional)]
     pub name: String,
 
-    /// latest known-good version of the product bundle.
+    /// known-good version of the product bundle. The bisection search window begins here.
     #[argh(option)]
     pub from_success: String,
 
-    /// earliest known-bad version of the product bundle.
+    /// known-bad version of the product bundle. The bisection search window ends here.
     #[argh(option)]
     pub to_failure: String,
 
-    /// directory to write assembled images and other artifacts.
+    /// slot to bisect over (a or r). Defaults to slot A.
+    #[argh(option, default = "Default::default()")]
+    pub slot: assembly_artifact_cache::Slot,
+
+    /// search strategy to use. Defaults to "AllDimensions", which bisects across all pb artifact
+    /// types (platform, product, board...) simultaneously.
+    #[argh(option, default = "Strategy::default()")]
+    pub strategy: Strategy,
+
+    /// directory to write assembled images and other artifacts. Defaults to ~/<plan_directory>/out
     #[argh(option)]
     pub out_dir: Option<Utf8PathBuf>,
 
-    /// directory to write intermediate files.
+    /// directory to write intermediate files. Defaults to ~/<plan_directory>/gen
     #[argh(option)]
     pub gen_dir: Option<Utf8PathBuf>,
 
     /// authentication method to use.
     #[argh(option, default = "AuthFlowChoice::Default")]
     pub auth: AuthFlowChoice,
-
-    /// search strategy to use.
-    #[argh(option, default = "Strategy::default()")]
-    pub strategy: Strategy,
-
-    /// slot to bisect over (a or r).
-    #[argh(option, default = "Default::default()")]
-    pub slot: assembly_artifact_cache::Slot,
 }
