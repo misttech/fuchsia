69fe8f240fa7e1a16c8a284755f7e9a368e5519a
David Dorwin <ddorwin@google.com>
2026-03-03 10:40:20 -0800

    [bazel][fidl] Support the _cpp_testing bindings library
    Add the testing library to the Bazel FIDL C++ natural bindings.
    
    Also make this library Fuchsia-only in the GN template.
    
    Bug: 454977301
    Test: fx bazel build --config=fuchsia --copt="-ffuchsia-api-level=4293918720" //build/bazel/bazel_idk/tests:test_executable_using_fidl_cc
    Change-Id: I040aaaae833dd76223ee90ff392c5cd3cdd2824e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1492474
    Reviewed-by: Ian McKellar <ianloic@google.com>
    Fuchsia-Auto-Submit: David Dorwin <ddorwin@google.com>
    Commit-Queue: David Dorwin <ddorwin@google.com>

--- build/bazel/bazel_idk/tests/fidl_test.cc
+++ build/bazel/bazel_idk/tests/fidl_test.cc
@@ -5,11 +5,28 @@
 #include <fidl/fuchsia.math/cpp/fidl.h>
 #include <fidl/fuchsia.sysmem2/cpp/wire_types.h>
 
+// The testing library only works on Fuchsia.
+#ifdef __Fuchsia__
+#include <fidl/fuchsia.sysmem2/cpp/test_base.h>
+
+class FakeSysmemAllocator : public fidl::testing::TestBase<fuchsia_sysmem2::Allocator> {
+  void NotImplemented_(const std::string& name, fidl::CompleterBase& completer) override {}
+  void handle_unknown_method(::fidl::UnknownMethodMetadata<fuchsia_sysmem2::Allocator> metadata,
+                             ::fidl::UnknownMethodCompleter::Sync& completer) override {}
+};
+#endif
+
 int main() {
   // Use a C++ natural bindings method that is not defined in a header file.
   fuchsia_sysmem2::wire::BufferCollectionConstraints cpp_constraints;
   [[maybe_unused]] bool is_empty_cpp = cpp_constraints.IsEmpty();
   assert(is_empty_cpp);
+
+#ifdef __Fuchsia__
+  // Use a type from the testing library. It only contains headers so this is
+  // sufficient.
+  FakeSysmemAllocator fake_allocator;
+#endif
 
   fuchsia_math::Vec vec(1, 2);
   return vec.x() + vec.y();
