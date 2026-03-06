42cb58dcbacab31079c056c114b7b888be9f8bb8
Charles Celerier <chcl@google.com>
2026-03-06 03:28:58 -0800

    [honeydew][hello_world] Port HelloWorldAffordanceTests to AsyncFuchsiaBaseTest
    This change ports the HelloWorld affordance tests to use the new
    AsyncFuchsiaBaseTest class.
    
    Bug: 488299605
    Change-Id: Ib4f1c8eaafb5003b6e99aae8e86d00926a02cfd7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1528396
    Reviewed-by: Marc Khouri <mnck@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/testing/end_to_end/honeydew/honeydew/affordances/hello_world/tests/functional_tests/test_hello_world.py
+++ src/testing/end_to_end/honeydew/honeydew/affordances/hello_world/tests/functional_tests/test_hello_world.py
@@ -5,20 +5,20 @@
 
 import logging
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from mobly import asserts, test_runner
 
 LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
-class HelloWorldAffordanceTests(fuchsia_base_test.FuchsiaBaseTest):
+class HelloWorldAffordanceTests(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """HelloWorld affordance tests."""
 
-    def setup_class(self) -> None:
-        super().setup_class()
+    async def setup_class(self) -> None:
+        await super().setup_class()
         self.dut = self.fuchsia_devices[0]
 
-    def test_hello_world_greeting(self) -> None:
+    async def test_hello_world_greeting(self) -> None:
         """Test case for HelloWorld.greeting() method"""
         asserts.assert_equal(
             self.dut.hello_world.greeting(), f"Hello, {self.dut.device_name}!"
