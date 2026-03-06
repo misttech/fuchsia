0053cc2605fb9cda1a8fefddafa423e3c2b537b4
Gwen Mittertreiner <gmtr@google.com>
2026-03-06 12:14:32 -0800

    Reland "[trace-provider] Impl ProviderV2 Protocol"
    This is a reland of commit 9ab0f8904fb0659b442de2edbce5f5d87bd40410
    
    Original change's description:
    > Reland "[trace-provider] Impl ProviderV2 Protocol"
    >
    > This is a reland of commit d9f1443edc416a4bd76d9bb78bcc35a7ad0f7b05
    >
    > This patch as been updated to improve the memory management lifetimes:
    >
    > 1) Previously TraceSession was passed as a raw pointer to trace-engine
    >    and then destroys itself as part of the Session::TraceTerminated
    >    callback.
    >    TraceProviderImpl now owns a unique pointer to a TraceSession
    >    allowing it to better track and clean up the TraceSession when it is
    >    destroyed itself.
    > 2) The callbacks which were previously stored in static memory are now
    >    owned by the TraceSession. The handler functions
    >    (Session::Trace[Started, Stopped, Terminate]) are now member
    >    functions instead of static so that they can access the TraceSession
    >    state.
    > 3) The destruction sequence has been fixed to allow for trace-engine to
    >    update its state between Stopping and Terminate requests.
    >
    > Original change's description:
    > > [trace-provider] Impl ProviderV2 Protocol
    > >
    > > When compiling at HEAD, implement and enable the new V2 protocol. We
    > > need to support the previous protocol for the time being when compiling
    > > for lower api levels as systems with older trace_managers won't support
    > > the V2 protocol.
    > >
    > > Bug: 468117244
    > > Change-Id: I7fd33d0099d25e4b7f13fd2749ac03cc7f7669f6
    > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1504760
    > > Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    > > Commit-Queue: Gwen Mittertreiner <gmtr@google.com>
    > > Fuchsia-Auto-Submit: Gwen Mittertreiner <gmtr@google.com>
    >
    > Bug: 468117244
    > Change-Id: Icf0ca736dca611d96dacb062db0ac87f99cde9d7
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1523676
    > Commit-Queue: Gwen Mittertreiner <gmtr@google.com>
    > Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    > Fuchsia-Auto-Submit: Gwen Mittertreiner <gmtr@google.com>
    
    Multiply: trace-provider, fuchsia-pkg://fuchsia.com/audio_core_api_tests#meta/audio-gain-control-test.cm
    Bug: 468117244
    Change-Id: I4aee2e394710b12e103bedb94130b3c79f8f3698
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1531075
    Fuchsia-Auto-Submit: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Gwen Mittertreiner <gmtr@google.com>
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>

--- zircon/system/ulib/trace-provider/provider_impl.h
+++ zircon/system/ulib/trace-provider/provider_impl.h
@@ -8,14 +8,43 @@
 #include <fidl/fuchsia.tracing.provider/cpp/wire.h>
 #include <lib/trace-provider/provider.h>
 
+#include "session.h"
+
 // Provide a definition for the opaque type declared in provider.h.
 struct trace_provider {};
 
 namespace trace::internal {
 
 class TraceProviderImpl final : public trace_provider_t,
-                                public fidl::WireServer<fuchsia_tracing_provider::Provider> {
+#if FUCHSIA_API_LEVEL_AT_LEAST(HEAD)
+                                public fidl::WireServer<fuchsia_tracing_provider::ProviderV2>
+#else
+                                public fidl::WireServer<fuchsia_tracing_provider::Provider>
+#endif
+{
  public:
+  ~TraceProviderImpl() override;
+
+#if FUCHSIA_API_LEVEL_AT_LEAST(HEAD)
+  TraceProviderImpl(std::string name, async_dispatcher_t* dispatcher,
+                    fidl::ServerEnd<fuchsia_tracing_provider::ProviderV2> server_end);
+
+  void Initialize(fuchsia_tracing_provider::wire::ProviderV2InitializeRequest* request,
+                  InitializeCompleter::Sync& completer) override;
+
+  void Start(fuchsia_tracing_provider::wire::ProviderV2StartRequest* request,
+             StartCompleter::Sync& completer) override;
+
+  void NotifyBufferSaved(
+      fuchsia_tracing_provider::wire::ProviderV2NotifyBufferSavedRequest* request,
+      NotifyBufferSavedCompleter::Sync& completer) override;
+
+  void Flush(FlushCompleter::Sync& completer) override;
+
+  void handle_unknown_method(
+      fidl::UnknownMethodMetadata<fuchsia_tracing_provider::ProviderV2> metadata,
+      fidl::UnknownMethodCompleter::Sync& completer) override;
+#else
   TraceProviderImpl(std::string name, async_dispatcher_t* dispatcher,
                     fidl::ServerEnd<fuchsia_tracing_provider::Provider> server_end);
 
@@ -24,6 +53,7 @@ class TraceProviderImpl final : public trace_provider_t,
 
   void Start(fuchsia_tracing_provider::wire::ProviderStartRequest* request,
              StartCompleter::Sync& completer) override;
+#endif
 
   void Stop(StopCompleter::Sync& completer) override;
 
@@ -38,11 +68,14 @@ class TraceProviderImpl final : public trace_provider_t,
   const ProviderConfig& GetProviderConfig() const;
 
  private:
-  static void OnClose();
-
   const std::string name_;
   async_dispatcher_t* const dispatcher_;
   ProviderConfig provider_config_;
+#if FUCHSIA_API_LEVEL_AT_LEAST(HEAD)
+  fidl::ServerBindingRef<fuchsia_tracing_provider::ProviderV2> binding_;
+  std::unique_ptr<Session> session_;
+#endif
+
   trace::GetKnownCategoriesCallback get_known_categories_callback_;
 
   TraceProviderImpl(const TraceProviderImpl&) = delete;
--- zircon/system/ulib/trace-provider/test/session_test.cc
+++ zircon/system/ulib/trace-provider/test/session_test.cc
@@ -4,6 +4,7 @@
 
 #include "../session.h"
 
+#include <fidl/fuchsia.tracing.provider/cpp/fidl.h>
 #include <lib/async-loop/cpp/loop.h>
 #include <lib/async-loop/default.h>
 #include <lib/trace-provider/provider.h>
@@ -15,35 +16,36 @@ namespace trace {
 namespace {
 
 static constexpr size_t kBufferSize = 65535;
-static constexpr size_t kFifoCount = 4;
 static const std::string kAlertName = "alert_name";
 static const std::string kAlertNameMin = "a";
 static const std::string kAlertNameMax = "alert_name_max";
 
-void CheckAlertNameAndZeroPadding(const std::string& alert_name, const void* p, size_t size) {
-  const char* pchar = reinterpret_cast<const char*>(p);
-  for (size_t index = 0; size > 0; --size, ++pchar, ++index) {
-    if (index >= alert_name.size()) {
-      ASSERT_EQ(0, *pchar);
-      continue;
-    }
-
-    ASSERT_EQ(alert_name[index], *pchar);
+class DummyProvider : public fidl::Server<fuchsia_tracing_provider::ProviderV2> {
+ public:
+  void Initialize(InitializeRequest& request, InitializeCompleter::Sync& completer) override {}
+  void Start(StartRequest& request, StartCompleter::Sync& completer) override {}
+  void Stop(StopCompleter::Sync& completer) override {}
+  void GetKnownCategories(GetKnownCategoriesCompleter::Sync& completer) override {}
+  void NotifyBufferSaved(NotifyBufferSavedRequest& request,
+                         NotifyBufferSavedCompleter::Sync& completer) override {}
+  void Terminate(TerminateCompleter::Sync& completer) override {}
+  void Flush(FlushCompleter::Sync& completer) override {}
+  void handle_unknown_method(
+      fidl::UnknownMethodMetadata<fuchsia_tracing_provider::ProviderV2> metadata,
+      fidl::UnknownMethodCompleter::Sync& completer) override {
+    completer.Close(ZX_ERR_NOT_SUPPORTED);
   }
-}
+};
 
-// Tests that alerts are send over the fifo.
+// Tests that alerts are sent over FIDL events.
 TEST(SessionTest, AlertSent) {
   async::Loop loop{&kAsyncLoopConfigNoAttachToCurrentThread};
+  DummyProvider provider;
+  auto [client_end, server_end] = fidl::Endpoints<fuchsia_tracing_provider::ProviderV2>::Create();
+  auto binding = fidl::BindServer(loop.dispatcher(), std::move(server_end), &provider);
 
   zx::vmo buffer;
   zx_status_t status = zx::vmo::create(kBufferSize, 0, &buffer);
-  ASSERT_EQ(ZX_OK, status);
-
-  zx::fifo fifo_provider;
-  zx::fifo fifo_manager;
-  status = zx::fifo::create(kFifoCount, sizeof(trace_provider_packet_t), 0, &fifo_provider,
-                            &fifo_manager);
   ASSERT_EQ(ZX_OK, status);
 
   std::vector<std::string> categories = {
@@ -54,66 +56,86 @@ TEST(SessionTest, AlertSent) {
       // Empty filter to make sure the wildcard matcher can handle the empty case
       ""};
 
-  internal::Session::InitializeEngine(loop.dispatcher(), TRACE_BUFFERING_MODE_CIRCULAR,
-                                      std::move(buffer), std::move(fifo_provider), categories);
+  std::unique_ptr<internal::Session> session =
+      internal::Session::InitializeEngine(loop.dispatcher(), TRACE_BUFFERING_MODE_CIRCULAR,
+                                          std::move(buffer), categories, std::move(binding));
+
+  // Create a client to listen for events.
+
+  class EventHandler : public fidl::AsyncEventHandler<fuchsia_tracing_provider::ProviderV2> {
+   public:
+    void OnAlert(fidl::Event<fuchsia_tracing_provider::ProviderV2::OnAlert>& event) override {
+      alert_name_ = event.name();
+      alert_received_ = true;
+    }
+    // Let's check if we need to implement OnSaveBuffer.
+    void OnSaveBuffer(
+        fidl::Event<fuchsia_tracing_provider::ProviderV2::OnSaveBuffer>& event) override {}
+
+    void handle_unknown_event(
+        fidl::UnknownEventMetadata<fuchsia_tracing_provider::ProviderV2> metadata) override {}
+
+    std::string alert_name_;
+    bool alert_received_ = false;
+  };
+
+  EventHandler event_handler;
+  fidl::Client<fuchsia_tracing_provider::ProviderV2> client(std::move(client_end),
+                                                            loop.dispatcher(), &event_handler);
 
   // Not started yet.
   TRACE_ALERT("test_category", kAlertName.c_str());
+  loop.RunUntilIdle();
+  ASSERT_FALSE(event_handler.alert_received_);
 
-  trace_provider_packet_t packet;
-  size_t actual;
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_ERR_SHOULD_WAIT, status);
+  session->StartEngine(TRACE_START_CLEAR_ENTIRE_BUFFER, []() {});
+  loop.RunUntilIdle();
 
-  internal::Session::StartEngine(TRACE_START_CLEAR_ENTIRE_BUFFER);
+  // Alert with enabled category.
+  TRACE_ALERT("test_category", kAlertName.c_str());
+  loop.RunUntilIdle();
+  ASSERT_TRUE(event_handler.alert_received_);
+  ASSERT_EQ(kAlertName, event_handler.alert_name_);
 
-  // No alerts since start.
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_ERR_SHOULD_WAIT, status);
+  // Reset
+  event_handler.alert_received_ = false;
+  event_handler.alert_name_.clear();
 
   // Alert name neither min nor max length.
   TRACE_ALERT("wildcard_category", kAlertName.c_str());
+  loop.RunUntilIdle();
+  ASSERT_TRUE(event_handler.alert_received_);
+  ASSERT_EQ(kAlertName, event_handler.alert_name_);
 
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_OK, status);
-  ASSERT_EQ(1, actual);
-
-  ASSERT_EQ(TRACE_PROVIDER_ALERT, packet.request);
-  CheckAlertNameAndZeroPadding(
-      kAlertName, &packet.data16,
-      sizeof(packet.data16) + sizeof(packet.data32) + sizeof(packet.data64));
+  // Reset
+  event_handler.alert_received_ = false;
+  event_handler.alert_name_.clear();
 
   // Alert name of min length (1).
   TRACE_ALERT("test_category", kAlertNameMin.c_str());
+  loop.RunUntilIdle();
+  ASSERT_TRUE(event_handler.alert_received_);
+  ASSERT_EQ(kAlertNameMin, event_handler.alert_name_);
 
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_OK, status);
-  ASSERT_EQ(1, actual);
-
-  ASSERT_EQ(TRACE_PROVIDER_ALERT, packet.request);
-  CheckAlertNameAndZeroPadding(
-      kAlertNameMin, &packet.data16,
-      sizeof(packet.data16) + sizeof(packet.data32) + sizeof(packet.data64));
+  // Reset
+  event_handler.alert_received_ = false;
+  event_handler.alert_name_.clear();
 
   // Alert name of max length (14).
   TRACE_ALERT("wildcard_category", kAlertNameMax.c_str());
-
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_OK, status);
-  ASSERT_EQ(1, actual);
-
-  ASSERT_EQ(TRACE_PROVIDER_ALERT, packet.request);
-  CheckAlertNameAndZeroPadding(
-      kAlertNameMax, &packet.data16,
-      sizeof(packet.data16) + sizeof(packet.data32) + sizeof(packet.data64));
-
-  // Disabled category.
-  TRACE_ALERT("other_category", kAlertName.c_str());
-
-  status = fifo_manager.read(sizeof(trace_provider_packet_t), &packet, 1, &actual);
-  ASSERT_EQ(ZX_ERR_SHOULD_WAIT, status);
-
   loop.RunUntilIdle();
+  ASSERT_TRUE(event_handler.alert_received_);
+  ASSERT_EQ(kAlertNameMax, event_handler.alert_name_);
+
+  // Reset
+  event_handler.alert_received_ = false;
+  event_handler.alert_name_.clear();
+
+  // Alert with disabled category.
+  TRACE_ALERT("other_category", kAlertName.c_str());
+  loop.RunUntilIdle();
+  ASSERT_FALSE(event_handler.alert_received_);
+
   loop.Shutdown();
 }
 
