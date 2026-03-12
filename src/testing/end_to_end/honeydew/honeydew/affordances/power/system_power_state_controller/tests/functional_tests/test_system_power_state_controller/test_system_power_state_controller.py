f5b1f27892a0a40ab841744910a8ea61886b5ef0
Charles Celerier <chcl@google.com>
2026-03-11 17:39:40 -0700

    [power] Port SystemPowerStateControllerAffordanceTests to AsyncFuchsiaBaseTest
    This change ports the SystemPowerStateController affordance tests to use the new
    AsyncFuchsiaBaseTest class.
    
    Bug: 488299605
    Change-Id: I72ee7187f5f47626ebbb429c42b88beb89f36d64
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1528747
    Reviewed-by: Marc Khouri <mnck@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/testing/end_to_end/honeydew/honeydew/affordances/power/system_power_state_controller/tests/functional_tests/test_system_power_state_controller/test_system_power_state_controller.py
+++ src/testing/end_to_end/honeydew/honeydew/affordances/power/system_power_state_controller/tests/functional_tests/test_system_power_state_controller/test_system_power_state_controller.py
@@ -5,7 +5,7 @@
 
 import logging
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from mobly import asserts, test_runner
 
 from honeydew import errors
@@ -14,24 +14,24 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
 class SystemPowerStateControllerAffordanceTests(
-    fuchsia_base_test.FuchsiaBaseTest
+    fuchsia_base_test.AsyncFuchsiaBaseTest
 ):
     """SystemPowerStateController affordance tests"""
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """setup_class is called once before running tests."""
-        super().setup_class()
+        await super().setup_class()
         self.device = self.fuchsia_devices[0]
 
-    def test_idle_suspend_timer_based_resume(self) -> None:
+    async def test_idle_suspend_timer_based_resume(self) -> None:
         """Test case for SystemPowerStateController.idle_suspend_timer_based_resume()"""
         if self.user_params["is_starnix_supported"]:
-            self.device.system_power_state_controller.idle_suspend_timer_based_resume(
+            await self.device.system_power_state_controller.idle_suspend_timer_based_resume(
                 duration=3
             )
         else:
             with asserts.assert_raises(errors.NotSupportedError):
-                self.device.system_power_state_controller.idle_suspend_timer_based_resume(
+                await self.device.system_power_state_controller.idle_suspend_timer_based_resume(
                     duration=3,
                 )
 
