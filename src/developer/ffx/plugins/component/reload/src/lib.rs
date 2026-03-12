21c3a81d2dccb3113fb7f5369c801ac64ca0d599
Casey Dahlin <sadmac@google.com>
2026-03-12 02:06:52 -0700

    [ffx] Port component reload to FDomain
    Change-Id: I6a6a69646375f7f9cbd3a39decaae4f269b89195
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1535066
    Commit-Queue: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/plugins/component/reload/src/lib.rs
+++ src/developer/ffx/plugins/component/reload/src/lib.rs
@@ -3,13 +3,13 @@
 // found in the LICENSE file.
 
 use async_trait::async_trait;
-use component_debug::cli::reload_cmd;
+use component_debug_fdomain::cli::reload_cmd;
 use errors::ffx_error;
-use ffx_component::rcs::{connect_to_lifecycle_controller, connect_to_realm_query};
+use ffx_component::rcs::{connect_to_lifecycle_controller_f, connect_to_realm_query_f};
 use ffx_component_reload_args::ReloadComponentCommand;
 use ffx_writer::SimpleWriter;
 use fho::{FfxMain, FfxTool};
-use target_holders::RemoteControlProxyHolder;
+use target_holders::fdomain::RemoteControlProxyHolder;
 
 #[derive(FfxTool)]
 pub struct ReloadTool {
@@ -25,8 +25,8 @@ impl FfxMain for ReloadTool {
     type Writer = SimpleWriter;
 
     async fn main(self, writer: Self::Writer) -> fho::Result<()> {
-        let lifecycle_controller = connect_to_lifecycle_controller(&self.rcs).await?;
-        let realm_query = connect_to_realm_query(&self.rcs).await?;
+        let lifecycle_controller = connect_to_lifecycle_controller_f(&self.rcs).await?;
+        let realm_query = connect_to_realm_query_f(&self.rcs).await?;
 
         // All errors from component_debug library are user-visible.
         reload_cmd(self.cmd.query, lifecycle_controller, realm_query, writer)
