bae712ee7a17d19011e6ff7d56d45449771d06a0
Charles Celerier <chcl@google.com>
2026-03-11 17:19:46 -0700

    [starnix] Port StarnixAffordanceTests to AsyncFuchsiaBaseTest
    This change ports the Starnix affordance tests to use the new
    AsyncFuchsiaBaseTest class.
    
    Bug: 488299605
    Change-Id: I84007b0da2855f3f8431fa6696b6f6afb0976ff5
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1528746
    Reviewed-by: Marc Khouri <mnck@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>

--- src/testing/end_to_end/honeydew/honeydew/affordances/starnix/tests/functional_tests/test_starnix.py
+++ src/testing/end_to_end/honeydew/honeydew/affordances/starnix/tests/functional_tests/test_starnix.py
@@ -3,27 +3,29 @@
 # found in the LICENSE file.
 """Mobly test for Starnix affordance."""
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from mobly import asserts, test_runner
 
 from honeydew import errors
 
 
-class StarnixAffordanceTests(fuchsia_base_test.FuchsiaBaseTest):
+class StarnixAffordanceTests(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """Starnix affordance tests"""
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """setup_class is called once before running tests."""
-        super().setup_class()
+        await super().setup_class()
         self.device = self.fuchsia_devices[0]
 
-    def test_run_console_shell_cmd(self) -> None:
+    async def test_run_console_shell_cmd(self) -> None:
         """Test case for Starnix.run_console_shell_cmd()"""
         if self.user_params["is_starnix_supported"]:
-            self.device.starnix.run_console_shell_cmd(["echo", "hello"])
+            await self.device.starnix.run_console_shell_cmd(["echo", "hello"])
         else:
             with asserts.assert_raises(errors.NotSupportedError):
-                self.device.starnix.run_console_shell_cmd(["echo", "hello"])
+                await self.device.starnix.run_console_shell_cmd(
+                    ["echo", "hello"]
+                )
 
 
 if __name__ == "__main__":
