dafaafd9c202eb11396df766bad21e3089ba83f2
Charles Celerier <chcl@google.com>
2026-03-11 17:59:41 -0700

    [bluetooth] Port BluetoothAvrcpAffordanceTests to AsyncFuchsiaBaseTest
    This change ports the Bluetooth AVRCP affordance tests to use the new
    AsyncFuchsiaBaseTest class.
    
    Bug: 488299605
    Change-Id: I620576d2d3d6b3cb35b9c68dee8e670ccc5d8301
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1528748
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>
    Reviewed-by: Marc Khouri <mnck@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>

--- src/testing/end_to_end/honeydew/honeydew/affordances/connectivity/bluetooth/avrcp/tests/functional_tests/test_bluetooth_avrcp.py
+++ src/testing/end_to_end/honeydew/honeydew/affordances/connectivity/bluetooth/avrcp/tests/functional_tests/test_bluetooth_avrcp.py
@@ -5,7 +5,7 @@
 
 import logging
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from mobly import asserts, test_runner
 
 from honeydew.affordances.connectivity.bluetooth.utils import (
@@ -18,42 +18,42 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 BluetoothAvrcpCommand = bluetooth_types.BluetoothAvrcpCommand
 
 
-class BluetoothAvrcpAffordanceTests(fuchsia_base_test.FuchsiaBaseTest):
+class BluetoothAvrcpAffordanceTests(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """BluetoothAvrcp affordance tests"""
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """setup_class is called once before running tests.
 
         It does the following things:
             * Assigns `device` variable with FuchsiaDevice object
         """
-        super().setup_class()
+        await super().setup_class()
         self.device = self.fuchsia_devices[0]
 
-    def test_avrcp_init(self) -> None:
+    async def test_avrcp_init(self) -> None:
         """Test for Bluetooth.avrcp_init() method."""
-        self.device.bluetooth_avrcp.init_avrcp(target_id="0")
+        await self.device.bluetooth_avrcp.init_avrcp(target_id="0")
 
-    def test_list_received_requests(self) -> None:
+    async def test_list_received_requests(self) -> None:
         """Test for Bluetooth.list_received_requests() method."""
-        res = self.device.bluetooth_avrcp.list_received_requests()
+        res = await self.device.bluetooth_avrcp.list_received_requests()
         assert len(res) == 0
 
-    def test_publish_mock_player(self) -> None:
+    async def test_publish_mock_player(self) -> None:
         """Test for Bluetooth.publish_mock_player() method."""
-        self.device.bluetooth_avrcp.publish_mock_player()
+        await self.device.bluetooth_avrcp.publish_mock_player()
 
-    def test_send_avrcp_command(self) -> None:
+    async def test_send_avrcp_command(self) -> None:
         """Test for Bluetooth.send_avrcp_command() method."""
         # Currently fails sending commands since we only test single device
         with asserts.assert_raises(Sl4fError):
-            self.device.bluetooth_avrcp.send_avrcp_command(
+            await self.device.bluetooth_avrcp.send_avrcp_command(
                 BluetoothAvrcpCommand.PLAY
             )
 
-    def test_stop_mock_player(self) -> None:
+    async def test_stop_mock_player(self) -> None:
         """Test for Bluetooth.stop_mock_player() method"""
-        self.device.bluetooth_avrcp.stop_mock_player()
+        await self.device.bluetooth_avrcp.stop_mock_player()
 
 
 if __name__ == "__main__":
