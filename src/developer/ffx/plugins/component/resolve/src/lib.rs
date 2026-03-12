317b75e45944630ab64a2d618c57004f47f19d90
Casey Dahlin <sadmac@google.com>
2026-03-12 03:11:51 -0700

    [ffx] Port component resolve to FDomain
    Change-Id: I6a6a696437ed22b0c6f407b2e56ff51125e9913e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1535067
    Reviewed-by: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/plugins/component/resolve/src/lib.rs
+++ src/developer/ffx/plugins/component/resolve/src/lib.rs
@@ -3,13 +3,13 @@
 // found in the LICENSE file.
 
 use async_trait::async_trait;
-use component_debug::cli::resolve_cmd;
+use component_debug_fdomain::cli::resolve_cmd;
 use errors::ffx_error;
-use ffx_component::rcs::{connect_to_lifecycle_controller, connect_to_realm_query};
+use ffx_component::rcs::{connect_to_lifecycle_controller_f, connect_to_realm_query_f};
 use ffx_component_resolve_args::ComponentResolveCommand;
 use ffx_writer::SimpleWriter;
 use fho::{FfxMain, FfxTool};
-use target_holders::RemoteControlProxyHolder;
+use target_holders::fdomain::RemoteControlProxyHolder;
 
 #[derive(FfxTool)]
 pub struct ResolveTool {
@@ -25,8 +25,8 @@ impl FfxMain for ResolveTool {
     type Writer = SimpleWriter;
 
     async fn main(self, writer: Self::Writer) -> fho::Result<()> {
-        let lifecycle_controller = connect_to_lifecycle_controller(&self.rcs).await?;
-        let realm_query = connect_to_realm_query(&self.rcs).await?;
+        let lifecycle_controller = connect_to_lifecycle_controller_f(&self.rcs).await?;
+        let realm_query = connect_to_realm_query_f(&self.rcs).await?;
 
         // All errors from component_debug library are user-visible.
         resolve_cmd(self.cmd.query, lifecycle_controller, realm_query, writer)
