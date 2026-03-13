617b538ea82d4675c50e66fc5f530e8be61995b2
Charles Celerier <chcl@google.com>
2026-03-13 12:48:30 -0700

    [test_usb_disconnect] Port test_usb_disconnect to AsyncFuchsiaBaseTest
    Bug: 488299605
    Test: fx build "//src/testing/end_to_end/examples/test_usb_disconnect:usb_disconnect_test(//build/toolchain:host_x64)"
    Change-Id: Ic43d3f22c6c841a420a42088cfc4b10d9729b5ea
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1530520
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>
    Reviewed-by: Marc Khouri <mnck@google.com>

--- src/testing/end_to_end/examples/test_usb_disconnect/test_usb_disconnect.py
+++ src/testing/end_to_end/examples/test_usb_disconnect/test_usb_disconnect.py
@@ -3,17 +3,17 @@
 # found in the LICENSE file.
 """Usb Power Hub test."""
 
+import asyncio
 import logging
-import time
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from honeydew.auxiliary_devices.usb_power_hub import usb_power_hub
 from mobly import expects, test_runner
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
-class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
+class UsbDisconnectTest(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """Mobly test for testing the UsbPowerHub that works locally or in infra.
 
      Attributes:
@@ -24,7 +24,7 @@ class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
             executed.
     """
 
-    def pre_run(self) -> None:
+    async def pre_run(self) -> None:
         """Mobly method used to generate the test cases at run time."""
         test_arg_tuple_list: list[tuple[int]] = []
 
@@ -39,9 +39,9 @@ class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
             arg_sets=test_arg_tuple_list,
         )
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """setup_class is called once before running tests."""
-        super().setup_class()
+        await super().setup_class()
         self.dut = self.fuchsia_devices[0]
         self._usb_power_hub: usb_power_hub.UsbPowerHub
         self._usb_port: int | None
@@ -49,7 +49,7 @@ class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
             self.dut
         )
 
-    def _test_logic(self, iteration: int) -> None:
+    async def _test_logic(self, iteration: int) -> None:
         """Test case logic that disconnects the USB from a fuchsia device."""
         _LOGGER.info(
             "Starting the Usb Disconnect test iteration# %s", iteration
@@ -62,7 +62,7 @@ class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
         try:
             self._usb_power_hub.power_off(port=self._usb_port)
             _LOGGER.info("Waiting 10 seconds for the usb to disconnect")
-            time.sleep(10)
+            await asyncio.sleep(10)
             expects.expect_false(
                 self.dut.fastboot.is_in_fastboot_mode(),
                 "Fasboot device is still visible",
@@ -77,8 +77,8 @@ class UsbDisconnectTest(fuchsia_base_test.FuchsiaBaseTest):
                 self.dut.fastboot.boot_to_fuchsia_mode()
             except:
                 self.dut.fastboot.wait_for_fuchsia_mode()
-                self.dut.wait_for_online()
-                self.dut.on_device_boot()
+                await self.dut.wait_for_online()
+                await self.dut.on_device_boot()
 
         _LOGGER.info(
             "Successfully ended the Usb Disconnect test iteration# %s",
