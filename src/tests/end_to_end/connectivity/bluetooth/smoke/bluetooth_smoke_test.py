0ead089f9e58c97c46c109d7c75adcf386d73bb4
Charles Celerier <chcl@google.com>
2026-03-17 10:38:01 -0700

    [bluetooth] Port bluetooth_smoke_test to AsyncFuchsiaBaseTest
    Bug: 488299605
    Test: fx build "//src/tests/end_to_end/connectivity/bluetooth/smoke:bluetooth_smoke_test_fc(//build/toolchain:host_x64)"
    Change-Id: I6e91df2b052004c6b8df6ff7eccb017b4caa30ca
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1530538
    Commit-Queue: Charles Celerier <chcl@google.com>
    Reviewed-by: Jonathan Chang <jnchang@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/tests/end_to_end/connectivity/bluetooth/smoke/bluetooth_smoke_test.py
+++ src/tests/end_to_end/connectivity/bluetooth/smoke/bluetooth_smoke_test.py
@@ -3,11 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Bluetooth Smoke Test"""
+import asyncio
 import logging
-import time
 from typing import List, Tuple
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from mobly import test_runner
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
@@ -17,8 +17,8 @@ class MultipleFuchsiaDevicesNotFound(Exception):
     """When there are less than two Fuchsia devices available."""
 
 
-class BluetoothSmokeTest(fuchsia_base_test.FuchsiaBaseTest):
-    def pre_run(self) -> None:
+class BluetoothSmokeTest(fuchsia_base_test.AsyncFuchsiaBaseTest):
+    async def pre_run(self) -> None:
         """Mobly method used to generate the test cases at run time."""
         test_arg_tuple_list: List[Tuple[int]] = []
 
@@ -31,39 +31,41 @@ class BluetoothSmokeTest(fuchsia_base_test.FuchsiaBaseTest):
             arg_sets=test_arg_tuple_list,
         )
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """Initialize DUT"""
-        super().setup_class()
+        await super().setup_class()
         self.device = self.fuchsia_devices[0]
 
-    def _test_logic(self, iteration: int) -> None:
+    async def _test_logic(self, iteration: int) -> None:
         """Test Logic for Bluetooth Smoke Test
         1. Turn on BT discoverability on both devices
         2. Retrieve the receiver's BT address
         """
         _LOGGER.info("Initializing Bluetooth and setting discoverability")
-        self.device.bluetooth_gap.request_discovery(True)
-        self.device.bluetooth_gap.set_discoverable(True)
+        await self.device.bluetooth_gap.request_discovery(True)
+        await self.device.bluetooth_gap.set_discoverable(True)
         # TODO(b/309011914): Remove sleep once polling for discoverability is added.
-        time.sleep(3)
+        await asyncio.sleep(3)
 
-        bt_address = self.device.bluetooth_gap.get_active_adapter_address()
+        bt_address = (
+            await self.device.bluetooth_gap.get_active_adapter_address()
+        )
         _LOGGER.info("Receiver address: %s", bt_address)
         _LOGGER.info(
             "Completed Bluetooth state checks. "
             "Successfully ended the Bluetooth Smoke test."
         )
 
-    def teardown_test(self) -> None:
+    async def teardown_test(self) -> None:
         """Teardown Test logic
         1. Turn off discoverability on device.
         2. Turn off discovery on device.
         """
 
-        self.device.bluetooth_gap.set_discoverable(False)
-        self.device.bluetooth_gap.request_discovery(False)
-        self.device.bluetooth_gap.reset_state()
-        return super().teardown_class()
+        await self.device.bluetooth_gap.set_discoverable(False)
+        await self.device.bluetooth_gap.request_discovery(False)
+        await self.device.bluetooth_gap.reset_state()
+        return await super().teardown_test()
 
     def _name_func(self, iteration: int) -> str:
         """This function generates the names of each test case based on each
