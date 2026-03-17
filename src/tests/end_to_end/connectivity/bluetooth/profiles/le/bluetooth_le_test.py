4ed0c8adfcf9e5abcec27aecb208957522c17a55
Charles Celerier <chcl@google.com>
2026-03-17 12:25:58 -0700

    [bluetooth] Port bluetooth_le_test to AsyncFuchsiaBaseTest
    Bug: 488299605
    Test: fx build "//src/tests/end_to_end/connectivity/bluetooth/profiles/le:bluetooth_le_test_fc(//build/toolchain:host_x64)"
    Change-Id: I5cc453e36fd5e216907661afd6fde398fb69a465
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1530542
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>
    Reviewed-by: Jonathan Chang <jnchang@google.com>
    Commit-Queue: Charles Celerier <chcl@google.com>

--- src/tests/end_to_end/connectivity/bluetooth/profiles/le/bluetooth_le_test.py
+++ src/tests/end_to_end/connectivity/bluetooth/profiles/le/bluetooth_le_test.py
@@ -3,12 +3,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Sample test that demonstrates the usage of 2 Fuchsia devices in one test"""
+import asyncio
 import logging
 import re
-import time
 from typing import Any, List
 
-from fuchsia_base_test import fuchsia_base_test
+import fuchsia_base_test
 from honeydew.affordances.connectivity.bluetooth.utils.types import (
     BluetoothAcceptPairing,
     BluetoothLEAppearance,
@@ -22,10 +22,10 @@ class MultipleFuchsiaDevicesNotFound(Exception):
     """When there are less than two Fuchsia devices available."""
 
 
-class MultiDeviceSampleTest(fuchsia_base_test.FuchsiaBaseTest):
+class MultiDeviceSampleTest(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """Sample test that uses multiple Fuchsia devices"""
 
-    def pre_run(self) -> None:
+    async def pre_run(self) -> None:
         """Mobly method used to generate the test cases at run time."""
         test_arg_tuple_list: list[tuple[int]] = []
 
@@ -41,9 +41,9 @@ class MultiDeviceSampleTest(fuchsia_base_test.FuchsiaBaseTest):
     def _name_func(self, iteration: int) -> str:
         return f"test_bluetooth_sample_test_{iteration}"
 
-    def setup_class(self) -> None:
+    async def setup_class(self) -> None:
         """Initialize all DUTs."""
-        super().setup_class()
+        await super().setup_class()
         if len(self.fuchsia_devices) < 2:
             raise MultipleFuchsiaDevicesNotFound(
                 "Two FuchsiaDevices are" "required to run BluetoothSampleTest"
@@ -51,7 +51,7 @@ class MultiDeviceSampleTest(fuchsia_base_test.FuchsiaBaseTest):
         self.central = self.fuchsia_devices[0]
         self.peripheral = self.fuchsia_devices[1]
 
-    def _test_logic(self, iteration: int) -> None:
+    async def _test_logic(self, iteration: int) -> None:
         """Minimal Viable Code for BLE testing
         1. Start Pairing Delegate Server
         2. Advertise from Peripheral end
@@ -59,36 +59,36 @@ class MultiDeviceSampleTest(fuchsia_base_test.FuchsiaBaseTest):
         4. Call connect from the central
         5. Call get_known_remote_devices from central to verify "connect: True"
         """
-        self.peripheral.bluetooth_le.accept_pairing(
+        await self.peripheral.bluetooth_le.accept_pairing(
             input_mode=BluetoothAcceptPairing.DEFAULT_INPUT_MODE,
             output_mode=BluetoothAcceptPairing.DEFAULT_OUTPUT_MODE,
         )
-        self._set_discoverability_on()
+        await self._set_discoverability_on()
         _LOGGER.info(
             "Starting the Bluetooth Sample test iteration# %s", iteration
         )
         _LOGGER.info("Initializing Bluetooth and setting discoverability")
         _LOGGER.info(self.peripheral.device_name)
-        self.peripheral.bluetooth_le.advertise(
+        await self.peripheral.bluetooth_le.advertise(
             appearance=BluetoothLEAppearance.GLUCOSE_MONITOR,
             name=self.peripheral.device_name,
         )
-        known_device = self.central.bluetooth_le.scan()
+        known_device = await self.central.bluetooth_le.scan()
         _LOGGER.info(known_device)
         _LOGGER.info("Finished scanning, now going to connect.")
         for k in known_device.keys():
-            self.central.bluetooth_le.connect(k)
+            await self.central.bluetooth_le.connect(k)
             _LOGGER.info("Finished attempting to connect")
-            self.peripheral.bluetooth_le.run_advertise_connection()
-        time.sleep(5)
-        print(self.central.bluetooth_le.get_known_remote_devices())
+            await self.peripheral.bluetooth_le.run_advertise_connection()
+        await asyncio.sleep(5)
+        print(await self.central.bluetooth_le.get_known_remote_devices())
 
-    def teardown_test(self) -> None:
+    async def teardown_test(self) -> None:
         """Teardown test will turn off discoverability for all the devices."""
         _LOGGER.info("Forgetting all connected devices on Central/Peripheral.")
-        self.central.bluetooth_le.reset_state()
-        self.peripheral.bluetooth_le.reset_state()
-        return super().teardown_class()
+        await self.central.bluetooth_le.reset_state()
+        await self.peripheral.bluetooth_le.reset_state()
+        return await super().teardown_test()
 
     def _sl4f_bt_mac_address(self, mac_address: str) -> list[int]:
         """Converts BT mac addresses to reversed BT byte lists.
@@ -135,24 +135,24 @@ class MultiDeviceSampleTest(fuchsia_base_test.FuchsiaBaseTest):
                 return True
         return False
 
-    def _set_discoverability_on(self) -> None:
+    async def _set_discoverability_on(self) -> None:
         """Turns on discoverability for the devices."""
         _LOGGER.info("TRYING TO SET DISCOVERABLE")
-        self.central.bluetooth_le.request_discovery(True)
-        self.central.bluetooth_le.set_discoverable(True)
-        self.peripheral.bluetooth_le.set_discoverable(True)
+        await self.central.bluetooth_le.request_discovery(True)
+        await self.central.bluetooth_le.set_discoverable(True)
+        await self.peripheral.bluetooth_le.set_discoverable(True)
 
-    def _forget_all_bt_devices(self, device: Any, id: Any = None) -> None:
+    async def _forget_all_bt_devices(self, device: Any, id: Any = None) -> None:
         """Unpairs and deletes any BT peer pairing data from the device."""
         if id is None:
-            data = device.bluetooth_le.get_known_remote_devices()
+            data = await device.bluetooth_le.get_known_remote_devices()
             _LOGGER.info(data)
             for device_id in data.keys():
-                device.bluetooth_le.forget_device(
+                await device.bluetooth_le.forget_device(
                     identifier=data[device_id]["id"]
                 )
         else:
-            device.bluetooth_le.forget_device(identifier=id)
+            await device.bluetooth_le.forget_device(identifier=id)
 
 
 if __name__ == "__main__":
