01572acd83c789c0fd39f8886b07a29a56348ac4
Andrew Davies <awdavies@google.com>
2026-03-17 18:52:47 -0700

    [fuchsia-controller] Enable mypy for fuchsia_controller_isolate_directory_test
    Bug: 346628306
    Change-Id: I0ceb827f61934344e5b456252a4632106a6a636c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1538432
    Commit-Queue: Andrew Davies <awdavies@google.com>
    Reviewed-by: Charles Celerier <chcl@google.com>

--- src/developer/ffx/lib/fuchsia-controller/tests/isolate_directory.py
+++ src/developer/ffx/lib/fuchsia-controller/tests/isolate_directory.py
@@ -1,7 +1,8 @@
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-# mypy: ignore-errors
+
+from __future__ import annotations
 
 import os
 import shutil
@@ -12,32 +13,35 @@ from fuchsia_controller_py import IsolateDir
 
 
 class IsolateDirectory(unittest.TestCase):
-    def test_isolate_dir_creation_empty(self):
-        isolate_dir = IsolateDir()
-
+    def test_isolate_dir_creation_empty(self) -> None:
+        isolate_dir: IsolateDir | None = IsolateDir()
+        assert isolate_dir is not None
         temp_dir = isolate_dir.directory()
+
         self.assertTrue(os.path.exists(temp_dir))
 
         isolate_dir = None
         self.assertFalse(os.path.exists(temp_dir))
 
-    def test_isolate_dir_creation_new_dir(self):
+    def test_isolate_dir_creation_new_dir(self) -> None:
         temp_dir = tempfile.mkdtemp()  # Get a random directory path
         shutil.rmtree(temp_dir)  # Guarantee directory doesn't exist yet
         self.assertFalse(os.path.exists(temp_dir))
 
-        isolate_dir = IsolateDir(dir=temp_dir)
+        isolate_dir: IsolateDir | None = IsolateDir(dir=temp_dir)
+        assert isolate_dir is not None
         self.assertEqual(isolate_dir.directory(), temp_dir)
         self.assertTrue(os.path.exists(temp_dir))
 
         isolate_dir = None
         self.assertFalse(os.path.exists(temp_dir))
 
-    def test_isolate_dir_creation_existing_dir(self):
+    def test_isolate_dir_creation_existing_dir(self) -> None:
         temp_dir = tempfile.mkdtemp()
         self.assertTrue(os.path.exists(temp_dir))
 
-        isolate_dir = IsolateDir(dir=temp_dir)
+        isolate_dir: IsolateDir | None = IsolateDir(dir=temp_dir)
+        assert isolate_dir is not None
         self.assertEqual(isolate_dir.directory(), temp_dir)
         self.assertTrue(os.path.exists(temp_dir))
 
