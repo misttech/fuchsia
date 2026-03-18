4231d8b7a03c8071bfd097bb23cce0c160d7967f
Casey Dahlin <sadmac@google.com>
2026-03-17 19:57:53 -0700

    [ffx] Expose fake_async_proxy from target_holders
    Change-Id: I6a6a6964ed07f80d207b1ca6c509333401f910cc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1535227
    Reviewed-by: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/lib/target/holders/src/fdomain.rs
+++ src/developer/ffx/lib/target/holders/src/fdomain.rs
@@ -10,7 +10,9 @@ mod from_toolbox;
 mod remote_control_proxy;
 
 pub use from_toolbox::{toolbox, toolbox_or};
-pub use remote_control_proxy::{RemoteControlProxyHolder, fake_proxy, open_moniker_fdomain};
+pub use remote_control_proxy::{
+    RemoteControlProxyHolder, fake_async_proxy, fake_proxy, open_moniker_fdomain,
+};
 
 /// A decorator for proxy types in [`crate::FfxTool`] implementations so you can
 /// specify the moniker for the component exposing the proxy you're loading.
