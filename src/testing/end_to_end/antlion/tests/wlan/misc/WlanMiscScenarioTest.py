7af4f0439178ee63776fcf7af57fbccb34a77c7f
Vison Li <visonli@google.com>
2026-03-19 04:37:42 -0700

    [wlan e2e] Support OpenWRT in WlanMiscScenarioTest
    Update the WlanMiscScenarioTest suite to support OpenWRT-based access
    points alongside existing hostapd controllers. This change introduces a
    unified setup abstraction that dynamically selects the available access
    point controller.
    
    Bug: 492058300
    Test: fx test --build --e2e --output //src/testing/end_to_end/antlion/tests/wlan/misc:wlan_misc_scenario
    Change-Id: I8a0610bfcbf269c2ef756e3a3a4b191f81544fae
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1537820
    Fuchsia-Auto-Submit: Vison Li <visonli@google.com>
    Reviewed-by: Charles Celerier <chcl@google.com>
    Commit-Queue: Vison Li <visonli@google.com>

--- src/testing/end_to_end/antlion/tests/wlan/misc/WlanMiscScenarioTest.py
+++ src/testing/end_to_end/antlion/tests/wlan/misc/WlanMiscScenarioTest.py
@@ -9,12 +9,21 @@ import logging
 from antlion import utils
 from antlion.controllers.access_point import setup_ap
 from antlion.controllers.ap_lib import hostapd_constants
-from antlion.controllers.ap_lib.hostapd_security import Security, SecurityMode
-from antlion.controllers.ap_lib.hostapd_utils import generate_random_password
+from antlion.controllers.ap_lib.hostapd_security import (
+    Security as HostapdSecurity,
+)
+from antlion.controllers.ap_lib.hostapd_security import SecurityMode
 from antlion.test_utils.abstract_devices.wlan_device import AssociationMode
 from antlion.test_utils.wifi import base_test
 from mobly import asserts, signals, test_runner
-from mobly.records import TestResultRecord
+from mobly_controller.openwrt_access_point.lib.access_point_config import (
+    AccessPointConfig,
+    Band,
+    Security,
+)
+from mobly_controller.openwrt_access_point.lib.access_point_config_mapper import (
+    AccessPointConfigMapper as ConfigMapper,
+)
 
 
 class WlanMiscScenarioTest(base_test.WifiBaseTest):
@@ -28,23 +37,49 @@ class WlanMiscScenarioTest(base_test.WifiBaseTest):
         self.log = logging.getLogger()
         self.dut = self.get_dut(AssociationMode.POLICY)
 
-        if len(self.access_points) == 0:
+        if self.openwrt_aps:
+            self.openwrt_ap = self.openwrt_aps[0]
+        elif self.access_points:
+            self.access_point = self.access_points[0]
+            self.access_point.stop_all_aps()
+        else:
             raise signals.TestAbortClass("Requires at least one access point")
-        self.access_point = self.access_points[0]
-
-    def teardown_class(self) -> None:
-        self.dut.disconnect()
-        self.access_point.stop_all_aps()
 
     def teardown_test(self) -> None:
         self.dut.disconnect()
         self.download_logs()
-        self.access_point.stop_all_aps()
+        if hasattr(self, "access_point"):
+            self.access_point.stop_all_aps()
 
-    def on_fail(self, record: TestResultRecord) -> None:
-        super().on_fail(record)
-        self.dut.disconnect()
-        self.access_point.stop_all_aps()
+    def setup_ap(
+        self,
+        band: Band,
+        ssid: str,
+        security: Security,
+        password: str | None = None,
+    ) -> None:
+        if hasattr(self, "openwrt_ap"):
+            config = AccessPointConfig.generate(
+                band=band,
+                ssid=ssid,
+                password=password,
+                security=security,
+            )
+            self.openwrt_ap.configure_wifi(config)
+            self.openwrt_ap.verify_wifi_status(band=band)
+        elif hasattr(self, "access_point"):
+            hostapd_band = ConfigMapper.to_hostapd_band(band)
+            hostapd_security = ConfigMapper.to_hostapd_security(security)
+            setup_ap(
+                access_point=self.access_point,
+                profile_name="whirlwind",
+                channel=hostapd_band.default_channel(),
+                ssid=ssid,
+                security=HostapdSecurity(
+                    security_mode=hostapd_security,
+                    password=password,
+                ),
+            )
 
     def test_connect_to_wpa2_after_wpa3_rejection(self) -> None:
         """Test association to non-WPA3 network after receiving a WPA3
@@ -54,16 +89,14 @@ class WlanMiscScenarioTest(base_test.WifiBaseTest):
         """
         # Setup a WPA3 network
         wpa3_ssid = utils.rand_ascii_str(hostapd_constants.AP_SSID_LENGTH_5G)
-        setup_ap(
-            access_point=self.access_point,
-            profile_name="whirlwind",
-            channel=hostapd_constants.AP_DEFAULT_CHANNEL_5G,
+        wpa3_password = AccessPointConfig.random_string()
+        self.setup_ap(
+            band=Band.BAND_5G,
             ssid=wpa3_ssid,
-            security=Security(
-                security_mode=SecurityMode.WPA3,
-                password=generate_random_password(SecurityMode.WPA3),
-            ),
+            security=Security.WPA3,
+            password=wpa3_password,
         )
+
         # Attempt to associate with wrong password, expecting failure
         self.log.info("Attempting to associate WPA3 with wrong password.")
         asserts.assert_false(
@@ -73,19 +106,17 @@ class WlanMiscScenarioTest(base_test.WifiBaseTest):
             "Associated with WPA3 network using the wrong password",
         )
 
-        self.access_point.stop_all_aps()
+        if hasattr(self, "access_point"):
+            self.access_point.stop_all_aps()
 
         # Setup a WPA2 Network
         wpa2_ssid = utils.rand_ascii_str(hostapd_constants.AP_SSID_LENGTH_5G)
-        wpa2_password = generate_random_password(SecurityMode.WPA2)
-        setup_ap(
-            access_point=self.access_point,
-            profile_name="whirlwind",
-            channel=hostapd_constants.AP_DEFAULT_CHANNEL_5G,
+        wpa2_password = AccessPointConfig.random_string()
+        self.setup_ap(
+            band=Band.BAND_5G,
             ssid=wpa2_ssid,
-            security=Security(
-                security_mode=SecurityMode.WPA2, password=wpa2_password
-            ),
+            security=Security.WPA2,
+            password=wpa2_password,
         )
 
         # Attempt to associate, expecting success
