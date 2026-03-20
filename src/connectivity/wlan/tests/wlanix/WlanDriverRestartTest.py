91889e1231eeb6da617b529c34288e336f7c9ad3
Kevin Sakuma <sakuma@google.com>
2026-03-20 16:14:21 -0700

    [wlan][test] Test killing WLAN driver host.
    This change adds a test to kill the WLAN driver host and
    ensure that it eventually resumes.
    
    Bug: 493663193
    Test: fx test -o wlan_driver_restart_test
    Change-Id: I15defa26718644c1e0770d8cdafb7ac1080b6866
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1543104
    Reviewed-by: Charles Celerier <chcl@google.com>
    Commit-Queue: Kevin Sakuma <sakuma@google.com>

---
+++ src/connectivity/wlan/tests/wlanix/WlanDriverRestartTest.py
@@ -0,0 +1,91 @@
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import asyncio
+import logging
+
+import fidl_fuchsia_wlan_common as fw_common
+import fidl_fuchsia_wlan_sme as fw_sme
+from core_testing import base_test
+from mobly import test_runner
+from mobly.asserts import assert_true
+
+logger = logging.getLogger(__name__)
+
+
+class WlanDriverRestartTest(base_test.CoreBaseTestClass):
+    async def test_driver_host_restart(self) -> None:
+        # TODO(b/494309251): If a driver debug side channel is created, use that to query out the
+        # KOID and kill that instead.
+        logger.info("Killing driver-host-#wlan...")
+        self.fuchsia_device.ffx.run_ssh_cmd("killall driver-host-#wlan")
+
+        logger.info("Polling for PHY to be removed")
+        phy_removal_timeout = 10
+        poll_interval = 0.25
+
+        for attempt in range(int(phy_removal_timeout / poll_interval)):
+            response = await self.test_kit.device_monitor.list_phys()
+            phy_list = response.phy_list
+            logger.info(f"Attempt {attempt + 1}: Found PHY list: {phy_list}")
+
+            if len(phy_list) == 0:
+                logger.info("Successfully observed PHY removal")
+                break
+
+            await asyncio.sleep(poll_interval)
+        else:
+            assert_true(False, "Timed out waiting for PHYs to be removed")
+
+        logger.info("Polling for PHY to return")
+        phy_return_timeout = 30
+        poll_interval = 1.0
+
+        for attempt in range(int(phy_return_timeout / poll_interval)):
+            response = await self.test_kit.device_monitor.list_phys()
+            phy_list = response.phy_list
+            logger.info(f"Attempt {attempt + 1}: Found PHY list: {phy_list}")
+
+            if len(phy_list) > 0:
+                logger.info(f"Successfully found restored PHY(s): {phy_list}")
+                break
+
+            await asyncio.sleep(poll_interval)
+        else:
+            assert_true(False, "Timed out waiting for PHYs to be restored")
+
+        logger.info("Creating client interface...")
+        create_iface_response = (
+            await self.test_kit.device_monitor.create_iface(
+                phy_id=phy_list[0],
+                role=fw_common.WlanMacRole.CLIENT,
+                sta_address=[0, 0, 0, 0, 0, 0],
+            )
+        ).unwrap()
+        assert (
+            create_iface_response.iface_id is not None
+        ), "DeviceMonitor.CreateIface() response is missing an iface_id"
+        iface_id = create_iface_response.iface_id
+
+        logger.info(
+            f"Created interface with ID {iface_id}, obtaining ClientSme..."
+        )
+        proxy, server = self.fuchsia_device.fuchsia_controller.channel_create()
+        (
+            await self.test_kit.device_monitor.get_client_sme(
+                iface_id=iface_id,
+                sme_server=server.take(),
+            )
+        ).unwrap()
+        client_sme = fw_sme.ClientSmeClient(proxy)
+
+        logger.info("Issuing scan request...")
+        scan_req = fw_sme.ScanRequest(passive=fw_sme.PassiveScanRequest())
+        scan_response = (
+            await client_sme.scan_for_controller(req=scan_req)
+        ).unwrap()
+
+
+if __name__ == "__main__":
+    test_runner.main()
