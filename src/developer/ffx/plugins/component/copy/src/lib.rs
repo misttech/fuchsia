9228198fd8532172a1fe579e40f22ed2545afb95
Casey Dahlin <sadmac@google.com>
2026-03-23 13:05:37 -0700

    [ffx] Port component copy to FDomain
    Change-Id: I6a6a6964856ef73d20ee45cb2429de19d7ffc95a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1535273
    Fuchsia-Auto-Submit: Brian Bosak <bbosak@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>
    Reviewed-by: Colin Nelson <colnnelson@google.com>

--- src/developer/ffx/plugins/component/copy/src/lib.rs
+++ src/developer/ffx/plugins/component/copy/src/lib.rs
@@ -4,12 +4,13 @@
 
 use async_trait::async_trait;
 use component_debug::copy::copy_cmd;
+use component_debug_fdomain as component_debug;
 use errors::ffx_bail;
-use ffx_component::rcs::connect_to_realm_query;
+use ffx_component::rcs::connect_to_realm_query_f as connect_to_realm_query;
 use ffx_component_copy_args::CopyComponentCommand;
 use ffx_writer::SimpleWriter;
 use fho::{FfxMain, FfxTool};
-use target_holders::RemoteControlProxyHolder;
+use target_holders::fdomain::RemoteControlProxyHolder;
 
 #[derive(FfxTool)]
 pub struct CopyTool {
