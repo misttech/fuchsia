8d33f1d51ba708935494039a5f8f93cfc0129f07
Charles Celerier <chcl@google.com>
2026-03-27 17:27:08 -0700

    [lacewing] Port test_cases_example to async
    Bug: 488299605
    Change-Id: I7d382760c883fbdae607cfb47746157e81755cfa
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1556444
    Commit-Queue: Charles Celerier <chcl@google.com>
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>

--- src/testing/end_to_end/examples/test_cases_example/test_cases_example.py
+++ src/testing/end_to_end/examples/test_cases_example/test_cases_example.py
@@ -1,44 +1,44 @@
 # Copyright 2026 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Example test demonstrating FuchsiaTestCases."""
+"""Example test demonstrating AsyncFuchsiaTestCases."""
 
 import logging
 import pathlib
 from typing import Callable
 
-from fuchsia_base_test import fuchsia_base_test
-from honeydew.fuchsia_device.fuchsia_device import FuchsiaDevice
+import fuchsia_base_test
+from honeydew.fuchsia_device.async_fuchsia_device import AsyncFuchsiaDevice
 from mobly import test_runner
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
-class ExampleTestCases(fuchsia_base_test.FuchsiaTestCases):
+class ExampleTestCases(fuchsia_base_test.AsyncFuchsiaTestCases):
     """Example test cases."""
 
-    def setup_test(
+    async def setup_test(
         self,
-        fuchsia_devices: list[FuchsiaDevice],
+        fuchsia_devices: list[AsyncFuchsiaDevice],
         output_file_path: Callable[[str], pathlib.Path],
     ) -> None:
-        super().setup_test(fuchsia_devices, output_file_path)
+        await super().setup_test(fuchsia_devices, output_file_path)
         self.fuchsia_devices = fuchsia_devices
         self.output_file_path = output_file_path
         _LOGGER.info("ExampleTestCases.setup_test() called")
 
-    def teardown_test(self) -> None:
+    async def teardown_test(self) -> None:
         _LOGGER.info("ExampleTestCases.teardown_test() called")
-        super().teardown_test()
+        await super().teardown_test()
 
-    def test_example_case(self) -> None:
+    async def test_example_case(self) -> None:
         for fuchsia_device in self.fuchsia_devices:
             _LOGGER.info(
                 "%s inside test_example_case!", fuchsia_device.device_name
             )
 
 
-class ExampleTest(fuchsia_base_test.FuchsiaBaseTest):
+class ExampleTest(fuchsia_base_test.AsyncFuchsiaBaseTest):
     """Example test using TEST_CASES."""
 
     TEST_CASES = [ExampleTestCases]
