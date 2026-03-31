15c1d42c27d59ffd259951c9809a86af1cdab3b9
Filip Filmar <fmil@google.com>
2026-03-31 16:10:13 -0700

    [zircon][test] Create a concurrent monotonicity time read test
    In theory, this never fails. Practice, however, begs to differ.
    We have been seeing kselftest running under Starnix failing on
    specific machines under unclear circumstances.
    
    This change brings the test closer to Fuchsia, so we have a version
    that's sitting on a shorter software stack. The test is modeled after
    the kselftest.
    
    The test sets up a high contention list with several threads reading the
    monotonic clock and adding to a shared list, in a tight loop. Once we
    collect a batch of readings, we check that the read values are
    monotonically nondecreasing.
    
    Bug: 476408483
    Change-Id: I785e2c94621c2c200353f9c04f6e627eb427ec89
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1546944
    Reviewed-by: John Grossman <johngro@google.com>
    Commit-Queue: Filip Filmar <fmil@google.com>
    Fuchsia-Auto-Submit: Filip Filmar <fmil@google.com>

--- zircon/system/utest/core/clock/clock.cc
+++ zircon/system/utest/core/clock/clock.cc
@@ -2,11 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/zircon-internal/thread_annotations.h>
 #include <lib/zx/clock.h>
 #include <lib/zx/time.h>
 
 #include <array>
-#include <utility>
+#include <chrono>
+#include <cstddef>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 #include <zxtest/zxtest.h>
 
@@ -45,6 +50,76 @@ TEST(ClockTest, DeadlineAfter) {
 
     ASSERT_GE(deadline, before);
     ASSERT_LE(deadline, after);
+  }
+}
+
+// Have multiple threads compete over putting monotonic clock readings into a
+// shared list in a very hot loop over a long period of time. This test is
+// similar to a Linux kselftest which exercises the monotonic time readings.
+TEST(ClockTest, MultithreadedContentionClockMonotonic) {
+  // Parameters comparable to those used in threadtest.c from Linux kselftest.
+  constexpr int kNumThreads = 8;
+  constexpr int kRuntimeSeconds = 30;
+  constexpr int kListSize = 100;
+
+  struct {
+    std::mutex mut;
+    std::vector<zx::time> TA_GUARDED(mut) timestamps;
+    bool TA_GUARDED(mut) failed = false;
+    bool TA_GUARDED(mut) done = false;
+  } shared_items;
+
+  {
+    std::lock_guard<std::mutex> lock(shared_items.mut);
+    shared_items.timestamps.reserve(kListSize);
+  }
+
+  std::vector<std::thread> threads;
+  threads.reserve(kNumThreads + 1);
+  for (int t = 0; t < kNumThreads; ++t) {
+    threads.emplace_back([&]() {
+      while (true) {
+        std::lock_guard<std::mutex> lock(shared_items.mut);
+
+        if (shared_items.done || shared_items.failed) {
+          break;
+        }
+
+        shared_items.timestamps.push_back(zx::clock::get_monotonic());
+        // Once we collect enough values, check the values, clear the list and repeat.
+        // Note that the last batch of timestamps is not checked, but that might
+        // not be a big deal for detection.
+        if (unlikely(shared_items.timestamps.size() >= kListSize)) {
+          static_assert(kListSize > 1);
+          for (size_t i = 0; i < shared_items.timestamps.size() - 1; ++i) {
+            const int64_t& sooner = shared_items.timestamps[i].get();
+            const int64_t& later = shared_items.timestamps[i + 1].get();
+            EXPECT_LE(sooner, later);
+            if (sooner > later) {
+              shared_items.failed = true;
+              break;
+            }
+          }
+          // Keep the last element so the next iterations could check monotonicity
+          // across kListSize boundary.
+          const zx::time last = shared_items.timestamps.back();
+          shared_items.timestamps.clear();
+          shared_items.timestamps.push_back(last);
+        }
+      }  // while
+    });
+  }
+
+  threads.emplace_back([&] {
+    // This thread doesn't seem to be bothered by contention, so I leave the
+    // `done` flag flip as is.
+    std::this_thread::sleep_for(std::chrono::seconds(kRuntimeSeconds));
+    std::lock_guard<std::mutex> lock(shared_items.mut);
+    shared_items.done = true;
+  });
+
+  for (auto& thread : threads) {
+    thread.join();
   }
 }
 
