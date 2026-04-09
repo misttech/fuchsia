7e35f714f32e0cb59edf5ca533b23988a469f7ff
Casey Dahlin <sadmac@google.com>
2026-04-08 18:53:10 -0700

    [ffx] Get rid of Overnet version of ffx speedtest
    Change-Id: I388023352280152680510ffaaa1fbf1a6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1548224
    Reviewed-by: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Brian Bosak <bbosak@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- src/developer/ffx/tools/speedtest/src/lib.rs
+++ src/developer/ffx/tools/speedtest/src/lib.rs
@@ -5,68 +5,48 @@
 use std::num::NonZeroU32;
 use std::time::Duration;
 
-use component_debug::lifecycle::{self as lifecycle_overnet, CreateError as OvernetCreateError};
 use component_debug_fdomain::lifecycle::{self, CreateError};
 use errors::ffx_error;
+use fdomain_fuchsia_developer_ffx_speedtest as fspeedtest;
 use ffx_speedtest_args::{Ping, Socket, SpeedtestCommand, Subcommand};
 use ffx_writer::{SimpleWriter, ToolIO as _};
 use fho::{Deferred, FfxMain, FfxTool};
+use fuchsia_async as fasync;
 use fuchsia_url::fuchsia_pkg::AbsoluteComponentUrl;
 use moniker::Moniker;
-use speedtest::client as client_overnet;
 use speedtest_fdomain::client;
-use target_holders::RemoteControlProxyHolder as OvernetRemoteControlProxyHolder;
 use target_holders::fdomain::RemoteControlProxyHolder;
-use {
-    fdomain_fuchsia_developer_ffx_speedtest as fspeedtest,
-    fidl_fuchsia_developer_ffx_speedtest as fspeedtest_overnet, fuchsia_async as fasync,
-    rcs as rcs_overnet,
-};
 
 #[derive(FfxTool)]
 pub struct SpeedtestTool {
     remote_control: Deferred<RemoteControlProxyHolder>,
-    remote_control_overnet: Deferred<OvernetRemoteControlProxyHolder>,
     #[command]
     cmd: SpeedtestCommand,
 }
 
 fho::embedded_plugin!(SpeedtestTool);
 
-enum Client {
-    Regular(client::Client),
-    Overnet(client_overnet::Client),
-}
-
 #[async_trait::async_trait(?Send)]
 impl FfxMain for SpeedtestTool {
     type Writer = SimpleWriter;
 
     async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
-        let Self { cmd, remote_control, remote_control_overnet } = self;
+        let Self { cmd, remote_control } = self;
         let moniker = Moniker::parse_str("/core/ffx-laboratory:speedtest").unwrap();
         let SpeedtestCommand { overnet, mut repeat, delay, cmd } = cmd;
         let client = if overnet {
-            let proxy =
-                start_speedtest_component_overnet(&moniker, &remote_control_overnet.await?).await?;
-            Client::Overnet(client_overnet::Client::new(proxy).await.map_err(|e| fho::bug!(e))?)
+            return Err(fho::user_error!("Overnet no longer supported"));
         } else {
             let proxy = start_speedtest_component(&moniker, &remote_control.await?).await?;
-            Client::Regular(client::Client::new(proxy).await.map_err(|e| fho::bug!(e))?)
+            client::Client::new(proxy).await.map_err(|e| fho::bug!(e))?
         };
 
         loop {
             match cmd {
-                Subcommand::Ping(Ping { count }) => match &client {
-                    Client::Regular(client) => {
-                        let report = client.ping(count).await.map_err(|e| fho::bug!(e))?;
-                        writer.line(report)?;
-                    }
-                    Client::Overnet(client) => {
-                        let report = client.ping(count).await.map_err(|e| fho::bug!(e))?;
-                        writer.line(report)?;
-                    }
-                },
+                Subcommand::Ping(Ping { count }) => {
+                    let report = client.ping(count).await.map_err(|e| fho::bug!(e))?;
+                    writer.line(report)?;
+                }
                 Subcommand::Socket(Socket {
                     transfer_mb,
                     buffer_kb,
@@ -80,55 +60,23 @@ impl FfxMain for SpeedtestTool {
                     let buffer_len = buffer_kb
                         .checked_mul(NonZeroU32::new(1_000).unwrap())
                         .ok_or_else(|| fho::user_error!("buffer size too large"))?;
-                    match &client {
-                        Client::Regular(client) => {
-                            let writes_in_flight = fdomain_writes_in_flight
-                                .unwrap_or_else(|| data_len.div_ceil(buffer_len));
-                            let direction =
-                                if rx { client::Direction::Rx } else { client::Direction::Tx };
-                            let params = client::SocketTransferParams {
-                                direction,
-                                params: client::TransferParams {
-                                    data_len,
-                                    buffer_len,
-                                    fdomain_params: client::FDomainTransferParams {
-                                        streaming_read: !fdomain_individual_reads,
-                                        writes_in_flight,
-                                    },
-                                },
-                            };
+                    let writes_in_flight =
+                        fdomain_writes_in_flight.unwrap_or_else(|| data_len.div_ceil(buffer_len));
+                    let direction = if rx { client::Direction::Rx } else { client::Direction::Tx };
+                    let params = client::SocketTransferParams {
+                        direction,
+                        params: client::TransferParams {
+                            data_len,
+                            buffer_len,
+                            fdomain_params: client::FDomainTransferParams {
+                                streaming_read: !fdomain_individual_reads,
+                                writes_in_flight,
+                            },
+                        },
+                    };
 
-                            let report = client.socket(params).await.map_err(|e| fho::bug!(e))?;
-                            writer.line(report)?;
-                        }
-                        Client::Overnet(client) => {
-                            if fdomain_individual_reads {
-                                return Err(fho::user_error!(
-                                    "--fdomain-individual-reads and --overnet \
-                                    are mutually exclusive"
-                                ));
-                            }
-
-                            if fdomain_writes_in_flight.is_some() {
-                                return Err(fho::user_error!(
-                                    "--fdomain-writes-in-flight and --overnet \
-                                    are mutually exclusive"
-                                ));
-                            }
-                            let direction = if rx {
-                                client_overnet::Direction::Rx
-                            } else {
-                                client_overnet::Direction::Tx
-                            };
-                            let params = client_overnet::SocketTransferParams {
-                                direction,
-                                params: client_overnet::TransferParams { data_len, buffer_len },
-                            };
-
-                            let report = client.socket(params).await.map_err(|e| fho::bug!(e))?;
-                            writer.line(report)?;
-                        }
-                    }
+                    let report = client.socket(params).await.map_err(|e| fho::bug!(e))?;
+                    writer.line(report)?;
                 }
             }
 
@@ -186,40 +134,6 @@ async fn start_speedtest_component(
     })?;
 
     rcs_fdomain::connect_with_timeout::<fspeedtest::SpeedtestMarker>(
-        Duration::from_secs(10),
-        moniker.as_ref(),
-        &remote_control,
-    )
-    .await
-    .map_err(Into::into)
-}
-
-async fn start_speedtest_component_overnet(
-    moniker: &Moniker,
-    remote_control: &OvernetRemoteControlProxyHolder,
-) -> fho::Result<fspeedtest_overnet::SpeedtestProxy> {
-    let lifecycle_controller =
-        ffx_component::rcs::connect_to_lifecycle_controller(&remote_control).await?;
-
-    let (parent, child_name, collection) = unpack_moniker(moniker);
-    let url = AbsoluteComponentUrl::parse("fuchsia-pkg://fuchsia.com/speedtest#meta/speedtest.cm")
-        .unwrap();
-    lifecycle_overnet::create_instance_in_collection(
-        &lifecycle_controller,
-        &parent,
-        collection,
-        child_name,
-        &url,
-        vec![],
-        None,
-    )
-    .await
-    .or_else(|e| match e {
-        OvernetCreateError::InstanceAlreadyExists => Ok(()),
-        e => Err(ffx_error!(e)),
-    })?;
-
-    rcs_overnet::connect_with_timeout::<fspeedtest_overnet::SpeedtestMarker>(
         Duration::from_secs(10),
         moniker.as_ref(),
         &remote_control,
