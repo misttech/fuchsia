0257c1664a812ad422be93a7e96d4a6a26a4e719
Chris Masone <cmasone@google.com>
2026-04-13 14:51:01 -0700

    [honeydew][tracing] Implement FFX-based tracing affordance
    Implement `TracingUsingFfx` to provide tracing capabilities via the
    FFX CLI, matching the async-only pattern established in `TracingUsingFc`.
    
    Bug: 495898947
    Test: fx test tracing_using_fc_test
    Test: fx test tracing_using_ffx_test
    
    Change-Id: Ib6510c7288fe9cdc3a4c1746614f4e4f62aea7ce
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1556925
    Reviewed-by: Charles Celerier <chcl@google.com>
    Commit-Queue: Chris Masone <cmasone@google.com>

---
+++ src/testing/end_to_end/honeydew/honeydew/affordances/tracing/tests/unit_tests/tracing_using_ffx_test.py
@@ -0,0 +1,422 @@
+# Copyright 2026 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Unit tests for honeydew.affordances.tracing.tracing_using_ffx.py."""
+
+import os
+import subprocess
+import tempfile
+import unittest
+from collections.abc import Callable
+from typing import Any
+from unittest import mock
+
+import fidl_fuchsia_tracing as f_tracing
+from parameterized import param, parameterized
+
+from honeydew import affordances_capable
+from honeydew.affordances.tracing import tracing_using_ffx
+from honeydew.affordances.tracing.errors import TracingError, TracingStateError
+from honeydew.transports.ffx import ffx
+from honeydew.transports.ffx.errors import FfxCommandError
+
+
+def _custom_test_name_func(
+    testcase_func: Callable[..., None], _: str, param_arg: param
+) -> str:
+    """Custom name function method."""
+    test_func_name: str = testcase_func.__name__
+
+    params_dict: dict[str, Any] = param_arg.args[0]
+    test_label: str = parameterized.to_safe_name(params_dict["label"])
+
+    return f"{test_func_name}_{test_label}"
+
+
+class TracingFfxTests(unittest.IsolatedAsyncioTestCase):
+    """Unit tests for honeydew.affordances.tracing.tracing_using_ffx.py."""
+
+    async def asyncSetUp(self) -> None:
+        super().setUp()
+        self.reboot_affordance_obj = mock.MagicMock(
+            spec=affordances_capable.RebootCapableDevice
+        )
+        self.ffx_obj = mock.MagicMock(spec=ffx.FFX)
+
+        self.tracing_obj = tracing_using_ffx.TracingUsingFfx(
+            device_name="fuchsia-emulator",
+            ffx_inst=self.ffx_obj,
+            reboot_affordance=self.reboot_affordance_obj,
+        )
+
+    async def test_verify_supported(self) -> None:
+        """Test if verify_supported works."""
+        self.tracing_obj.verify_supported()
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "with_no_categories_and_no_buffer_size",
+                },
+            ),
+            (
+                {
+                    "label": "with_categories_and_buffer_size",
+                    "categories": ["category1", "category2"],
+                    "buffer_size": 1024,
+                },
+            ),
+            (
+                {
+                    "label": "when_session_already_initialized",
+                    "session_initialized": True,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_initialize(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.initialize() method."""
+        # Perform setup based on parameters.
+        if parameterized_dict.get("session_initialized"):
+            self.tracing_obj.initialize()
+
+        # Check whether an `TracingStateError` exception is raised when
+        # calling `initialize()` on a session that is already initialized.
+        if parameterized_dict.get("session_initialized"):
+            with self.assertRaises(TracingStateError):
+                self.tracing_obj.initialize()
+        else:
+            self.tracing_obj.initialize(
+                categories=parameterized_dict.get("categories"),
+                buffer_size=parameterized_dict.get("buffer_size"),
+            )
+            self.assertTrue(self.tracing_obj.is_session_initialized())
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "when_session_is_not_initialized",
+                    "session_initialized": False,
+                    "tracing_active": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_session_is_initialized",
+                    "session_initialized": True,
+                    "tracing_active": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_tracing_already_started",
+                    "session_initialized": True,
+                    "tracing_active": True,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_start(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.start() method."""
+        # Perform setup based on parameters.
+        if parameterized_dict.get("session_initialized"):
+            self.tracing_obj.initialize()
+        if parameterized_dict.get("tracing_active"):
+            await self.tracing_obj.start()
+
+        # Check whether an `TracingStateError` exception is raised when
+        # state is not valid.
+        if not parameterized_dict.get(
+            "session_initialized"
+        ) or parameterized_dict.get("tracing_active"):
+            with self.assertRaises(TracingStateError):
+                await self.tracing_obj.start()
+        else:
+            await self.tracing_obj.start()
+            self.ffx_obj.run.assert_called()
+            self.assertTrue(self.tracing_obj.is_active())
+
+    async def test_start_with_args(self) -> None:
+        """Test for Tracing.start() to verify FFX arguments."""
+        self.tracing_obj.initialize(
+            categories=["category1"],
+            buffer_size=1024,
+            buffering_mode=f_tracing.BufferingMode.ONESHOT,
+        )
+        await self.tracing_obj.start()
+        self.ffx_obj.run.assert_called_with(
+            [
+                "trace",
+                "start",
+                "--background",
+                "--categories",
+                "category1",
+                "--buffer-size",
+                "1024",
+                "--buffering-mode",
+                "oneshot",
+                "--nocompress",
+            ]
+        )
+
+    async def test_start_error_ffx_command(self) -> None:
+        """Test for Tracing.start() when FFX raises FfxCommandError."""
+        self.tracing_obj.initialize()
+        self.ffx_obj.run.side_effect = FfxCommandError("ffx error")
+        with self.assertRaises(TracingError):
+            await self.tracing_obj.start()
+
+    async def test_start_error_timeout(self) -> None:
+        """Test for Tracing.start() when FFX raises subprocess.TimeoutExpired."""
+        self.tracing_obj.initialize()
+        self.ffx_obj.run.side_effect = subprocess.TimeoutExpired("ffx", 1)
+        with self.assertRaises(subprocess.TimeoutExpired):
+            await self.tracing_obj.start()
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "when_session_is_not_initialized",
+                    "session_initialized": False,
+                    "tracing_active": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_session_is_initialized",
+                    "session_initialized": True,
+                    "tracing_active": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_tracing_already_started",
+                    "session_initialized": True,
+                    "tracing_active": True,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_stop(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.stop() method."""
+        # Perform setup based on parameters.
+        session_initialized = parameterized_dict.get("session_initialized")
+        tracing_active = parameterized_dict.get("tracing_active")
+        if session_initialized:
+            self.tracing_obj.initialize()
+        if tracing_active:
+            await self.tracing_obj.start()
+
+        # Check whether an `TracingStateError` exception is raised when
+        # state is not valid.
+        if not session_initialized or not tracing_active:
+            with self.assertRaises(TracingStateError):
+                await self.tracing_obj.stop()
+            return
+
+        await self.tracing_obj.stop()
+
+        self.assertIsNotNone(self.tracing_obj._temp_trace_file)
+        self.ffx_obj.run.assert_called_with(
+            ["trace", "stop", "--output", self.tracing_obj._temp_trace_file]
+        )
+        self.assertFalse(self.tracing_obj.is_active())
+
+        # Cleanup
+        if self.tracing_obj._temp_trace_file and os.path.exists(
+            self.tracing_obj._temp_trace_file
+        ):
+            os.remove(self.tracing_obj._temp_trace_file)
+
+    async def test_stop_error_ffx_command(self) -> None:
+        """Test for Tracing.stop() when FFX raises FfxCommandError."""
+        self.tracing_obj.initialize()
+        await self.tracing_obj.start()
+
+        self.ffx_obj.run.side_effect = FfxCommandError("ffx error")
+        with self.assertRaises(TracingError):
+            await self.tracing_obj.stop()
+
+        # Cleanup
+        if self.tracing_obj._temp_trace_file and os.path.exists(
+            self.tracing_obj._temp_trace_file
+        ):
+            os.remove(self.tracing_obj._temp_trace_file)
+
+    async def test_stop_error_timeout(self) -> None:
+        """Test for Tracing.stop() when FFX raises subprocess.TimeoutExpired."""
+        self.tracing_obj.initialize()
+        await self.tracing_obj.start()
+
+        self.ffx_obj.run.side_effect = subprocess.TimeoutExpired("ffx", 1)
+        with self.assertRaises(subprocess.TimeoutExpired):
+            await self.tracing_obj.stop()
+
+        # Cleanup
+        if self.tracing_obj._temp_trace_file and os.path.exists(
+            self.tracing_obj._temp_trace_file
+        ):
+            os.remove(self.tracing_obj._temp_trace_file)
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "when_session_is_not_initialized",
+                    "session_initialized": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_session_is_initialized",
+                    "session_initialized": True,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_terminate(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.terminate() method."""
+        temp_file_path = None
+        # Perform setup based on parameters.
+        if parameterized_dict.get("session_initialized"):
+            self.tracing_obj.initialize()
+            fd, temp_file_path = tempfile.mkstemp(suffix=".fxt")
+            os.close(fd)
+            self.tracing_obj._temp_trace_file = temp_file_path
+
+        await self.tracing_obj.terminate()
+
+        if temp_file_path:
+            self.assertFalse(os.path.exists(temp_file_path))
+
+        self.assertFalse(self.tracing_obj.is_active())
+        self.assertFalse(self.tracing_obj.is_session_initialized())
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "without_session_initialized",
+                    "session_initialized": False,
+                },
+            ),
+            (
+                {
+                    "label": "with_tracing_download_default_file_name",
+                    "return_value": "samp_trace_data",
+                    "session_initialized": True,
+                },
+            ),
+            (
+                {
+                    "label": "with_tracing_download_given_file_name",
+                    "trace_file": "trace.fxt",
+                    "return_value": "samp_trace_data",
+                    "session_initialized": True,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_terminate_and_download(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.terminate_and_download() method."""
+        # Perform setup based on parameters.
+        if parameterized_dict.get("session_initialized"):
+            self.tracing_obj.initialize()
+            await self.tracing_obj.start()
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            if not parameterized_dict.get("session_initialized"):
+                with self.assertRaises(TracingStateError):
+                    await self.tracing_obj.terminate_and_download(tmpdir)
+                return
+
+            trace_file: str = parameterized_dict.get("trace_file", "")
+            trace_path: str = await self.tracing_obj.terminate_and_download(
+                tmpdir, trace_file
+            )
+            self.assertFalse(self.tracing_obj.is_active())
+
+            # Check the return value of the terminate method.
+            if trace_file:
+                self.assertEqual(trace_path, f"{tmpdir}/{trace_file}")
+            else:
+                self.assertRegex(trace_path, f"{tmpdir}/trace_.*.fxt")
+
+            self.assertTrue(os.path.exists(trace_path))
+
+    @parameterized.expand(
+        [
+            (
+                {
+                    "label": "when_session_is_not_initialized",
+                    "session_initialized": False,
+                },
+            ),
+            (
+                {
+                    "label": "when_session_is_initialized",
+                    "session_initialized": True,
+                },
+            ),
+            (
+                {
+                    "label": "with_tracing_download",
+                    "download_trace": True,
+                    "trace_file": "trace.fxt",
+                    "session_initialized": False,
+                },
+            ),
+        ],
+        name_func=_custom_test_name_func,
+    )
+    async def test_trace_session(
+        self,
+        parameterized_dict: dict[str, Any],
+    ) -> None:
+        """Test for Tracing.trace_session() method."""
+        with tempfile.TemporaryDirectory() as tmpdir:
+            trace_file: str = parameterized_dict.get("trace_file", "")
+            download_trace: bool = parameterized_dict.get(
+                "download_trace", False
+            )
+            return_value: str = parameterized_dict.get("return_value", "")
+
+            if parameterized_dict.get("session_initialized"):
+                self.tracing_obj.initialize()
+
+            async with self.tracing_obj.trace_session(
+                download=download_trace, directory=tmpdir, trace_file=trace_file
+            ):
+                self.assertTrue(self.tracing_obj.is_active())
+            self.assertFalse(self.tracing_obj.is_active())
+
+            if download_trace:
+                trace_path: str = os.path.join(tmpdir, trace_file)
+                self.assertTrue(os.path.exists(trace_path))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/testing/end_to_end/honeydew/honeydew/affordances/tracing/tracing_using_ffx.py
@@ -0,0 +1,261 @@
+# Copyright 2024 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tracing affordance implementation using ffx."""
+
+import logging
+import os
+import shutil
+import tempfile
+from datetime import datetime
+
+import fidl_fuchsia_tracing as f_tracing
+
+from honeydew import affordances_capable
+from honeydew.affordances.tracing import tracing
+from honeydew.affordances.tracing.errors import TracingError, TracingStateError
+from honeydew.transports.ffx import ffx
+from honeydew.transports.ffx.errors import FfxCommandError
+
+_LOGGER: logging.Logger = logging.getLogger(__name__)
+
+DEFAULT_CATEGORIES: list[str] = [
+    "app",
+    "audio",
+    "benchmark",
+    "blobfs",
+    "gfx",
+    "input",
+    "magma",
+    "modular",
+    "system_metrics",
+    "view",
+]
+
+
+class TracingUsingFfx(tracing.Tracing):
+    """Async Tracing affordance implementation using ffx."""
+
+    def __init__(
+        self,
+        device_name: str,
+        ffx_inst: ffx.FFX,
+        reboot_affordance: affordances_capable.RebootCapableDevice,
+    ) -> None:
+        self._name: str = device_name
+        self._ffx: ffx.FFX = ffx_inst
+        self._reboot_affordance = reboot_affordance
+        self._session_initialized: bool = False
+        self._session_started: bool = False
+        self._buffer_size: int | None = None
+        self._categories: list[str] | None = None
+        self._buffering_mode: f_tracing.BufferingMode | None = None
+        self._temp_trace_file: str | None = None
+
+        reboot_affordance.register_for_on_device_boot(fn=self.reboot_handler)
+
+    async def reboot_handler(self) -> None:
+        """A method for handling reboots meant to be passed to the reboot affordance."""
+        _LOGGER.info("Received device boot signal. Resetting")
+        await self._reset_state()
+
+    async def _reset_state(self) -> None:
+        """Resets internal state tracking variables to correspond to an inactive
+        state; i.e. tracing uninitialized and not started.
+        """
+        try:
+            await self.terminate()
+        except TracingError:
+            pass
+
+        self._buffer_size = None
+        self._categories = None
+        self._buffering_mode = None
+
+    def verify_supported(self) -> None:
+        """Check if Trace is supported on the DUT.
+        Raises:
+            NotSupportedError: Tracing affordance is not supported by Fuchsia device.
+        """
+        # TODO(http://b/409625325): Implement the method logic
+
+    def is_active(self) -> bool:
+        """Checks if there is a currently active trace.
+
+        Returns:
+            True if the tracing is currently running, False otherwise.
+        """
+        return self._session_started
+
+    def is_session_initialized(self) -> bool:
+        """Checks if the session is initialized or not."""
+        return self._session_initialized
+
+    def initialize(
+        self,
+        categories: list[str] | None = None,
+        buffer_size: int | None = None,
+        start_timeout_milliseconds: int | None = None,
+        buffering_mode: f_tracing.BufferingMode | None = None,
+        defer_transfer: bool | None = None,
+    ) -> None:
+        """Initializes a trace session.
+
+        Args:
+            categories: list of categories to trace.
+            buffer_size: buffer size to use in MB.
+            start_timeout_milliseconds: milliseconds to wait for trace providers
+                to acknowledge that they've started tracing. NB: trace providers
+                that don't ACK by this deadline may still emit tracing events
+                starting at some later point.
+            buffering_mode: Tells tracing providers how to buffer data
+                ONESHOT - When the buffer fills the provider drops subsequent records
+                CIRCULAR - When the buffer fills, older records are discarded to make space
+                STREAMING - Data is streamed back to the trace_manager. Providers may still drop
+                            records if events are produced faster than they can be streamed
+            defer_transfer: Ignored by this implementation. Instead, this behavior is triggered
+                automatically when using STREAMING mode.
+
+        Raises:
+            TracingStateError: When trace session is already initialized.
+        """
+        if categories is None:
+            categories = DEFAULT_CATEGORIES
+        else:
+            new_categories: set[str] = set()
+            for category in categories:
+                if category == "#default" or category == "default":
+                    new_categories.update(DEFAULT_CATEGORIES)
+                else:
+                    new_categories.add(category)
+            categories = list(new_categories)
+
+        if self._session_initialized:
+            raise TracingStateError(
+                f"Trace session is already initialized on {self._name}. "
+                "Can be initialized only once"
+            )
+        _LOGGER.info("Initializing trace session via FFX on '%s'", self._name)
+
+        self._categories = categories
+        self._buffer_size = buffer_size
+        self._buffering_mode = buffering_mode
+        self._session_initialized = True
+
+    async def start(self) -> None:
+        """Starts tracing."""
+        if not self._session_initialized:
+            raise TracingStateError(
+                f"Trace session is not initialized on {self._name}"
+            )
+        if self._session_started:
+            raise TracingStateError(
+                f"Trace session is already started on {self._name}"
+            )
+        _LOGGER.info("Starting trace session via FFX on '%s'", self._name)
+
+        # While `ffx trace start` can take a duration, we run it infinitely with
+        # --background instead so that it will keep going until we stop it.
+        cmd = ["trace", "start", "--background"]
+        if self._categories:
+            cmd.extend(["--categories", ",".join(self._categories)])
+        if self._buffer_size is not None:
+            cmd.extend(["--buffer-size", str(self._buffer_size)])
+        if self._buffering_mode is not None:
+            if self._buffering_mode == f_tracing.BufferingMode.ONESHOT:
+                cmd.extend(["--buffering-mode", "oneshot"])
+            elif self._buffering_mode == f_tracing.BufferingMode.CIRCULAR:
+                cmd.extend(["--buffering-mode", "circular"])
+            elif self._buffering_mode == f_tracing.BufferingMode.STREAMING:
+                cmd.extend(["--buffering-mode", "streaming"])
+
+        cmd.append("--nocompress")
+
+        try:
+            self._ffx.run(cmd)
+        except FfxCommandError as err:
+            raise TracingError(
+                f"Failed to start FFX trace on {self._name}: {err}"
+            ) from err
+
+        self._session_started = True
+
+    async def stop(self) -> None:
+        """Stops the current trace."""
+        if not self._session_started:
+            raise TracingStateError(
+                f"Trace session is not started on {self._name}"
+            )
+        _LOGGER.info("Stopping trace session via FFX on '%s'", self._name)
+
+        # FFX trace stop does both stopping and downloading.
+        # We save it to a tempfile, so terminate_and_download can move it later.
+        fd, temp_file_path = tempfile.mkstemp(suffix=".fxt")
+        os.close(fd)
+
+        try:
+            self._ffx.run(["trace", "stop", "--output", temp_file_path])
+        except FfxCommandError as err:
+            raise TracingError(
+                f"Failed to stop FFX trace on {self._name}: {err}"
+            ) from err
+
+        self._temp_trace_file = temp_file_path
+        self._session_started = False
+
+    async def terminate(self) -> None:
+        """Terminates the trace session."""
+        if self._session_started:
+            try:
+                self._ffx.run(["trace", "stop", "--abort"])
+            except FfxCommandError as err:
+                raise TracingError(
+                    f"Failed to abort FFX trace on {self._name}: {err}"
+                ) from err
+            self._session_started = False
+
+        if self._temp_trace_file and os.path.exists(self._temp_trace_file):
+            try:
+                os.remove(self._temp_trace_file)
+            except OSError:
+                pass
+            self._temp_trace_file = None
+
+        self._session_initialized = False
+
+    async def terminate_and_download(
+        self, directory: str, trace_file: str | None = None
+    ) -> str:
+        """Terminates the trace session and downloads the trace data."""
+        if not self._session_initialized:
+            raise TracingStateError(
+                f"Trace session is not initialized on {self._name}"
+            )
+
+        if not os.path.isabs(directory):
+            raise ValueError(
+                f"Provide a valid absolute path to download the trace. Given: {directory}"
+            )
+        os.makedirs(directory, exist_ok=True)
+
+        if not trace_file:
+            now = datetime.now()
+            trace_file = (
+                f"trace_{self._name}_{now.strftime('%Y-%m-%d-%I-%M-%S-%p')}.fxt"
+            )
+
+        dest_path = os.path.join(directory, trace_file)
+
+        if self._session_started:
+            await self.stop()
+
+        if self._temp_trace_file and os.path.exists(self._temp_trace_file):
+            shutil.move(self._temp_trace_file, dest_path)
+            self._temp_trace_file = None
+        else:
+            raise TracingError(
+                f"Trace on {self._name} was not properly stopped or file is missing."
+            )
+
+        self._session_initialized = False
+        return dest_path
