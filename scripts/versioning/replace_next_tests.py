b862f0368e82261aa0c97f7d513fb77b74920aa4
Ian McKellar <ianloic@google.com>
2026-04-13 18:28:46 -0700

    [versioning] Add unit tests for replace_next.py
    I was thinking about trying to improve the script but didn't want to
    mess with regular expressions without some tests to ensure that we're
    fixing what we mean to fix without breaking anything else.
    
    Bug: 453685340
    Bug: 453724925
    Change-Id: I1fd1ab1111bea209e0f16fc4294372923dca6e15
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1480920
    Reviewed-by: David Dorwin <ddorwin@google.com>
    Commit-Queue: Ian McKellar <ianloic@google.com>
    Fuchsia-Auto-Submit: Ian McKellar <ianloic@google.com>
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>

---
+++ scripts/versioning/replace_next_tests.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env fuchsia-vendored-python
+# Copyright 2026 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import pathlib
+import unittest
+
+import replace_next
+
+
+class TestSubstitution(unittest.TestCase):
+    def assertSubstitution(
+        self, path: pathlib.Path, contents: str, expected: str
+    ) -> None:
+        contents = contents + "\n"
+        expected = expected + "\n"
+        for p in replace_next.PATTERNS:
+            if p.path_matches(path):
+                contents = p.substitute(contents, "42")
+        self.assertEqual(contents, expected)
+
+    def test_cpp(self) -> None:
+        path = pathlib.Path("test.h")
+        self.assertSubstitution(
+            path,
+            "#if FUCHSIA_API_LEVEL_AT_LEAST(NEXT)",
+            "#if FUCHSIA_API_LEVEL_AT_LEAST(42)",
+        )
+
+        # TODO(https://fxbug.dev/453724925):
+        # self.assertSubstitution(
+        #     path,
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST(NEXT) || !defined(__Fuchsia__)",
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST(42) || !defined(__Fuchsia__)",
+        # )
+
+        # https://fxbug.dev/453724925
+        # self.assertSubstitution(
+        #     path,
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST(NEXT) && FUCHSIA_API_LEVEL_LESS_THAN(HEAD)",
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST(42) && FUCHSIA_API_LEVEL_LESS_THAN(HEAD)",
+        # )
+
+        # self.assertSubstitution(
+        #     path,
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST( NEXT )",
+        #     "#if FUCHSIA_API_LEVEL_AT_LEAST( 42 )",
+        # )
+
+        self.assertSubstitution(
+            path, "} ZX_AVAILABLE_SINCE(NEXT)", "} ZX_AVAILABLE_SINCE(42)"
+        )
+
+        self.assertSubstitution(
+            path,
+            """) ZX_REMOVED_SINCE(1, 19,
+                                NEXT,
+                                "Use DoNewThing() instead")""",
+            """) ZX_REMOVED_SINCE(1, 19,
+                                42,
+                                "Use DoNewThing() instead")""",
+        )
+
+        # self.assertSubstitution(
+        #     path,
+        #     "ZX_REMOVED_SINCE((1), 19, NEXT)",
+        #     "ZX_REMOVED_SINCE((1), 19, 42)",
+        # )
+
+        # https://fxbug.dev/453685340
+        # self.assertSubstitution(
+        #     path,
+        #     """ZX_REMOVED_SINCE(1, NEXT, NEXT, "Use ProtocolReceive instead");""",
+        #     """ZX_REMOVED_SINCE(1, 42, 42, "Use ProtocolReceive instead");""",
+        # )
+
+    def test_rust(self) -> None:
+        path = pathlib.Path("test.rs")
+        self.assertSubstitution(
+            path,
+            """cfg(fuchsia_api_level_at_least = "NEXT")""",
+            """cfg(fuchsia_api_level_at_least = "42")""",
+        )
+
+        self.assertSubstitution(
+            path,
+            """cfg(all(fuchsia_api_level_at_least = "NEXT", fuchsia_api_level_less_than = "HEAD"))""",
+            """cfg(all(fuchsia_api_level_at_least = "42", fuchsia_api_level_less_than = "HEAD"))""",
+        )
+
+        # I don't think this is actually an interesting, valid case, but it's mentioned in the pattern definitions...
+        # self.assertSubstitution(
+        #     path,
+        #     """cfg(all(fuchsia_api_level_at_least = "NEXT", fuchsia_api_level_less_than = "NEXT"))""",
+        #     """cfg(all(fuchsia_api_level_at_least = "42", fuchsia_api_level_less_than = "42"))""",
+        # )
+
+    def test_fidl(self) -> None:
+        path = pathlib.Path("test.fidl")
+        self.assertSubstitution(
+            path,
+            """@available(added=NEXT)""",
+            """@available(added=42)""",
+        )
+
+        self.assertSubstitution(
+            path,
+            """@available(added=15, deprecated=22, removed=NEXT)""",
+            """@available(added=15, deprecated=22, removed=42)""",
+        )
+
+        self.assertSubstitution(
+            path,
+            """@available(added=15,
+                deprecated=22,
+                removed=NEXT)""",
+            """@available(added=15,
+                deprecated=22,
+                removed=42)""",
+        )
+
+        self.assertSubstitution(
+            path,
+            """@available(added=15, removed=NEXT, deprecated=22)""",
+            """@available(added=15, removed=42, deprecated=22)""",
+        )
+
+        self.assertSubstitution(
+            path,
+            """@available(added=NEXT, deprecated=NEXT)""",
+            """@available(added=42, deprecated=42)""",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
