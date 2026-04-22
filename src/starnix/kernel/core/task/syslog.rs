e3db0c69e5d8a9a3296c721ec17fd46c9a0fda2d
Brian Bosak <bbosak@google.com>
2026-04-21 18:18:56 -0700

    [logs] Remove unused FIDL definitions
    Remove the now-unused FIDL fields from the
    previous FXT migration.
    
    Bug: 315520045
    Change-Id: I7409cd10143df43a4d4ff47a4699926ec8715684
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1566536
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>
    API-Review: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Clayton Mccray <claytonmccray@google.com>
    Reviewed-by: Adam Perry <adamperry@google.com>

--- src/starnix/kernel/core/task/syslog.rs
+++ src/starnix/kernel/core/task/syslog.rs
@@ -307,7 +307,6 @@ impl LogIterator {
             client_selector_configuration: Some(
                 fdiagnostics::ClientSelectorConfiguration::SelectAll(true),
             ),
-            subscribe_to_manifest: Some(true),
             ..fdiagnostics::StreamParameters::default()
         };
         let (client_end, server_end) =
