9825d8f396cba5eece996a20587e22c4ae3a6039
David Koloski <dkoloski@google.com>
2026-04-23 12:38:01 -0700

    [async_rs] Remove async_rs
    We're consolidating implementations in sdk/lib/async/rust, so this
    directory is no longer needed.
    
    Change-Id: Ia90f93171b1cca71d0cc9572ec911f675359dad6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1585928
    Commit-Queue: David Koloski <dkoloski@google.com>
    Reviewed-by: Megan Batty <mgnb@google.com>
    Fuchsia-Auto-Submit: David Koloski <dkoloski@google.com>

--- src/lib/async_rs/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2026 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#[cfg(target_os = "fuchsia")]
-use async_rs_default as _;
-use async_rs_sys as _;
