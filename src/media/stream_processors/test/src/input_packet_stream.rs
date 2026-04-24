a3e2fff98fd64b7a3f011ad46d391e210dd7a3ab
Jerry Belton <jcecil@google.com>
2026-04-23 21:10:22 -0700

    [licenses] Move the copyright to the top of the file
    Change-Id: I2f2918536759bd15ac00083353a1f466722081c6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1584941
    Reviewed-by: Dustin Green <dustingreen@google.com>
    Fuchsia-Auto-Submit: Jerry Belton <jcecil@google.com>
    Commit-Queue: Jerry Belton <jcecil@google.com>

--- src/media/stream_processors/test/src/input_packet_stream.rs
+++ src/media/stream_processors/test/src/input_packet_stream.rs
@@ -1,9 +1,9 @@
-use crate::buffer_set::*;
-use crate::elementary_stream::*;
 // Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use crate::buffer_set::*;
+use crate::elementary_stream::*;
 use fidl_fuchsia_media::*;
 use fuchsia_stream_processors::*;
 
