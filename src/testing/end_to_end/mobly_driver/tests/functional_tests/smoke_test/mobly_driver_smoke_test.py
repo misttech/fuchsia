81ab36eac8a4c8ee7b2c2b8e9bde7e6618488d41
Charles Celerier <chcl@google.com>
2026-04-24 13:48:43 -0700

    [mobly_driver] Remove duplicate source in test dependencies
    The `fuchsia_device.py` file was actuallypart of the library not the
    test. Instead of just moving it to the right place though, I went ahead
    and overhauled the entire Mobly driver smoke test and renamed the
    `fuchsia_device` controller to `trivial_controller`. This new name
    reduces the number of "fuchsia device" controllers we have in tree when
    searching for them.
    
    Change-Id: Ic4a568374a28fa2891ff0d3b9e6a97483107dd30
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1584677
    Commit-Queue: Charles Celerier <chcl@google.com>
    Fuchsia-Auto-Submit: Charles Celerier <chcl@google.com>
    Reviewed-by: Paul Sethi <paulsethi@google.com>

--- src/testing/end_to_end/mobly_driver/tests/functional_tests/smoke_test/mobly_driver_smoke_test.py
+++ src/testing/end_to_end/mobly_driver/tests/functional_tests/smoke_test/mobly_driver_smoke_test.py
@@ -3,42 +3,66 @@
 # found in the LICENSE file.
 """Mobly Driver smoke test.
 
-This E2E smoke test exercises the Mobly Driver. Specifically, it tests the
-following functionalities:
+This E2E smoke test exercises the Mobly Driver's ability to conduct the following:
 
-1) Execution-environment dependent logic.
-2) Device config generation.
-3) Mobly test execution.
+  - Register a trivial controller
+  - Device config generation.
+  - Mobly test execution.
 
-The successful execution of this test in Fuchsia infra also proves the following
-infra integration points:
-1) Test parsing.
-2) Test artifact reporting.
+Furthermore, this test can be used as a canary to verify Fuchsia infra
+correctly parses Mobly test results and uploads artifacts from the test.
 """
+
 import os
 
-import fuchsia_device
+import trivial_controller
 from mobly import asserts, base_test, test_runner
+from mobly.expects import asserts
 
 
 class MoblyDriverSmokeTest(base_test.BaseTestClass):
     """Mobly Driver smoke tests."""
 
+    def setup_class(self) -> None:
+        super().setup_class()
+        # Infra testbed config ignores the local config and only contains real
+        # devices. Inject fake configs here to test controller registration.
+        self.controller_configs["TrivialController"] = [{"name": "fake_name"}]
+
     def test_mobly_controller_init(self) -> None:
-        """Test case to ensure Mobly controller initializes successfully"""
-        # Use Mobly's built-in register_controller() to parse configs and create
-        # devices controllers for test usage.
-        fuchsia_devices = self.register_controller(fuchsia_device)
-        asserts.assert_true(
-            fuchsia_devices, "Expect at least 1 created controller."
-        )
+        """Asserts a trivial controller can be registered."""
+        trivial_controllers = self.register_controller(trivial_controller)
+        assert len(trivial_controllers) > 0
 
     def test_params_exist(self) -> None:
-        """Test case to ensure test params are included in the Mobly config"""
-        asserts.assert_true(self.user_params, "Test params are missing.")
+        """Asserts user_params exactly match params.yaml."""
+        assert self.user_params is not None, "Test params are missing."
+        asserts.assert_equal(
+            set(self.user_params.keys()),
+            {"bool_param", "str_param", "dict_param", "list_param"},
+        )
+        asserts.assert_true(
+            self.user_params["bool_param"],
+            "bool_param does not match params.yaml",
+        )
+        asserts.assert_equal(
+            self.user_params["str_param"],
+            "some_string",
+            "str_param does not match params.yaml",
+        )
+        asserts.assert_equal(
+            self.user_params["dict_param"],
+            {"fld_1": "val_1", "fld_2": "val_2"},
+            "dict_param does not match params.yaml",
+        )
+        asserts.assert_equal(
+            self.user_params["list_param"],
+            [1, 2, 3, 4],
+            "list_param does not match params.yaml",
+        )
 
     def test_output_dir(self) -> None:
-        """Test case to ensure Mobly output dir can be written to"""
+        """Asserts log_path is writable."""
         test_artifact_path = os.path.join(self.log_path, "artifact.txt")
         with open(test_artifact_path, "w+", encoding="utf8") as file_handle:
             file_handle.write("data")
--- src/testing/end_to_end/mobly_driver/tests/functional_tests/smoke_test/trivial_controller/__init__.py
+++ src/testing/end_to_end/mobly_driver/tests/functional_tests/smoke_test/trivial_controller/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Simple Fuchsia Mobly controller.
+"""Trivial Mobly controller.
 
 This implements the required APIs to make this module compatible with Mobly.
  - create()
@@ -11,24 +11,21 @@ This implements the required APIs to make this module compatible with Mobly.
 
 from typing import Any
 
-MOBLY_CONTROLLER_CONFIG_NAME = "FuchsiaDevice"
+MOBLY_CONTROLLER_CONFIG_NAME = "TrivialController"
 CONFIG_KEY_NAME = "name"
 
 
-class FuchsiaDevice:
-    """Trivial Fuchsia device controller class."""
-
+class TrivialController:
     def __init__(self, config: dict[str, Any]) -> None:
         self._name = config[CONFIG_KEY_NAME]
 
 
-def create(configs: list[dict[str, Any]]) -> list[FuchsiaDevice]:
-    """Parses config YAML and create device controller(s)."""
+def create(configs: list[dict[str, Any]]) -> list[TrivialController]:
     fds = []
     for config in configs:
-        fds.append(FuchsiaDevice(config))
+        fds.append(TrivialController(config))
     return fds
 
 
-def destroy(unused_devices: list[FuchsiaDevice]) -> None:
-    """Tears down the controller(s)."""
+def destroy(unused_controllers: list[TrivialController]) -> None:
+    pass
