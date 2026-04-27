a54c2e14b9171ca7af49f7424ddf3b82f2a381d5
Ali Saeed <saeedali@google.com>
2026-04-27 15:04:14 -0700

    [bt][affordances] Add proxies.rs
    First step of decomposition of lib.rs in multiple files.
    
    Test: refactor only
    Bug: 505062674
    Change-Id: I96d1a4915123e307f6c287cf680668af39f584ba
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1583165
    Reviewed-by: Devan Yu <devanyu@google.com>
    Commit-Queue: Ali Saeed <saeedali@google.com>

--- src/connectivity/bluetooth/testing/bt-affordances/affordances/src/lib.rs
+++ src/connectivity/bluetooth/testing/bt-affordances/affordances/src/lib.rs
@@ -8,24 +8,21 @@ use fidl::endpoints::ClientEnd;
 use fidl_fuchsia_bluetooth::{ChannelMode, ChannelParameters, DeviceClass, PeerId, Uuid};
 use fidl_fuchsia_bluetooth_bredr::{
     ConnectParameters, ConnectionReceiverMarker, ConnectionReceiverRequest,
-    ConnectionReceiverRequestStream, DataElement, L2capParameters, ProfileMarker, ProfileProxy,
-    ProtocolDescriptor, ProtocolIdentifier, ServiceDefinition,
+    ConnectionReceiverRequestStream, DataElement, L2capParameters, ProtocolDescriptor,
+    ProtocolIdentifier, ServiceDefinition,
 };
 use fidl_fuchsia_bluetooth_gatt2::{
-    Characteristic, LocalServiceMarker, LocalServiceRequestStream, RemoteServiceProxy,
-    Server_Marker, Server_Proxy, ServiceHandle, ServiceInfo,
+    Characteristic, LocalServiceMarker, LocalServiceRequestStream, ServiceHandle, ServiceInfo,
 };
 use fidl_fuchsia_bluetooth_le::{
-    AdvertisedPeripheralMarker, AdvertisedPeripheralRequest, AdvertisingParameters, CentralMarker,
-    CentralProxy, ConnectionMarker, ConnectionOptions, ConnectionProxy, Filter,
-    PrivilegedPeripheralMarker, PrivilegedPeripheralProxy, ScanOptions, ScanResultWatcherMarker,
+    AdvertisedPeripheralMarker, AdvertisedPeripheralRequest, AdvertisingParameters,
+    ConnectionMarker, ConnectionOptions, Filter, ScanOptions, ScanResultWatcherMarker,
     ScanResultWatcherProxy,
 };
 use fidl_fuchsia_bluetooth_sys::{
-    AccessMarker, AccessProxy, AccessSetConnectionPolicyRequest, HostInfo, HostWatcherMarker,
-    HostWatcherProxy, InputCapability, OutputCapability, PairingDelegateMarker,
-    PairingDelegateRequest, PairingDelegateRequestStream, PairingMarker, PairingOptions,
-    PairingProxy, Peer, ProcedureTokenProxy,
+    AccessMarker, AccessProxy, AccessSetConnectionPolicyRequest, HostInfo, InputCapability,
+    OutputCapability, PairingDelegateMarker, PairingDelegateRequest, PairingDelegateRequestStream,
+    PairingOptions, Peer,
 };
 use fuchsia_async::{LocalExecutor, Task, TimeoutExt, Timer};
 use fuchsia_bluetooth::types::{Channel, Uuid as BtUuid};
@@ -36,6 +33,9 @@ use futures::{FutureExt, StreamExt, select};
 use std::ffi::{CStr, CString};
 use std::sync::Arc;
 use std::thread;
+
+mod proxies;
+use proxies::Proxies;
 
 // TODO(b/414848887): Pass more descriptive errors.
 enum Request {
@@ -657,64 +657,7 @@ impl WorkThread {
     }
 }
 
-struct Proxies {
-    access_proxy: AccessProxy,
-    profile_proxy: ProfileProxy,
-    central_proxy: CentralProxy,
-    gatt_server_proxy: Server_Proxy,
-    peripheral_proxy: PrivilegedPeripheralProxy,
-    pairing_proxy: PairingProxy,
-    host_watcher_stream: HangingGetStream<HostWatcherProxy, Vec<HostInfo>>,
-    peer_watcher_stream: HangingGetStream<AccessProxy, (Vec<Peer>, Vec<PeerId>)>,
-    discovery_session: Mutex<Option<ProcedureTokenProxy>>,
-    discoverability_session: Mutex<Option<ProcedureTokenProxy>>,
-    suppress_connections_session: Mutex<Option<ProcedureTokenProxy>>,
-    le_scan_task: Mutex<Option<Task<()>>>,
-    central_connection: Mutex<Option<ConnectionProxy>>,
-    gatt_client: Option<fidl_fuchsia_bluetooth_gatt2::ClientProxy>,
-    remote_service_proxy: Mutex<Option<RemoteServiceProxy>>,
-    characteristic_notifier_task: Mutex<Option<Task<()>>>,
-    pairing_delegate_state: Mutex<Option<(Task<()>, oneshot::Sender<()>)>>,
-}
-
 impl Proxies {
-    fn connect() -> Result<Self, anyhow::Error> {
-        // TODO(https://fxbug.dev/485277855): Consider exposing some of these proxies to clients
-        // in order to enable RAII, e.g. `gatt_client` and `remote_service_proxy`.
-        let access_proxy = connect_to_protocol::<AccessMarker>()?;
-        let profile_proxy = connect_to_protocol::<ProfileMarker>()?;
-        let central_proxy = connect_to_protocol::<CentralMarker>()?;
-        let gatt_server_proxy = connect_to_protocol::<Server_Marker>()?;
-        let peripheral_proxy = connect_to_protocol::<PrivilegedPeripheralMarker>()?;
-        let pairing_proxy = connect_to_protocol::<PairingMarker>()?;
-        let host_watcher_stream = HangingGetStream::new_with_fn_ptr(
-            connect_to_protocol::<HostWatcherMarker>()?,
-            HostWatcherProxy::watch,
-        );
-        let peer_watcher_stream =
-            HangingGetStream::new_with_fn_ptr(access_proxy.clone(), AccessProxy::watch_peers);
-
-        Ok(Proxies {
-            access_proxy,
-            profile_proxy,
-            central_proxy,
-            gatt_server_proxy,
-            peripheral_proxy,
-            pairing_proxy,
-            host_watcher_stream,
-            peer_watcher_stream,
-            discovery_session: Mutex::new(None),
-            discoverability_session: Mutex::new(None),
-            suppress_connections_session: Mutex::new(None),
-            le_scan_task: Mutex::new(None),
-            central_connection: Mutex::new(None),
-            gatt_client: None,
-            remote_service_proxy: Mutex::new(None),
-            characteristic_notifier_task: Mutex::new(None),
-            pairing_delegate_state: Mutex::new(None),
-        })
-    }
-
     async fn connect_peer(&self, peer_id: &PeerId) -> Result<(), anyhow::Error> {
         self.access_proxy
             .connect(peer_id)
---
+++ src/connectivity/bluetooth/testing/bt-affordances/affordances/src/proxies.rs
@@ -0,0 +1,79 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use async_utils::hanging_get::client::HangingGetStream;
+use fidl_fuchsia_bluetooth::PeerId;
+use fidl_fuchsia_bluetooth_bredr::{ProfileMarker, ProfileProxy};
+use fidl_fuchsia_bluetooth_gatt2::{RemoteServiceProxy, Server_Marker, Server_Proxy};
+use fidl_fuchsia_bluetooth_le::{
+    CentralMarker, CentralProxy, ConnectionProxy, PrivilegedPeripheralMarker,
+    PrivilegedPeripheralProxy,
+};
+use fidl_fuchsia_bluetooth_sys::{
+    AccessMarker, AccessProxy, HostInfo, HostWatcherMarker, HostWatcherProxy, PairingMarker,
+    PairingProxy, Peer, ProcedureTokenProxy,
+};
+use fuchsia_async::Task;
+use fuchsia_component::client::connect_to_protocol;
+use fuchsia_sync::Mutex;
+use futures::channel::oneshot;
+
+pub(crate) struct Proxies {
+    pub(crate) access_proxy: AccessProxy,
+    pub(crate) profile_proxy: ProfileProxy,
+    pub(crate) central_proxy: CentralProxy,
+    pub(crate) gatt_server_proxy: Server_Proxy,
+    pub(crate) peripheral_proxy: PrivilegedPeripheralProxy,
+    pub(crate) pairing_proxy: PairingProxy,
+    pub(crate) host_watcher_stream: HangingGetStream<HostWatcherProxy, Vec<HostInfo>>,
+    pub(crate) peer_watcher_stream: HangingGetStream<AccessProxy, (Vec<Peer>, Vec<PeerId>)>,
+    pub(crate) discovery_session: Mutex<Option<ProcedureTokenProxy>>,
+    pub(crate) discoverability_session: Mutex<Option<ProcedureTokenProxy>>,
+    pub(crate) suppress_connections_session: Mutex<Option<ProcedureTokenProxy>>,
+    pub(crate) le_scan_task: Mutex<Option<Task<()>>>,
+    pub(crate) central_connection: Mutex<Option<ConnectionProxy>>,
+    pub(crate) gatt_client: Option<fidl_fuchsia_bluetooth_gatt2::ClientProxy>,
+    pub(crate) remote_service_proxy: Mutex<Option<RemoteServiceProxy>>,
+    pub(crate) characteristic_notifier_task: Mutex<Option<Task<()>>>,
+    pub(crate) pairing_delegate_state: Mutex<Option<(Task<()>, oneshot::Sender<()>)>>,
+}
+
+impl Proxies {
+    pub(crate) fn connect() -> Result<Self, anyhow::Error> {
+        // TODO(https://fxbug.dev/485277855): Consider exposing some of these proxies to clients
+        // in order to enable RAII, e.g. `gatt_client` and `remote_service_proxy`.
+        let access_proxy = connect_to_protocol::<AccessMarker>()?;
+        let profile_proxy = connect_to_protocol::<ProfileMarker>()?;
+        let central_proxy = connect_to_protocol::<CentralMarker>()?;
+        let gatt_server_proxy = connect_to_protocol::<Server_Marker>()?;
+        let peripheral_proxy = connect_to_protocol::<PrivilegedPeripheralMarker>()?;
+        let pairing_proxy = connect_to_protocol::<PairingMarker>()?;
+        let host_watcher_stream = HangingGetStream::new_with_fn_ptr(
+            connect_to_protocol::<HostWatcherMarker>()?,
+            HostWatcherProxy::watch,
+        );
+        let peer_watcher_stream =
+            HangingGetStream::new_with_fn_ptr(access_proxy.clone(), AccessProxy::watch_peers);
+
+        Ok(Proxies {
+            access_proxy,
+            profile_proxy,
+            central_proxy,
+            gatt_server_proxy,
+            peripheral_proxy,
+            pairing_proxy,
+            host_watcher_stream,
+            peer_watcher_stream,
+            discovery_session: Mutex::new(None),
+            discoverability_session: Mutex::new(None),
+            suppress_connections_session: Mutex::new(None),
+            le_scan_task: Mutex::new(None),
+            central_connection: Mutex::new(None),
+            gatt_client: None,
+            remote_service_proxy: Mutex::new(None),
+            characteristic_notifier_task: Mutex::new(None),
+            pairing_delegate_state: Mutex::new(None),
+        })
+    }
+}
