1b9c4faf17d39c80d6dea22b53c7c42194b0c5ad
Mohammed Habibulla <moch@google.com>
2026-04-28 06:28:44 -0700

    Revert "[beads][rust] Add target for rustc cmd comparison"
    This reverts commit 945cd18d7edce9a81646ffd9bc94b10af19f1ead.
    
    Reason for revert: failing core.x64-rust_determinism-build_only
    
    Original change's description:
    > [beads][rust] Add target for rustc cmd comparison
    >
    > //build/beads:verifications:verify_build_commands_for_example runs GN
    > and Bazel command comparison in CI/CQ during build time. It currently
    > takes ~3s to finish on warm Bazel cache, and ~7s on cold Bazel cache.
    > Most of the time is spent in `ninja -t command`, and `bazel aquery` to
    > get the actual commands executed by those build systems. This target
    > batches those queries, so adding new target pairs for verification
    > should have minimal impact on runtime of this verification target.
    >
    > NOTE: This action is not parallelizable with other bazel_actions,
    > because it needs to execute a `bazel aquery`, which will block if
    > another Bazel command is running.
    >
    > Bug: 502778944
    > Change-Id: Ie71744bd45772545ff02ce69554cb123a037992f
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1570854
    > Reviewed-by: David Turner <digit@google.com>
    > Fuchsia-Auto-Submit: Jay Zhuang <jayzhuang@google.com>
    > Commit-Queue: Jay Zhuang <jayzhuang@google.com>
    
    Bug: 502778944
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I265254d84b77fd347e3e644ee92075c472ee9d38
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1590745
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    Commit-Queue: Mohammed Habibulla <moch@google.com>
    Fuchsia-Auto-Submit: Mohammed Habibulla <moch@google.com>

--- build/beads/verifications/rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-use rust_example_lib::add;
-
-fn main() {
-    println!("2 + 2 = {}", add(2, 2));
-}
