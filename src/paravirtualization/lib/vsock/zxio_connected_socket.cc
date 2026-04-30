d2d6e19b1c6c9c15146b0405b5b771cfab50e2f3
Ian McKellar <ianloic@google.com>
2026-04-30 13:17:24 -0700

    [vsock] Copy private functions from zxio
    This code uses private functions from an internal zxio header. Bazel is
    stricter than GN about accessing private headers so the functions have
    been copied into the vsock implementation.
    
    Bug: 454452299
    Change-Id: I8fa26eb232edc37fc7b613ce6d8d7777d0f1e9c0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1588386
    Reviewed-by: James Robinson <jamesr@google.com>
    Fuchsia-Auto-Submit: Ian McKellar <ianloic@google.com>
    Commit-Queue: Ian McKellar <ianloic@google.com>

--- src/paravirtualization/lib/vsock/zxio_connected_socket.cc
+++ src/paravirtualization/lib/vsock/zxio_connected_socket.cc
@@ -7,7 +7,6 @@
 #include <lib/syslog/cpp/macros.h>
 #include <lib/zxio/null.h>
 #include <lib/zxio/ops.h>
-#include <lib/zxio/vector.h>
 #include <lib/zxio/zxio.h>
 
 #include <type_traits>
@@ -20,6 +19,75 @@ static_assert(sizeof(ZxioConnectedSocket) <= sizeof(zxio_storage_t));
 static_assert(std::is_standard_layout_v<ZxioConnectedSocket>);
 
 namespace {
+// NOTE: zxio_do_vector and zxio_stream_do_vector are copied from
+// //sdk/lib/zxio/vector.h which is a private API.
+
+// Calls the callback |fn| for every buffer in |vector| or until the callback
+// returns |ZX_ERR_STOP|.
+//
+// **NOTE**: This method will return an error even if *some* progress has been
+// made which is typically not what you want and does not match some POSIX APIs
+// (generally on stream-like objects like Files, TCP sockets) which will return
+// success for short reads and writes. If this method is used by such APIs, you
+// most likely want to use |zxio_stream_do_vector| below, which will return
+// partial success.
+//
+// TODO(https://fxbug.dev/377534441): This function is not thread-safe! Multithreaded reads/writes
+// can become interleaved with each other.
+template <typename F>
+zx_status_t zxio_do_vector(const zx_iovec_t* vector, size_t vector_count, size_t* out_actual,
+                           F fn) {
+  size_t total = 0;
+  for (size_t i = 0; i < vector_count; ++i) {
+    size_t actual = 0;
+    zx_status_t status = fn(vector[i].buffer, vector[i].capacity, total, &actual);
+    switch (status) {
+      case ZX_OK:
+      case ZX_ERR_NEXT:
+        total += actual;
+        if (actual == vector[i].capacity) {
+          continue;
+        }
+        // Short read.
+        break;
+      case ZX_ERR_STOP:
+        total += actual;
+        break;
+      default:
+        return status;
+    }
+
+    break;
+  }
+  *out_actual = total;
+  return ZX_OK;
+}
+
+// This method is like |zxio_do_vector| except it implements stream semantics.
+// That is, this method will swallow the error returned by |fn|, stop iterating
+// the buffers and return |ZX_OK| as long as a previous call to |fn| successfully
+// performed some I/O on at least one byte. If |fn| returns an error before any
+// I/O was performed (no bytes were read/written), then that error is returned
+// to the caller.
+//
+// TODO(https://fxbug.dev/377534441): This function is not thread-safe! Multithreaded reads/writes
+// can become interleaved with each other.
+template <typename F>
+zx_status_t zxio_stream_do_vector(const zx_iovec_t* vector, size_t vector_count, size_t* out_actual,
+                                  F fn) {
+  return zxio_do_vector(
+      vector, vector_count, out_actual,
+      [&fn](void* buffer, size_t capacity, size_t total_so_far, size_t* out_actual) {
+        zx_status_t status = fn(buffer, capacity, out_actual);
+        if (status != ZX_OK) {
+          if (total_so_far == 0) {
+            return status;
+          }
+          return ZX_ERR_STOP;
+        }
+        return ZX_ERR_NEXT;
+      });
+}
 
 ZxioConnectedSocket& connected_socket(zxio_t* io) {
   return *reinterpret_cast<ZxioConnectedSocket*>(io);
