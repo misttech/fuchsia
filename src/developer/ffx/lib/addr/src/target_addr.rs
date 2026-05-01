1f376975a6a0431534886e4714b8e556f679a04e
Colin Nelson <colnnelson@google.com>
2026-04-30 22:13:17 -0700

    [ffx][addr] remove anyhow dependency
    Change-Id: I4a0e9bca5773006eaa8d1f3f3b1d239d6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1592334
    Commit-Queue: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Colin Nelson <colnnelson@google.com>
    Reviewed-by: Andrew Davies <awdavies@google.com>

--- src/developer/ffx/lib/addr/src/target_addr.rs
+++ src/developer/ffx/lib/addr/src/target_addr.rs
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Result;
 use fidl_fuchsia_developer_ffx::{
     TargetAddrInfo, TargetIp, TargetIpAddrInfo, TargetIpPort, TargetVSockCtx, TargetVSockNamespace,
 };
