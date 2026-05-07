e437c96ffe8afab9c00a9ef029de040699df58d8
Casey Dahlin <sadmac@google.com>
2026-05-07 16:45:38 -0700

    [ffx] Make ffx session restart support --machine json
    Fixed: 336823019
    Change-Id: Ibe4eb42a921ae2bcd822aa43accbeb9a6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1602070
    Reviewed-by: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/developer/ffx/plugins/session/restart/src/lib.rs
+++ src/developer/ffx/plugins/session/restart/src/lib.rs
@@ -6,8 +6,9 @@ use anyhow::{Result, format_err};
 use async_trait::async_trait;
 use fdomain_fuchsia_session::RestarterProxy;
 use ffx_session_restart_args::SessionRestartCommand;
-use ffx_writer::SimpleWriter;
+use ffx_writer::{MachineWriter, ToolIO};
 use fho::{FfxMain, FfxTool};
+use std::io::Write;
 use target_holders::fdomain::moniker;
 #[derive(FfxTool)]
 pub struct RestartTool {
@@ -21,20 +22,24 @@ fho::embedded_plugin!(RestartTool);
 
 #[async_trait(?Send)]
 impl FfxMain for RestartTool {
-    // TODO(b/472310565) Support actual "json" output, not just "raw"
-    type Writer = SimpleWriter;
+    type Writer = MachineWriter<()>;
     async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
         restart_impl(self.restarter_proxy, self.cmd, &mut writer).await?;
+        if writer.is_machine() {
+            writer.machine(&())?;
+        }
         Ok(())
     }
 }
 
-pub async fn restart_impl<W: std::io::Write>(
+pub async fn restart_impl(
     restarter_proxy: RestarterProxy,
     _cmd: SessionRestartCommand,
-    writer: &mut W,
+    writer: &mut MachineWriter<()>,
 ) -> Result<()> {
-    writeln!(writer, "Restarting the current session")?;
+    if !writer.is_machine() {
+        writeln!(writer, "Restarting the current session")?;
+    }
     restarter_proxy.restart().await?.map_err(|err| format_err!("{:?}", err))
 }
 
@@ -54,10 +59,33 @@ mod test {
         });
 
         let restart_cmd = SessionRestartCommand {};
-        let mut writer = Vec::new();
+        let test_buffers = ffx_writer::TestBuffers::default();
+        let mut writer = MachineWriter::new_test(None, &test_buffers);
         let result = restart_impl(proxy, restart_cmd, &mut writer).await;
         assert!(result.is_ok());
-        let output = String::from_utf8(writer).unwrap();
+        let output = test_buffers.into_stdout_str();
         assert_eq!(output, "Restarting the current session\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_machine_output_is_valid_json() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client.clone(), |req| match req {
+            RestarterRequest::Restart { responder } => {
+                let _ = responder.send(Ok(()));
+            }
+        });
+
+        let restart_cmd = SessionRestartCommand {};
+        let test_buffers = ffx_writer::TestBuffers::default();
+        let writer = MachineWriter::new_test(Some(ffx_writer::Format::Json), &test_buffers);
+
+        let tool = RestartTool { cmd: restart_cmd, restarter_proxy: proxy };
+
+        let result = tool.main(writer).await;
+        assert!(result.is_ok());
+
+        let output = test_buffers.into_stdout_str();
+        assert_eq!(output, "null\n");
     }
 }
