0ad14c1238ecd415e688e09f8b12f935746bde9f
Jacob Rutherford <jruthe@google.com>
2026-05-12 12:54:37 -0700

    [pydantic] Import pydantic and dependencies
    This makes the pydantic python module available to fuchsia.git.
    
    In particular it:
      - Updates pylibs manifest.
      - Adds BUILD.gn and OWNERS files to each new third_party repository
        produced in //third_party/pylibs.
      - Imports //prebuilt/third_party/pydantic-core-wheel as a prebuilt
        wheel file.
      - Extracts the wheel file with a jiri hook script to
        //prebuilt/third_party/pydantic-core, adds a BUILD.gn and wraps the
        __init__.py such that the DSO is found at runtime. Licensing and
        other metadata shipped as part of the wheel are also located in this
        directory.
      - Exempts the wheel file itself from license checking, the licensing
        metadata is kept alongside the extracted wheel in
        //prebuilt/third_party/pydantic-core, but not checked into fuchsia.git.
    
    Notes on pydantic_core:
    
    pydantic_core is a module extension written primarily in Rust which is
    vendored via a python wheel in CIPD. The wheel file is downloaded and
    extracted during typical jiri operations. A BUILD.gn file is produced to
    place the DSO and wrapped __init__.py in the build directory so python
    code can import the module without modifications.
    
    See the bug below for more details about why this approach was taken in
    stead of building pydantic_core from source.
    
    OSRB approval bug: b/509813567
    
    Bug: 511152625
    Change-Id: Ieff8dc397c3a9bcf0c5f20d5ec8ef4996a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1604830
    Reviewed-by: Adam Barth <abarth@google.com>
    Reviewed-by: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>
    Fuchsia-Auto-Submit: Jacob Rutherford <jruthe@google.com>
    Commit-Queue: Jacob Rutherford <jruthe@google.com>

---
+++ tools/build/scripts/extract_pydantic_core_wheel.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+# Copyright 2026 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# A Jiri hook used to extract the pydantic_core wheel file to
+# a hard-coded destination directory to make it directly usable
+# before the build.
+set -e
+
+# Where to find the source wheel file. Exact name is versioned and unknown here.
+SOURCE_SUBDIR="prebuilt/third_party/pydantic-core-wheel"
+
+# Where to extract the wheel file.
+DEST_SUBDIR="prebuilt/third_party/pydantic-core"
+
+function die {
+  echo >&2 "ERROR: $*"
+  exit 1
+}
+
+# Assume this script lives under //tools/build/scripts/
+_SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+FUCHSIA_DIR="$(readlink -f "${_SCRIPT_DIR}/../../..")"
+[[ -f "${FUCHSIA_DIR}/.jiri_manifest" ]] ||
+    die "Cannot locate proper FUCHSIA_DIR, got: ${FUCHSIA_DIR}"
+
+# The source and destination directories are hard-coded here.
+SOURCE_DIR="$FUCHSIA_DIR/$SOURCE_SUBDIR"
+echo "Using source directory: $SOURCE_DIR"
+
+# Locate the wheel file from the source directory.
+SRC_FILE=$(ls "$SOURCE_DIR"/*.whl 2>/dev/null)
+[[ -n "$SRC_FILE" ]] || die "No .whl found in source directory: ${SOURCE_DIR}"
+
+echo "Found wheel file: $SRC_FILE"
+
+# Extract version number from the wheel file name, as this
+# will be required for the README.fuchsia file.
+SRC_FILENAME=$(basename "${SRC_FILE}")
+SRC_VERSION=$(echo "${SRC_FILENAME}" | sed -E -e 's|pydantic_core-([0-9.]+)-.*|\1|')
+[[ -n "${SRC_VERSION}" ]] || die "Could not find version in filename: ${SRC_FILENAME}"
+echo "Found version: [$SRC_VERSION]"
+
+# A file used to verify that the extracted content is correct.
+CHECK_FILE=pydantic_core/__init__.py
+
+DEST_DIR="${FUCHSIA_DIR}/${DEST_SUBDIR}"
+echo "Using destination directory: ${DEST_DIR}"
+mkdir -p "${DEST_DIR}"
+rm -rf "${DEST_DIR:?}"/*
+
+echo "Extracting archive"
+unzip -o -q "${SRC_FILE}" -d "${DEST_DIR}" || die "Could not unzip ${SRC_FILE}"
+[[ -f "${DEST_DIR}/${CHECK_FILE}" ]] ||
+    die "Missing file from extracted archive: ${DEST_DIR}/${CHECK_FILE}"
+
+echo "Modifying __init__.py to support dynamic loading of shared library"
+INIT_FILE="${DEST_DIR}/${CHECK_FILE}"
+TEMP_INIT="${INIT_FILE}.tmp"
+
+cat > "${TEMP_INIT}" <<EOF
+from __future__ import annotations
+
+import sys
+from importlib.abc import Loader
+import importlib.util
+import importlib.machinery
+
+def _init():
+    finder = importlib.machinery.PathFinder()
+
+    # Relative path from root_build_dir.
+    shlib_dir = f'host_x64/gen/prebuilt/third_party/pydantic-core/pydantic_core'
+    search_path = [shlib_dir] + sys.path
+    spec = finder.find_spec('_pydantic_core', path=search_path)
+    if spec is None:
+        raise Exception(f'Could not find _pydantic_core in {search_path}')
+    mod = importlib.util.module_from_spec(spec)
+    mod.__name__ = 'pydantic_core._pydantic_core'
+    sys.modules['pydantic_core._pydantic_core'] = mod
+    assert isinstance(spec.loader, Loader)
+    spec.loader.exec_module(mod)
+
+_init()
+EOF
+
+# Append original content, skipping 'from __future__' lines.
+grep -v "^from __future__" "${INIT_FILE}" >> "${TEMP_INIT}"
+mv "${TEMP_INIT}" "${INIT_FILE}"
+
+echo "Generating BUILD.gn file"
+cat > "${DEST_DIR}/BUILD.gn" <<EOF
+# Copyright 2026 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import("//build/python/python_library.gni")
+
+# Copy the extracted files and the DSO to the target gen directory
+# so they can be used by python_library.
+copy("copy_pydantic_core_files") {
+  sources = [
+    "pydantic_core/__init__.py",
+    "pydantic_core/core_schema.py",
+    "pydantic_core/_pydantic_core.cpython-311-x86_64-linux-gnu.so",
+  ]
+  outputs = [
+    "\$target_gen_dir/pydantic_core/{{source_file_part}}",
+  ]
+}
+
+python_library("pydantic_core") {
+  enable_mypy = false
+  sources = [
+    "__init__.py",
+    "core_schema.py",
+  ]
+  source_root = "\$target_gen_dir/pydantic_core"
+
+  deps = [
+    ":copy_pydantic_core_files",
+    "//third_party/pylibs/typing_extensions",
+  ]
+}
+EOF
+
+echo "Writing README.fuchsia file"
+cat > "${DEST_DIR}/README.fuchsia" <<EOF
+Name: pydantic_core
+URL: https://chrome-infra-packages.appspot.com/p/infra/python/wheels/pydantic_core/linux-amd64_cp311_cp311
+
+License File: pydantic_core-${SRC_VERSION}.dist-info/licenses/LICENSE
+ -> License File Format: Single License
+ -> License Classifications: MIT
+
+Description:
+Prebuilt Pydantic Core library, which includes a native extension.
+This directory contains the extracted wheel file.
+The extraction script is in \$FUCHSIA_DIR/tools/build/scripts, and invoked
+as a Jiri hook.
+
+To update to a new version:
+
+1) Modify the "version" attribute of the "<package>" element called
+   "pydantic_core" in \$FUCHSIA_DIR/manifests/prebuilts
+
+2) Run: \$FUCHSIA_DIR/manifests/update-lockfiles.sh
+   This verifies that the version exists and updates critical Jiri files.
+
+3) Run: jiri update -local-manifest-project=fuchsia
+   This installs the new wheel file to \$FUCHSIA_DIR/${SOURCE_SUBDIR}
+   then runs a hook to update \$FUCHSIA_DIR/${DEST_SUBDIR}
+
+4) Verify that everything works
+
+5) Update new CL to Gerrit with your modifications.
+EOF
+
+echo "Done"
+exit 0
