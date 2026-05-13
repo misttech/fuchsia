cb6f5b7ed1e27acc981cac4be4d144a3ba2b8dcd
Braden Kell <bradenkell@google.com>
2026-05-13 10:42:09 -0700

    [spi][dw] Add initial Rust version of dw-spi
    This is sufficient for dw-spi to bind and for SPI core to bind to it,
    but does nothing else. Functionality will be added until it matches the
    C++ version, at which point the C++ version can be removed.
    
    Bug: 512102116
    Test: CQ
    Change-Id: I24ad6a5e74898a2c806adfe392ad10ca5c99aa18
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1609008
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Fuchsia-Auto-Submit: Braden Kell <bradenkell@google.com>
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Reviewed-by: David Koloski <dkoloski@google.com>
    Commit-Queue: Braden Kell <bradenkell@google.com>

---
+++ src/devices/spi/drivers/dw-spi/src/lib.rs
@@ -0,0 +1,180 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use fdf_component::{Driver, DriverContext, Node, NodeBuilder, ServiceOffer, driver_register};
+use fdf_metadata::MetadataServer;
+use fidl::Serializable;
+use fidl_fuchsia_hardware_platform_device as fpdev;
+use fidl_fuchsia_hardware_spi_businfo as fspi_businfo;
+use fidl_next::{Request, Responder, ServerEnd};
+use fidl_next_fuchsia_hardware_spiimpl::{self, spi_impl as fspi_impl};
+use fuchsia_async as fasync;
+use fuchsia_component::server::ServiceFs;
+use futures::StreamExt;
+use log::{error, info};
+use zx::Status;
+
+struct DwSpiDriver {
+    _node: Node,
+    _scope: fasync::Scope,
+    _businfo_server: MetadataServer,
+}
+
+driver_register!(DwSpiDriver);
+
+struct SpiImplServer {}
+
+impl fidl_next_fuchsia_hardware_spiimpl::SpiImplServerHandler for SpiImplServer {
+    async fn get_chip_select_count(&mut self, responder: Responder<fspi_impl::GetChipSelectCount>) {
+        let _ = responder.respond(1).await;
+    }
+
+    async fn transmit_vector(
+        &mut self,
+        _request: Request<fspi_impl::TransmitVector>,
+        responder: Responder<fspi_impl::TransmitVector>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn receive_vector(
+        &mut self,
+        _request: Request<fspi_impl::ReceiveVector>,
+        responder: Responder<fspi_impl::ReceiveVector>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn exchange_vector(
+        &mut self,
+        _request: Request<fspi_impl::ExchangeVector>,
+        responder: Responder<fspi_impl::ExchangeVector>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn lock_bus(
+        &mut self,
+        _request: Request<fspi_impl::LockBus>,
+        responder: Responder<fspi_impl::LockBus>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn unlock_bus(
+        &mut self,
+        _request: Request<fspi_impl::UnlockBus>,
+        responder: Responder<fspi_impl::UnlockBus>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn register_vmo(
+        &mut self,
+        _request: Request<fspi_impl::RegisterVmo>,
+        responder: Responder<fspi_impl::RegisterVmo>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn unregister_vmo(
+        &mut self,
+        _request: Request<fspi_impl::UnregisterVmo>,
+        responder: Responder<fspi_impl::UnregisterVmo>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn release_registered_vmos(
+        &mut self,
+        _request: Request<fspi_impl::ReleaseRegisteredVmos>,
+    ) {
+    }
+
+    async fn transmit_vmo(
+        &mut self,
+        _request: Request<fspi_impl::TransmitVmo>,
+        responder: Responder<fspi_impl::TransmitVmo>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn receive_vmo(
+        &mut self,
+        _request: Request<fspi_impl::ReceiveVmo>,
+        responder: Responder<fspi_impl::ReceiveVmo>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+
+    async fn exchange_vmo(
+        &mut self,
+        _request: Request<fspi_impl::ExchangeVmo>,
+        responder: Responder<fspi_impl::ExchangeVmo>,
+    ) {
+        let _ = responder.respond_err(Status::NOT_SUPPORTED.into_raw()).await;
+    }
+}
+
+struct SpiImplService {
+    scope: fasync::ScopeHandle,
+}
+
+impl fidl_next_fuchsia_hardware_spiimpl::ServiceHandler for SpiImplService {
+    fn device(&self, server_end: ServerEnd<fidl_next_fuchsia_hardware_spiimpl::SpiImpl>) {
+        server_end.spawn_on(SpiImplServer {}, &self.scope);
+    }
+}
+
+impl Driver for DwSpiDriver {
+    const NAME: &str = "dw-spi";
+
+    async fn start(mut context: DriverContext) -> Result<Self, Status> {
+        info!("Starting dw-spi driver");
+
+        let pdev: fpdev::DeviceProxy = context
+            .incoming
+            .service_marker(fpdev::ServiceMarker)
+            .connect()?
+            .connect_to_device()
+            .map_err(|err| {
+                error!("Failed to connect to platform device: {err}");
+                Status::INTERNAL
+            })?;
+
+        let businfo_server = MetadataServer::new(fspi_businfo::SpiBusMetadata::SERIALIZABLE_NAME)
+            .forward_from_pdev(&pdev)
+            .await
+            .inspect_err(|err| error!("Failed to forward SPI bus metadata: {err}"))?;
+
+        let mut outgoing = ServiceFs::new();
+
+        let scope = fasync::Scope::new_with_name(Self::NAME);
+
+        let offer = ServiceOffer::<fidl_next_fuchsia_hardware_spiimpl::Service>::new_next()
+            .add_default_named_next(
+                &mut outgoing,
+                "default",
+                SpiImplService { scope: scope.to_handle() },
+            )
+            .build_driver_offer();
+
+        let mut node_args = NodeBuilder::new(Self::NAME).add_offer(offer);
+        if let Some(offer) = businfo_server.serve(&mut outgoing, scope.to_handle(), "default") {
+            node_args = node_args.add_offer(offer);
+        }
+
+        let node = context.take_node()?;
+        node.add_child(node_args.build()).await?;
+
+        context.serve_outgoing(&mut outgoing)?;
+        scope.spawn(outgoing.collect());
+
+        info!("dw-spi driver initialized successfully");
+
+        Ok(Self { _node: node, _scope: scope, _businfo_server: businfo_server })
+    }
+
+    async fn stop(&self) {}
+}
