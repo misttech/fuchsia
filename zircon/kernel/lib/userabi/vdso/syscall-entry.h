0593b77f539a8235b7648c65acdf0f96a4a42277
Roland McGrath <mcgrathr@google.com>
2026-05-20 16:33:57 -0700

    [kernel][vdso] Modernize assembly code
    This moves the old assembly code in the vDSO to using the
    <lib/arch/asm.h> macros.  As well as modernizing the style, this
    removes some unnecessary duplication with now-formalized macros.
    The old code had manual repetitions that missed some directives
    and left symbol table details inconsistent in a few places.
    
    The new use of the canonical .function macro puts each assembly
    function into its own section, making them consistent with
    compiled functions.  This required some updates to the keepalive
    symbol lists that omitted some things unnoticed because they were
    not independently GC'able (but now are).
    
    Change-Id: I2fa76c1637ec9649d673be6cb185aec24248c604
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1619115
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/lib/userabi/vdso/syscall-entry.h
+++ zircon/kernel/lib/userabi/vdso/syscall-entry.h
@@ -4,43 +4,31 @@
 
 #pragma once
 
+#include <lib/arch/asm.h>
+
+#ifdef __ASSEMBLER__
+// clang-format off
+
 .macro syscall_entry_begin name
-.globl SYSCALL_\name
-.hidden SYSCALL_\name
-.type SYSCALL_\name,STT_FUNC
-SYSCALL_\name:
-.cfi_startproc
+  .function SYSCALL_\name, global
 .endm
 
-.macro syscall_entry_end name public=1
-.cfi_endproc
-.size SYSCALL_\name, . - SYSCALL_\name
+.macro syscall_entry_end name, public=1
+  .end_function
 
-// Create a hidden alias for the syscall which is prefixed with CODE_.  This
-// allows the macros which perform redirection in the kernel to redirect a VDSO
-// entry to either an explicit CODE_ alternate, or to another syscall if needed.
-.globl CODE_SYSCALL_\name
-.hidden CODE_SYSCALL_\name
-CODE_SYSCALL_\name = SYSCALL_\name
-.size CODE_SYSCALL_\name, . - SYSCALL_\name
+  // Create a hidden alias for the syscall which is prefixed with CODE_.  This
+  // allows the macros which perform redirection in the kernel to redirect a
+  // VDSO entry to either an explicit CODE_ alternate, or to another syscall if
+  // needed.
+  .alias CODE_SYSCALL_\name, SYSCALL_\name
 
-// For wrapper functions, aliasing is handled by the generator.
-.if \public
-.globl _\name
-.type _\name,STT_FUNC
-_\name = SYSCALL_\name
-.size _\name, . - SYSCALL_\name
-
-.weak \name
-.type \name,STT_FUNC
-\name = SYSCALL_\name
-.size \name, . - SYSCALL_\name
-
-.globl VDSO_\name
-.hidden VDSO_\name
-.type VDSO_\name,STT_FUNC
-VDSO_\name = SYSCALL_\name
-.size VDSO_\name, . - SYSCALL_\name
-.endif
-
+  // For wrapper functions, aliasing is handled by the generator.
+  .if \public
+    .alias _\name, SYSCALL_\name, export
+    .alias \name, SYSCALL_\name, weak
+    .alias VDSO_\name, SYSCALL_\name
+  .endif
 .endm
+
+// clang-format on
+#endif  // __ASSEMBLER__
