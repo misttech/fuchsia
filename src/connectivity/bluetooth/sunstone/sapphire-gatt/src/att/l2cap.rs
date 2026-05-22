c9c8c8e24225332bc975cb617d9f0b2ca3243722
Adam Snaider <asnaider@google.com>
2026-05-22 08:47:47 -0700

    [bt][sunstone-att] Define the L2CAP trait for ATT
    For now, this is a private trait for ATT to use, however
    we may decide to expand upon it later on and make it a
    more widely used interface
    
    Change-Id: Icc8f9ac1c533eda584310aa6e3029b326a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1622554
    Commit-Queue: Jason Yi <jsonyi@google.com>
    Reviewed-by: Jason Graffius <jgraff@google.com>

---
+++ src/connectivity/bluetooth/sunstone/sapphire-gatt/src/att/l2cap.rs
@@ -0,0 +1,69 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(jsonyi): Remove once it's being used in ATT
+#![allow(unused)]
+
+pub mod cid;
+
+use core::mem::MaybeUninit;
+
+pub use cid::{Cid, FixedCid};
+
+#[non_exhaustive]
+pub enum L2CapEstablishChannelError {
+    /// The requested channel already has an owner
+    AlreadyInUse,
+    /// Underlying logical link was closed
+    LinkClosed,
+}
+
+#[non_exhaustive]
+pub enum L2CapSendError {
+    /// Underlying logical link was closed
+    LinkClosed,
+    /// The outgoing SDU exceeds the SDU size limit
+    SduTooLarge,
+}
+
+#[non_exhaustive]
+pub enum L2CapRecvError {
+    /// Underlying logical link was closed
+    LinkClosed,
+    /// The incoming SDU won't fit in the provided buffer
+    BufferTooSmall,
+}
+
+/// Represents an L2CAP logical link to a peer
+pub trait L2CapLogicalLink {
+    type Tx: L2CapChannelTx;
+    type Rx: L2CapChannelRx;
+
+    /// Establishes a connection to the given channel
+    async fn claim_fixed_channel(
+        &mut self,
+        channel: FixedCid,
+    ) -> Result<L2CapChannel<Self::Tx, Self::Rx>, L2CapEstablishChannelError>;
+}
+
+/// Sender and receiver ends to to an L2CAP channel
+pub struct L2CapChannel<Tx, Rx> {
+    pub sender: Tx,
+    pub receiver: Rx,
+}
+
+/// Sender end to an L2CAP channel
+pub trait L2CapChannelTx {
+    /// Sends the given SDU to the channel
+    async fn send(&mut self, sdu: &[u8]) -> Result<(), L2CapSendError>;
+}
+
+/// Receiver end to an L2CAP channel
+pub trait L2CapChannelRx {
+    /// Receives the next SDU sent by the peer
+    async fn recv<'a>(
+        &mut self,
+        buffer: &'a mut [MaybeUninit<u8>],
+    ) -> Result<&'a mut [u8], L2CapRecvError>;
+}
---
+++ src/connectivity/bluetooth/sunstone/sapphire-gatt/src/att/l2cap/cid.rs
@@ -0,0 +1,38 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use core::num::NonZero;
+
+/// L2CAP Channel identifier
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Cid(NonZero<u16>);
+
+impl Cid {
+    /// Constructs a CID from the raw value.
+    ///
+    /// If the provided `cid` is 0, then this returns None
+    pub const fn new(cid: u16) -> Option<Self> {
+        match NonZero::new(cid) {
+            Some(cid) => Some(Self(cid)),
+            None => None,
+        }
+    }
+}
+
+/// Represents a fixed CID as defined by the Bluetooth Core specification.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct FixedCid(Cid);
+
+impl FixedCid {
+    /// Returns the underlying CID
+    pub const fn cid(&self) -> Cid {
+        self.0
+    }
+}
+
+/// Fixed CIDs as specified in Core Spec v6.0, Vol 3, Part A, Table 2.[1, 2, 3]
+impl FixedCid {
+    /// CID for the LE Attribute Protocol channel
+    pub const ATTRIBUTE_PROTOCOL: Self = Self(Cid::new(0x0004).expect("Fixed value is non-zero"));
+}
