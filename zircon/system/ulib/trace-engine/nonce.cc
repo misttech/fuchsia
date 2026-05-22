9db82e214f4c9dd8c17d32d7b62ee63b75e43bb5
Gwen Mittertreiner <gmtr@google.com>
2026-05-21 18:31:48 -0700

    [trace-engine] Align C++ trace ID generation with Rust
    Perfetto expects for trace ids to be globally unique, not unique per
    process as flows can travel from one process to another.
    
    Update trace_generate_nonce() in nonce.cc to combine the current boot
    timestamp in the upper 48 bits with CPRNG random 16-bit value in the
    lower 16 bits.
    
    This matches what the rust library currently does to create a random id
    and helps prevent collisions templorally close to each other making it
    unlikely that the two flows with the a colliding id are active at the
    same time.
    
    Bug: 42054669
    Test: fx test trace_manager_tests trace-engine-test-pkg provider-test-pkg
    Change-Id: I067c77c47472ad76f3bac0ae963535bff77b1071
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1613057
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Fuchsia-Auto-Submit: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Gwen Mittertreiner <gmtr@google.com>

--- zircon/system/ulib/trace-engine/nonce.cc
+++ zircon/system/ulib/trace-engine/nonce.cc
@@ -4,25 +4,23 @@
 
 #include <lib/trace-engine/instrumentation.h>
 #include <lib/zx/clock.h>
-
-#include <atomic>
-
-namespace {
-
-std::atomic_uint64_t g_nonce{1u};
-
-}  // namespace
+#include <zircon/compiler.h>
+#include <zircon/syscalls.h>
 
 __EXPORT uint64_t trace_generate_nonce() {
-  return g_nonce.fetch_add(1u, std::memory_order_relaxed);
+  uint64_t time = zx::clock::get_boot().get();
+  uint64_t high_order = time << 16;
+  uint16_t random_val;
+  zx_cprng_draw(&random_val, sizeof(random_val));
+  uint64_t nonce = high_order | random_val;
+  if (unlikely(nonce == 0)) {
+    return 1;
+  }
+  return nonce;
 }
 
 #if FUCHSIA_API_LEVEL_AT_LEAST(28)
 __EXPORT uint64_t trace_time_based_id(zx_koid_t thread_id) {
-  uint64_t time = zx::clock::get_boot().get();
-  uint64_t ts = time << 16;
-  uint64_t tid = (thread_id & ((1ULL << 9) - 1)) << 8;          // only keep u8
-  uint64_t nonce = trace_generate_nonce() & ((1ULL << 9) - 1);  // only keep u8
-  return ts | tid | nonce;
+  return (trace_generate_nonce() & ~0xFF00) | ((thread_id & 0xFF) << 8);
 }
 #endif
