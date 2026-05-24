276a1a8eb1f71b90aa08bfccdfabbbfe5f27f175
Roland McGrath <mcgrathr@google.com>
2026-05-23 21:57:08 -0700

    [kernel] Test Rust<->C++ global variables accesses
    Add some more simple Rust test cases to look for issues in
    code generation producing GOT indirection when it should not.
    Update the Rust compilation flags to make them work.
    
    Bug: 511265181
    Fixed: 510905115
    Cq-Include-Trybots: luci.turquoise.global.try:run-postsubmit-tryjobs
    Change-Id: I88772d9af45596a32cc7a26a6ad67374cbf8f354
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1603728
    Reviewed-by: Paul Kirth <paulkirth@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Roland McGrath <mcgrathr@google.com>

--- zircon/kernel/tests/rust/rust-tests.cc
+++ zircon/kernel/tests/rust/rust-tests.cc
@@ -11,6 +11,16 @@ extern "C" {  // Defined in Rust (src/lib.rs).
 
 int32_t add_one_in_rust(int32_t);
 
+extern const int32_t kConstVarDefinedInRust;
+
+extern int32_t gVarDefinedInRust;
+
+extern const int32_t kConstVarExportedToRust = 23;
+int32_t get_const_var_exported_to_rust();
+
+int32_t gVarExportedToRust;
+int32_t fetch_add_var_exported_to_rust(int32_t);
+
 }  // extern "C"
 
 namespace {
@@ -21,8 +31,38 @@ bool add_one_test() {
   END_TEST;
 }
 
+bool defined_const_test() {
+  BEGIN_TEST;
+  EXPECT_EQ(kConstVarDefinedInRust, 17);
+  END_TEST;
+}
+
+bool exported_const_test() {
+  BEGIN_TEST;
+  EXPECT_EQ(get_const_var_exported_to_rust(), 23);
+  END_TEST;
+}
+
+bool defined_var_test() {
+  BEGIN_TEST;
+  EXPECT_EQ(gVarDefinedInRust, 42);
+  END_TEST;
+}
+
+bool exported_var_test() {
+  BEGIN_TEST;
+  gVarExportedToRust = 17;
+  EXPECT_EQ(fetch_add_var_exported_to_rust(23), 17);
+  EXPECT_EQ(gVarExportedToRust, 40);
+  END_TEST;
+}
+
 UNITTEST_START_TESTCASE(rust_tests)
 UNITTEST("test a trivial Rust function called from C++", add_one_test)
+UNITTEST("test a Rust-defined global variable read from C++", defined_const_test)
+UNITTEST("test a C++-defined global variable read from Rust", exported_const_test)
+UNITTEST("test a Rust-defined global variable written from C++", defined_var_test)
+UNITTEST("test a C++-defined global variable written from Rust", exported_var_test)
 UNITTEST_END_TESTCASE(rust_tests, "rust", "Tests for Rust")
 
 }  // namespace
--- zircon/kernel/tests/rust/src/lib.rs
+++ zircon/kernel/tests/rust/src/lib.rs
@@ -7,8 +7,33 @@
 use test_macro::plus_one;
 
 #[unsafe(no_mangle)]
+pub static kConstVarDefinedInRust: i32 = 17;
+
+#[unsafe(no_mangle)]
+pub static mut gVarDefinedInRust: i32 = 42;
+
+unsafe extern "C" {
+    static kConstVarExportedToRust: i32;
+    static mut gVarExportedToRust: i32;
+} // extern "C"
+
+#[unsafe(no_mangle)]
 pub extern "C" fn add_one_in_rust(x: i32) -> i32 {
     plus_one!(x)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn get_const_var_exported_to_rust() -> i32 {
+    unsafe { kConstVarExportedToRust }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn fetch_add_var_exported_to_rust(x: i32) -> i32 {
+    unsafe {
+        let old = gVarExportedToRust;
+        gVarExportedToRust += x;
+        old
+    }
 }
 
 use core::panic::PanicInfo;
