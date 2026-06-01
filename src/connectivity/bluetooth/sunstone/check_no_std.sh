9a0b519206a7008f98a84c2cd137da627b8968ab
Jason Yi <jsonyi@google.com>
2026-06-01 12:03:06 -0700

    [bluetooth][sunstone-att] Fix check_no_std target
    Fix: 518817102
    
    Change-Id: I56a56366262745b0c674c76077eec6bf6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1634594
    Commit-Queue: Jason Yi <jsonyi@google.com>
    Reviewed-by: Adam Snaider <asnaider@google.com>

--- src/connectivity/bluetooth/sunstone/check_no_std.sh
+++ src/connectivity/bluetooth/sunstone/check_no_std.sh
@@ -2,4 +2,5 @@
 # Copyright 2026 The Fuchsia Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-exec cargo check --target thumbv7em-none-eabi
+exec cargo check --target thumbv7em-none-eabihf
+
