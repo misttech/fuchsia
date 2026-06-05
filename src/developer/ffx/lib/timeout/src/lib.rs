9f001917e9c5ae8b6ff8b77ff47fc310b16b006e
Colin Nelson <colnnelson@google.com>
2026-06-05 11:04:46 -0700

    [ffx][timeout] include duration in timeout error
    Change-Id: Ie2470873a2b5ca58519fc8686279efaf6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1640524
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Colin Nelson <colnnelson@google.com>
    Fuchsia-Auto-Submit: Colin Nelson <colnnelson@google.com>

--- src/developer/ffx/lib/timeout/src/lib.rs
+++ src/developer/ffx/lib/timeout/src/lib.rs
@@ -9,14 +9,16 @@ use std::time::Duration;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq)]
-#[error("Timed out")]
-pub struct TimeoutError();
+#[error("Timed out after: {duration:?}")]
+pub struct TimeoutError {
+    duration: Duration,
+}
 
 pub async fn timeout<F, T>(dur: Duration, f: F) -> Result<T, TimeoutError>
 where
     F: Future<Output = T>,
 {
-    f.map(|o| Ok(o)).or(Timer::new(dur).map(|_| Err(TimeoutError {}))).await
+    f.map(|o| Ok(o)).or(Timer::new(dur).map(|_| Err(TimeoutError { duration: dur }))).await
 }
 
 #[cfg(test)]
@@ -32,7 +34,7 @@ mod test {
         assert_eq!(
             timeout(Duration::from_millis(1), async { Timer::new(Duration::from_secs(1)).await })
                 .await,
-            Err(TimeoutError {})
+            Err(TimeoutError { duration: Duration::from_millis(1) })
         );
     }
 }
