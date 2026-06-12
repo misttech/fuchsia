44774686ff3d26fe8fb42716d4a7a0ba62b0beee
Wez <wez@google.com>
2026-06-12 02:26:02 -0700

    [sestarnix] Add explicit tests for sendfile and splice permission checks
    Test: sestarnix_userspace_tests
    Test: run_on_linux.py (host-side QEMU)
    
    TAG=agy
    CONV=57245017-e475-40ce-9eb1-6ff2071f434d
    
    Change-Id: I71afb9cc1e7590c90b751d1c93e51de3ee370e19
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1648422
    Commit-Queue: Wez <wez@google.com>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Reviewed-by: Marina Ciocea <marinaciocea@google.com>
    Fuchsia-Auto-Submit: Wez <wez@google.com>

--- src/starnix/tests/selinux/userspace/tests/fs_test.cc
+++ src/starnix/tests/selinux/userspace/tests/fs_test.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
+#include <sys/sendfile.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -78,5 +79,118 @@ TEST(FsTest, FallocateDenied) {
     EXPECT_THAT(fallocate(test_file.fd(), 0, 0, 1024), SyscallFailsWithErrno(EACCES));
   }));
 }
+
+constexpr char kSendfileSrcLabel[] = "test_u:object_r:test_fs_sendfile_src_t:s0";
+constexpr char kSendfileDstLabel[] = "test_u:object_r:test_fs_sendfile_dst_t:s0";
+constexpr char kPayload[] = "foo";
+constexpr size_t kPayloadSize = sizeof(kPayload) - 1;
+
+struct FsSecurityTestParams {
+  const char* test_domain;
+  bool src_readable;
+  bool dst_writable;
+};
+
+class FsSecurityTest : public ::testing::TestWithParam<FsSecurityTestParams> {};
+
+// Verify that sendfile enforces SELinux read/write permissions.
+TEST_P(FsSecurityTest, Sendfile) {
+  const auto& params = GetParam();
+  auto src_file = ScopedTempFDWithLabel(kSendfileSrcLabel);
+  auto dst_file = ScopedTempFDWithLabel(kSendfileDstLabel);
+  ASSERT_TRUE(src_file.is_valid());
+  ASSERT_TRUE(dst_file.is_valid());
+
+  // Write some data to the src file.
+  ASSERT_THAT(write(src_file.fd(), kPayload, kPayloadSize), SyscallSucceedsWithValue(kPayloadSize));
+  ASSERT_THAT(lseek(src_file.fd(), 0, SEEK_SET), SyscallSucceedsWithValue(0));
+
+  auto enforcing = ScopedEnforcement::SetEnforcing();
+  std::string context = MakeTestSecurityContext(params.test_domain);
+
+  EXPECT_TRUE(RunSubprocessAs(context, [&]() {
+    if (params.src_readable && params.dst_writable) {
+      EXPECT_THAT(sendfile(dst_file.fd(), src_file.fd(), nullptr, kPayloadSize), SyscallSucceeds());
+    } else {
+      EXPECT_THAT(sendfile(dst_file.fd(), src_file.fd(), nullptr, kPayloadSize),
+                  SyscallFailsWithErrno(EACCES));
+    }
+  }));
+}
+
+// Verify that splice from a src file to a pipe enforces SELinux read permissions.
+TEST_P(FsSecurityTest, SpliceFromSrc) {
+  const auto& params = GetParam();
+  auto src_file = ScopedTempFDWithLabel(kSendfileSrcLabel);
+  ASSERT_TRUE(src_file.is_valid());
+  ASSERT_THAT(write(src_file.fd(), kPayload, kPayloadSize), SyscallSucceedsWithValue(kPayloadSize));
+  ASSERT_THAT(lseek(src_file.fd(), 0, SEEK_SET), SyscallSucceedsWithValue(0));
+
+  int pipe_fds[2];
+  ASSERT_THAT(pipe(pipe_fds), SyscallSucceeds());
+  fbl::unique_fd pipe_in(pipe_fds[0]);
+  fbl::unique_fd pipe_out(pipe_fds[1]);
+
+  auto enforcing = ScopedEnforcement::SetEnforcing();
+  std::string context = MakeTestSecurityContext(params.test_domain);
+
+  EXPECT_TRUE(RunSubprocessAs(context, [&]() {
+    if (params.src_readable) {
+      EXPECT_THAT(splice(src_file.fd(), nullptr, pipe_out.get(), nullptr, kPayloadSize, 0),
+                  SyscallSucceeds());
+    } else {
+      EXPECT_THAT(splice(src_file.fd(), nullptr, pipe_out.get(), nullptr, kPayloadSize, 0),
+                  SyscallFailsWithErrno(EACCES));
+    }
+  }));
+}
+
+// Verify that splice from a pipe to a dst file enforces SELinux write permissions.
+TEST_P(FsSecurityTest, SpliceToDst) {
+  const auto& params = GetParam();
+  auto dst_file = ScopedTempFDWithLabel(kSendfileDstLabel);
+  ASSERT_TRUE(dst_file.is_valid());
+
+  int pipe_fds[2];
+  ASSERT_THAT(pipe(pipe_fds), SyscallSucceeds());
+  fbl::unique_fd pipe_in(pipe_fds[0]);
+  fbl::unique_fd pipe_out(pipe_fds[1]);
+  ASSERT_THAT(write(pipe_out.get(), kPayload, kPayloadSize),
+              SyscallSucceedsWithValue(kPayloadSize));
+
+  auto enforcing = ScopedEnforcement::SetEnforcing();
+  std::string context = MakeTestSecurityContext(params.test_domain);
+
+  EXPECT_TRUE(RunSubprocessAs(context, [&]() {
+    if (params.dst_writable) {
+      EXPECT_THAT(splice(pipe_in.get(), nullptr, dst_file.fd(), nullptr, kPayloadSize, 0),
+                  SyscallSucceeds());
+    } else {
+      EXPECT_THAT(splice(pipe_in.get(), nullptr, dst_file.fd(), nullptr, kPayloadSize, 0),
+                  SyscallFailsWithErrno(EACCES));
+    }
+  }));
+}
+
+INSTANTIATE_TEST_SUITE_P(FsSecurity, FsSecurityTest,
+                         ::testing::Values(
+                             FsSecurityTestParams{
+                                 .test_domain = "test_fs_t",
+                                 .src_readable = true,
+                                 .dst_writable = true,
+                             },
+                             FsSecurityTestParams{
+                                 .test_domain = "test_fs_no_read_t",
+                                 .src_readable = false,
+                                 .dst_writable = true,
+                             },
+                             FsSecurityTestParams{
+                                 .test_domain = "test_fs_no_write_t",
+                                 .src_readable = true,
+                                 .dst_writable = false,
+                             }),
+                         [](const ::testing::TestParamInfo<FsSecurityTest::ParamType>& info) {
+                           return info.param.test_domain;
+                         });
 
 }  // namespace
