43ed370365b15d02d20ecac0b20eed502a0c2817
Oliver Newman <olivernewman@google.com>
2026-06-17 16:22:16 -0700

    [fint] Delete built_archives field
    It's no longer used by recipes now that we don't produce or use build
    archives anymore.
    
    Depends-on: Ied4404061eb463e455b60396ba5acf5d1df7bb0e
    Change-Id: I8e50027074034544143d90e00148add1dafab953
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1662908
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
    Dependencies-Satisfied: cl-deps-checker@fuchsia-internal-service-accts.iam.gserviceaccount.com <cl-deps-checker@fuchsia-internal-service-accts.iam.gserviceaccount.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Ina Huh <ihuh@google.com>

--- tools/integration/fint/proto/build_artifacts_pb2.py
+++ tools/integration/fint/proto/build_artifacts_pb2.py
@@ -31,7 +31,7 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x62uild_artifacts.proto\x12\x04\x66int\x1a\x1cgoogle/protobuf/struct.proto\"\xc0\x01\n\x12NinjaActionMetrics\x12\x17\n\x0finitial_actions\x18\x01 \x01(\x05\x12\x15\n\rfinal_actions\x18\x02 \x01(\x05\x12\x44\n\x0f\x61\x63tions_by_type\x18\x03 \x03(\x0b\x32+.fint.NinjaActionMetrics.ActionsByTypeEntry\x1a\x34\n\x12\x41\x63tionsByTypeEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x05:\x02\x38\x01\".\n\tDebugFile\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x13\n\x0bupload_dest\x18\x02 \x01(\t\"\xe2\x04\n\x0e\x42uildArtifacts\x12\x17\n\x0f\x66\x61ilure_summary\x18\x01 \x01(\t\x12-\n\x0c\x62uilt_images\x18\x02 \x03(\x0b\x32\x17.google.protobuf.Struct\x12/\n\x0e\x62uilt_archives\x18\x03 \x03(\x0b\x32\x17.google.protobuf.Struct\x12\x1d\n\x15ninjatrace_json_files\x18\x04 \x03(\t\x12\x1d\n\x15\x62uildstats_json_files\x18\x05 \x03(\t\x12\x1e\n\x16ninja_duration_seconds\x18\x06 \x01(\x05\x12\x36\n\x14ninja_action_metrics\x18\x07 \x01(\x0b\x32\x18.fint.NinjaActionMetrics\x12\x35\n\tlog_files\x18\x08 \x03(\x0b\x32\".fint.BuildArtifacts.LogFilesEntry\x12$\n\x0b\x64\x65\x62ug_files\x18\t \x03(\x0b\x32\x0f.fint.DebugFile\x12\x1a\n\x12\x62uild_not_affected\x18\n \x01(\x08\x12\x16\n\x0e\x61\x66\x66\x65\x63ted_tests\x18\x0b \x03(\t\x12\x46\n\x12non_fatal_failures\x18\x0c \x03(\x0b\x32*.fint.BuildArtifacts.NonFatalFailuresEntry\x1a/\n\rLogFilesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a\x37\n\x15NonFatalFailuresEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x35Z3go.fuchsia.dev/fuchsia/tools/integration/fint/protob\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x62uild_artifacts.proto\x12\x04\x66int\x1a\x1cgoogle/protobuf/struct.proto\"\xc0\x01\n\x12NinjaActionMetrics\x12\x17\n\x0finitial_actions\x18\x01 \x01(\x05\x12\x15\n\rfinal_actions\x18\x02 \x01(\x05\x12\x44\n\x0f\x61\x63tions_by_type\x18\x03 \x03(\x0b\x32+.fint.NinjaActionMetrics.ActionsByTypeEntry\x1a\x34\n\x12\x41\x63tionsByTypeEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x05:\x02\x38\x01\".\n\tDebugFile\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x13\n\x0bupload_dest\x18\x02 \x01(\t\"\xb1\x04\n\x0e\x42uildArtifacts\x12\x17\n\x0f\x66\x61ilure_summary\x18\x01 \x01(\t\x12-\n\x0c\x62uilt_images\x18\x02 \x03(\x0b\x32\x17.google.protobuf.Struct\x12\x1d\n\x15ninjatrace_json_files\x18\x03 \x03(\t\x12\x1d\n\x15\x62uildstats_json_files\x18\x04 \x03(\t\x12\x1e\n\x16ninja_duration_seconds\x18\x05 \x01(\x05\x12\x36\n\x14ninja_action_metrics\x18\x06 \x01(\x0b\x32\x18.fint.NinjaActionMetrics\x12\x35\n\tlog_files\x18\x07 \x03(\x0b\x32\".fint.BuildArtifacts.LogFilesEntry\x12$\n\x0b\x64\x65\x62ug_files\x18\x08 \x03(\x0b\x32\x0f.fint.DebugFile\x12\x1a\n\x12\x62uild_not_affected\x18\t \x01(\x08\x12\x16\n\x0e\x61\x66\x66\x65\x63ted_tests\x18\n \x03(\t\x12\x46\n\x12non_fatal_failures\x18\x0b \x03(\x0b\x32*.fint.BuildArtifacts.NonFatalFailuresEntry\x1a/\n\rLogFilesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a\x37\n\x15NonFatalFailuresEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x35Z3go.fuchsia.dev/fuchsia/tools/integration/fint/protob\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -52,9 +52,9 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_DEBUGFILE']._serialized_start=256
   _globals['_DEBUGFILE']._serialized_end=302
   _globals['_BUILDARTIFACTS']._serialized_start=305
-  _globals['_BUILDARTIFACTS']._serialized_end=915
-  _globals['_BUILDARTIFACTS_LOGFILESENTRY']._serialized_start=811
-  _globals['_BUILDARTIFACTS_LOGFILESENTRY']._serialized_end=858
-  _globals['_BUILDARTIFACTS_NONFATALFAILURESENTRY']._serialized_start=860
-  _globals['_BUILDARTIFACTS_NONFATALFAILURESENTRY']._serialized_end=915
+  _globals['_BUILDARTIFACTS']._serialized_end=866
+  _globals['_BUILDARTIFACTS_LOGFILESENTRY']._serialized_start=762
+  _globals['_BUILDARTIFACTS_LOGFILESENTRY']._serialized_end=809
+  _globals['_BUILDARTIFACTS_NONFATALFAILURESENTRY']._serialized_start=811
+  _globals['_BUILDARTIFACTS_NONFATALFAILURESENTRY']._serialized_end=866
 # @@protoc_insertion_point(module_scope)
