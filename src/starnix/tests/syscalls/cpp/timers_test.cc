c2fd06d8bc7fc96980288038a184de41a49e65a6
Wez <wez@google.com>
2026-06-23 13:45:54 -0700

    [starnix] Cleanup implementation of timer API tests
    Change-Id: Iac3cc83478985bafca0a26268ae18840f64a10d9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1674795
    Fuchsia-Auto-Submit: Wez <wez@google.com>
    Commit-Queue: Wez <wez@google.com>
    Reviewed-by: Kevin Lindkvist <lindkvist@google.com>

--- src/starnix/tests/syscalls/cpp/timers_test.cc
+++ src/starnix/tests/syscalls/cpp/timers_test.cc
@@ -13,19 +13,20 @@
 #include <linux/capability.h>
 
 #include "src/starnix/tests/syscalls/cpp/capabilities_helper.h"
+#include "src/starnix/tests/syscalls/cpp/syscall_matchers.h"
 #include "src/starnix/tests/syscalls/cpp/test_helper.h"
 
 TEST(Timers, NoWakeAlarmCap) {
-  test_helper::UnsetCapability(CAP_WAKE_ALARM);
-  timer_t timer_id;
-  struct sigevent sev = {};
-  sev.sigev_notify = SIGEV_NONE;
+  test_helper::ForkHelper helper;
+  helper.RunInForkedProcess([]() {
+    test_helper::UnsetCapability(CAP_WAKE_ALARM);
+    timer_t timer_id;
+    struct sigevent sev = {};
+    sev.sigev_notify = SIGEV_NONE;
 
-  EXPECT_EQ(-1, timer_create(CLOCK_BOOTTIME_ALARM, &sev, &timer_id));
-  EXPECT_EQ(errno, EPERM);
-
-  EXPECT_EQ(-1, timer_create(CLOCK_REALTIME_ALARM, &sev, &timer_id));
-  EXPECT_EQ(errno, EPERM);
+    EXPECT_THAT(timer_create(CLOCK_BOOTTIME_ALARM, &sev, &timer_id), SyscallFailsWithErrno(EPERM));
+    EXPECT_THAT(timer_create(CLOCK_REALTIME_ALARM, &sev, &timer_id), SyscallFailsWithErrno(EPERM));
+  });
 }
 
 TEST(Timers, RealtimeAlarm) {
@@ -34,18 +35,18 @@ TEST(Timers, RealtimeAlarm) {
         << "The CAP_WAKE_ALARM capability is required to create a CLOCK_REALTIME_ALARM timer.";
   }
   timespec begin = {};
-  ASSERT_EQ(0, clock_gettime(CLOCK_REALTIME, &begin));
+  ASSERT_THAT(clock_gettime(CLOCK_REALTIME, &begin), SyscallSucceeds());
 
   timer_t timer_id;
   struct sigevent sev = {};
   sev.sigev_notify = SIGEV_NONE;
-  SAFE_SYSCALL(timer_create(CLOCK_REALTIME_ALARM, &sev, &timer_id));
+  ASSERT_THAT(timer_create(CLOCK_REALTIME_ALARM, &sev, &timer_id), SyscallSucceeds());
 
   // Test timer 1 second in the future.
   struct itimerspec its = {};
   its.it_value = begin;
   its.it_value.tv_sec += 1;
-  SAFE_SYSCALL(timer_settime(timer_id, TIMER_ABSTIME, &its, nullptr));
+  ASSERT_THAT(timer_settime(timer_id, TIMER_ABSTIME, &its, nullptr), SyscallSucceeds());
 
   struct timespec sleep_t = {
       .tv_sec = 1,
@@ -55,10 +56,10 @@ TEST(Timers, RealtimeAlarm) {
 
   // The timer should count down to 0 and stop since the interval is zero. No overruns should be
   // counted.
-  EXPECT_EQ(0, timer_gettime(timer_id, &its));
+  EXPECT_THAT(timer_gettime(timer_id, &its), SyscallSucceeds());
   EXPECT_EQ(0, its.it_value.tv_sec);
   EXPECT_EQ(0, its.it_value.tv_nsec);
-  EXPECT_EQ(0, timer_getoverrun(timer_id));
+  EXPECT_THAT(timer_getoverrun(timer_id), SyscallSucceedsWithValue(0));
 
-  SAFE_SYSCALL(timer_delete(timer_id));
+  EXPECT_THAT(timer_delete(timer_id), SyscallSucceeds());
 }
