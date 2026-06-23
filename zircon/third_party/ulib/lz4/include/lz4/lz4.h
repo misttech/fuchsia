69b9d896498a9120e01afb99bac57874b897a375
Jay Zhuang <jayzhuang@google.com>
2026-06-22 23:16:15 -0700

    [3p][lz4] Track upstream at //third_party/lz4/src
    This makes it easier to define and sync Bazel targets with GN targets,
    which will be done in followup changes.
    
    Change-Id: I46aba1915e1575cda107dc8d3d0670b4d093becb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1655954
    Commit-Queue: Jay Zhuang <jayzhuang@google.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Jerry Belton <jcecil@google.com>

--- zircon/third_party/ulib/lz4/include/lz4/lz4.h
+++ zircon/third_party/ulib/lz4/include/lz4/lz4.h
@@ -2,4 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "../../../../../third_party/lz4/lib/lz4.h"
+#include "../../../../../third_party/lz4/src/lib/lz4.h"
