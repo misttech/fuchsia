470dace3ff5fdb9ee82feec7755cd963939dc719
Hunter Freyer <hjfreyer@google.com>
2026-06-24 12:20:19 -0700

    [pcf8563] Re-import driver from OOT, verbatim.
    This is the first step in bringing this code back into the tree. Prior to this
    process, the code lived in
    https://fuchsia.googlesource.com/drivers/rtc/nxp/pcf8563/
    
    This represents the state of the code as of commit
    cf22923bdbd476ec304debc6dcbc3f98fd0ca9fa in that repository.
    
    Bug: 524973776
    
    Change-Id: I1a12d6c629fe2f52fcf2b9247696f5ad6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1651058
    Reviewed-by: Sarah Chan <spqchan@google.com>

---
+++ src/devices/rtc/drivers/nxp/pcf8563/pcf8563-server.h
@@ -0,0 +1,38 @@
+// Copyright 2023 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SRC_DRIVERS_PCF8563_PCF8563_SERVER_H_
+#define SRC_DRIVERS_PCF8563_PCF8563_SERVER_H_
+
+#include <fidl/fuchsia.hardware.rtc/cpp/fidl.h>
+
+#include "pcf8563.h"
+
+namespace pcf8563 {
+
+class RtcDriver;
+class RtcServer : public fidl::Server<fuchsia_hardware_rtc::Device> {
+ public:
+  explicit RtcServer(RtcDriver* device) : device_(device) {}
+
+  fuchsia_hardware_rtc::Service::InstanceHandler GetInstanceHandler();
+
+  // fuchsia_hardware_rtc::Device protocol.
+  void Get(GetCompleter::Sync& completer) override;
+  void Set2(Set2Request& req, Set2Completer::Sync& completer) override;
+  void handle_unknown_method(fidl::UnknownMethodMetadata<fuchsia_hardware_rtc::Device> metadata,
+                             fidl::UnknownMethodCompleter::Sync& completer) override {}  // No-op
+
+  void OnUnbound(fidl::UnbindInfo info, fidl::ServerEnd<fuchsia_hardware_rtc::Device> server_end);
+
+  fidl::ServerBindingGroup<fuchsia_hardware_rtc::Device>& bindings() { return bindings_; }
+
+ private:
+  RtcDriver* device_;  // Must outlive this class.
+  fidl::ServerBindingGroup<fuchsia_hardware_rtc::Device> bindings_;
+};
+
+}  // namespace pcf8563
+
+#endif  // SRC_DRIVERS_PCF8563_PCF8563_SERVER_H_
