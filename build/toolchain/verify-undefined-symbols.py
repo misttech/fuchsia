37c8cff4619b31159e4bef8aff797c84e06cb8ef
Roland McGrath <mcgrathr@google.com>
2026-06-24 14:08:38 -0700

    [kernel][vdso] Check for writable PT_LOAD at build time
    Long ago this was checked by old scripts that were removed when
    they were no longer needed for other purposes.  This is always
    caught at boot time, but it's nicer to catch it at build time.
    Reuse the verify-undefined-symbols.py script for the new check,
    and modernize its code a bit along the way.
    
    Bug: 527515871
    Change-Id: I2b480879fae7b82e64dcea0c2cd90f4c5b258468
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1678894
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Leonard Chan <leonardchan@google.com>

--- build/toolchain/verify-undefined-symbols.py
+++ build/toolchain/verify-undefined-symbols.py
@@ -37,19 +37,25 @@ def main():
         default=False,
     )
     parser.add_argument(
+        "--read-only-segments",
+        action="store_true",
+        help="Disallow PT_LOAD segments with PF_W",
+        default=False,
+    )
+    parser.add_argument(
         "--depfile",
-        type=argparse.FileType("w"),
+        type=Path,
         required=True,
     )
     parser.add_argument(
         "--stamp",
-        type=argparse.FileType("w"),
+        type=Path,
         help="Stamp file written on success",
         required=True,
     )
     parser.add_argument(
         "--rspfile",
-        type=argparse.FileType("r"),
+        type=Path,
         help="Response file listing name of ET_REL file to report",
         required=True,
     )
@@ -57,34 +63,41 @@ def main():
         "--objfile",
         type=Path,
         help="ET_REL file to examine",
-        required=True,
     )
-    parser.add_argument("readelf", help="llvm-readelf binary", nargs=1)
+    parser.add_argument(
+        "readelf",
+        help="llvm-readelf binary",
+        type=Path,
+        nargs=1,
+    )
     args = parser.parse_args()
 
-    [file] = [line.strip() for line in args.rspfile.readlines()]
-    args.rspfile.close()
+    [file] = [line.strip() for line in args.rspfile.read_text().splitlines()]
+    file = Path(file)
+
+    objfile = args.objfile or file
 
     depfile_deps = " ".join(
-        [args.readelf[0], str(args.objfile), args.rspfile.name]
+        str(f) for f in [args.readelf[0], objfile, args.rspfile]
     )
-    args.depfile.write(f"{args.stamp.name}: {depfile_deps}\n")
-    args.depfile.close()
+    args.depfile.write_text(f"{args.stamp.name}: {depfile_deps}\n")
 
-    data = json.loads(
-        subprocess.check_output(
-            [
-                args.readelf[0],
-                "--elf-output-style=JSON",
-                "--sections",
-                "--symbols",
-                args.objfile,
-            ]
-        )
-    )
+    readelf_cmd = [
+        str(args.readelf[0]),
+        "--elf-output-style=JSON",
+        "--sections",
+        "--symbols",
+        "--program-headers",
+        str(objfile),
+    ]
+    with subprocess.Popen(readelf_cmd, stdout=subprocess.PIPE) as proc:
+        data = json.load(proc.stdout)
+        if proc.wait() != 0:
+            raise subprocess.CalledProcessError(proc.returncode, readelf_cmd)
 
     sections = [entry["Section"] for entry in data[0]["Sections"]]
     symbols = [entry["Symbol"] for entry in data[0]["Symbols"]]
+    phdrs = [entry["ProgramHeader"] for entry in data[0]["ProgramHeaders"]]
 
     # The section with index 0 is always the null section header, but
     # llvm-readelf includes it just like any other.
@@ -105,6 +118,19 @@ def main():
             section["Name"]["Name"]
             for section in sections
             if section["Type"]["Name"] == "SHT_FINI_ARRAY"
+        ]
+    )
+
+    writable_load_segments = set(
+        [
+            phdr["VirtualAddress"]
+            for phdr in phdrs
+            if (
+                phdr["Type"]["Name"] == "PT_LOAD"
+                and any(
+                    flag["Name"] == "PF_W" for flag in phdr["Flags"]["Flags"]
+                )
+            )
         ]
     )
 
@@ -146,10 +172,16 @@ def main():
         for name in sorted(fini_sections):
             sys.stderr.write(f"*** Unexpected SHT_FINI_ARRAY section {name}\n")
 
+    if writable_load_segments and args.read_only_segments:
+        ok = False
+        sys.stderr.write(f"*** {file} FAILED writable PT_LOAD check ***\n")
+        for vaddr in sorted(writable_load_segments):
+            sys.stderr.write(f"*** Unexpected writable PT_LOAD at {vaddr:#x}\n")
+
     if not ok:
         return 1
 
-    args.stamp.write("OK\n")
+    args.stamp.write_text("OK\n")
     return 0
 
 
