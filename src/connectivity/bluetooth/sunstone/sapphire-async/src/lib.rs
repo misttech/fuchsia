2b6d77d8e5a99614c65dc3cb81caa80f3ea0acd0
Adam Snaider <asnaider@google.com>
2026-06-29 14:33:24 -0700

    [bt][sunstone] Set up the sapphire-async crate
    Sets up sapphire-async, which will include the asynchronous foundation
    for the Sunstone stack. This commit only sets up the crate and future
    commits will implement the async runtime and data structures
    
    Bug: 518915192
    Change-Id: I6c6fc6e3f3610320ab6c2c67853468c06a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1660356
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Adam Snaider <asnaider@google.com>
    Reviewed-by: Thomas Zander <thomasalpinus@google.com>
    Fuchsia-Auto-Submit: Adam Snaider <asnaider@google.com>

---
+++ src/connectivity/bluetooth/sunstone/sapphire-async/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![allow(unused_crate_dependencies)]
+#![cfg_attr(not(feature = "std"), no_std)]
