295ea8b64c611b5c40d28624de5004127daeaf3e
Brian Bosak <bbosak@google.com>
2026-06-29 19:23:32 -0700

    [logs] Convert attribution.rs to the new interface
    Convert attribution.rs to the new interface.
    
    Fixed: 529487609
    Change-Id: I61ea09519c6e9227859802fcd9c24cb789a8976e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1685835
    Reviewed-by: Chris Suter <csuter@google.com>
    Commit-Queue: Brian Bosak <bbosak@google.com>

--- src/diagnostics/archivist/tests/integration/test_cases/src/logs/attribution.rs
+++ src/diagnostics/archivist/tests/integration/test_cases/src/logs/attribution.rs
@@ -2,16 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use crate::logs::common::LogFormat;
 use crate::{test_topology, utils};
-use diagnostics_assertions::assert_data_tree;
-use diagnostics_reader::{ArchiveReader, Severity};
+use diagnostics_reader::Severity;
+use fidl_fuchsia_archivist_test as ftest;
 use fidl_fuchsia_archivist_test::LogPuppetLogRequest;
+use fidl_fuchsia_diagnostics::Format;
+use fidl_fuchsia_diagnostics_types as fdiagnostics;
 use futures::StreamExt;
-use {fidl_fuchsia_archivist_test as ftest, fidl_fuchsia_diagnostics_types as fdiagnostics};
+use test_case::test_case;
 
 // This test verifies that Archivist knows about logging from this component.
+#[test_case(LogFormat::Rust(Format::Json))]
+#[cfg_attr(fuchsia_api_level_at_least = "HEAD", test_case(LogFormat::Rust(Format::Fxt)))]
+#[cfg_attr(fuchsia_api_level_at_least = "HEAD", test_case(LogFormat::Ffi))]
 #[fuchsia::test]
-async fn log_attribution() {
+async fn log_attribution(format: LogFormat) {
     const REALM_NAME: &str = "child";
     let realm = test_topology::create_realm(ftest::RealmOptions {
         puppets: Some(vec![test_topology::PuppetDeclBuilder::new(REALM_NAME).into()]),
@@ -21,10 +27,7 @@ async fn log_attribution() {
     .expect("create base topology");
 
     let accessor = utils::connect_accessor(&realm, utils::ALL_PIPELINE).await;
-    let mut result = ArchiveReader::logs()
-        .with_archive(accessor)
-        .snapshot_then_subscribe()
-        .expect("snapshot then subscribe");
+    let mut result = format.build(accessor).get_test_snapshot_then_subscribe().await;
 
     let puppet = test_topology::connect_to_puppet(&realm, REALM_NAME).await.unwrap();
     let messages = ["This is a syslog message", "This is another syslog message"];
@@ -40,13 +43,9 @@ async fn log_attribution() {
     }
 
     for log_str in &messages {
-        let log_record = result.next().await.expect("received log").expect("log is not an error");
-        assert_eq!(log_record.moniker.to_string(), REALM_NAME);
-        assert_eq!(log_record.metadata.severity, Severity::Info);
-        assert_data_tree!(log_record.payload.unwrap(), root: contains {
-            message: {
-              value: log_str.to_string(),
-            }
-        });
+        let log_record = result.next().await.expect("received log");
+        assert_eq!(log_record.tags[0], REALM_NAME);
+        assert_eq!(log_record.severity, Severity::Info);
+        assert!(log_record.message.contains(log_str));
     }
 }
