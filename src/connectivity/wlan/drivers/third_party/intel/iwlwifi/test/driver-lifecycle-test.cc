7a7ca6421ad27a43cb6cd69cfdba036754111be9
Novin Changizi <novinc@google.com>
2026-07-01 12:21:09 -0700

    [fdf][test] Migrate Intel iwlwifi
    Migrate the deprecated driver lifecycle testing classes to the modern
    `fdf_testing::ForegroundDriverTest` framework using `TestConfig`.
    
    Implemented `IwlwifiTestEnvironment` to serve service dependencies.
    Track `driver_stopped_` to prevent double-cleanup crashes when tests
    assert or fail.
    
    Bug: 328303454
    Test: fx test driver_lifecycle_test iwlwifi_test_pkg TAG=agy
    CONV=2785e46a-e1dc-430d-b40f-e444d195a105
    
    Change-Id: I160c500b61f5ff3c8d2f9b0181bee18b6a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1685814
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>
    Fuchsia-Auto-Submit: Novin Changizi <novinc@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

--- src/connectivity/wlan/drivers/third_party/intel/iwlwifi/test/driver-lifecycle-test.cc
+++ src/connectivity/wlan/drivers/third_party/intel/iwlwifi/test/driver-lifecycle-test.cc
@@ -4,14 +4,7 @@
 
 #include <fidl/fuchsia.hardware.pci/cpp/wire.h>
 #include <lib/async/default.h>
-#include <lib/async_patterns/testing/cpp/dispatcher_bound.h>
-#include <lib/driver/testing/cpp/driver_runtime.h>
-
-// TODO(b/407791868): Some test classes we use were deprecated in F24. Update to use new FDF testing
-// classes.
-#include <lib/driver/testing/cpp/internal/driver_lifecycle.h>
-#include <lib/driver/testing/cpp/internal/test_environment.h>
-#include <lib/driver/testing/cpp/test_node.h>
+#include <lib/driver/testing/cpp/driver_test.h>
 #include <lib/fake-bti/bti.h>
 
 #include <gtest/gtest.h>
@@ -27,10 +20,10 @@ constexpr int kTestSubsysDeviceId = 0x9e10;
 // FIDL.
 class FakePciParent : public fidl::WireServer<fuchsia_hardware_pci::Device> {
  public:
-  fuchsia_hardware_pci::Service::InstanceHandler GetInstanceHandler() {
+  fuchsia_hardware_pci::Service::InstanceHandler GetInstanceHandler(async_dispatcher_t* dispatcher) {
     return fuchsia_hardware_pci::Service::InstanceHandler({
         .device = binding_group_.CreateHandler(
-            this, fdf_dispatcher_get_async_dispatcher(fdf_dispatcher_get_current_dispatcher()),
+            this, dispatcher,
             fidl::kIgnoreBindingClosure),
     });
   }
@@ -124,79 +117,55 @@ class FakePciParent : public fidl::WireServer<fuchsia_hardware_pci::Device> {
   fidl::ServerBindingGroup<fuchsia_hardware_pci::Device> binding_group_;
 };
 
-// TODO(b/407791868): Some test classes we use were deprecated in F24. Update to use new FDF testing
-// classes.
-using fdf_testing::internal::TestEnvironment;
+class IwlwifiTestEnvironment : public fdf_testing::Environment {
+ public:
+  zx::result<> Serve(fdf::OutgoingDirectory& to_driver_vfs) override {
+    async_dispatcher_t* dispatcher = fdf::Dispatcher::GetCurrent()->async_dispatcher();
+    auto handler = fake_pci_parent_.GetInstanceHandler(dispatcher);
+    return to_driver_vfs.AddService<fuchsia_hardware_pci::Service>(std::move(handler));
+  }
+
+ private:
+  FakePciParent fake_pci_parent_;
+};
+
+class TestConfig final {
+ public:
+  using DriverType = wlan::iwlwifi::PcieIwlwifiDriver;
+  using EnvironmentType = IwlwifiTestEnvironment;
+};
 
 class DriverLifeCycleTest : public ::testing::Test {
  public:
   void SetUp() override {
-    // Create start args
-    zx::result start_args = node_server_.SyncCall(&fdf_testing::TestNode::CreateStartArgsAndServe);
-    EXPECT_EQ(ZX_OK, start_args.status_value());
-
-    // Start the test environment with incoming directory returned from the start args
-    zx::result init_result =
-        test_environment_.SyncCall(&fdf_testing::internal::TestEnvironment::Initialize,
-                                   std::move(start_args->incoming_directory_server));
-    EXPECT_EQ(ZX_OK, init_result.status_value());
-
-    // Get service handler from the fake_pci_parent_ object.
-    auto handler = fake_pci_parent_.SyncCall(&FakePciParent::GetInstanceHandler);
-
-    test_environment_.SyncCall(
-        [](TestEnvironment* env, fuchsia_hardware_pci::Service::InstanceHandler&& handler) {
-          zx::result result = env->incoming_directory().AddService<fuchsia_hardware_pci::Service>(
-              std::move(handler));
-          ASSERT_TRUE(result.is_ok());
-        },
-        std::move(handler));
-
-    zx::result start_result =
-        runtime_.RunToCompletion(driver_.Start(std::move(start_args->start_args)));
-    EXPECT_EQ(ZX_OK, start_result.status_value());
+    zx::result<> result = driver_test().StartDriver();
+    ASSERT_EQ(ZX_OK, result.status_value());
+    driver_stopped_ = false;
   }
 
-  void DriverPrepareStop() {
-    zx::result prepare_stop_result = runtime_.RunToCompletion(driver_.PrepareStop());
-    EXPECT_EQ(ZX_OK, prepare_stop_result.status_value());
-  }
-
-  void DriverStop() {
-    zx::result stop_result = driver_.Stop();
-    EXPECT_EQ(ZX_OK, stop_result.status_value());
+  void TearDown() override {
+    if (!driver_stopped_) {
+      zx::result<> result = driver_test().StopDriver();
+      ASSERT_EQ(ZX_OK, result.status_value());
+      driver_test().ShutdownAndDestroyDriver();
+    }
   }
 
   size_t GetNodeNumber() {
-    return node_server_.SyncCall(
-        [](fdf_testing::TestNode* node) { return node->children().size(); });
+    size_t count = 0;
+    driver_test().RunInNodeContext([&count](fdf_testing::TestNode& node) {
+      count = node.children().size();
+    });
+    return count;
   }
 
-  // TODO(b/407791868): Some test classes we use were deprecated in F24. Update to use new FDF
-  // testing classes.
-  fdf_testing::internal::DriverUnderTest<wlan::iwlwifi::PcieIwlwifiDriver>& driver() {
-    return driver_;
-  }
+  fdf_testing::ForegroundDriverTest<TestConfig>& driver_test() { return driver_test_; }
 
-  async_dispatcher_t* env_dispatcher() { return env_dispatcher_->async_dispatcher(); }
+ protected:
+  bool driver_stopped_ = false;
 
  private:
-  // Attaches a foreground dispatcher for us automatically.
-  fdf_testing::DriverRuntime runtime_;
-
-  // Env dispatcher runs in the background because we need to make sync calls into it.
-  fdf::UnownedSynchronizedDispatcher env_dispatcher_ = runtime_.StartBackgroundDispatcher();
-
-  async_patterns::TestDispatcherBound<fdf_testing::TestNode> node_server_{
-      env_dispatcher(), std::in_place, std::string("root")};
-
-  async_patterns::TestDispatcherBound<TestEnvironment> test_environment_{env_dispatcher(),
-                                                                         std::in_place};
-
-  fdf_testing::internal::DriverUnderTest<wlan::iwlwifi::PcieIwlwifiDriver> driver_;
-
-  async_patterns::TestDispatcherBound<FakePciParent> fake_pci_parent_{env_dispatcher(),
-                                                                      std::in_place};
+  fdf_testing::ForegroundDriverTest<TestConfig> driver_test_;
 };
 
 TEST_F(DriverLifeCycleTest, DeviceLifeCycle) {
@@ -207,20 +176,20 @@ TEST_F(DriverLifeCycleTest, DeviceLifeCycle) {
   // TODO(b/290283534): Add more operations here like AddWlansoftmacDevice() and
   // RemoveWlansoftmacDevice().
 
-  DriverPrepareStop();
+  zx::result<> stop_result = driver_test().StopDriver();
+  EXPECT_EQ(ZX_OK, stop_result.status_value());
   // We don't shutdown any of the children devices during PrepareStop(), this will be handled by
   // driver framework.
   EXPECT_EQ(GetNodeNumber(), (size_t)1);
 
-  DriverStop();
+  driver_test().ShutdownAndDestroyDriver();
+  driver_stopped_ = true;
 
-  // TODO(b/293654612): Make it more elegant when a helper function is available to wait for node
-  // removal.
-  // While the driver is stopping, we cannot guarantee the node number is zero while we
-  // check. We have to block-wait until it becomes zero.
-  while (GetNodeNumber() != 0) {
-    zx_nanosleep(zx_deadline_after(ZX_MSEC(200)));
-  };
+  while (GetNodeNumber() > 0) {
+    driver_test().runtime().RunUntilIdle();
+  }
+
+  EXPECT_EQ(GetNodeNumber(), 0UL);
 }
 
 }  // namespace
