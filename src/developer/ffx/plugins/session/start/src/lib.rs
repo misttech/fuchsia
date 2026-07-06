7b423959ef732f814f1dbbcd16d18b7c4fe568ed
Andrew Davies <awdavies@google.com>
2026-07-06 14:07:22 -0700

    [ffx] Migrate session start to machine output
    Bug: 472310565
    Test: ffx_session_start_test
    TAG: agy
    CONV: 79b7c4cd-2437-4e8c-820e-44b027db038f
    Change-Id: Ifd588e8c16ff5b7c25478d01a98cb3276a6a636c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1687662
    Reviewed-by: Jianpeng Chao <chaopeng@google.com>
    Fuchsia-Auto-Submit: Andrew Davies <awdavies@google.com>
    Commit-Queue: Andrew Davies <awdavies@google.com>

--- src/developer/ffx/plugins/session/start/src/lib.rs
+++ src/developer/ffx/plugins/session/start/src/lib.rs
@@ -2,12 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{Result, format_err};
 use async_trait::async_trait;
 use fdomain_fuchsia_session::{LifecycleProxy, LifecycleStartRequest};
+use ffx_session_common::CommandStatus;
 use ffx_session_start_args::SessionStartCommand;
-use ffx_writer::SimpleWriter;
+use ffx_writer::{ToolIO, VerifiedMachineWriter};
 use fho::{FfxMain, FfxTool};
+use std::io::Write;
 use target_holders::fdomain::moniker;
 
 const STARTING_SESSION: &str = "Starting the default session\n";
@@ -24,50 +25,114 @@ fho::embedded_plugin!(StartTool);
 
 #[async_trait(?Send)]
 impl FfxMain for StartTool {
-    // TODO(b/472310565) Support actual "json" output, not just "raw"
-    type Writer = SimpleWriter;
+    type Writer = VerifiedMachineWriter<CommandStatus>;
     type Error = ::fho::Error;
 
     async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
-        start_impl(self.lifecycle_proxy, self.cmd, &mut writer).await?;
-        Ok(())
+        start_impl(self.lifecycle_proxy, self.cmd, &mut writer).await
     }
 }
 
-pub async fn start_impl<W: std::io::Write>(
+pub async fn start_impl(
     lifecycle_proxy: LifecycleProxy,
     _cmd: SessionStartCommand,
-    writer: &mut W,
-) -> Result<()> {
-    write!(writer, "{}", STARTING_SESSION)?;
-    lifecycle_proxy
-        .start(&LifecycleStartRequest { ..Default::default() })
-        .await?
-        .map_err(|err| format_err!("{:?}", err))
+    writer: &mut VerifiedMachineWriter<CommandStatus>,
+) -> fho::Result<()> {
+    if !writer.is_machine() {
+        write!(writer, "{}", STARTING_SESSION)?;
+    }
+    match lifecycle_proxy.start(&LifecycleStartRequest { ..Default::default() }).await {
+        Ok(Ok(())) => writer.machine(&CommandStatus::Ok { message: None }).map_err(Into::into),
+        Ok(Err(err)) => Err(fho::Error::User(anyhow::anyhow!("{:?}", err))),
+        Err(err) => {
+            Err(fho::Error::User(anyhow::anyhow!("Transport error starting session: {:?}", err)))
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use fdomain_fuchsia_session::LifecycleRequest;
+    use ffx_writer::{Format, TestBuffers};
     use target_holders::fdomain::fake_proxy;
 
     #[fuchsia::test]
-    async fn test_start_session() -> Result<()> {
+    async fn test_start_session() {
         let client = fdomain_local::local_client_empty();
         let proxy = fake_proxy(client, |req| match req {
             LifecycleRequest::Start { payload, responder, .. } => {
                 assert_eq!(payload.session_url, None);
                 let _ = responder.send(Ok(()));
             }
-            _ => panic!("Unxpected Lifecycle request"),
+            _ => panic!("Unexpected Lifecycle request"),
         });
 
         let start_cmd = SessionStartCommand {};
-        let mut writer = Vec::new();
-        start_impl(proxy, start_cmd, &mut writer).await?;
-        let output = String::from_utf8(writer).unwrap();
+        let test_buffers = TestBuffers::default();
+        let mut writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
+        start_impl(proxy, start_cmd, &mut writer).await.unwrap();
+        let output = test_buffers.into_stdout_str();
         assert_eq!(output, STARTING_SESSION);
-        Ok(())
+    }
+
+    #[fuchsia::test]
+    async fn test_start_session_machine() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, |req| match req {
+            LifecycleRequest::Start { payload, responder, .. } => {
+                assert_eq!(payload.session_url, None);
+                let _ = responder.send(Ok(()));
+            }
+            _ => panic!("Unexpected Lifecycle request"),
+        });
+
+        let start_cmd = SessionStartCommand {};
+        let test_buffers = TestBuffers::default();
+        let mut writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+        start_impl(proxy, start_cmd, &mut writer).await.unwrap();
+        let output = test_buffers.into_stdout_str();
+        let status: CommandStatus = serde_json::from_str(&output).unwrap();
+        assert_eq!(status, CommandStatus::Ok { message: None });
+    }
+
+    #[fuchsia::test]
+    async fn test_start_session_error() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, |req| match req {
+            LifecycleRequest::Start { responder, .. } => {
+                let _ = responder.send(Err(fdomain_fuchsia_session::LifecycleError::NotFound));
+            }
+            _ => panic!("Unexpected Lifecycle request"),
+        });
+
+        let start_cmd = SessionStartCommand {};
+        let test_buffers = TestBuffers::default();
+        let mut writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+        let response = start_impl(proxy, start_cmd, &mut writer).await;
+        let err = response.unwrap_err();
+        assert!(err.to_string().contains("NotFound"));
+        let output = test_buffers.into_stdout_str();
+        assert!(output.is_empty());
+    }
+
+    #[fuchsia::test]
+    async fn test_start_session_transport_error() {
+        let client = fdomain_local::local_client_empty();
+        let (proxy, server) =
+            client.create_proxy_and_stream::<fdomain_fuchsia_session::LifecycleMarker>();
+        drop(server);
+
+        let start_cmd = SessionStartCommand {};
+        let test_buffers = TestBuffers::default();
+        let mut writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+        let response = start_impl(proxy, start_cmd, &mut writer).await;
+        assert!(response.is_err());
+        assert!(response.unwrap_err().to_string().starts_with("Transport error starting session"));
+        let output = test_buffers.into_stdout_str();
+        assert!(output.is_empty());
     }
 }
