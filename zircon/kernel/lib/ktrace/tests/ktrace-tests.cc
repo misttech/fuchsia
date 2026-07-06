6ba0d193f90812321cfc3ff91e08fe1dbabfc30b
Mohammed Habibulla <moch@google.com>
2026-07-06 08:52:00 -0700

    Revert "[ktrace] Use native Rust unittests"
    This reverts commit 94cda5f5b718fc3e832e6345bf979a1d749152a1.
    
    Reason for revert: bringup.[arm64|riscv64|x64]-kernel-thinlto-cfi builders failing
    
    Failure Link: go/bbid/8677045765348919729
    
    Original change's description:
    > [ktrace] Use native Rust unittests
    >
    > Changes the Rust ktrace unittests to use the Rust unittest library
    > allowing for them to be directly defined instead of requiring a C++
    > launcher.
    >
    > Change-Id: I073498fec8c14bc0401ea2c4d430d6899865edf6
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1693675
    > Commit-Queue: Adrian Danis <adanis@google.com>
    > Fuchsia-Auto-Submit: Adrian Danis <adanis@google.com>
    > Reviewed-by: Abdulla Kamar <abdulla@google.com>
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia62e1305ae540e8c196232ebd7ff9d759cae2862
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1694315
    Commit-Queue: Mohammed Habibulla <moch@google.com>
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>

--- zircon/kernel/lib/ktrace/tests/ktrace-tests.cc
+++ zircon/kernel/lib/ktrace/tests/ktrace-tests.cc
@@ -20,6 +20,11 @@
 extern "C" {
 int32_t rust_ktrace_test_interop(uint64_t header, uint64_t val);
 void rust_ktrace_test_macros();
+bool rust_ktrace_test_init_and_size();
+bool rust_ktrace_test_write();
+bool rust_ktrace_test_dropped_record_tracking();
+bool rust_ktrace_test_emit_drop_stats();
+bool rust_ktrace_test_global_lifecycle();
 }
 
 // A test version of the per-CPU KTrace instance that disables diagnostic logs and overrides
@@ -640,6 +645,37 @@ class KTraceTests {
 
     END_TEST;
   }
+
+  static bool TestRustInitAndSize() {
+    BEGIN_TEST;
+    EXPECT_TRUE(rust_ktrace_test_init_and_size());
+    END_TEST;
+  }
+
+  static bool TestRustWrite() {
+    BEGIN_TEST;
+    EXPECT_TRUE(rust_ktrace_test_write());
+    END_TEST;
+  }
+
+  static bool TestRustDroppedRecordTracking() {
+    BEGIN_TEST;
+    EXPECT_TRUE(rust_ktrace_test_dropped_record_tracking());
+    END_TEST;
+  }
+
+  static bool TestRustEmitDropStats() {
+    BEGIN_TEST;
+    EXPECT_TRUE(rust_ktrace_test_emit_drop_stats());
+    END_TEST;
+  }
+
+  static bool TestRustGlobalLifecycle() {
+    BEGIN_TEST;
+    EXPECT_TRUE(rust_ktrace_test_global_lifecycle());
+    END_TEST;
+  }
+
   static bool TestRustMacros() {
     BEGIN_TEST;
 
@@ -830,5 +866,10 @@ UNITTEST("rewind", KTraceTests::TestRewind)
 UNITTEST("read_user", KTraceTests::TestReadUser)
 UNITTEST("dropped_records", KTraceTests::TestDroppedRecordTracking)
 UNITTEST("rust_interop", KTraceTests::TestRustInterop)
+UNITTEST("rust_init_and_size", KTraceTests::TestRustInitAndSize)
+UNITTEST("rust_write", KTraceTests::TestRustWrite)
+UNITTEST("rust_dropped_record_tracking", KTraceTests::TestRustDroppedRecordTracking)
+UNITTEST("rust_emit_drop_stats", KTraceTests::TestRustEmitDropStats)
+UNITTEST("rust_global_lifecycle", KTraceTests::TestRustGlobalLifecycle)
 UNITTEST("rust_macros", KTraceTests::TestRustMacros)
 UNITTEST_END_TESTCASE(ktrace_tests, "ktrace", "KTrace tests")
