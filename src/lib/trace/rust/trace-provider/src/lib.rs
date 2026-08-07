ad6abb5997f04faba0a8891ea243dc406288b3e6
Megan Batty <mgnb@google.com>
2026-08-07 16:09:55 -0700

    [trace][rust] Add async_dispatcher-based trace provider
    This adds a more general implementation of the rust trace provider that
    doesn't need to spawn an additional thread. It leverages the bindings
    that have been written for rust against the libasync API to allow you to
    specify the dispatcher you want it to run on.
    
    Together with the implementation of ScopeDispatcher, this lets you run
    the C++ trace provider on fuchsia-async, allowing rust processes to not
    need to spawn a thread just for tracing.
    
    Bug: 543880476
    Change-Id: I347321e4d6cd40114cb62de516074a3225b6b818
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1748729
    Fuchsia-Auto-Submit: Megan Batty <mgnb@google.com>
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>

--- src/lib/trace/rust/trace-provider/src/lib.rs
+++ src/lib/trace/rust/trace-provider/src/lib.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use core::ffi::CStr;
+use core::ptr::{NonNull, null};
+use libasync::{AsAsyncDispatcherRef, AsyncDispatcher};
 use zx::sys::zx_handle_t;
 
 /// Creates a trace provider service that enables traces created by a process
@@ -33,6 +36,62 @@ pub fn trace_provider_wait_for_init() {
     }
 }
 
+/// Use this object to start and run a trace provider under an existing [`libasync`] dispatcher
+/// handle.
+///
+/// You can use this in combination with `libasync_scope_dispatcher` to run the trace
+/// provider on top of fuchsia-async, for example:
+///
+/// ```no_run
+/// #[fuchsia::main]
+/// async fn main() {
+///     // create a new scoped async dispatcher object for the current fuchsia-async executor.
+///     let scope_dispatcher = libasync_scope_dispatcher::ScopeDispatcher::new();
+///     // create a trace provider on it.
+///     let trace_provider =
+///         fuchsia_trace_provider::TraceProvider::new_with_fdio(&scope_dispatcher, None);
+///
+///     // run your program here...
+///
+///     // drop the trace provider before shutting down the dispatcher since it will queue work
+///     // on the dispatcher.
+///     drop(trace_provider);
+///     // shutdown the dispatcher.
+///     scope_dispatcher.shutdown().await;
+/// }
+/// ```
+///
+/// Note: As in the example above, make sure that this object is dropped before the dispatcher
+/// shuts down.
+pub struct TraceProvider {
+    trace_provider: NonNull<core::ffi::c_void>,
+    // just kept here to ensure that the dispatcher object is kept alive for memory safety reasons.
+    #[expect(unused)]
+    dispatcher: AsyncDispatcher,
+}
+
+impl TraceProvider {
+    pub fn new_with_fdio(
+        dispatcher: &impl AsAsyncDispatcherRef,
+        name: Option<&CStr>,
+    ) -> Option<Self> {
+        let dispatcher = AsyncDispatcher::new(dispatcher);
+        let trace_provider = NonNull::new(unsafe {
+            sys::trace_provider_create_with_fdio(
+                dispatcher.as_ptr().as_ptr(),
+                name.map_or(null(), CStr::as_ptr),
+            )
+        })?;
+        Some(Self { trace_provider, dispatcher })
+    }
+}
+
+impl Drop for TraceProvider {
+    fn drop(&mut self) {
+        unsafe { sys::trace_provider_destroy(self.trace_provider.as_ptr()) }
+    }
+}
+
 mod sys {
     // From librust-trace-provider.so
     unsafe extern "C" {
@@ -40,5 +99,12 @@ mod sys {
         pub(super) fn trace_provider_create_with_fdio_rust();
         pub(super) fn trace_provider_create_with_service_rust(to_service_h: zx::sys::zx_handle_t);
         pub(super) fn trace_provider_wait_for_init();
+
+        // These are directly imported from the C++ trace library.
+        pub(super) fn trace_provider_create_with_fdio(
+            dispatcher: *const libasync_sys::async_dispatcher_t,
+            name: *const core::ffi::c_char,
+        ) -> *mut core::ffi::c_void;
+        pub(super) fn trace_provider_destroy(trace_provider: *const core::ffi::c_void);
     }
 }
