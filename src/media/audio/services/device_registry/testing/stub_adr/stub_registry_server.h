7da53855b78a52083546152bd985c11debfd745b
Sergey Ulanov <sergeyu@google.com>
2026-08-06 23:36:43 -0700

    Revert "[audio] Update stub_device_registry to return a stub device."
    This reverts commit 8a606ad10e941ab0d3839ced3151cec7d0e9d2ba.
    
    Reason for revert: Didn't resolve the problem, crashes android_aidl_audio_package_hal
    
    Original change's description:
    > [audio] Update stub_device_registry to return a stub device.
    >
    > Bug: 542241185
    > Change-Id: I9f95d25c9827ffdeaae3666f112b293bcdad401b
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1746689
    > Reviewed-by: Andres Oportus <andresoportus@google.com>
    > Commit-Queue: Sergey Ulanov <sergeyu@google.com>
    > Fuchsia-Auto-Submit: Sergey Ulanov <sergeyu@google.com>
    
    Bug: 542241185
    Change-Id: I18fcfeba322dacb91c059f8c222b3d9d3f502add
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1743815
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    Fuchsia-Auto-Submit: Sergey Ulanov <sergeyu@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@google.com>

--- src/media/audio/services/device_registry/testing/stub_adr/stub_registry_server.h
+++ src/media/audio/services/device_registry/testing/stub_adr/stub_registry_server.h
@@ -37,34 +37,9 @@ class StubRegistryServer
   void WatchDevicesAdded(WatchDevicesAddedCompleter::Sync& completer) final {
     // Here we assume the parent has already sent us the InitialDeviceDiscoveryIsComplete().
     if (!responded_to_initial_watch_devices_added_) {
-      ADR_LOG_STATIC(kLogStubRegistryServer) << " initial call; returning stub composite device";
-      fuchsia_audio_device::ElementRingBufferFormatSet rb_format_set;
-      rb_format_set.element_id(1);
-      rb_format_set.format_sets(std::vector<fuchsia_audio_device::PcmFormatSet>{});
-
-      fuchsia_hardware_audio_signalprocessing::Element element;
-      element.id(1);
-      element.type(fuchsia_hardware_audio_signalprocessing::ElementType::kRingBuffer);
-
-      fuchsia_hardware_audio_signalprocessing::Topology topology;
-      topology.id(1);
-      topology.processing_elements_edge_pairs(
-          std::vector<fuchsia_hardware_audio_signalprocessing::EdgePair>{});
-
-      fuchsia_audio_device::Info stub_device;
-      stub_device.token_id(1);
-      stub_device.device_type(fuchsia_audio_device::DeviceType::kComposite);
-      stub_device.device_name("Stub Primary Audio Device");
-      stub_device.clock_domain(fuchsia_hardware_audio::kClockDomainMonotonic);
-      stub_device.ring_buffer_format_sets(
-          std::vector<fuchsia_audio_device::ElementRingBufferFormatSet>{rb_format_set});
-      stub_device.signal_processing_elements(
-          std::vector<fuchsia_hardware_audio_signalprocessing::Element>{element});
-      stub_device.signal_processing_topologies(
-          std::vector<fuchsia_hardware_audio_signalprocessing::Topology>{topology});
-
+      ADR_LOG_STATIC(kLogStubRegistryServer) << " initial call; returning empty list";
       completer.Reply(fit::success(fuchsia_audio_device::RegistryWatchDevicesAddedResponse{{
-          .devices = std::vector<fuchsia_audio_device::Info>{stub_device},
+          .devices = std::vector<fuchsia_audio_device::Info>{},
       }}));
       responded_to_initial_watch_devices_added_ = true;
       return;
