ea2a03b8dfdad332e00e65db0b0f4fe08e412eb0
Aaron Wood <aaronwood@google.com>
2026-08-10 01:52:15 -0700

    [bazel] Make ninja-delayed bazel actions the default
    This is a reland of https://fxrev.dev/1699837, now that
    a version of Ninja that contains https://fxrev.dev/1745455
    has been rolled into the tree.
    
    Fixed: 542470406
    Change-Id: I5fe90f74de971195c590a1cdcc87f5b0f42e0221
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1746893
    Fuchsia-Auto-Submit: David Turner <digit@google.com>
    Commit-Queue: David Turner <digit@google.com>
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>

--- build/regenerator.py
+++ build/regenerator.py
@@ -728,7 +728,7 @@ def main() -> int:
         ninja_worker_script_path = build_dir / ninja_worker_script_name
 
         enable_delayed_bazel_actions = args_json.get(
-            "enable_delayed_bazel_actions", False
+            "enable_delayed_bazel_actions", True
         )
         if enable_delayed_bazel_actions:
             # Symlink the ninja_bazel_action_worker script into the build outdir
