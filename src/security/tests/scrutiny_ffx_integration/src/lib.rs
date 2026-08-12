5064872ab2d4d0c296f4711baa48747978a57bed
Aaron Wood <aaronwood@google.com>
2026-08-12 16:03:01 -0700

    [scrutiny] Change scrutiny_ffx_integration_test to take a product bundle
    This changes the extraction tests to take the path to a product bundle
    via env var, and then reference the paths to the desired images from
    that.
    
    This is in preparation to moving all the test-input product bundles used
    by scrutiny tests to Bazel.
    
    Change-Id: Ia1d21b02e1aa8113d45755037c1d82a86a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1756411
    Fuchsia-Auto-Submit: Aaron Wood <aaronwood@google.com>
    Reviewed-by: Eric Stone <ecstone@google.com>
    Commit-Queue: Aaron Wood <aaronwood@google.com>

--- src/security/tests/scrutiny_ffx_integration/src/lib.rs
+++ src/security/tests/scrutiny_ffx_integration/src/lib.rs
@@ -2,55 +2,78 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::path::PathBuf;
 use std::process::Command;
 use tempfile::TempDir;
 
 const FFX_TOOL_PATH: &str = env!("FFX_TOOL_PATH");
-const BLOBFS_PATH: &str = env!("BLOBFS_PATH");
-const ZBI_PATH: &str = env!("ZBI_PATH");
-const RECOVERY_ZBI_PATH: &str = env!("RECOVERY_ZBI_PATH");
+const PRODUCT_BUNDLE_PATH: &str = env!("PRODUCT_BUNDLE_PATH");
 
 #[test]
 fn extract_blobfs() {
+    let pb_path = PathBuf::from(PRODUCT_BUNDLE_PATH);
+    let blobfs_path = pb_path.join("system_a/blob.blk");
     let tmp_dir = TempDir::new().unwrap();
     let tmp_path = tmp_dir.path().to_str().unwrap();
-    assert!(Command::new(FFX_TOOL_PATH)
-        .args(vec![
-            "scrutiny",
-            "shell",
-            &format!("tool.blobfs.extract --input {} --output {}/blobfs", BLOBFS_PATH, tmp_path),
-        ])
-        .status()
-        .unwrap()
-        .success());
+    assert!(
+        Command::new(FFX_TOOL_PATH)
+            .args(vec![
+                "scrutiny",
+                "shell",
+                &format!(
+                    "tool.blobfs.extract --input {} --output {}/blobfs",
+                    blobfs_path.to_str().unwrap(),
+                    tmp_path
+                ),
+            ])
+            .status()
+            .unwrap()
+            .success()
+    );
 }
 
 #[test]
 fn extract_zbi() {
+    let pb_path = PathBuf::from(PRODUCT_BUNDLE_PATH);
+    let zbi_path = pb_path.join("system_a/assembly.zbi");
     let tmp_dir = TempDir::new().unwrap();
     let tmp_path = tmp_dir.path().to_str().unwrap();
-    assert!(Command::new(FFX_TOOL_PATH)
-        .args(vec![
-            "scrutiny",
-            "shell",
-            &format!("tool.zbi.extract --input {} --output {}/zbi", ZBI_PATH, tmp_path),
-        ])
-        .status()
-        .unwrap()
-        .success());
+    assert!(
+        Command::new(FFX_TOOL_PATH)
+            .args(vec![
+                "scrutiny",
+                "shell",
+                &format!(
+                    "tool.zbi.extract --input {} --output {}/zbi",
+                    zbi_path.to_str().unwrap(),
+                    tmp_path
+                ),
+            ])
+            .status()
+            .unwrap()
+            .success()
+    );
 }
 
 #[test]
 fn extract_recovery_zbi() {
+    let pb_path = PathBuf::from(PRODUCT_BUNDLE_PATH);
+    let recovery_zbi_path = pb_path.join("system_r/assembly_recovery.zbi");
     let tmp_dir = TempDir::new().unwrap();
     let tmp_path = tmp_dir.path().to_str().unwrap();
-    assert!(Command::new(FFX_TOOL_PATH)
-        .args(vec![
-            "scrutiny",
-            "shell",
-            &format!("tool.zbi.extract --input {} --output {}/zbi", RECOVERY_ZBI_PATH, tmp_path),
-        ])
-        .status()
-        .unwrap()
-        .success());
+    assert!(
+        Command::new(FFX_TOOL_PATH)
+            .args(vec![
+                "scrutiny",
+                "shell",
+                &format!(
+                    "tool.zbi.extract --input {} --output {}/zbi",
+                    recovery_zbi_path.to_str().unwrap(),
+                    tmp_path
+                ),
+            ])
+            .status()
+            .unwrap()
+            .success()
+    );
 }
