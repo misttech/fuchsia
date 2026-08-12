b07b4372e58b6df6f25b8e6905488d5ceab3fdd5
Ethan Williams <ethanws@google.com>
2026-08-12 12:03:25 -0700

    [kernel][rust] PageQueues constants in Rust
    Make the PageQueues flags and constants accessible from Rust. Obtain the
    numeric values with bindgen.
    
    This is part of an effort to provide Rust bindings for the VM even where
    they may not be immediately needed.
    
    Duplicate comments from the C++ source.
    
    Bug: 539998429
    Change-Id: Ie780c1b0ab5ebfa28b551b6d572c2d62d7582d24
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1752952
    Commit-Queue: Ethan Williams <ethanws@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>

--- zircon/kernel/vm/page_queues.rs
+++ zircon/kernel/vm/page_queues.rs
@@ -8,6 +8,15 @@ use crate::vm::page::VmPagePtr;
 use core::marker::{PhantomData, PhantomPinned};
 use page_queues_bindings as bindings;
 use zr::Opaque;
+use zx_types::zx_duration_mono_t;
+
+/// Used to identify the reason that aging is triggered, mostly for debugging and informational
+/// purposes.
+pub type AgeReason = bindings::PageQueues_AgeReason;
+
+/// Describes any action to take when processing the LRU queue. This is applied to pages that would
+/// otherwise have to be moved from the old LRU queue into the isolate queue.
+pub type LruAction = bindings::PageQueues_LruAction;
 
 #[derive(Debug)]
 pub struct QueueAge(pub usize);
@@ -19,6 +28,57 @@ pub struct PageQueues {
 }
 
 impl PageQueues {
+    /// The number of reclamation queues is slightly arbitrary, but to be useful you want at least 3
+    /// representing
+    ///  * Very new pages that you probably don't want to evict as doing so probably implies you are
+    ///    in swap death
+    ///  * Slightly old pages that could be evicted if needed
+    ///  * Very old pages that you'd be happy to evict
+    ///
+    /// With two active queues 8 page queues are used so that there is some fidelity of information
+    /// in the inactive queues. Additional queues have reduced value as sufficiently old pages
+    /// quickly become equivalently unlikely to be used in the future.
+    pub const NUM_RECLAIM: usize = bindings::PageQueues_kNumReclaim;
+
+    /// Two active queues are used to allow for better fidelity of active information. This prevents
+    /// a race between aging once and needing to collect/harvest age information.
+    pub const NUM_ACTIVE_QUEUES: usize = bindings::PageQueues_kNumActiveQueues;
+
+    /// The amount of pages that will have to move around the queues before the active/inactive
+    /// ratio is re-checked. This therefore represents how much error the active ratio aging process
+    /// might have, or how delayed the MRU generation might be. In the worst case once the active
+    /// ratio is triggered this value is how much page data needs to then change queues before the
+    /// aging process happens.
+    pub const ACTIVE_INACTIVE_ERROR_MARGIN: usize = bindings::PageQueues_kActiveInactiveErrorMargin;
+
+    /// In addition to active and inactive, we want to consider some of the queues as 'oldest' to
+    /// provide an additional way to limit eviction. Presently the processing of the LRU queue to
+    /// make room for aging is not integrated with the Evictor, and so will not trigger eviction,
+    /// therefore to have a non-zero number of pages ever appear in an oldest queue for eviction the
+    /// last two queues are considered the oldest.
+    pub const NUM_OLDEST_QUEUES: usize = bindings::PageQueues_kNumOldestQueues;
+
+    /// Number of different isolate queues that are available. Different isolate queues allow for
+    /// separating isolate pages into different buckets such that more nuanced choices on what page
+    /// to reclaim can be made.
+    ///
+    /// We use 2 queues to separate "Don't Need" pages (high reclamation priority, index 0) from
+    /// standard aged pages (standard reclamation priority, index 1).
+    pub const ISOLATE_QUEUE_DONT_NEED: usize = bindings::PageQueues_kIsolateQueueDontNeed;
+    pub const ISOLATE_QUEUE_STANDARD: usize = bindings::PageQueues_kIsolateQueueStandard;
+    pub const NUM_ISOLATE_QUEUES: usize = bindings::PageQueues_kNumIsolateQueues;
+
+    pub const DEFAULT_MIN_MRU_ROTATE_TIME: zx_duration_mono_t =
+        bindings::PageQueues_kDefaultMinMruRotateTime;
+    pub const DEFAULT_MAX_MRU_ROTATE_TIME: zx_duration_mono_t =
+        bindings::PageQueues_kDefaultMaxMruRotateTime;
+
+    /// This is presently an arbitrary constant, since the min and max mru rotate time are currently
+    /// fixed at the same value, meaning that the active ratio can not presently trigger, or
+    /// prevent, aging.
+    pub const DEFAULT_ACTIVE_RATIO_MULTIPLIER: u64 =
+        bindings::PageQueues_kDefaultActiveRatioMultiplier;
+
     /// Domain-specific conversion: returns raw pointer for `PageQueues`.
     pub fn as_raw(&self) -> *mut bindings::PageQueues {
         self.raw.get()
