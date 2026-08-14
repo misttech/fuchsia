7475d4210f20d096ec29b728b6f940f9e1af9a0e
Casey Dahlin <sadmac@google.com>
2026-08-14 16:39:15 -0700

    [ffx] Fix terminal injection in debug limbo
    Fixed: 516823321
    Change-Id: Id23881b1e6230d731bf44d93d1b3e4e96a6a6964
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1763553
    Commit-Queue: Casey Dahlin <sadmac@google.com>
    Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    Reviewed-by: Colin Nelson <colnnelson@google.com>

--- src/developer/ffx/plugins/debug/limbo/src/lib.rs
+++ src/developer/ffx/plugins/debug/limbo/src/lib.rs
@@ -8,6 +8,7 @@ use fdomain_fuchsia_exception::ProcessLimboProxy;
 use ffx_debug_limbo_args::{LimboCommand, LimboSubCommand};
 use ffx_writer::SimpleWriter;
 use fho::{FfxMain, FfxTool};
+use safe_string::TermSafe;
 use target_holders::moniker;
 use zx_status::Status;
 use zx_types::{ZX_ERR_NOT_FOUND, ZX_ERR_UNAVAILABLE};
@@ -90,8 +91,12 @@ async fn list<W: std::io::Write>(limbo_proxy: ProcessLimboProxy, mut writer: W) 
                 writeln!(writer, "Processes currently on limbo:")?;
                 for metadada in exceptions {
                     let info = metadada.info.expect("missing info");
-                    let process_name = metadada.process_name.expect("missing process_name");
-                    let thread_name = metadada.thread_name.expect("missing thread_name");
+                    let process_name = TermSafe::from_str_escaped(
+                        metadada.process_name.expect("missing process_name"),
+                    );
+                    let thread_name = TermSafe::from_str_escaped(
+                        metadada.thread_name.expect("missing thread_name"),
+                    );
                     writeln!(
                         writer,
                         "- {} (pid: {}), thread {} (tid: {}) on exception: {:?}",
@@ -134,4 +139,182 @@ async fn release<W: std::io::Write>(
         },
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fdomain_fuchsia_exception::{
+        ExceptionInfo, ExceptionType, ProcessExceptionInfo, ProcessLimboRequest,
+    };
+    use target_holders::fake_proxy;
+
+    #[fuchsia::test]
+    async fn test_list_clean_names() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::ListProcessesWaitingOnException { responder } => {
+                let _ = responder.send(Ok(&[ProcessExceptionInfo {
+                    info: Some(ExceptionInfo {
+                        process_koid: 1234,
+                        thread_koid: 5678,
+                        type_: ExceptionType::SwBreakpoint,
+                    }),
+                    process_name: Some("clean_proc".to_string()),
+                    thread_name: Some("clean_thread".to_string()),
+                    ..Default::default()
+                }]));
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        list(proxy, &mut output).await.unwrap();
+        let output_str = String::from_utf8(output).unwrap();
+        assert_eq!(
+            output_str,
+            "Processes currently on limbo:\n- clean_proc (pid: 1234), thread clean_thread (tid: 5678) on exception: SwBreakpoint\n"
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_list_sanitizes_control_characters() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::ListProcessesWaitingOnException { responder } => {
+                let _ = responder.send(Ok(&[ProcessExceptionInfo {
+                    info: Some(ExceptionInfo {
+                        process_koid: 1001,
+                        thread_koid: 2002,
+                        type_: ExceptionType::FatalPageFault,
+                    }),
+                    process_name: Some("proc\x1b[31m_evil\n\r\t\0".to_string()),
+                    thread_name: Some("thread\x1b]0;hacked\x07_name".to_string()),
+                    ..Default::default()
+                }]));
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        list(proxy, &mut output).await.unwrap();
+        let output_str = String::from_utf8(output).unwrap();
+
+        // Ensure raw control characters are NOT present
+        assert!(!output_str.contains('\x1b'));
+        assert!(!output_str.contains('\0'));
+        assert!(!output_str.contains('\x07'));
+        assert!(!output_str.contains('\r'));
+
+        // Ensure escaped representation is printed
+        assert_eq!(
+            output_str,
+            "Processes currently on limbo:\n- proc\\u{1b}[31m_evil\\n\\r\\t\\u{0} (pid: 1001), thread thread\\u{1b}]0;hacked\\u{7}_name (tid: 2002) on exception: FatalPageFault\n"
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_list_empty() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::ListProcessesWaitingOnException { responder } => {
+                let _ = responder.send(Ok(&[]));
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        list(proxy, &mut output).await.unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "No processes currently on limbo.\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_list_unavailable() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::ListProcessesWaitingOnException { responder } => {
+                let _ = responder.send(Err(zx_types::ZX_ERR_UNAVAILABLE));
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        list(proxy, &mut output).await.unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "Process limbo is not active.\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_status() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::GetActive { responder } => {
+                let _ = responder.send(true);
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        status(proxy, &mut output).await.unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "Limbo is active.\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_enable() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::GetActive { responder } => {
+                let _ = responder.send(false);
+            }
+            ProcessLimboRequest::SetActive { active, responder } => {
+                assert!(active);
+                let _ = responder.send();
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        enable(proxy, &mut output).await.unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "Activated the process limbo.\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_disable() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::GetActive { responder } => {
+                let _ = responder.send(true);
+            }
+            ProcessLimboRequest::SetActive { active, responder } => {
+                assert!(!active);
+                let _ = responder.send();
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        disable(proxy, &mut output).await.unwrap();
+        assert_eq!(
+            String::from_utf8(output).unwrap(),
+            "Deactivated the process limbo. All contained processes have been freed.\n"
+        );
+    }
+
+    #[fuchsia::test]
+    async fn test_release_success() {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(client, move |req| match req {
+            ProcessLimboRequest::ReleaseProcess { process_koid, responder } => {
+                assert_eq!(process_koid, 42);
+                let _ = responder.send(Ok(()));
+            }
+            other => panic!("Unexpected request: {:?}", other),
+        });
+
+        let mut output = Vec::new();
+        release(proxy, 42, &mut output).await.unwrap();
+        assert_eq!(
+            String::from_utf8(output).unwrap(),
+            "Successfully release process 42 from limbo.\n"
+        );
+    }
 }
