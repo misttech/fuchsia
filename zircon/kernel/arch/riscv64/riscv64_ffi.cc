8de13037c39aa3df37db3548217e0f00a179d303
Travis Geiselbrecht <travisg@google.com>
2026-08-13 17:02:34 -0700

    [kernel][platform][interrupt] Export platform power and interrupt FFI wrappers
    Move platform_panic_start and platform_halt_cpu to platform/power_ffi.cc,
    and move interrupt_send_ipi, interrupt_init_percpu, int_handler_start, and
    int_handler_finish to dev/pdev/interrupt/interrupt_ffi.cc.
    
    Bug: 543136284
    Test: ./scripts/fx run-boot-test --no-build zbi-chain-load-core-tests
    Change-Id: If11db5583c0024fd31a24d4cc2fd2ca35ed63b3a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1758076
    Commit-Queue: Travis Geiselbrecht <travisg@google.com>
    Reviewed-by: Adrian Danis <adanis@google.com>

--- zircon/kernel/arch/riscv64/riscv64_ffi.cc
+++ zircon/kernel/arch/riscv64/riscv64_ffi.cc
@@ -4,8 +4,11 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+#include <arch/mp.h>
 #include <arch/riscv64/mp.h>
+#include <dev/interrupt.h>
 #include <kernel/ffi.h>
+#include <kernel/interrupt.h>
 
 extern "C" {
 
@@ -14,5 +17,24 @@ uint32_t cpp_riscv64_boot_hart_id();
 
 FFI_ALWAYS_INLINE uint32_t cpp_riscv64_curr_hart_id() { return riscv64_curr_hart_id(); }
 FFI_ALWAYS_INLINE uint32_t cpp_riscv64_boot_hart_id() { return riscv64_boot_hart_id(); }
+
+zx_status_t cpp_interrupt_send_ipi(cpu_mask_t cpu_mask, uint8_t ipi);
+void cpp_interrupt_init_percpu();
+void cpp_int_handler_start(uint64_t* state);
+uint32_t cpp_int_handler_finish(uint64_t* state);
+
+FFI_ALWAYS_INLINE zx_status_t cpp_interrupt_send_ipi(cpu_mask_t cpu_mask, uint8_t ipi) {
+  return interrupt_send_ipi(cpu_mask, static_cast<mp_ipi>(ipi));
+}
+
+FFI_ALWAYS_INLINE void cpp_interrupt_init_percpu() { interrupt_init_percpu(); }
+
+FFI_ALWAYS_INLINE void cpp_int_handler_start(uint64_t* state) {
+  int_handler_start(reinterpret_cast<int_handler_saved_state_t*>(state));
+}
+
+FFI_ALWAYS_INLINE uint32_t cpp_int_handler_finish(uint64_t* state) {
+  return int_handler_finish(reinterpret_cast<int_handler_saved_state_t*>(state)) ? 1 : 0;
+}
 
 }  // extern "C"
--- zircon/kernel/platform/power_ffi.cc
+++ zircon/kernel/platform/power_ffi.cc
@@ -7,13 +7,21 @@
 #include <platform.h>
 #include <zircon/boot/crash-reason.h>
 
+#include <arch/regs.h>
+
 extern "C" {
 
 void cpp_platform_halt(uint32_t action, uint32_t reason);
+void cpp_platform_panic_start();
+void cpp_platform_halt_cpu();
 
 void cpp_platform_halt(uint32_t action, uint32_t reason) {
   platform_halt(static_cast<platform_halt_action>(action),
                 static_cast<zircon_crash_reason_t>(reason));
 }
+
+void cpp_platform_panic_start() { platform_panic_start(); }
+
+void cpp_platform_halt_cpu() { platform_halt_cpu(); }
 
 }  // extern "C"
