02e5f2e20f4aaea60d775d736fe180145766a873
Andrew Davies <awdavies@google.com>
2026-08-21 13:54:25 -0700

    [ffx] Migrate session drop-power-lease to machine output
    Bug: 472310565
    Test: ffx_session_drop_power_lease_test
    TAG: agy
    CONV: 79b7c4cd-2437-4e8c-820e-44b027db038f
    Change-Id: Id946b95c22b039c46eba01cfa5817a9c6a6a636c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1687665
    Reviewed-by: Caroline Liu <carolineliu@google.com>
    Fuchsia-Auto-Submit: Andrew Davies <awdavies@google.com>
    Commit-Queue: Andrew Davies <awdavies@google.com>

--- src/developer/ffx/plugins/session/drop-power-lease/src/lib.rs
+++ src/developer/ffx/plugins/session/drop-power-lease/src/lib.rs
@@ -5,9 +5,11 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use fdomain_fuchsia_session_power::HandoffProxy;
+use ffx_session_common::CommandStatus;
 use ffx_session_drop_power_lease_args::SessionDropPowerLeaseCommand;
-use ffx_writer::SimpleWriter;
+use ffx_writer::{ToolIO, VerifiedMachineWriter};
 use fho::{FfxMain, FfxTool, user_error};
+use std::io::Write;
 use target_holders::moniker;
 
 #[derive(FfxTool)]
@@ -22,43 +24,54 @@ fho::embedded_plugin!(DropPowerLeaseTool);
 
 #[async_trait(?Send)]
 impl FfxMain for DropPowerLeaseTool {
-    type Writer = SimpleWriter;
+    type Writer = VerifiedMachineWriter<CommandStatus>;
 
     type Error = ::fho::Error;
 
     async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
-        drop_power_lease_impl(self.handoff_proxy, self.cmd, &mut writer).await?;
-        Ok(())
+        drop_power_lease_impl(self.handoff_proxy, self.cmd, &mut writer).await
     }
 }
 
-pub async fn drop_power_lease_impl<W: std::io::Write>(
+pub async fn drop_power_lease_impl(
     handoff_proxy: HandoffProxy,
     cmd: SessionDropPowerLeaseCommand,
-    writer: &mut W,
-) -> Result<()> {
-    writeln!(writer, "Requesting to dropping power lease on execution state")?;
-    let lease = match handoff_proxy.take().await? {
-        Ok(lease) => Some(lease),
-        Err(fdomain_fuchsia_session_power::HandoffError::AlreadyTaken) if cmd.allow_missing => {
-            writeln!(writer, "Lease already dropped, ignoring error.")?;
-            None
+    writer: &mut VerifiedMachineWriter<CommandStatus>,
+) -> fho::Result<()> {
+    if !writer.is_machine() {
+        writeln!(writer, "Requesting to drop power lease on execution state")?;
+    }
+    match handoff_proxy.take().await {
+        Ok(Ok(lease)) => {
+            if !writer.is_machine() {
+                writeln!(writer, "Success!")?;
+            }
+            writer.machine(&CommandStatus::Ok { message: None })?;
+            drop(lease);
+            Ok(())
         }
-        Err(err) => {
-            return Err(
-                user_error!("Failed to take power lease from session manager: {:?}", err).into()
-            );
+        Ok(Err(fdomain_fuchsia_session_power::HandoffError::AlreadyTaken)) if cmd.allow_missing => {
+            if !writer.is_machine() {
+                writeln!(writer, "Lease already dropped, ignoring error.")?;
+                writeln!(writer, "Success!")?;
+            }
+            writer.machine(&CommandStatus::Ok {
+                message: Some("Lease already dropped, ignoring error.".to_string()),
+            })?;
+            Ok(())
         }
-    };
-    writeln!(writer, "Success!")?;
-    drop(lease);
-    Ok(())
+        Ok(Err(err)) => {
+            Err(user_error!("Failed to take power lease from session manager: {:?}", err))
+        }
+        Err(err) => Err(user_error!("Transport error taking power lease: {:?}", err)),
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use fdomain_fuchsia_session_power::HandoffRequest;
+    use ffx_writer::{Format, TestBuffers};
     use target_holders::fake_proxy;
 
     #[fuchsia::test]
@@ -70,15 +83,16 @@ mod test {
             HandoffRequest::Take { responder } => {
                 let _ = responder.send(Ok(client_clone.create_event().into()));
             }
-            x @ _ => unimplemented!("{x:?}"),
+            x => unimplemented!("{x:?}"),
         });
 
         let drop_power_lease_cmd = SessionDropPowerLeaseCommand { allow_missing: false };
-        let mut writer = Vec::new();
+        let test_buffers = TestBuffers::default();
+        let mut writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
         let result = drop_power_lease_impl(proxy, drop_power_lease_cmd, &mut writer).await;
         assert!(result.is_ok());
-        let output = String::from_utf8(writer).unwrap();
-        assert_eq!(output, "Requesting to dropping power lease on execution state\nSuccess!\n");
+        let output = test_buffers.into_stdout_str();
+        assert_eq!(output, "Requesting to drop power lease on execution state\nSuccess!\n");
     }
 
     #[fuchsia::test]
@@ -89,11 +103,12 @@ mod test {
                 let _ =
                     responder.send(Err(fdomain_fuchsia_session_power::HandoffError::AlreadyTaken));
             }
-            x @ _ => unimplemented!("{x:?}"),
+            x => unimplemented!("{x:?}"),
         });
 
         let drop_power_lease_cmd = SessionDropPowerLeaseCommand { allow_missing: false };
-        let mut writer = Vec::new();
+        let test_buffers = TestBuffers::default();
+        let mut writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
         let result = drop_power_lease_impl(proxy, drop_power_lease_cmd, &mut writer).await;
         assert!(result.is_err());
     }
@@ -106,17 +121,70 @@ mod test {
                 let _ =
                     responder.send(Err(fdomain_fuchsia_session_power::HandoffError::AlreadyTaken));
             }
-            x @ _ => unimplemented!("{x:?}"),
+            x => unimplemented!("{x:?}"),
         });
 
         let drop_power_lease_cmd = SessionDropPowerLeaseCommand { allow_missing: true };
-        let mut writer = Vec::new();
+        let test_buffers = TestBuffers::default();
+        let mut writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
         let result = drop_power_lease_impl(proxy, drop_power_lease_cmd, &mut writer).await;
         assert!(result.is_ok());
-        let output = String::from_utf8(writer).unwrap();
+        let output = test_buffers.into_stdout_str();
         assert_eq!(
             output,
-            "Requesting to dropping power lease on execution state\nLease already dropped, ignoring error.\nSuccess!\n"
+            "Requesting to drop power lease on execution state\nLease already dropped, ignoring error.\nSuccess!\n"
         );
+    }
+
+    #[fuchsia::test]
+    async fn test_drop_power_lease_machine() -> Result<()> {
+        let client = fdomain_local::local_client_empty();
+        let client_clone = std::sync::Arc::clone(&client);
+
+        let proxy = fake_proxy(client, move |req| match req {
+            HandoffRequest::Take { responder } => {
+                let _ = responder.send(Ok(client_clone.create_event().into()));
+            }
+            x => unimplemented!("{x:?}"),
+        });
+
+        let drop_power_lease_cmd = SessionDropPowerLeaseCommand { allow_missing: false };
+        let test_buffers = TestBuffers::default();
+        let mut writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+        let result = drop_power_lease_impl(proxy, drop_power_lease_cmd, &mut writer).await;
+        assert!(result.is_ok());
+        let output = test_buffers.into_stdout_str();
+        let status: CommandStatus = serde_json::from_str(&output)?;
+        assert_eq!(status, CommandStatus::Ok { message: None });
+        Ok(())
+    }
+
+    #[fuchsia::test]
+    async fn test_drop_power_lease_machine_already_taken_allow_missing() -> Result<()> {
+        let client = fdomain_local::local_client_empty();
+        let proxy = fake_proxy(std::sync::Arc::clone(&client), |req| match req {
+            HandoffRequest::Take { responder } => {
+                let _ =
+                    responder.send(Err(fdomain_fuchsia_session_power::HandoffError::AlreadyTaken));
+            }
+            x => unimplemented!("{x:?}"),
+        });
+
+        let drop_power_lease_cmd = SessionDropPowerLeaseCommand { allow_missing: true };
+        let test_buffers = TestBuffers::default();
+        let mut writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+        let result = drop_power_lease_impl(proxy, drop_power_lease_cmd, &mut writer).await;
+        assert!(result.is_ok());
+        let output = test_buffers.into_stdout_str();
+        let status: CommandStatus = serde_json::from_str(&output)?;
+        assert_eq!(
+            status,
+            CommandStatus::Ok {
+                message: Some("Lease already dropped, ignoring error.".to_string())
+            }
+        );
+        Ok(())
     }
 }
