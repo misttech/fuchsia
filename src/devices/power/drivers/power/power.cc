875279649239d83669f0a69228729a1fd6a24cbe
Suraj Malhotra <surajmalhotra@google.com>
2026-08-24 12:56:32 -0700

    Reland "[power] Publish fuchsia.ID and fuchsia.NAME on power child nodes"
    This is a reland of commit 53a329f64c2506b5b889e318067fb897d935c74f
    
    Original change's description:
    > [power] Publish fuchsia.ID and fuchsia.NAME on power child nodes
    >
    > Bug: 532275818
    > Change-Id: I5bff8ea7aea649fe3c47a33fc80384316a6a6964
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1758202
    > API-Review: Suraj Malhotra <surajmalhotra@google.com>
    > Reviewed-by: Garratt Gallagher <garratt@google.com>
    > Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    > Commit-Queue: Suraj Malhotra <surajmalhotra@google.com>
    
    Bug: 532275818
    Change-Id: I409021ee5f7729db36b6f0e8809aa86507e64591
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1777453
    API-Review: Suraj Malhotra <surajmalhotra@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/power/drivers/power/power.cc
+++ src/devices/power/drivers/power/power.cc
@@ -17,6 +17,7 @@
 
 #include <memory>
 
+#include <bind/fuchsia/cpp/bind.h>
 #include <bind/fuchsia/power/cpp/bind.h>
 #include <ddktl/metadata_server.h>
 #include <fbl/alloc_checker.h>
@@ -389,9 +390,15 @@ zx_status_t PowerDomain::Create(void* ctx, zx_device_t* parent,
   if (!ac.check()) {
     return ZX_ERR_NO_MEMORY;
   }
-  zx_device_str_prop_t props[] = {
+  std::vector<zx_device_str_prop_t> props = {
       {bind_fuchsia_power::POWER_DOMAIN, str_prop_int_val(index)},
   };
+  if (domain_info.global_id().has_value()) {
+    props.push_back({bind_fuchsia::ID, str_prop_int_val(*domain_info.global_id())});
+  }
+  if (domain_info.name().has_value()) {
+    props.push_back({bind_fuchsia::NAME, str_prop_str_val(domain_info.name()->c_str())});
+  }
 
   auto endpoints = fidl::CreateEndpoints<fuchsia_io::Directory>();
   if (endpoints.is_error()) {
