2acaf98c4e171a7c5abe2f195f83a03d501ad3bf
Roland McGrath <mcgrathr@google.com>
2026-08-25 15:45:37 -0700

    [x86-umip] Rewrite the test using gtest and captive-thread
    This test is a perfect fit for the new library.
    
    This is not testing functionality crucial to bringing up a system
    far enough to run component tests, so there's no reason not to
    make it a component test instead of a bootfs test.
    
    Change-Id: I7482b3c71c60b8d1e7edb76ba2c64d90f0c80e20
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1777273
    Commit-Queue: Roland McGrath <mcgrathr@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Reviewed-by: Travis Geiselbrecht <travisg@google.com>

--- src/zircon/tests/x86-umip/main.cc
+++ src/zircon/tests/x86-umip/main.cc
@@ -2,192 +2,134 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <assert.h>
 #include <cpuid.h>
-#include <lib/test-exceptions/exception-catcher.h>
-#include <lib/test-exceptions/exception-handling.h>
-#include <lib/zx/channel.h>
-#include <lib/zx/process.h>
-#include <lib/zx/thread.h>
-#include <lib/zx/time.h>
-#include <lib/zx/vmo.h>
-#include <stdio.h>
-#include <threads.h>
-#include <zircon/compiler.h>
-#include <zircon/syscalls.h>
-#include <zircon/syscalls/exception.h>
-#include <zircon/syscalls/port.h>
-#include <zircon/threads.h>
+#include <lib/arch/x86/descriptor.h>
+#include <lib/captive-thread/captive-thread.h>
+#include <lib/captive-thread/testing/matchers.h>
 
-#include <mutex>
+#include <optional>
+#include <string>
 
-#include <zxtest/zxtest.h>
-
-enum class Instruction {
-  SGDT,
-  SIDT,
-  SLDT,
-  STR,
-  SMSW,
-  NOOP,          // Used to ensure harness does not always report failure
-  MOV_NONCANON,  // Used to ensure harness does not always report success
-};
+#include <gtest/gtest.h>
 
 namespace {
 
-bool is_umip_supported() {
-  uint32_t eax, ebx, ecx, edx;
-  if (__get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx) != 1) {
-    return false;
-  }
-  return ecx & (1u << 2);
-}
+using namespace std::literals;
 
-// If this returns true, the instruction is expected to cause a #GP if it
-// is executed.
-bool isn_should_crash(Instruction isn) {
-  switch (isn) {
-    case Instruction::SGDT:
-    case Instruction::SIDT:
-    case Instruction::SLDT:
-    case Instruction::STR:
-    case Instruction::SMSW:
-      // If UMIP is supported, the kernel should have turned it on.
-      return is_umip_supported();
-    case Instruction::NOOP:
-      return false;
-    case Instruction::MOV_NONCANON:
-      return true;
-  }
-  __builtin_trap();
-}
+using captive_thread::CaptiveThread;
+using captive_thread::testing::GotException;
+using captive_thread::testing::WithType;
+using ::testing::Not;
 
-struct ThreadFuncArg {
-  Instruction isn;
-  std::mutex mutex;
+class X86UmipTests : public ::testing::Test {
+ public:
+  static void SetUpTestSuite() {
+    uint32_t values[4];
+    is_umip_supported_ =
+        __get_cpuid_count(7, 0, &values[0], &values[1], &values[2], &values[3]) == 1 &&
+        (values[2] & (1u << 2));
+
+    __cpuid(0x40000000, values[0], values[1], values[2], values[3]);
+    std::string_view name{reinterpret_cast<const char*>(&values[1]), 12};
+    on_kvm_ = name == "KVMKVMKVM\0\0\0"sv;
+  }
+
+  static bool IsUmipSupported() { return *is_umip_supported_; }
+
+  static bool OnKvm() { return *on_kvm_; }
+
+ private:
+  static inline std::optional<bool> is_umip_supported_ = std::nullopt;
+  static inline std::optional<bool> on_kvm_ = std::nullopt;
 };
 
-int isn_thread_func(void* raw_arg) {
-  auto arg = static_cast<ThreadFuncArg*>(raw_arg);
-  // The thread that created this thread holds the lock when it spawns the
-  // thread, so that execution is blocked initially.
-  arg->mutex.lock();
+constexpr auto NoException() { return Not(GotException()); }
 
-  auto isn = arg->isn;
+constexpr auto GotGpf() { return GotException(WithType(ZX_EXCP_GENERAL)); }
 
-  alignas(16) uint8_t scratch_buf[16];
-
-  switch (isn) {
-    case Instruction::SGDT: {
-      __asm__ volatile("sgdt %0" : "=m"(*scratch_buf));
-      break;
-    }
-    case Instruction::SIDT: {
-      __asm__ volatile("sidt %0" : "=m"(*scratch_buf));
-      break;
-    }
-    case Instruction::SLDT: {
-      __asm__ volatile("sldt %0" : "=m"(*scratch_buf));
-      break;
-    }
-    case Instruction::STR: {
-      __asm__ volatile("str %0" : "=m"(*scratch_buf));
-      break;
-    }
-    case Instruction::SMSW: {
-      uint64_t msw = 0;
-      __asm__ volatile("smsw %0" : "=r"(msw) : : "memory");
-      break;
-    }
-    case Instruction::NOOP: {
-      __asm__ volatile("nop");
-      break;
-    }
-    case Instruction::MOV_NONCANON: {
-      // We use a non-canonical address in order to produce a #GP, which we
-      // specifically want to test (as opposed to other fault types such as
-      // page faults).
-      uint8_t* v = reinterpret_cast<uint8_t*>(1ULL << 63);
-      __asm__ volatile("movq $0, %0" : "=m"(*v));
-      break;
-    }
-  }
-
-  arg->mutex.unlock();
-  return 0;
+// Turn a string literal into an array that can be a template parameter.
+template <size_t N>
+consteval auto Literal(const char (&str)[N]) {
+  std::array<char, N - 1> out;
+  std::string_view{str, N - 1}.copy(out.data(), out.size());
+  return out;
 }
 
-void test_instruction(Instruction isn) {
-  ThreadFuncArg arg;
-  arg.isn = isn;
+// Attempt the instruction that should write something to its output memory
+// operand without UMIP and #GPF instead with UMIP.
+template <auto Insn, typename T = arch::GdtRegister64>
+void StoreInsn() {
+  T out;
+  constexpr std::string tmpl = std::string(Insn.data(), Insn.size()) + " %0";
+  __asm__ volatile((tmpl) : "=m"(out));
+}
 
-  zx::thread thread;
-  zx::channel exception_channel;
-  test_exceptions::ExceptionCatcher catcher;
-  {
-    std::lock_guard<std::mutex> guard(arg.mutex);
+TEST_F(X86UmipTests, Noop) {
+  CaptiveThread thread([] { __asm__ volatile("nop"); });
+  EXPECT_THAT(thread.WaitForException(), NoException());
+}
 
-    thrd_t thread_obj;
-    ASSERT_EQ(thrd_create(&thread_obj, isn_thread_func, static_cast<void*>(&arg)), thrd_success);
-    ASSERT_EQ(zx::unowned_thread(thrd_get_zx_handle(thread_obj))
-                  ->duplicate(ZX_RIGHT_SAME_RIGHTS, &thread),
-              ZX_OK);
-    thrd_detach(thread_obj);
+TEST_F(X86UmipTests, Noncanonical) {
+  CaptiveThread thread([] {
+    constexpr uint64_t kNoncanonical = uint64_t{1} << 63;
+    volatile uint64_t* ptr;
+    // The compiler doesn't know what the ptr value is, so it can't remove the
+    // volatile store.
+    __asm__("" : "=r"(ptr) : "0"(kNoncanonical));
+    *ptr = 0;
+  });
+  EXPECT_THAT(thread.WaitForException(), GotGpf());
+}
 
-    ASSERT_EQ(catcher.Start(thread), ZX_OK);
-    // Release the lock, so that the thread can run.
-  }
-
-  // Wait for crash or thread completion.
-  zx::result<zx::exception> result = catcher.ExpectException();
-  if (result.is_ok()) {
-    zx_exception_report_t report = {};
-    ASSERT_EQ(thread.get_info(ZX_INFO_THREAD_EXCEPTION_REPORT, &report, sizeof(report), NULL, NULL),
-              ZX_OK);
-    EXPECT_TRUE(isn_should_crash(isn));
-    // These instructions should cause a GPF
-    EXPECT_EQ(report.header.type, ZX_EXCP_GENERAL);
-    // Exit the thread.
-    test_exceptions::ExitExceptionCThread(std::move(result.value()));
+TEST_F(X86UmipTests, Sgdt) {
+  CaptiveThread thread(StoreInsn<Literal("sgdt")>);
+  if (IsUmipSupported()) {
+    EXPECT_THAT(thread.WaitForException(), GotGpf());
   } else {
-    ASSERT_EQ(result.status_value(), ZX_ERR_PEER_CLOSED);
-    // Thread terminated normally so the instruction did not crash
-    ASSERT_FALSE(isn_should_crash(isn));
+    EXPECT_THAT(thread.WaitForException(), NoException());
   }
 }
 
-TEST(X86UmipTestCase, Sgdt) { ASSERT_NO_FAILURES(test_instruction(Instruction::SGDT)); }
+TEST_F(X86UmipTests, Sidt) {
+  CaptiveThread thread(StoreInsn<Literal("sidt")>);
+  if (IsUmipSupported()) {
+    EXPECT_THAT(thread.WaitForException(), GotGpf());
+  } else {
+    EXPECT_THAT(thread.WaitForException(), NoException());
+  }
+}
 
-TEST(X86UmipTestCase, Sidt) { ASSERT_NO_FAILURES(test_instruction(Instruction::SIDT)); }
+TEST_F(X86UmipTests, Sldt) {
+  CaptiveThread thread(StoreInsn<Literal("sldt")>);
+  if (IsUmipSupported()) {
+    EXPECT_THAT(thread.WaitForException(), GotGpf());
+  } else {
+    EXPECT_THAT(thread.WaitForException(), NoException());
+  }
+}
 
-TEST(X86UmipTestCase, Sldt) { ASSERT_NO_FAILURES(test_instruction(Instruction::SLDT)); }
+TEST_F(X86UmipTests, Str) {
+  CaptiveThread thread(StoreInsn<Literal("str")>);
+  if (IsUmipSupported()) {
+    EXPECT_THAT(thread.WaitForException(), GotGpf());
+  } else {
+    EXPECT_THAT(thread.WaitForException(), NoException());
+  }
+}
 
-TEST(X86UmipTestCase, Smsw) {
-  bool should_skip = false;
-  if (is_umip_supported()) {
+TEST_F(X86UmipTests, Smsw) {
+  CaptiveThread thread(StoreInsn<Literal("smsw"), uint16_t>);
+  if (IsUmipSupported()) {
     // If UMIP is supported, check if we're running under KVM.  On host
     // hardware that does not support UMIP, KVM misemulates UMIP's effect on
     // the SMSW instruction.
-    uint32_t eax;
-    uint32_t name[3];
-    __cpuid(0x40000000, eax, name[0], name[1], name[2]);
-
-    if (!memcmp(reinterpret_cast<const char*>(name), "KVMKVMKVM\0\0\0", sizeof(name))) {
-      should_skip = true;
+    if (OnKvm()) {
+      GTEST_SKIP() << "KVM may misemulate SMSW instruction under UMIP";
     }
+    EXPECT_THAT(thread.WaitForException(), GotGpf());
+  } else {
+    EXPECT_THAT(thread.WaitForException(), NoException());
   }
-
-  if (!should_skip) {
-    ASSERT_NO_FAILURES(test_instruction(Instruction::SMSW));
-  }
-}
-
-TEST(X86UmipTestCase, Str) { ASSERT_NO_FAILURES(test_instruction(Instruction::STR)); }
-
-TEST(X86UmipTestCase, Noop) { ASSERT_NO_FAILURES(test_instruction(Instruction::NOOP)); }
-
-TEST(X86UmipTestCase, MoveNoncanonical) {
-  ASSERT_NO_FAILURES(test_instruction(Instruction::MOV_NONCANON));
 }
 
 }  // namespace
