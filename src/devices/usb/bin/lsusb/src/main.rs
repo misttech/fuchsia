5e2828d74b0b0f221bc1ad1e23bf8bb76d267e1a
Rui Qi Sim <rqsim@google.com>
2026-08-25 17:18:38 -0700

    [lsusb] Request PERM_READABLE
    As part of wrapping up io2 rights enforcement (e.g. ENUMERATE), we are
    updating open calls to explicitly request needed capabilities.
    Currently, lsusb passes empty flags to open usb-device directory to
    watch for devices, relying on `fuchsia_fs` to inject `PERM_READABLE`.
    
    Explicitly passing `PERM_READABLE` ensures robustness against future
    removals of auto-injection or migrating these patterns directly to open
    FIDL calls, where empty rights will fail.
    
    TAG=agy
    CONV=850ab459-19aa-4f99-9fd8-0a80535ddc51
    
    Bug: 548035274
    Change-Id: Iefd82211e9a1d4f2aaefa1c32f101d93d7a41c4d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1781217
    Commit-Queue: Rui Qi Sim <rqsim@google.com>
    Reviewed-by: Suraj Malhotra <surajmalhotra@google.com>

--- src/devices/usb/bin/lsusb/src/main.rs
+++ src/devices/usb/bin/lsusb/src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<()> {
     let args: Args = argh::from_env();
     let proxy = fuchsia_fs::directory::open_in_namespace(
         "/dev/class/usb-device",
-        fuchsia_fs::Flags::empty(),
+        fuchsia_fs::PERM_READABLE,
     )?;
     lsusb::lsusb(proxy, args).await
 }
