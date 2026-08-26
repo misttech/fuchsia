33b1231afa8ff586bb9cebf222a93f7b607f0602
Mohammed Habibulla <moch@google.com>
2026-08-26 01:22:05 -0700

    Revert "[ffx][flash] Fix fastboot target rediscovery"
    This reverts commit c1aa04360ee8e3c0996b30b79340381cc78a1241.
    
    Reason for revert: Reverting suspect CL due to builder failure
    
    Failure Link: go/bbid/8672432696731279409
    
    Original change's description:
    > [ffx][flash] Fix fastboot target rediscovery
    > 
    > When rebooting a device from Product mode to Bootloader/Fastboot
    > mode in ffx target flash, the target no longer advertises its
    > Fuchsia nodename. Previously, if the target lacked a serial number
    > (e.g., TCP fastboot devices over Pontis), target rediscovery queried
    > by Fuchsia nodename, causing discovery to fail with "Target
    > specification was not found."
    > 
    > This updates reboot_target_to_bootloader_and_rediscover to query
    > by serial number when one is available, and otherwise fall back to
    > TargetInfoQuery::First to rediscover the rebooted fastboot target.
    > 
    > Bug: 534353954
    > Test: fx test ffx_flash_lib_test
    > Change-Id: I9f28537133c8a646f124b578ee78cbe66a6a6964
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1743529
    > Fuchsia-Auto-Submit: Colin Nelson <colnnelson@google.com>
    > Reviewed-by: Andrew Davies <awdavies@google.com>
    > Commit-Queue: Colin Nelson <colnnelson@google.com>
    
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Bug: 534353954
    Change-Id: I7962b35cab1ba38a60d9d60886ce53fd9e33b58d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1780497
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    Commit-Queue: Mohammed Habibulla <moch@google.com>
    Fuchsia-Auto-Submit: Mohammed Habibulla <moch@google.com>

--- src/developer/ffx/plugins/target/flash/src/lib.rs
+++ src/developer/ffx/plugins/target/flash/src/lib.rs
@@ -471,6 +471,7 @@ async fn reboot_target_to_bootloader_and_rediscover(
     ctx: &EnvironmentContext,
     device_proxy: DeviceProxy,
     power_proxy: AdminProxy,
+    node_name: &Option<String>,
 ) -> fho::Result<TargetHandle> {
     // Wait to allow the Target to fully cycle to the bootloader
     writeln!(writer, "Waiting for Target to reboot...")
@@ -498,12 +499,10 @@ async fn reboot_target_to_bootloader_and_rediscover(
         Err(e) => handle_fidl_connection_err(e)?,
     };
 
-    // When rebooting from Product mode to Fastboot mode, the target will no longer advertise
-    // its Fuchsia nodename. We query by serial number if one is available; otherwise, we fall
-    // back to TargetInfoQuery::First to rediscover the fastboot target.
-    let query = match info.serial_number.filter(|sn| !sn.is_empty()) {
-        Some(sn) => TargetInfoQuery::Id(sn),
-        None => TargetInfoQuery::First,
+    let query = match (info.serial_number, node_name) {
+        (Some(sn), _) => TargetInfoQuery::Id(sn),
+        (None, Some(nn)) => TargetInfoQuery::NodenameOrId(nn.clone()),
+        (None, None) => TargetInfoQuery::First,
     };
     ffx_target::discover_fastboot_target(&ctx, query, Some(100000)).await.map_err(|e| e.into())
 }
@@ -541,6 +540,7 @@ Reboot the Target to the bootloader and re-run this command."
                     &self.ctx,
                     device_proxy,
                     power_proxy,
+                    &handle.node_name,
                 )
                 .await?
             }
