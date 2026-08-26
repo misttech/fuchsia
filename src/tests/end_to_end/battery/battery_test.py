66300349552ca70d7e0469d3d0c5976d5692ddf0
Jonathan Chang <jnchang@google.com>
2026-08-26 16:51:49 -0700

    [Battery][Lacewing] Add State of Charge test
    Utilize fake battery via ffx to change the state of charge. Then
    validate the charge status was changed.
    
    Change-Id: I4abd72ce4e60933605839c03e5a9e5abe4ecca8e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1783877
    Reviewed-by: Erik Macias <erikmacias@google.com>
    Commit-Queue: Jonathan Chang <jnchang@google.com>

--- src/tests/end_to_end/battery/battery_test.py
+++ src/tests/end_to_end/battery/battery_test.py
@@ -7,17 +7,28 @@
 import logging
 
 import fuchsia_base_test
+from honeydew.affordances.drivers.battery import ChargeStatus
 from mobly import asserts, test_runner
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
 class BatteryTest(fuchsia_base_test.FuchsiaBaseTest):
-    """Test battery affordance get_status."""
+    """Test battery and fake battery affordances."""
 
     async def setup_test(self) -> None:
         await super().setup_test()
         self.battery = self.dut.battery
+        self.fake_battery = self.dut.fake_battery
+
+    async def teardown_test(self) -> None:
+        try:
+            self.fake_battery.set_charging()
+        except Exception as err:
+            _LOGGER.warning(
+                "Failed to reset fake battery state in teardown: %s", err
+            )
+        await super().teardown_test()
 
     async def test_get_status(self) -> None:
         """Verify reading battery telemetry status via get_status."""
@@ -26,6 +37,26 @@ class BatteryTest(fuchsia_base_test.FuchsiaBaseTest):
         _LOGGER.info("Battery status received: %s", status)
         asserts.assert_is_not_none(
             status, msg="Battery status should not be None"
+        )
+
+    async def test_set_discharging_status(self) -> None:
+        """Verify setting battery charge status to discharging via fake battery and verifying with get_status."""
+        _LOGGER.info(
+            "Setting fake battery charge status to discharging on %s",
+            self.dut.device_name,
+        )
+        self.fake_battery.set_discharging()
+
+        _LOGGER.info(
+            "Verifying battery status via get_status on %s",
+            self.dut.device_name,
+        )
+        status = await self.battery.get_status()
+        _LOGGER.info("Battery status received: %s", status)
+        asserts.assert_equal(
+            status.charge_status,
+            ChargeStatus.DISCHARGING,
+            f"Expected battery charge status to be DISCHARGING, got {status.charge_status}",
         )
 
 
