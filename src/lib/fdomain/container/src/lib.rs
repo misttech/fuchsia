9acccbd98b05d40e3f5e63c11295750372d985a6
Casey Dahlin <sadmac@google.com>
2026-08-26 17:38:26 -0700

    Reland "[fdomain_local] Add local_client_fdomain"
    This reverts commit 694fe1cb80357a5196c7f0ac89f3bd3741d77f64.
    
    Reason for revert: Incorret revert due to infra failure
    
    Original change's description:
    > Revert "[fdomain_local] Add local_client_fdomain"
    >
    > This reverts commit 47dcedecd3656c984c99f810c37d15daf05c7d66.
    >
    > Reason for revert: Reverting suspect CL due to builder failure
    >
    > Failure Link: go/bbid/8672431104270991905
    >
    > Original change's description:
    > > [fdomain_local] Add local_client_fdomain
    > > 
    > > This lets you create a local FDomain and populate the namespace of that
    > > FDomain using only FDomain handles. Previously to have your local
    > > FDomain have a namespace you had to use real or emulated/Overnet
    > > handles.
    > > 
    > > This enables host-side tests to not depend on Overnet or emulated
    > > handles.
    > > 
    > > Change-Id: Ic1a0244d0d2f823c9298a99811ca347e6a6a6964
    > > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1783278
    > > Reviewed-by: Steven Grady <slgrady@google.com>
    > > Commit-Queue: Casey Dahlin <sadmac@google.com>
    > > Fuchsia-Auto-Submit: Casey Dahlin <sadmac@google.com>
    >
    > No-Try: true
    > No-Presubmit: true
    > No-Tree-Checks: true
    > Change-Id: I617caa9a6333dd7fa2fa0ed3f028077281760d48
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1784397
    > Fuchsia-Auto-Submit: Mohammed Habibulla <moch@google.com>
    > Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    > Commit-Queue: Mohammed Habibulla <moch@google.com>
    
    Change-Id: I2d4321e16817f11d00432e3d55a6a8cba673ad2c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1785477
    Reviewed-by: Steven Grady <slgrady@google.com>
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    Commit-Queue: Casey Dahlin <sadmac@google.com>

--- src/lib/fdomain/container/src/lib.rs
+++ src/lib/fdomain/container/src/lib.rs
@@ -828,13 +828,18 @@ impl CloseAction {
     }
 }
 
+enum Namespace {
+    Native(Box<dyn Fn() -> Result<ClientEnd<fio::DirectoryMarker>, fidl::Status> + Send>),
+    Channel(Box<dyn Fn(proto::HandleId) + Send>),
+}
+
 /// This is a container of handles that is manipulable via the FDomain protocol.
 /// See [RFC-0228].
 ///
 /// Most of the methods simply handle FIDL requests from the FDomain protocol.
 #[pin_project::pin_project]
 pub struct FDomain {
-    namespace: Box<dyn Fn() -> Result<ClientEnd<fio::DirectoryMarker>, fidl::Status> + Send>,
+    namespace: Namespace,
     handles: HashMap<proto::HandleId, HandleState>,
     closing_handles: Vec<ClosingHandle>,
     event_queue: VecDeque<UnprocessedFDomainEvent>,
@@ -853,7 +858,20 @@ impl FDomain {
         namespace: impl Fn() -> Result<ClientEnd<fio::DirectoryMarker>, fidl::Status> + Send + 'static,
     ) -> Self {
         FDomain {
-            namespace: Box::new(namespace),
+            namespace: Namespace::Native(Box::new(namespace)),
+            handles: HashMap::new(),
+            closing_handles: Vec::new(),
+            event_queue: VecDeque::new(),
+            waker: None,
+        }
+    }
+
+    /// Create a new FDomain with a callback that receives a channel handle ID to serve.
+    pub fn new_with_namespace_channel(
+        namespace: impl Fn(proto::HandleId) + Send + 'static,
+    ) -> Self {
+        FDomain {
+            namespace: Namespace::Channel(Box::new(namespace)),
             handles: HashMap::new(),
             closing_handles: Vec::new(),
             event_queue: VecDeque::new(),
@@ -983,12 +1001,26 @@ impl FDomain {
     }
 
     pub fn get_namespace(&mut self, request: proto::FDomainGetNamespaceRequest) -> Result<()> {
-        match (self.namespace)() {
-            Ok(endpoint) => self.alloc_client_handles(
-                [request.new_handle],
-                [AnyHandle::Channel(endpoint.into_channel())],
-            ),
-            Err(e) => Err(proto::Error::TargetError(e.into_raw())),
+        match &self.namespace {
+            Namespace::Native(namespace) => {
+                let endpoint = namespace();
+                match endpoint {
+                    Ok(endpoint) => self.alloc_client_handles(
+                        [request.new_handle],
+                        [AnyHandle::Channel(endpoint.into_channel())],
+                    ),
+                    Err(e) => Err(proto::Error::TargetError(e.into_raw())),
+                }
+            }
+            Namespace::Channel(_) => {
+                let (client_chan, server_chan) = fidl::Channel::create();
+                self.alloc_client_handles([request.new_handle], [AnyHandle::Channel(client_chan)])?;
+                let server_hid = self.alloc_fdomain_handle(AnyHandle::Channel(server_chan))?;
+                if let Namespace::Channel(callback) = &self.namespace {
+                    callback(server_hid);
+                }
+                Ok(())
+            }
         }
     }
 
