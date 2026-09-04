5eb21b046fbff2a21de58262630f6c83e7e0961a
Betram Lalusha <betramlalusha@google.com>
2026-09-04 02:58:40 -0700

    Revert "[zircon][tests] Clean up and consolidate register context switch tests"
    This reverts commit ab173daee10d58af7e0b41d6edc41e30c6723461.
    
    Reason for revert: Broke bringup.nelson-hwasan
    
    Failure Link: <LINK TO FAILURE>
    
    Original change's description:
    > [zircon][tests] Clean up and consolidate register context switch tests
    >
    > Rename the "fpu" category of utest/core register-context-switch,
    > better representing what it's testing.  Move the separate (x86
    > only) register-state-test into there under a file name that
    > represents what it's testing specifically.  Modernize the test
    > code a bit.
    >
    > Change-Id: I689af84c82a80040d485b14d00d19e98ed0f8235
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1803217
    > Reviewed-by: Travis Geiselbrecht <travisg@google.com>
    > Commit-Queue: Roland McGrath <mcgrathr@google.com>
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I934a15a6079a549421da354cdbf8349f2b9cf45f
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1806557
    Exempt: android-build-ayeaye@system.gserviceaccount.com <android-build-ayeaye@system.gserviceaccount.com>
    Commit-Queue: Betram Lalusha <betramlalusha@google.com>

--- src/zircon/tests/register-state/register-state-test.cc
+++ src/zircon/tests/register-state/register-state-test.cc
@@ -2,26 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cpuid.h>
-#include <lib/zx/result.h>
 #include <pthread.h>
-#include <x86intrin.h>
 #include <zircon/syscalls.h>
-
-#include <thread>
 
 #include <zxtest/zxtest.h>
 
-namespace {
+#if defined(__x86_64__)
+
+#include <cpuid.h>
+#include <x86intrin.h>
+
+static pthread_barrier_t g_barrier;
 
 // Returns whether the CPU supports the {rd,wr}{fs,gs}base instructions.
-bool x86_feature_fsgsbase() {
+static bool x86_feature_fsgsbase() {
   uint32_t eax, ebx, ecx, edx;
   __cpuid_count(7, 0, eax, ebx, ecx, edx);
   return ebx & bit_FSGSBASE;
 }
 
-[[gnu::target("fsgsbase")]] void GsBaseTestThread(pthread_barrier_t& barrier, uintptr_t gs_base) {
+__attribute__((target("fsgsbase"))) static void* gs_base_test_thread(void* thread_arg) {
+  uintptr_t gs_base = (uintptr_t)thread_arg;
   uintptr_t fs_base = 0;
   if (x86_feature_fsgsbase()) {
     _writegsbase_u64(gs_base);
@@ -31,13 +32,15 @@ bool x86_feature_fsgsbase() {
   }
 
   // Wait until all the test threads reach this point.
-  int rv = pthread_barrier_wait(&barrier);
+  int rv = pthread_barrier_wait(&g_barrier);
   EXPECT_TRUE(rv == 0 || rv == PTHREAD_BARRIER_SERIAL_THREAD);
 
   if (x86_feature_fsgsbase()) {
-    EXPECT_EQ(_readgsbase_u64(), gs_base);
-    EXPECT_EQ(_readfsbase_u64(), fs_base);
+    EXPECT_TRUE(_readgsbase_u64() == gs_base);
+    EXPECT_TRUE(_readfsbase_u64() == fs_base);
   }
+
+  return nullptr;
 }
 
 // This tests whether the gs_base register on x86 is preserved across
@@ -50,36 +53,36 @@ TEST(RegisterStateTest, ContextSwitchOfGsBase) {
   // We run the rest of the test even if the fsgsbase instructions aren't
   // available, so that at least the test's threading logic gets
   // exercised.
+  printf("fsgsbase available = %d\n", x86_feature_fsgsbase());
 
   // We launch more threads than there are CPUs.  This ensures that there
   // should be at least one CPU that has >1 of our threads scheduled on
   // it, so saving and restoring gs_base between those threads should get
   // exercised.
-  const uint32_t thread_count = zx_system_get_num_cpus() * 2;
+  uint32_t thread_count = zx_system_get_num_cpus() * 2;
   ASSERT_GT(thread_count, 0);
 
-  pthread_barrier_t barrier;
-  ASSERT_EQ(pthread_barrier_init(&barrier, nullptr, thread_count), 0);
-  {
-    // All the threads will be joined when the vector is destroyed.
-    std::vector<std::jthread> threads;
-    threads.reserve(thread_count);
-    for (uint32_t i = 0; i < thread_count; ++i) {
-      // Give each thread a different test value for gs_base.
-      threads.emplace_back(GsBaseTestThread, std::ref(barrier), i * 0x10004);
-    }
+  pthread_t tids[thread_count];
+  ASSERT_EQ(pthread_barrier_init(&g_barrier, nullptr, thread_count), 0);
+  for (uint32_t i = 0; i < thread_count; ++i) {
+    // Give each thread a different test value for gs_base.
+    void* gs_base = (void*)(uintptr_t)(i * 0x10004);
+    ASSERT_EQ(pthread_create(&tids[i], nullptr, gs_base_test_thread, gs_base), 0);
   }
-  ASSERT_EQ(pthread_barrier_destroy(&barrier), 0);
+  for (uint32_t i = 0; i < thread_count; ++i) {
+    ASSERT_EQ(pthread_join(tids[i], nullptr), 0);
+  }
+  ASSERT_EQ(pthread_barrier_destroy(&g_barrier), 0);
 }
 
-#define DEFINE_REGISTER_ACCESSOR(REG)                                                            \
-  [[gnu::no_stack_protector, clang::no_sanitize("safe-stack")]] void set_##REG(uint16_t value) { \
-    __asm__ volatile("mov %0, %%" #REG : : "r"(value));                                          \
-  }                                                                                              \
-  [[gnu::no_stack_protector, clang::no_sanitize("safe-stack")]] uint16_t get_##REG(void) {       \
-    uint16_t value;                                                                              \
-    __asm__ volatile("mov %%" #REG ", %0" : "=r"(value));                                        \
-    return value;                                                                                \
+#define DEFINE_REGISTER_ACCESSOR(REG)                     \
+  static inline void set_##REG(uint16_t value) {          \
+    __asm__ volatile("mov %0, %%" #REG : : "r"(value));   \
+  }                                                       \
+  static inline uint16_t get_##REG(void) {                \
+    uint16_t value;                                       \
+    __asm__ volatile("mov %%" #REG ", %0" : "=r"(value)); \
+    return value;                                         \
   }
 
 DEFINE_REGISTER_ACCESSOR(ds)
@@ -95,8 +98,12 @@ DEFINE_REGISTER_ACCESSOR(gs)
 // IRET instruction has the side effect of resetting these registers when
 // returning from the kernel to userland (but not when returning to kernel
 // code).
-TEST(RegisterContextSwitchTests, SegmentSelectorsZeroedOnInterrupt) {
-  ZXTEST_SKIP() << "Disabled because some versions of non-KVM QEMU don't implement IRET fully";
+TEST(RegisterStateTest, SegmentSelectorsZeroedOnInterrupt) {
+  // Disable this test because some versions of non-KVM QEMU don't
+  // implement the part of IRET described above.
+  //
+  // TODO(https://fxbug.dev/42109668): Replace this return statement with ZXTEST_SKIP.
+  return;
 
   // We skip setting %fs because that breaks libc's TLS.
   set_ds(1);
@@ -107,35 +114,45 @@ TEST(RegisterContextSwitchTests, SegmentSelectorsZeroedOnInterrupt) {
   // switch, but on an unloaded machine it is more likely to be
   // interrupted by an interrupt where the handler returns without doing
   // a context switch.
-  while (get_gs() == 1) {
+  while (get_gs() == 1)
     __asm__ volatile("pause");
-  }
 
   EXPECT_EQ(get_ds(), 0);
   EXPECT_EQ(get_es(), 0);
   EXPECT_EQ(get_gs(), 0);
 }
 
-struct Bases {
-  uint64_t fs, gs;
-};
+__attribute__((target("fsgsbase"))) static uintptr_t read_gs_base() { return _readgsbase_u64(); }
 
-// This function can mess with %fs.base, which is part of the Fuchsia Compiler
-// ABI.  It's used for both stack-protector and safe-stack, so those must be
-// disabled to ensure that nothing will use it, as well as avoiding anything
-// using thread_local variables.  No functions compiled with the normal Fuchsia
-// Compiler ABI can be called from here, so it calls only the vDSO and the
-// helpers above that use the same attributes.
-[[gnu::target("fsgsbase"), gnu::no_stack_protector, clang::no_sanitize("safe-stack")]]
-zx::result<Bases> TestBasesWithContextSwitches() {
+__attribute__((target("fsgsbase"))) static uintptr_t read_fs_base() { return _readfsbase_u64(); }
+
+__attribute__((target("fsgsbase"))) static void write_gs_base(uintptr_t gs_base) {
+  return _writegsbase_u64(gs_base);
+}
+
+__attribute__((target("fsgsbase"))) static void write_fs_base(uintptr_t fs_base) {
+  return _writefsbase_u64(fs_base);
+}
+
+// Test that the kernel also resets the segment selector registers on a
+// context switch, to avoid leaking their values and to match what happens
+// on an interrupt.
+TEST(RegisterStateTest, SegmentSelectorsZeroedOnContextSwitch) {
   set_gs(1);
-  uint64_t orig_fs_base = 0;
+  uintptr_t orig_fs_base = 0;
   if (x86_feature_fsgsbase()) {
-    orig_fs_base = _readfsbase_u64();
+    // libc uses fs_base so we must save its original value before setting it.  Also, once we've set
+    // it, we must be very careful to not call any code that might use fs_base (transitively) until
+    // we have restored the original value.
+    orig_fs_base = read_fs_base();
+
     set_gs(1);
-    _writegsbase_u64(1);
+    write_gs_base(1);
     set_fs(1);
-    _writefsbase_u64(1);
+    write_fs_base(1);
+
+    // Now that we've set fs_base, we must not touch any TLS (thread-local storage) call anything
+    // that might touch TLS until we have stored the original value.
   }
   set_es(1);
   set_ds(1);
@@ -167,28 +184,18 @@ zx::result<Bases> TestBasesWithContextSwitches() {
     duration *= 2;
   }
 
-  Bases bases;
   if (x86_feature_fsgsbase()) {
-    bases = {.fs = _readfsbase_u64(), .gs = _readgsbase_u64()};
-    _writefsbase_u64(orig_fs_base);
-    _writegsbase_u64(0);
-  } else {
-    bases = {1, 1};
+    // Save gs_base and fs_base.  We'll verify they are 0 after we've restored the original fs_base.
+    uintptr_t gs_base = read_gs_base();
+    uintptr_t fs_base = read_fs_base();
+    // Restore fs_base.
+    write_fs_base(orig_fs_base);
+
+    // See that gs_base and fs_base are preserved across a context switch.
+    EXPECT_EQ(gs_base, 1);
+    EXPECT_EQ(fs_base, 1);
   }
-
-  return zx::make_result(status, bases);
-}
-
-// Test that the kernel also resets the segment selector registers on a
-// context switch, to avoid leaking their values and to match what happens
-// on an interrupt.
-TEST(RegisterContextSwitchTests, SegmentSelectorsZeroedOnContextSwitch) {
-  auto result = TestBasesWithContextSwitches();
-  ASSERT_OK(result);
-
-  // See that gs_base and fs_base are preserved across a context switch.
-  EXPECT_EQ(result->gs, 1);
-  EXPECT_EQ(result->fs, 1);
+  ASSERT_OK(status);
 
   // See that ds, es, fs, and gs are cleared by a context switch.
   EXPECT_EQ(get_ds(), 0);
@@ -197,4 +204,4 @@ TEST(RegisterContextSwitchTests, SegmentSelectorsZeroedOnContextSwitch) {
   EXPECT_EQ(get_gs(), 0);
 }
 
-}  // namespace
+#endif
--- zircon/system/utest/core/fpu/fpu_basic.cc
+++ zircon/system/utest/core/fpu/fpu_basic.cc
@@ -2,24 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <format>
+#include <stdio.h>
+#include <string.h>
 
 #include <zxtest/zxtest.h>
 
 namespace {
 
-[[gnu::noinline]] double fpu_test_loop(int ex_loops, int factor) {
+double fpu_test_loop(long ex_loops, long factor) {
   double ev[4] = {1.0, -1.0, -1.0, -1.0};
-  double t = 0.49999975;
+  double t =  0.49999975;
 
-  const int inner = 120 * factor;
+  const long inner = 120 * factor;
 
   for (int ix = 0; ix < ex_loops; ix++) {
     for (int iz = 0; iz < inner; iz++) {
-      ev[0] = (ev[0] + ev[1] + ev[2] - ev[3]) * t;
-      ev[1] = (ev[0] + ev[1] - ev[2] + ev[3]) * t;
-      ev[2] = (ev[0] - ev[1] + ev[2] + ev[3]) * t;
-      ev[3] = (-ev[0] + ev[1] + ev[2] + ev[3]) * t;
+          ev[0] = (ev[0] + ev[1] + ev[2] - ev[3]) * t;
+          ev[1] = (ev[0] + ev[1] - ev[2] + ev[3]) * t;
+          ev[2] = (ev[0] - ev[1] + ev[2] + ev[3]) * t;
+          ev[3] = (-ev[0] + ev[1] + ev[2] + ev[3]) * t;
     }
     t = 1.0 - t;
   }
@@ -31,9 +32,10 @@ namespace {
 // restoring the floating point registers, in particular for ARM.
 // For reference, with the parameters below it takes about 500ms
 // to complete in the arm-qemu-kvm bots.
-TEST(RegisterContextSwitchTests, FpuLongComputeLoop) {
-  double result = fpu_test_loop(5, 100);
-  std::string result_str = std::format("{:3.18f}", result);
+TEST(FPUTest, LongComputeLoop) {
+  auto result = fpu_test_loop(5, 100);
+  char result_str[64] = {};
+  sprintf(result_str, "%3.18f", result);
   ASSERT_STREQ(result_str, "-1.123982548697285422");
 }
 
