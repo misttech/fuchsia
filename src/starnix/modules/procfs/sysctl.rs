f2db34f8733c7b644ee4e98054465a56cd20dc95
Marina Ciocea <marinaciocea@google.com>
2026-09-09 21:35:20 -0700

    [starnix] Fix sysctl boot_id file mode
    Bug: 521344735
    Change-Id: I9aed785b56261007bbe79b428e7671547d957ef6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1799659
    Commit-Queue: Marina Ciocea <marinaciocea@google.com>
    Reviewed-by: Wez <wez@google.com>

--- src/starnix/modules/procfs/sysctl.rs
+++ src/starnix/modules/procfs/sysctl.rs
@@ -213,7 +213,11 @@ pub fn sysctl_directory(fs: &FileSystemHandle) -> FsNodeHandle {
         dir.subdir("random", 0o555, |dir| {
             // Generate random UUID
             let boot_id = Uuid::new_v4().hyphenated().to_string();
-            dir.entry("boot_id", BytesFile::new_node(boot_id.as_bytes().to_vec()), mode);
+            dir.entry(
+                "boot_id",
+                BytesFile::new_node(boot_id.as_bytes().to_vec()),
+                mode!(IFREG, 0o444),
+            );
             dir.entry(
                 "uuid",
                 BytesFile::new_node(|| Ok(format!("{}\n", Uuid::new_v4().hyphenated()))),
