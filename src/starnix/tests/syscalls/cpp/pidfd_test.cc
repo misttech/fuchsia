6d492f432fce8321ef68208e5f9e9743dfa3da57
Benjamin Lerman <qsr@google.com>
2026-09-11 05:57:01 -0700

    [starnix] Fix path of resource file in pidfd test
    Change-Id: I03385ab96494ce8b8b73c84a24c30586ef27ad3e
    Fixed: 560042244
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1819849
    Reviewed-by: Nathaniel Manista <nathaniel@google.com>
    Fuchsia-Auto-Submit: Benjamin Lerman <qsr@google.com>
    Commit-Queue: Benjamin Lerman <qsr@google.com>

--- src/starnix/tests/syscalls/cpp/pidfd_test.cc
+++ src/starnix/tests/syscalls/cpp/pidfd_test.cc
@@ -300,13 +300,7 @@ TEST(PidFdTest, PidFdSendSignalZeroSignal) {
 }
 
 std::string GetExecChildBinaryPath() {
-  std::string test_binary = "data/tests/deps/procfs_test_exec_child";
-  if (!files::IsFile(test_binary)) {
-    char self_path[PATH_MAX];
-    realpath("/proc/self/exe", self_path);
-    test_binary = files::JoinPath(files::GetDirectoryName(self_path), "procfs_test_exec_child");
-  }
-  return test_binary;
+  return test_helper::GetTestResourcePath("procfs_test_exec_child");
 }
 
 // Verify that polling a pidfd across execve does not wake up prematurely.
