c785ad95910ee87fb7a59d5ca7be235b544f53d1
Thomas Gales <tgales@google.com>
2026-09-11 12:55:52 -0700

    [feedback] Migrate IntlProvider to natural FIDL
    This contains two minor behavior adjustments:
    
    - The client will reconnect on ZX_ERR_UNAVAILABLE. If a server is
      unavailable, ZX_ERR_NOT_FOUND is now returned instead
      (https://fxbug.dev/296284010).
    - The backoff is reset after a successful method call
      (https://fxbug.dev/516904883).
    
    Bug: 519289418
    Bug: 516904883
    Test: `fx test //src/developer/forensics`
    Test: `fx shell crasher` @ 78b7acea70400000
    Change-Id: I9c699bfa62b864deb9169e7b2d024b2dd0c92eea
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1816672
    Commit-Queue: Thomas Gales <tgales@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>

--- src/developer/forensics/feedback/annotations/intl_provider.cc
+++ src/developer/forensics/feedback/annotations/intl_provider.cc
@@ -5,11 +5,15 @@
 #include "src/developer/forensics/feedback/annotations/intl_provider.h"
 
 #include <lib/async/cpp/task.h>
+#include <lib/fidl/cpp/wire/connect_service.h>
 #include <lib/fit/function.h>
+#include <lib/syslog/cpp/macros.h>
 
 #include <memory>
+#include <utility>
 
 #include "src/developer/forensics/feedback/annotations/constants.h"
+#include "src/developer/forensics/feedback/annotations/fidl_provider.h"
 #include "src/developer/forensics/utils/errors.h"
 #include "src/lib/backoff/backoff.h"
 
@@ -18,36 +22,7 @@ namespace forensics::feedback {
 IntlProvider::IntlProvider(async_dispatcher_t* dispatcher,
                            std::shared_ptr<sys::ServiceDirectory> services,
                            std::unique_ptr<backoff::Backoff> backoff)
-    : dispatcher_(dispatcher), services_(services), backoff_(std::move(backoff)) {
-  services_->Connect(property_provider_ptr_.NewRequest(dispatcher_));
-  property_provider_ptr_.events().OnChange =
-      ::fit::bind_member<&IntlProvider::GetInternationalization>(this);
-
-  property_provider_ptr_.set_error_handler([this](const zx_status_t status) {
-    if (status == ZX_ERR_UNAVAILABLE || status == ZX_ERR_NOT_FOUND) {
-      Annotations annotations;
-      for (const std::string& key : GetKeys()) {
-        annotations.insert({key, ErrorOrString(Error::kNotAvailableInProduct)});
-      }
-
-      on_update_(annotations);
-      return;
-    }
-
-    FX_PLOGS(WARNING, status) << "Lost connection to fuchsia.intl.PropertyProvider";
-
-    fxl::WeakPtr<IntlProvider> self = ptr_factory_.GetWeakPtr();
-    async::PostDelayedTask(
-        dispatcher_,
-        [self] {
-          if (self) {
-            self->services_->Connect(self->property_provider_ptr_.NewRequest(self->dispatcher_));
-            self->GetInternationalization();
-          }
-        },
-        backoff_->GetNext());
-  });
-
+    : dispatcher_(dispatcher), services_(std::move(services)), backoff_(std::move(backoff)) {
   GetInternationalization();
 }
 
@@ -60,7 +35,59 @@ std::set<std::string> IntlProvider::GetAnnotationKeys() {
 
 std::set<std::string> IntlProvider::GetKeys() const { return IntlProvider::GetAnnotationKeys(); }
 
+bool IntlProvider::Connect() {
+  if (client_.is_valid()) {
+    return true;
+  }
+
+  zx::result endpoints = fidl::CreateEndpoints<fuchsia_intl::PropertyProvider>();
+  if (endpoints.is_error()) {
+    FX_LOGS(ERROR) << "Failed to create endpoints: " << endpoints.status_string();
+    return false;
+  }
+
+  services_->Connect(fidl::DiscoverableProtocolName<fuchsia_intl::PropertyProvider>,
+                     endpoints->server.TakeChannel());
+
+  client_ =
+      fidl::Client<fuchsia_intl::PropertyProvider>(std::move(endpoints->client), dispatcher_, this);
+  return true;
+}
+
+void IntlProvider::OnChange() { GetInternationalization(); }
+
+void IntlProvider::on_fidl_error(fidl::UnbindInfo info) {
+  const internal::DisconnectResponse disconnect = internal::DisconnectResponse::BuildFrom(
+      info.status(), fidl::DiscoverableProtocolName<fuchsia_intl::PropertyProvider>);
+
+  client_ = fidl::Client<fuchsia_intl::PropertyProvider>();
+
+  if (!disconnect.should_reconnect) {
+    FX_LOGS(WARNING) << disconnect.log_message;
+    error_ = disconnect.error;
+    OnUpdate();
+    return;
+  }
+
+  FX_PLOGS(WARNING, info.status()) << disconnect.log_message;
+  reconnect_task_.PostDelayed(dispatcher_, backoff_->GetNext());
+}
+
 void IntlProvider::OnUpdate() {
+  if (!on_update_) {
+    return;
+  }
+
+  if (error_.has_value()) {
+    Annotations annotations;
+    for (const std::string& key : GetKeys()) {
+      annotations.insert({key, ErrorOrString(*error_)});
+    }
+
+    on_update_(annotations);
+    return;
+  }
+
   Annotations annotations;
 
   if (locale_.has_value()) {
@@ -83,19 +110,27 @@ void IntlProvider::GetOnUpdate(::fit::function<void(Annotations)> callback) {
 }
 
 void IntlProvider::GetInternationalization() {
-  FX_CHECK(property_provider_ptr_.is_bound());
+  if (!Connect()) {
+    return;
+  }
 
-  property_provider_ptr_->GetProfile([this](const fuchsia::intl::Profile profile) {
-    if (profile.has_locales()) {
-      if (const auto& locales = profile.locales(); !locales.empty()) {
-        locale_ = locales.front().id;
-      }
+  client_->GetProfile().Then([this](
+                                 fidl::Result<fuchsia_intl::PropertyProvider::GetProfile>& result) {
+    if (result.is_error()) {
+      FX_LOGS(WARNING) << "Failed to get internationalization profile: " << result.error_value();
+      return;
     }
 
-    if (profile.has_time_zones()) {
-      if (const auto& time_zones = profile.time_zones(); !time_zones.empty()) {
-        timezone_ = time_zones.front().id;
-      }
+    error_ = std::nullopt;
+    backoff_->Reset();
+
+    const fuchsia_intl::Profile& profile = result->profile();
+    if (profile.locales().has_value() && !profile.locales()->empty()) {
+      locale_ = profile.locales()->front().id();
+    }
+
+    if (profile.time_zones().has_value() && !profile.time_zones()->empty()) {
+      timezone_ = profile.time_zones()->front().id();
     }
 
     OnUpdate();
--- src/developer/forensics/feedback/annotations/intl_provider.h
+++ src/developer/forensics/feedback/annotations/intl_provider.h
@@ -5,25 +5,30 @@
 #ifndef SRC_DEVELOPER_FORENSICS_FEEDBACK_ANNOTATIONS_INTL_PROVIDER_H_
 #define SRC_DEVELOPER_FORENSICS_FEEDBACK_ANNOTATIONS_INTL_PROVIDER_H_
 
-#include <fuchsia/intl/cpp/fidl.h>
+#include <fidl/fuchsia.intl/cpp/fidl.h>
+#include <lib/async/cpp/task.h>
 #include <lib/async/dispatcher.h>
+#include <lib/fidl/cpp/client.h>
 #include <lib/fit/function.h>
 #include <lib/sys/cpp/service_directory.h>
 
 #include <memory>
+#include <optional>
+#include <set>
 #include <string>
 
 #include "src/developer/forensics/feedback/annotations/provider.h"
 #include "src/developer/forensics/feedback/annotations/types.h"
+#include "src/developer/forensics/utils/errors.h"
 #include "src/lib/backoff/backoff.h"
-#include "src/lib/fxl/memory/weak_ptr.h"
 
 namespace forensics::feedback {
 
 // Caches the most up-to-date version of the system locale and timezone.
 //
 // fuchsia.intl.PropertyProvider must be in |services|.
-class IntlProvider : public CachedAsyncAnnotationProvider {
+class IntlProvider : public CachedAsyncAnnotationProvider,
+                     public fidl::AsyncEventHandler<fuchsia_intl::PropertyProvider> {
  public:
   IntlProvider(async_dispatcher_t* dispatcher, std::shared_ptr<sys::ServiceDirectory> services,
                std::unique_ptr<backoff::Backoff> backoff);
@@ -33,22 +38,29 @@ class IntlProvider : public CachedAsyncAnnotationProvider {
 
   void GetOnUpdate(::fit::function<void(Annotations)> callback) override;
 
+  // fidl::AsyncEventHandler<fuchsia_intl::PropertyProvider>
+  void OnChange() override;
+  void on_fidl_error(fidl::UnbindInfo info) override;
+
  private:
+  bool Connect();
   void GetInternationalization();
-  void OnError(zx_status_t status);
   void OnUpdate();
 
   async_dispatcher_t* dispatcher_;
   const std::shared_ptr<sys::ServiceDirectory> services_;
 
-  std::optional<std::string> locale_{std::nullopt};
-  std::optional<std::string> timezone_{std::nullopt};
-  fuchsia::intl::PropertyProviderPtr property_provider_ptr_;
+  std::optional<std::string> locale_;
+  std::optional<std::string> timezone_;
+  std::optional<Error> error_;
+
+  fidl::Client<fuchsia_intl::PropertyProvider> client_;
   std::unique_ptr<backoff::Backoff> backoff_;
 
   ::fit::function<void(Annotations)> on_update_;
 
-  fxl::WeakPtrFactory<IntlProvider> ptr_factory_{this};
+  async::TaskClosureMethod<IntlProvider, &IntlProvider::GetInternationalization> reconnect_task_{
+      this};
 };
 
 }  // namespace forensics::feedback
